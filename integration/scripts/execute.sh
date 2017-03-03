78da530a4c3ce7e95f08ff593dd5315ed19c4285
Pritam Kadam <phkadam2008@gmail.com>
2017-03-03 16:06:06 +0530

    Pritam : Debug awk on travis.

--- integration/scripts/execute.sh
+++ integration/scripts/execute.sh
@@ -3,14 +3,29 @@ start_hcd_command='sbt integration/run'
 $start_hcd_command &
 hcd_app_pid=$!
 echo "------------ Starting HCD process with id : $hcd_app_pid ------------"
-sleep 10
+
+echo "Sleeping for 20 seconds : "
+date
+sleep 20
+echo "Waking up ..."
+date
 
 
 echo "------------ Starting integration test execution -------------"
 integration_test_command='sbt -DPORT=2553 integration/test'
 $integration_test_command
 
+
+echo "killing HCD process : $hcd_app_pid"
 kill -9 $hcd_app_pid
+
+echo "Searching for process : sbt"
+
+ps -ef | awk '$NF~"sbt" {print $2}'
+
+echo "Searching for process : integration/run"
+
+ps -ef | awk '$NF~"integration/run" {print $2}'
 
 int_tests_pid=`ps -ef | awk '$NF~"integration/run" {print $2}'`
 
