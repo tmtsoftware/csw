e62390c801b85995224b091b86e16bd2c1ec4b71
unmesh <unmeshjoshi@gmail.com>
2017-03-04 20:36:38 +0530

    Unmesh: Adding a dummy test to see green build on travis

---
+++ csw-location/src/test/scala/csw/services/location/models/ResolvedAkkaLocationTest.scala
@@ -0,0 +1,15 @@
+package csw.services.location.models
+
+import java.net.URI
+
+import csw.services.location.models.Connection.AkkaConnection
+import org.scalatest.FunSuite
+
+class ResolvedAkkaLocationTest extends FunSuite {
+  test("resolves actor referece") {
+    val componentId = ComponentId("hcd1", ComponentType.HCD)
+    val connection = AkkaConnection(componentId)
+    ResolvedAkkaLocation(connection, new URI("akka://hcd1"))
+  }
+
+}
