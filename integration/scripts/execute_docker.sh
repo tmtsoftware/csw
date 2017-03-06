aecafaf73f57c15fd3452d02e4a4988a7e0d8faf
unmesh <unmeshjoshi@gmail.com>
2017-03-06 10:23:33 +0530

    Unmesh: Factored out host directory mappping as parameters

--- integration/scripts/execute_docker.sh
+++ integration/scripts/execute_docker.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 
+#Map local project directories and .ivy directory from host to docker container
+#e.g.  ./integration/scripts/execute_docker.sh '-v /home/unmesh/work/csw-prod:/source -v /home/unmesh/.ivy2:/root/.ivy2'
+HOST_DIR_MAPPING=$1
+echo "$HOST_DIR_MAPPING"
+
 echo "Starting HCD docker container"
-docker run -d --rm --name node1 -v /home/unmesh/work/csw-prod:/source -v /home/unmesh/.ivy2:/root/.ivy2 centos-csw bash -c 'cd /source && sbt integration/run'
+docker run -d --rm --name node1 $HOST_DIR_MAPPING centos-csw bash -c 'cd /source && sbt integration/run'
+
+echo "Waiting for 10 seconds"
+sleep 10
 
 echo "Starting integration test docker container"
-docker run -it --rm --name node2 -v /home/unmesh/work/csw-prod:/source -v /home/unmesh/.ivy2:/root/.ivy2 centos-csw bash -c 'cd /source && sbt -DPORT=2552 integration/test'
+docker run -it --rm --name node2 $HOST_DIR_MAPPING centos-csw bash -c 'cd /source && sbt -DPORT=2552 integration/test'
 
 echo "stopping HCD docker container"
 docker stop node1
