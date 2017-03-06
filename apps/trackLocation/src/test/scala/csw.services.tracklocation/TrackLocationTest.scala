fa6b126d7d9a8691a53474c5ef82dce1de9fb67a
Nishant Vishwakarma <nishantv12@gmail.com>
2017-03-06 16:08:55 +0530

    #20:Nishant | Mahendra : Test load settings from configuration file and launch the service.

--- apps/trackLocation/src/test/scala/csw.services.tracklocation/TrackLocationTest.scala
+++ apps/trackLocation/src/test/scala/csw.services.tracklocation/TrackLocationTest.scala
@@ -1,6 +1,7 @@
 package csw.services.tracklocation
 
 import java.net.URI
+import java.nio.file.Paths
 
 import akka.util.Timeout
 import com.typesafe.scalalogging.LazyLogging
@@ -9,7 +10,7 @@ import csw.services.location.models.Connection.TcpConnection
 import csw.services.location.models._
 import csw.services.location.scaladsl.LocationService
 import csw.services.tracklocation.common.TestFutureExtension._
-import org.scalatest.{FunSuiteLike, Matchers}
+import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach, FunSuiteLike, Matchers}
 
 import scala.concurrent.Future
 import scala.concurrent.duration._
@@ -18,34 +19,49 @@ import scala.concurrent.duration._
   * Test the trackLocation app in-line
   */
 
-class TrackLocationTest extends FunSuiteLike with Matchers with LazyLogging {
+class TrackLocationTest
+  extends FunSuiteLike
+    with Matchers
+    with LazyLogging
+    with BeforeAndAfterEach
+    with BeforeAndAfterAll {
 
   private val actorRuntimePort = 2553
+
   val runtime = new ActorRuntime("track-location-test", Map("akka.remote.netty.tcp.port" -> actorRuntimePort))
   private val system = runtime.actorSystem
+  val locationService = LocationService.make(runtime)
+
+  implicit val dispatcher = system.dispatcher
+
+  override protected def afterEach(): Unit = {
+    locationService.unregisterAll().await
+  }
+
+  override protected def afterAll(): Unit = {
+    runtime.actorSystem.terminate().await
+  }
 
   implicit val timeout = Timeout(60.seconds)
   test("Test with command line args") {
     logger.debug("Test1 started")
     val name = "test1"
     val port = 9999
-    implicit val dispatcher = system.dispatcher
 
     Future {
       TrackLocation.main(
-        Array(
-          "--name",
-          name,
-          "--command",
-          "sleep 10",
-          "--port",
-          port.toString,
-          "--no-exit"
-        ))
+      Array(
+      "--name",
+      name,
+      "--command",
+      "sleep 10",
+      "--port",
+      port.toString,
+      "--no-exit"
+      ))
     }
 
     val connection = TcpConnection(ComponentId(name, ComponentType.Service))
-    val locationService = LocationService.make(runtime)
 
     val uri = new URI(s"tcp://${Networks.getPrimaryIpv4Address.getHostAddress}:$port")
 
@@ -57,28 +73,25 @@ class TrackLocationTest extends FunSuiteLike with Matchers with LazyLogging {
     logger.debug("Test1 done")
   }
 
-  //  test("Test with config file") {
-  //    logger.debug("Test2 started")
-  //    val name = "test2"
-  //    val port = 8888
-  //    val url = getClass.getResource("/test2.conf")
-  //    val configFile = Paths.get(url.toURI).toFile.getAbsolutePath
-  //
-  //    Future {
-  //      TrackLocation.main(Array("--name", name, "--no-exit", configFile))
-  //    }
-  //
-  //    val connection = TcpConnection(ComponentId(name, ComponentType.Service))
-  //    val locationsReady = Await.result(LocationService.resolve(Set(connection)), timeout.duration)
-  //    logger.debug(s"Found $locationsReady")
-  //    assert(locationsReady.locations.size == 1)
-  //    val loc = locationsReady.locations.head
-  //    assert(loc.isResolved)
-  //    assert(loc.connection.connectionType == TcpType)
-  //    assert(loc.connection.componentId.name == name)
-  //    val tcpLoc = loc.asInstanceOf[ResolvedTcpLocation]
-  //    assert(tcpLoc.port == port)
-  //    logger.debug(s"$name passed")
-  //    logger.debug("Test2 done")
-  //  }
+    test("Test with config file") {
+      logger.debug("Test2 started")
+      val name = "test2"
+      val port = 8888
+      val url = getClass.getResource("/test2.conf")
+      val configFile = Paths.get(url.toURI).toFile.getAbsolutePath
+
+      Future {
+        TrackLocation.main(Array("--name", name, "--no-exit", configFile))
+      }
+
+      val connection = TcpConnection(ComponentId(name, ComponentType.Service))
+      val resolvedConnection: Resolved = locationService.resolve(connection).await
+      val uri = new URI(s"tcp://${Networks.getPrimaryIpv4Address.getHostAddress}:$port")
+      resolvedConnection shouldBe ResolvedTcpLocation(connection, uri)
+
+      logger.debug(s"$name passed")
+      logger.debug("Test1 done")
+      logger.debug(s"$name passed")
+      logger.debug("Test2 done")
+    }
 }
