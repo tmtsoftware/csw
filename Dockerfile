db47af0a8fad031e800f5bfa83859d67e3fce50c
Pritam Kadam <phkadam2008@gmail.com>
2017-03-07 22:52:43 +0530

    #050 : Pritam : Upgrade docker version on travis and set env variable.

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,7 @@ FROM twtmt/centos-tmt:latest
 ENV	HOME /root
 ENV	LANG en_US.UTF-8
 ENV	LC_ALL en_US.UTF-8
+ENV NO_PROXY="*.local, 169.254/16"
+ENV no_proxy="*.local, 169.254/16"
 ADD . /source/csw/
 WORKDIR /source/csw
--- integration/scripts/execute_docker.sh
+++ integration/scripts/execute_docker.sh
@@ -10,12 +10,13 @@ NC='\033[0m' # No Color
 #docker build -t tmt/csw-centos .
 
 printf "${YELLOW}----------- Starting HCD Docker container -----------${NC}\n"
-docker run -d --name hcd-node -v ~/.ivy2/:/root/.ivy2/ tmt/local-csw-centos bash -c 'cd integration && sbt run'
+docker run -d -e no_proxy="*.local, 169.254/16" --name hcd-node -v ~/.ivy2/:/root/.ivy2/ tmt/local-csw-centos bash -c 'cd integration && sbt run'
+docker inspect --format='' hcd-node
 printf "${PURPLE}------ Waiting for 10 seconds to let HCD gets started ------${NC}\n"
 sleep 60
 
 printf "${YELLOW}------ Starting another Docker container to execute tests ------${NC}\n"
-docker run -it --rm --name it-node -v ~/.ivy2/:/root/.ivy2/ tmt/local-csw-centos bash -c 'cd integration && sbt -DPORT=2552 test'
-
+docker run -it --rm -e no_proxy="*.local, 169.254/16" --name it-node -v ~/.ivy2/:/root/.ivy2/ tmt/local-csw-centos bash -c 'cd integration && sbt -DPORT=2552 test'
+docker inspect --format='' it-node
 printf "${YELLOW}------------ force killing hcd node"
 docker rm -f hcd-node
