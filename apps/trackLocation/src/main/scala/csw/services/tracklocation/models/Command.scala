798b6cd103ba9c80260095fa7810786f29b029e5
Mahendra Kulkarni <mahendra.pk@gmail.com>
2017-03-07 10:48:09 +0530

    #20: Nishant | Mahendra: Merged the refactoring.

--- apps/trackLocation/src/main/scala/csw/services/tracklocation/models/Command.scala
+++ apps/trackLocation/src/main/scala/csw/services/tracklocation/models/Command.scala
@@ -3,7 +3,7 @@ package csw.services.tracklocation.models
 import java.io.File
 
 import com.typesafe.config.{Config, ConfigFactory, ConfigResolveOptions}
-import csw.services.tracklocation.utils.{PortOptHandler, StringOptHandler}
+import csw.services.tracklocation.utils.OptionsHandler
 
 case class Command(
     commandText: String,
@@ -15,8 +15,9 @@ case class Command(
 object Command{
   def parse(options:Options): Command = {
     val appConfig: Option[Config] = options.appConfigFile.flatMap(getAppConfig)
-    val port = PortOptHandler("port", options.port, options, appConfig)
-    val command = StringOptHandler("command", options.command, options, appConfig)
+    val optionsHandler = OptionsHandler(options, appConfig)
+    val port = optionsHandler.portOpt("port", options.port)
+    val command = optionsHandler.stringOpt("command", options.command)
       .get
       .replace("%port", port.toString)
     val defaultDelay = 1000
