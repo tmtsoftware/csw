86d5f84cef7906fd12ce15f89c53a9b017496c24
Saloni Vithalani <skvithalani@gmail.com>
2017-03-09 12:05:36 +0530

    #016 : Saloni | Mahendra : Corrected tests for exception during registration

--- csw-location/src/test/scala/csw/services/location/scaladsl/LocationServiceTest.scala
+++ csw-location/src/test/scala/csw/services/location/scaladsl/LocationServiceTest.scala
@@ -1,13 +1,13 @@
 package csw.services.location.scaladsl
 
-import javax.jmdns.JmDNS
+import javax.jmdns.{JmDNS, ServiceInfo}
 
-import csw.services.location.common.ActorRuntime
 import csw.services.location.common.TestFutureExtension.RichFuture
 import csw.services.location.models.Connection.TcpConnection
 import csw.services.location.models.{ComponentId, ComponentType, TcpRegistration}
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.{FunSuite, Matchers}
+import csw.services.location.common.{ActorRuntime, Constants}
 
 import scala.util.Failure
 
@@ -17,7 +17,9 @@ class LocationServiceTest extends FunSuite with Matchers with MockFactory {
     val registration = TcpRegistration(TcpConnection(ComponentId("", ComponentType.HCD)), 100)
 
     val jmDNS = stub[JmDNS]
+
     (jmDNS.registerService _).when(registration.serviceInfo).throws(CustomExp)
+    (jmDNS.list(_:String)).when(Constants.DnsType).returns(new Array[ServiceInfo](0))
 
     val runtime = new ActorRuntime("test")
     val locationService: LocationService = new LocationServiceImpl(jmDNS, runtime, null)
