b449f2920262d2189135bf7151881fba3e47148a
Mushtaq Ahmed <mushtaq.a@gmail.com>
2017-03-22 19:34:03 +0530

    #000: Mushtaq | Saloni : Crdt tests run in docker.

--- integration/scripts/runner-crdt.sh
+++ integration/scripts/runner-crdt.sh
@@ -9,36 +9,23 @@ NC='\033[0m' # No Color
 
 #Map local project directories and .ivy directory from host to docker container
 #e.g.  ./integration/scripts/execute_docker.sh '-v /home/unmesh/work/csw-prod:/source -v /home/unmesh/.ivy2:/root/.ivy2'
-HOST_DIR_MAPPING=$1
+#HOST_DIR_MAPPING=$1
 echo "$HOST_DIR_MAPPING"
 
-if [ "$#" -eq  "0" ]
-   then
-     printf "${RED} Please provide host directory mappings for source root and .ivy. e.g. ${NC} ./integration/scripts/execute_docker.sh '-v /home/unmesh/work/csw-prod:/source -v /home/unmesh/.ivy2:/root/.ivy2' \n"
-     exit 1
-fi
+#if [ "$#" -eq  "0" ]
+#   then
+#     printf "${RED} Please provide host directory mappings for source root and .ivy. e.g. ${NC} ./integration/scripts/execute_docker.sh '-v /home/unmesh/work/csw-prod:/source -v /home/unmesh/.ivy2:/root/.ivy2' \n"
+#     exit 1
+#fi
 
 
-docker run -d --name=HCD $HOST_DIR_MAPPING tmt/local-csw-centos tail -f /dev/null
-docker run -d --name=Reddis $HOST_DIR_MAPPING tmt/local-csw-centos tail -f /dev/null
-docker run -d --name=Test-App $HOST_DIR_MAPPING tmt/local-csw-centos tail -f /dev/null
+docker run -d --name=HCD $HOST_DIR_MAPPING tmt/local-csw-centos bash -c 'cd source && ./integration/target/universal/integration-10000/bin/trombone-h-c-d-crdt'
+docker run -d --name=Reddis $HOST_DIR_MAPPING tmt/local-csw-centos bash -c 'cd source && ./integration/target/universal/integration-10000/bin/test-service-crdt -DakkaSeed=172.17.0.2'
 
+sleep 5
 
-printf "${YELLOW}----------- Starting HCD App -----------${NC}\n"
-docker exec -d HCD bash -c 'cd source && ./integration/target/universal/integration-10000/bin/trombone-h-c-d-crdt'
+docker run -d --name=Test-App $HOST_DIR_MAPPING tmt/local-csw-centos bash -c 'cd source && ./integration/target/universal/integration-10000/bin/test-crdt-app -DakkaSeed=172.17.0.2'
 
-
-printf "${PURPLE}------ Waiting for 10 seconds to boot up HCD ------${NC}\n"
-sleep 10
-
-printf "${YELLOW}----------- Starting Reddis App -----------${NC}\n"
-#docker exec -d Reddis bash -c 'cd source && ./integration/target/universal/integration-10000/bin/test-service-crdt -DakkaSeed=1.1.1.1'
-
-printf "${PURPLE}------ Waiting for 10 seconds to boot up Reddis ------${NC}\n"
-sleep 10
-
-printf "${YELLOW}------ Starting Test App ------${NC}\n"
-#docker exec Test-App bash -c 'cd source && ./integration/target/universal/integration-10000/bin/test-crdt-app -DakkaSeed=1.1.1.1'
 test_exit_code=$?
 
 printf "${PURPLE}---------- Stopping and Removing all docker containers ---------- ${NC}"
