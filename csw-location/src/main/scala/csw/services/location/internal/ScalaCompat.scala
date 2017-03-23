04c33d7b8c6c5a66c98781ae1c3ee4aed342ae28
Saloni Vithalani <skvithalani@gmail.com>
2017-03-23 17:02:28 +0530

    #093 : Saloni | Nishant : Removed ScalaCompat, execute_docker and runner_crdt

--- csw-location/src/main/scala/csw/services/location/internal/ScalaCompat.scala
+++
@@ -1,7 +0,0 @@
-package csw.services.location.internal
-
-import scala.collection.JavaConverters._
-
-object ScalaCompat {
-  def toMap[K, V](javaMap: java.util.Map[K, V]): Map[K, V] = javaMap.asScala.toMap
-}
