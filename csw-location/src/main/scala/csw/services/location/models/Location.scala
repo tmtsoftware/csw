ab4995b81a3bd0fc03bfb485dbb338764ec33a82
Mushtaq Ahmed <mushtaq.a@gmail.com>
2017-03-25 08:59:20 +0530

    #094: Mushtaq : Remove leftover private from the refactoring.

--- csw-location/src/main/scala/csw/services/location/models/Location.scala
+++ csw-location/src/main/scala/csw/services/location/models/Location.scala
@@ -11,19 +11,19 @@ sealed abstract class Location extends TmtSerializable {
   def uri: URI
 }
 
-final case class AkkaLocation private (connection: AkkaConnection, uri: URI, actorRef: ActorRef) extends Location {
+final case class AkkaLocation(connection: AkkaConnection, uri: URI, actorRef: ActorRef) extends Location {
   def this(connection: AkkaConnection, actorRef: ActorRef) = {
     this(connection, new URI(ActorPath.fromString(Serialization.serializedActorPath(actorRef)).toString), actorRef)
   }
 }
 
-final case class TcpLocation private (connection: TcpConnection, uri: URI) extends Location {
+final case class TcpLocation(connection: TcpConnection, uri: URI) extends Location {
   def this(connection: TcpConnection, hostname: String, port: Int) = {
     this(connection, new URI(s"tcp://$hostname:$port"))
   }
 }
 
-final case class HttpLocation private (connection: HttpConnection, uri: URI) extends Location {
+final case class HttpLocation(connection: HttpConnection, uri: URI) extends Location {
   def this(connection: HttpConnection, hostname: String, port: Int, path: String) = {
     this(connection, new URI(s"http://$hostname:$port/$path"))
   }
