13261c7b2a709c55c0e19df85f5a4fd8e10bc15e
unmesh <unmeshjoshi@gmail.com>
2017-03-28 22:35:34 +0530

    Jenkins slaves are ubuntu changing yum to apt-get. Need to remove this and make part of provisioning

--- universal_package.sh
+++ universal_package.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-yum -y install unzip
+sudo apt-get install unzip
 sbt -DenableCoverage=false universal:packageBin
 echo "All" | unzip integration/target/universal/integration-0.1-SNAPSHOT.zip -d integration/target/universal/
