81fdd48f8c6687fa8c4dc17ee0990fe318ea2073
Pritam Kadam <phkadam2008@gmail.com>
2017-03-29 23:59:40 +0530

    #67 : Pritam : Add Multi-Node-Test stage in Jenkinsfile and get the list of multi node hosts from jenkins environment variables.

--- Jenkinsfile
+++ Jenkinsfile
@@ -38,8 +38,12 @@ node {
                         sh "exit 1"
                 }
 
-                stage('Multi-Jvm Test') { // These tests cover the scenario of multiple components in multiple containers on same machine
+                stage('Multi-Jvm Test') { // These tests cover the scenario of multiple components in multiple containers on same machine.
                     sh "sbt csw-location/multi-jvm:test"
+                }
+
+                stage('Multi-Node Test') { // These tests cover the scenario of multiple components in across machines.
+                   sh "sbt csw-location/multi-node-test"
                 }
 
                 stage('Package') {
--- project/AutoMultiJvm.scala
+++ project/AutoMultiJvm.scala
@@ -3,14 +3,15 @@ import sbt._
 
 object AutoMultiJvm extends AutoPlugin {
   import com.typesafe.sbt.SbtMultiJvm
-  import SbtMultiJvm.MultiJvmKeys
+  import SbtMultiJvm.MultiJvmKeys._
 
   override def projectSettings: Seq[Def.Setting[_]] = SbtMultiJvm.multiJvmSettings ++ Seq(
     test := {
       (test in Test).value
-      (test in MultiJvmKeys.MultiJvm).value
-    }
+      (test in MultiJvm).value
+    },
+    multiNodeHosts in MultiJvm := sys.env.getOrElse("multiNodeHosts", "localhost").split(",").toSeq
   )
 
-  override def projectConfigurations: Seq[Configuration] = List(MultiJvmKeys.MultiJvm)
+  override def projectConfigurations: Seq[Configuration] = List(MultiJvm)
 }
