23cd9934933aeab065d80b9848308171d042b281
Saloni Vithalani <skvithalani@gmail.com>
2017-03-29 16:47:49 +0530

    #040 : Saloni : Added comments for ConnectionType

--- csw-location/src/main/scala/csw/services/location/models/ConnectionType.scala
+++ csw-location/src/main/scala/csw/services/location/models/ConnectionType.scala
@@ -5,28 +5,34 @@ import enumeratum._
 import scala.collection.immutable.IndexedSeq
 
 /**
- * Connection type: Indicate if it is an http server or an akka actor.
- */
+  * Represents a type of connection offered by the `Component`
+  *
+  * @param entryName A name of the connection e.g. akka, http or tcp
+  */
 sealed abstract class ConnectionType(override val entryName: String) extends EnumEntry with TmtSerializable {
   def name: String = entryName
 }
 
 object ConnectionType extends Enum[ConnectionType] {
 
+  /**
+    * Return all `ConnectionType` values
+    */
   override def values: IndexedSeq[ConnectionType] = findValues
 
   /**
-   * Type of a REST/HTTP based service
-   */
+    * Type of a REST/HTTP based service
+    */
   case object HttpType extends ConnectionType("http")
 
   /**
-   * Type of a TCP based service
-   */
+    * Type of a TCP based service
+    */
   case object TcpType extends ConnectionType("tcp")
 
   /**
-   * Type of an Akka actor based service
-   */
+    * Type of an Akka actor based service
+    */
   case object AkkaType extends ConnectionType("akka")
+
 }
