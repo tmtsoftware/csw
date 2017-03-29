29eefc593679742cfb70f67f0089569dc3051638
Saloni Vithalani <skvithalani@gmail.com>
2017-03-29 17:34:21 +0530

    #040 : Saloni : Added comments for TrackingEvent

--- csw-location/src/main/scala/csw/services/location/models/TrackingEvent.scala
+++ csw-location/src/main/scala/csw/services/location/models/TrackingEvent.scala
@@ -1,10 +1,24 @@
 package csw.services.location.models
 
+/**
+  * Represents `Tracking events` for a given `Connection`
+  */
 sealed abstract class TrackingEvent {
   def connection: Connection
 }
 
+/**
+  * Represents `Update` event for a `Connection`
+  *
+  * @param location A `Location` that got updated
+  */
 case class LocationUpdated(location: Location) extends TrackingEvent {
   override def connection: Connection = location.connection
 }
+
+/**
+  * Represents `Removed` event for a `Connection`
+  *
+  * @param connection A `Connection` for which `Location` was removed
+  */
 case class LocationRemoved(connection: Connection) extends TrackingEvent
