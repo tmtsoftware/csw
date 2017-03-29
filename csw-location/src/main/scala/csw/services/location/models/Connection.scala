b6033c1711c0491cdf849a70ded5066ddcc6c849
Saloni Vithalani <skvithalani@gmail.com>
2017-03-29 16:11:14 +0530

    #040 : Saloni : Added comments for Connection

--- csw-location/src/main/scala/csw/services/location/models/Connection.scala
+++ csw-location/src/main/scala/csw/services/location/models/Connection.scala
@@ -3,32 +3,42 @@ package csw.services.location.models
 import csw.services.location.models.ConnectionType.{AkkaType, HttpType, TcpType}
 
 /**
-  * Describes a component and the way it is accessed (http, akka)
-  * connectionType: Indicates how the component is accessed (http, akka)
+  * Manages a unique connection based on `ConnectionType` and [[csw.services.location.models.ComponentId]]
+  *
+  * @param connectionType A type of connection offered by component e.g. akka, http or tcp
   */
 sealed abstract class Connection(val connectionType: ConnectionType) extends TmtSerializable {
   /**
-    * Holds the component name and type (assembly, hcd, etc.)
+    * A unique component id based on `Component name` and [[csw.services.location.models.ComponentType]]
     */
   def componentId: ComponentId
 
+  /**
+    * Creates a unique name for `Connection` composed of `Component name`, `ComponentType` and `ConnectionType`
+    */
   def name: String = s"${componentId.name}-${componentId.componentType.name}-${connectionType.name}"
 }
 
 object Connection {
 
   /**
-    * A connection to a remote akka actor based component
+    * A connection to a remote akka based component
+    *
+    * @param componentId A component providing akka services
     */
   final case class AkkaConnection(componentId: ComponentId) extends Connection(AkkaType)
 
   /**
     * A connection to a remote http based component
+    *
+    * @param componentId A component providing http services
     */
   final case class HttpConnection(componentId: ComponentId) extends Connection(HttpType)
 
   /**
     * A connection to a remote tcp based component
+    *
+    * @param componentId A component providing tcp services
     */
   final case class TcpConnection(componentId: ComponentId) extends Connection(TcpType)
 
