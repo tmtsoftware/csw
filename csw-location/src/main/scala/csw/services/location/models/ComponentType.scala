7154f0440d93c935a2fa300c5879e1522520ad30
Saloni Vithalani <skvithalani@gmail.com>
2017-03-29 16:43:09 +0530

    #040 : Saloni : Added comments for ComponentType

--- csw-location/src/main/scala/csw/services/location/models/ComponentType.scala
+++ csw-location/src/main/scala/csw/services/location/models/ComponentType.scala
@@ -6,33 +6,41 @@ import enumeratum.{Enum, EnumEntry}
 import scala.collection.immutable.IndexedSeq
 
 /**
- * CSW Component types
- */
+  * Represents different types of Component. `ComponentType` is serializable as it will be replicated on cluster
+  */
 sealed abstract class ComponentType extends EnumEntry with Lowercase with TmtSerializable {
+
+  /**
+    * Lowercase name of the `ComponentType` e.g. for HCD type of components, the name will be `hcd`
+    */
   def name: String = entryName
 }
 
-object ComponentType extends Enum[ComponentType]{
+object ComponentType extends Enum[ComponentType] {
 
+  /**
+    * Return all `ComponentType` values
+    */
   def values: IndexedSeq[ComponentType] = findValues
 
   /**
-   * A container for components (assemblies and HCDs)
-   */
+    * A container for components (assemblies and HCDs)
+    */
   case object Container extends ComponentType
 
   /**
-   * A component that controls a hardware device
-   */
+    * A component that controls a hardware device
+    */
   case object HCD extends ComponentType
 
   /**
-   * A component that controls one or more HCDs or assemblies
-   */
+    * A component that controls one or more HCDs or assemblies
+    */
   case object Assembly extends ComponentType
 
   /**
-   * A general purpose service component (actor and/or web service application)
-   */
+    * A general purpose service component e.g. actor and/or web service application
+    */
   case object Service extends ComponentType
+
 }
