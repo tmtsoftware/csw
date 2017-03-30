2ba51d8978fbf6b6c1bcafd9fbec4cccead57a00
Saloni Vithalani <skvithalani@gmail.com>
2017-03-30 11:29:05 +0530

    #040 : Saloni : Added comments for StreamExt

--- csw-location/src/main/scala/csw/services/location/internal/StreamExt.scala
+++ csw-location/src/main/scala/csw/services/location/internal/StreamExt.scala
@@ -9,12 +9,30 @@ import scala.concurrent.{Future, Promise}
 
 object StreamExt {
 
+  /**
+    * A [[akka.stream.scaladsl.Source]] with materialization of `ActorRef` is created and split to get the [[scala.concurrent.Future]]
+    * of materialization. A tuple of newly created `Source` that can be consumed and `Future` of materialization (i.e. `ActorRef`)
+    * is returned
+    */
   def actorCoupling[T]: (Source[T, NotUsed], Future[ActorRef]) = {
     Source.actorRef[T](256, OverflowStrategy.dropHead).splitMat
   }
 
+  /**
+    * An `Extension` of [[akka.stream.scaladsl.Source]]
+    *
+    * @param source A `Source` for which functionality extension is required
+    * @tparam Out The type of values this `Source` will contain
+    * @tparam Mat The type of materialization this `Source` will materialize to
+    */
   implicit class RichSource[Out, Mat](val source: Source[Out, Mat]) extends AnyVal {
 
+    /**
+      * Splits the [[akka.stream.scaladsl.Source]] to get a new `Source` that will receive values from the materialization
+      * once the `Source` is materialized and the `Future` of that materialization. The `Future` will complete when the
+      * `Source` will be materialized but we get the handle to that materialization right away to perform any operation
+      * on it.
+      */
     def splitMat: (Source[Out, NotUsed], Future[Mat]) = {
       val p = Promise[Mat]
       val s = source.mapMaterializedValue { m =>
@@ -24,6 +42,10 @@ object StreamExt {
       (s, p.future)
     }
 
+    /**
+      * A new `Source` is created which will be materialized to [[akka.stream.KillSwitch]]. This stream of `Source` can
+      * be terminated any time using the `KillSwitch`
+      */
     def cancellable: Source[Out, KillSwitch] = source.viaMat(KillSwitches.single)(Keep.right)
   }
 
