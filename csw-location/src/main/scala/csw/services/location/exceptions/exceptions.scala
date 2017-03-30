7679b0a0efc20f132026dda1d3551b2af8f81f4f
Saloni Vithalani <skvithalani@gmail.com>
2017-03-30 10:46:19 +0530

    #040 : Saloni : Added comments for Exceptions

--- csw-location/src/main/scala/csw/services/location/exceptions/exceptions.scala
+++ csw-location/src/main/scala/csw/services/location/exceptions/exceptions.scala
@@ -2,18 +2,37 @@ package csw.services.location.exceptions
 
 import csw.services.location.models.{Connection, Location}
 
+/**
+  * An Exception representing failure in registration
+  *
+  * @param connection A `Connection` for which registration failed
+  */
 case class RegistrationFailed(connection: Connection) extends RuntimeException(
   s"unable to register $connection"
 )
 
+/**
+  * An Exception representing failure in un-registration
+  *
+  * @param connection A `Connection` for which un-registration failed
+  */
 case class UnregistrationFailed(connection: Connection) extends RuntimeException(
   s"unable to unregister $connection"
 )
 
+/**
+  * An Exception representing failure in registration because of some other location registered for the same `Connection`
+  *
+  * @param location      The `Location` that component tried to register and failed
+  * @param otherLocation The `Location` which was already present against the `Connection` that component was trying to register
+  */
 case class OtherLocationIsRegistered(location: Location, otherLocation: Location) extends RuntimeException(
   s"there is other location=$otherLocation registered against name=${location.connection.name}."
 )
 
+/**
+  * An Exception representing failure in listing locations
+  */
 case object RegistrationListingFailed extends RuntimeException(
   s"unable to get the list of registered locations"
 )
