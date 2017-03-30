e00404424cf60476d4b2a91fbfbc525841da5c6b
unmesh <unmeshjoshi@gmail.com>
2017-03-30 15:57:34 +0530

    Unmesh: Removing multinode tests and fixing docker script

--- Jenkinsfile
+++ Jenkinsfile
@@ -41,11 +41,6 @@ node {
                 stage('Multi-Jvm Test') { // These tests cover the scenario of multiple components in multiple containers on same machine.
                     sh "sbt csw-location/multi-jvm:test"
                 }
-
-                stage('Multi-Node Test') { // These tests cover the scenario of multiple components in across machines.
-                   sh "sbt -DenableCoverage=false csw-location/multi-node-test"
-                }
-
                 stage('Package') {
                     sh "./universal_package.sh"
                     stash name: "repo"
--- integration/scripts/runner.sh
+++ integration/scripts/runner.sh
@@ -19,17 +19,17 @@ HOST_DIR_MAPPING=$1
 printf "${YELLOW}----------- Starting HCD App -----------${NC}\n"
 docker run -d --name=HCD $HOST_DIR_MAPPING tmt/local-csw-centos bash -c 'cd /source/csw && ./integration/target/universal/integration-0.1-SNAPSHOT/bin/trombone-h-c-d'
 
-akkaSeed=$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' HCD)
-printf "${PURPLE}----------- Akka Seed Node is : ${akkaSeed}-----------${NC}\n"
+clusterSeed=$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' HCD)
+printf "${PURPLE}----------- Akka Seed Node is : ${clusterSeed}-----------${NC}\n"
 sleep 5
 
 printf "${YELLOW}----------- Starting Reddis App -----------${NC}\n"
-docker run -d --name=Reddis --env akkaSeed=$akkaSeed $HOST_DIR_MAPPING tmt/local-csw-centos bash -c 'cd /source/csw && ./integration/target/universal/integration-0.1-SNAPSHOT/bin/test-service -DakkaSeed=$akkaSeed'
+docker run -d --name=Reddis --env clusterSeed=$clusterSeed $HOST_DIR_MAPPING tmt/local-csw-centos bash -c 'cd /source/csw && ./integration/target/universal/integration-0.1-SNAPSHOT/bin/test-service -DclusterSeed=$clusterSeed'
 
 sleep 5
 
 printf "${YELLOW}------ Starting Test App ------${NC}\n"
-docker run --name=Test-App --env akkaSeed=$akkaSeed $HOST_DIR_MAPPING tmt/local-csw-centos bash -c 'cd /source/csw && ./integration/target/universal/integration-0.1-SNAPSHOT/bin/test-app -DakkaSeed=$akkaSeed'
+docker run --name=Test-App --env clusterSeed=$clusterSeed $HOST_DIR_MAPPING tmt/local-csw-centos bash -c 'cd /source/csw && ./integration/target/universal/integration-0.1-SNAPSHOT/bin/test-app -DclusterSeed=$clusterSeed'
 test_exit_code=$?
 
 printf "${PURPLE}---------- Stopping and Removing all docker containers ---------- ${NC}"
