b0427630856ad454dc25fb22354f47e3d9334e0d
Pritam Kadam <phkadam2008@gmail.com>
2017-03-30 19:32:25 +0530

    #19 : Pritam : Provide clusterSeed to the container running multiple nic test.

--- integration/scripts/multiple_nic_test.sh
+++ integration/scripts/multiple_nic_test.sh
@@ -30,7 +30,7 @@ docker exec -itd Assembly bash -c 'ifconfig && cd /source/csw && ./integration/t
 printf "${PURPLE}------ Waiting for 10 seconds to let Assembly gets started ------${NC}\n"
 sleep 10
 printf "${YELLOW} Executing test in network : tmt_net_2 ${NC}\n"
-docker exec Test-App bash -c 'ifconfig && cd /source/csw && ./integration/target/universal/integration-0.1-SNAPSHOT/bin/test-multiple-nic-app -DakkaSeed=172.17.0.2'
+docker exec Test-App bash -c 'ifconfig && cd /source/csw && ./integration/target/universal/integration-0.1-SNAPSHOT/bin/test-multiple-nic-app -DclusterSeed=172.17.0.2'
 exit_code=$?
 
 printf "${ORANGE}------ [Debug] Inspecting docker bridge ------${NC}"
