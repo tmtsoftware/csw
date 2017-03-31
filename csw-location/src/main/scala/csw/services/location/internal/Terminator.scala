f28da9b04167ab3ed088c8425a0af628a70ec25a
Saloni Vithalani <skvithalani@gmail.com>
2017-03-31 11:35:24 +0530

    #040 : Saloni : Added comments in Terminator

--- csw-location/src/main/scala/csw/services/location/internal/Terminator.scala
+++ csw-location/src/main/scala/csw/services/location/internal/Terminator.scala
@@ -6,10 +6,21 @@ import akka.cluster.Cluster
 
 import scala.concurrent.{ExecutionContext, Future, Promise}
 
+/**
+  * Manages termination of `ActorSystem` and gracefully disconnecting from cluster
+  */
 object Terminator {
   //do not use the dying actorSystem's dispatcher for scheduling actions after its death.
   import ExecutionContext.Implicits.global
 
+  /**
+    * Performs the termination with two steps in sequence as follows :
+    *  - The `ActorSystem` leaves the cluster gracefully and then
+    *  - It is terminated
+    *
+    * @param actorSystem The `ActorSystem` that needs to be cleaned up
+    * @return A `Future` that completes on successful shutdown of `ActorSystem`
+    */
   def terminate(actorSystem: ActorSystem): Future[Done] = {
     val cluster = Cluster(actorSystem)
     val p = Promise[Terminated]
