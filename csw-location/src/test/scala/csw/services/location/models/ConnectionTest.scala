b8461ef21ed355af00bf8649350ba7519bc7540a
Pritam Kadam <phkadam2008@gmail.com>
2017-03-31 12:53:42 +0530

    #14 : Pritam : Add unit tests for Connection data model.

---
+++ csw-location/src/test/scala/csw/services/location/models/ConnectionTest.scala
@@ -0,0 +1,14 @@
+package csw.services.location.models
+
+import csw.services.location.models.Connection.AkkaConnection
+import org.scalatest.{FunSuite, Matchers}
+
+class ConnectionTest extends FunSuite with Matchers {
+
+  test("should able to form a connection name with componentId name, componentId type and connection type separated by '-'") {
+    val expectedAkkaConnectionName = "tromboneHcd-hcd-akka"
+    val akkaConnection = new AkkaConnection(new ComponentId("tromboneHcd", ComponentType.HCD))
+    akkaConnection.name shouldBe expectedAkkaConnectionName
+  }
+
+}
