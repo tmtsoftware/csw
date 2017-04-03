b6de5b378e3095418b810ddf128fa7a960f7a620
Saloni Vithalani <skvithalani@gmail.com>
2017-04-03 13:27:30 +0530

    #040 : Saloni : Updated comments for LocationServicefactory

--- csw-location/src/main/scala/csw/services/location/scaladsl/LocationServiceFactory.scala
+++ csw-location/src/main/scala/csw/services/location/scaladsl/LocationServiceFactory.scala
@@ -14,14 +14,14 @@ object LocationServiceFactory {
     * Creates a [[csw.services.location.scaladsl.LocationService]] instance and joins to the akka cluster. The data
     * of the akka cluster will now be replicated on this newly created node.
     *
-    * @note It is recommended to create
-    *       a single instance of `LocationService` and use it throughout.
+    * @note It is recommended to create a single instance of `LocationService` and use it throughout.
     * @return A `LocationService` instance
     */
   def make(): LocationService = withCluster(CswCluster.make())
 
   /**
-    * Creates a [[csw.services.location.scaladsl.LocationService]] instance
+    * Creates a [[csw.services.location.scaladsl.LocationService]] instance. The data of the akka cluster will now be replicated
+    * on this newly created node.
     *
     * @note It is highly recommended to use it for testing purposes only.
     * @param cswCluster An [[csw.services.location.scaladsl.CswCluster]] with custom configuration
