42e2355ed93e82b4d8c2ba3c25f7a3600b9df162
Pritam Kadam <phkadam2008@gmail.com>
2017-04-04 12:20:31 +0530

    #034 : Pritam: Add multi-node test for resolving and sending message to remote akka connection.

--- csw-location/src/multi-jvm/scala/csw/services/location/LocationServiceTest.scala
+++ csw-location/src/multi-jvm/scala/csw/services/location/LocationServiceTest.scala
@@ -1,6 +1,6 @@
 package csw.services.location
 
-import akka.actor.{Actor, Props}
+import akka.actor.{Actor, ActorSystem, Props}
 import akka.cluster.ddata.DistributedData
 import akka.cluster.ddata.Replicator.{GetReplicaCount, ReplicaCount}
 import akka.stream.scaladsl.Keep
@@ -9,17 +9,32 @@ import csw.services.location.common.TestFutureExtension.RichFuture
 import csw.services.location.helpers.{LSNodeSpec, OneMemberAndSeed}
 import csw.services.location.models.Connection.{AkkaConnection, HttpConnection, TcpConnection}
 import csw.services.location.models._
-import csw.services.location.scaladsl.LocationServiceFactory
+import csw.services.location.scaladsl.{LocationService, LocationServiceFactory}
+import org.scalatest.BeforeAndAfterEach
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
 
 class LocationServiceTestMultiJvmNode1 extends LocationServiceTest(0)
 class LocationServiceTestMultiJvmNode2 extends LocationServiceTest(0)
 
-class LocationServiceTest(ignore: Int) extends LSNodeSpec(config = new OneMemberAndSeed) {
+class LocationServiceTest(ignore: Int) extends LSNodeSpec(config = new OneMemberAndSeed) with BeforeAndAfterEach{
 
   import config._
 
   private val locationService = LocationServiceFactory.withCluster(cswCluster)
   import cswCluster.mat
+
+  val assemblyActorSystem = ActorSystem("assembly-actor-system")
+
+  override protected def afterEach(): Unit = {
+    Await.result(locationService.unregisterAll(), 10.seconds)
+  }
+
+  override def afterAll(): Unit = {
+    super.afterAll()
+    assemblyActorSystem.terminate()
+  }
 
   test("ensure that the cluster is up") {
     enterBarrier("nodes-joined")
@@ -106,4 +121,65 @@ class LocationServiceTest(ignore: Int) extends LSNodeSpec(config = new OneMember
     }
     enterBarrier("after-3")
   }
+
+  test("ensure that component is able to resolve and send message to remote actor created through separate actor system than LocationService actor system") {
+
+    val tcpConnection = TcpConnection(ComponentId("redis1", ComponentType.Service))
+
+    val akkaConnection = AkkaConnection(ComponentId("Assembly1", ComponentType.Assembly))
+
+
+    runOn(seed) {
+      val tcpPort = 470
+      val tcpRegistration = TcpRegistration(tcpConnection, tcpPort)
+
+      locationService.register(tcpRegistration).await
+      enterBarrier("Registration")
+
+      Thread.sleep(2000)
+
+      val resolvedLocation: Location = Await.result(locationService.resolve(akkaConnection), 10.seconds).get
+
+      val assemblyActorRef = resolvedLocation.asInstanceOf[AkkaLocation].actorRef
+
+      assemblyActorRef ! UnregisterConnection(akkaConnection)
+      Thread.sleep(2000)
+      enterBarrier("Unregistration")
+
+      val locations = locationService.list.await
+      val connections: Set[Connection] = locations.map(_.connection).toSet
+
+      locations.size shouldBe 1
+      connections shouldBe (Set(tcpConnection))
+    }
+
+    runOn(member) {
+      val actorRef = assemblyActorSystem.actorOf(AssemblyActor.props(locationService) , "assembly-actor")
+
+      locationService.register(AkkaRegistration(akkaConnection, actorRef)).await
+
+      enterBarrier("Registration")
+
+      Thread.sleep(2000)
+
+      enterBarrier("Unregistration")
+    }
+
+    enterBarrier("after-4")
+  }
+
+}
+
+case class UnregisterConnection(akkaConnection: AkkaConnection)
+
+class AssemblyActor(locationService: LocationService) extends Actor {
+  override def receive: Receive = {
+    case UnregisterConnection(akkaConnection) => {
+      Await.result(locationService.unregister(akkaConnection), 10.seconds)
+    }
+  }
+}
+
+object AssemblyActor {
+  def props(locationService: LocationService): Props = Props(new AssemblyActor(locationService))
 }
