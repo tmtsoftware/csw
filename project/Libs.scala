decbc2c4e0487ed5139dd4c56255e76b553ef66f
Mahendra Kulkarni <mahendra.pk@gmail.com>
2017-04-05 12:09:12 +0530

    #000: Saloni | Mahendra : Added license info for libraries dependencies.

--- project/Libs.scala
+++ project/Libs.scala
@@ -1,24 +1,24 @@
 import sbt._
 
 object Libs {
-  val `scalatest` = "org.scalatest" %% "scalatest" % "3.0.1"
-  val `scala-java8-compat` = "org.scala-lang.modules" %% "scala-java8-compat" % "0.8.0"
-  val `scala-async` = "org.scala-lang.modules" %% "scala-async" % "0.9.6"
-  val `scopt` = "com.github.scopt" %% "scopt" % "3.5.0"
-  val `scala-logging` = "com.typesafe.scala-logging" %% "scala-logging" % "3.5.0"
-  val `acyclic` = "com.lihaoyi" %% "acyclic" % "0.1.7" % Provided
-  val `enumeratum` = "com.beachape" %% "enumeratum" % "1.5.8"
-  val `junit` = "junit" % "junit" % "4.12"
-  val `junit-interface` = "com.novocode" % "junit-interface" % "0.11"
-  val `mockito-core` = "org.mockito" % "mockito-core" % "2.7.17"
-  val `logback-classic` = "ch.qos.logback" % "logback-classic" % "1.1.3" % Runtime
-  val `chill-akka` = "com.twitter" %% "chill-akka" % "0.9.2"
-  val `akka-management-cluster-http` = "com.lightbend.akka" %% "akka-management-cluster-http" % "0.1-RC1"
-  val `akka-http` = "com.typesafe.akka" %% "akka-http" % "10.0.5"
+  val `scalatest` = "org.scalatest" %% "scalatest" % "3.0.1" //Apache License 2.0
+  val `scala-java8-compat` = "org.scala-lang.modules" %% "scala-java8-compat" % "0.8.0" //BSD 3-clause "New" or "Revised" License
+  val `scala-async` = "org.scala-lang.modules" %% "scala-async" % "0.9.6" //BSD 3-clause "New" or "Revised" License
+  val `scopt` = "com.github.scopt" %% "scopt" % "3.5.0" //MIT License
+  val `scala-logging` = "com.typesafe.scala-logging" %% "scala-logging" % "3.5.0" //Apache License 2.0
+  val `acyclic` = "com.lihaoyi" %% "acyclic" % "0.1.7" % Provided //MIT License
+  val `enumeratum` = "com.beachape" %% "enumeratum" % "1.5.8" //MIT License
+  val `junit` = "junit" % "junit" % "4.12" //Eclipse Public License 1.0
+  val `junit-interface` = "com.novocode" % "junit-interface" % "0.11" //BSD 2-clause "Simplified" License
+  val `mockito-core` = "org.mockito" % "mockito-core" % "2.7.17" //MIT License
+  val `logback-classic` = "ch.qos.logback" % "logback-classic" % "1.1.3" % Runtime //Dual license: Either, Eclipse Public License v1.0 or GNU Lesser General Public License version 2.1
+  val `chill-akka` = "com.twitter" %% "chill-akka" % "0.9.2" //Apache License 2.0
+  val `akka-management-cluster-http` = "com.lightbend.akka" %% "akka-management-cluster-http" % "0.1-RC1" //N/A at the moment
+  val `akka-http` = "com.typesafe.akka" %% "akka-http" % "10.0.5" //Apache License 2.0
 }
 
 object Akka {
-  val Version = "2.5.0-RC2"
+  val Version = "2.5.0-RC2" //all akka is Apache License 2.0
   val `akka-stream` = "com.typesafe.akka" %% "akka-stream" % Version
   val `akka-remote` = "com.typesafe.akka" %% "akka-remote" % Version
   val `akka-stream-testkit` = "com.typesafe.akka" %% "akka-stream-testkit" % Version
