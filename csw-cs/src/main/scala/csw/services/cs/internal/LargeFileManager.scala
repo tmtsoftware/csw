ef7d5dc715a09aefc1c0d5b3f874fd39eb1612b5
Mahendra Kulkarni <mahendra.pk@gmail.com>
2017-04-06 12:20:47 +0530

    #000: Saloni | Mahendra : Added get for large binary file.

--- csw-cs/src/main/scala/csw/services/cs/internal/LargeFileManager.scala
+++ csw-cs/src/main/scala/csw/services/cs/internal/LargeFileManager.scala
@@ -3,10 +3,15 @@ package csw.services.cs.internal
 import java.io.{File, FileOutputStream}
 import java.nio.file.{Files, Path, Paths}
 
+import akka.http.scaladsl.model.HttpEntity.ChunkStreamPart
+import akka.http.scaladsl.model._
+import akka.stream.scaladsl.Source
 import net.codejava.security.HashGeneratorUtils
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
+import scala.util.Try
+import scala.util.control.NonFatal
 
 class LargeFileManager(settings: Settings) {
 
@@ -25,16 +30,33 @@ class LargeFileManager(settings: Settings) {
     else {
       val out = new FileOutputStream(outFile)
       Files.copy(inFile.toPath, out)
+      out.flush()
       out.close()
       if(FileUtils.validate(fileName, outFile)) {
         id
       }
       else {
         outFile.delete()
-        throw new RuntimeException(s" Error in creating file $id")
+        throw new RuntimeException(s" Error in creating file for $id")
       }
     }
   }
+
+  def get(id: String, outFile: File): Future[File] = Future {
+    val repoDir = settings.dir.replaceFirst("~", System.getProperty("user.home"))
+    val repoFilePath = makePath(new File(repoDir), new File(Uri(id).path.toString()))
+
+    if(repoFilePath.toFile.exists()) {
+      val out = new FileOutputStream(outFile)
+      Files.copy(repoFilePath, out)
+      out.flush()
+      out.close()
+      outFile
+    } else {
+      throw new RuntimeException(s" Error in locating file for $id")
+    }
+  }
+
 
   private def makePath(dir: File, file: File): Path = {
     val (subdir, name) = file.getName.splitAt(2)
