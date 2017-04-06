a1da194ae34bc46ba4402177569b07ccbda10b10
Nishant Vishwakarma <nishantv12@gmail.com>
2017-04-06 13:46:42 +0530

    #042: Nishant  : Added tests for get file by date

--- csw-cs/src/test/scala/csw/services/cs/internal/SvnConfigManagerTest.scala
+++ csw-cs/src/test/scala/csw/services/cs/internal/SvnConfigManagerTest.scala
@@ -2,10 +2,13 @@ package csw.services.cs.internal
 
 import java.io.{FileNotFoundException, IOException}
 import java.nio.file.Paths
+import java.util.Date
 
 import csw.services.cs.common.TestFutureExtension.RichFuture
 import csw.services.cs.models.{ConfigBytes, ConfigData, ConfigString}
 import org.scalatest.Matchers
+
+import scala.concurrent.ExecutionContext
 
 class SvnConfigManagerTest extends org.scalatest.FunSuite with Matchers {
   val settings = new Settings
@@ -13,6 +16,7 @@ class SvnConfigManagerTest extends org.scalatest.FunSuite with Matchers {
   private val largeFileManager = new LargeFileManager(settings)
   val configManager: SvnConfigManager = new SvnConfigManager(settings, largeFileManager)
 
+  implicit val ec = ExecutionContext.Implicits.global
   test("create and get") {
     svnAdmin.initSvnRepo(null)
 
@@ -85,6 +89,42 @@ class SvnConfigManagerTest extends org.scalatest.FunSuite with Matchers {
 
     configManager.get(file).await.get.asInstanceOf[ConfigBytes].toString shouldBe ConfigString("assemblyHCDCount = 5").str
     configManager.get(file, Some(configId)).await.get.asInstanceOf[ConfigBytes].toString shouldBe ConfigString("assemblyHCDCount = 3").str
+  }
+
+  test("get version by date") {
+    svnAdmin.initSvnRepo(null)
+
+    val file = Paths.get("/a.conf").toFile
+    configManager.create(
+      file, ConfigString("axisName = tromboneAxis"), oversize = false, "hello world"
+    ).await
+    configManager.get(file).await.get.asInstanceOf[ConfigBytes].toString shouldBe ConfigString("axisName = tromboneAxis").str
+
+
+    configManager.update(file, ConfigString("assemblyHCDCount = 3"), "Updated config to assembly").await
+    val date = new Date()
+    configManager.update(file, ConfigString("assemblyHCDCount = 5"), "Updated config to assembly").await
+
+    configManager.get(file).await.get.asInstanceOf[ConfigBytes].toString shouldBe ConfigString("assemblyHCDCount = 5").str
+    configManager.get(file, date).await.get.asInstanceOf[ConfigBytes].toString shouldBe ConfigString("assemblyHCDCount = 3").str
+  }
+
+  test("get initial version if the date is before the creation date") {
+    svnAdmin.initSvnRepo(null)
+
+    val date = new Date(0L)
+    val file = Paths.get("/a.conf").toFile
+    configManager.create(
+      file, ConfigString("axisName = tromboneAxis"), oversize = false, "hello world"
+    ).await
+    configManager.get(file).await.get.asInstanceOf[ConfigBytes].toString shouldBe ConfigString("axisName = tromboneAxis").str
+
+
+    configManager.update(file, ConfigString("assemblyHCDCount = 3"), "Updated config to assembly").await
+    configManager.update(file, ConfigString("assemblyHCDCount = 5"), "Updated config to assembly").await
+
+    configManager.get(file).await.get.asInstanceOf[ConfigBytes].toString shouldBe ConfigString("assemblyHCDCount = 5").str
+    configManager.get(file, date).await.get.asInstanceOf[ConfigBytes].toString shouldBe ConfigString("axisName = tromboneAxis").str
   }
 
   test("get history of all versions of a file") {
