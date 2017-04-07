e3cd7900aa6d5826e7f554761c39c46ccd381e2f
Mushtaq Ahmed <mushtaq.a@gmail.com>
2017-04-07 16:17:14 +0530

    #080: Mushtaq | Nishant :  Cleanup routes.

--- csw-config/src/main/scala/csw/services/config/server/ConfigServiceApp.scala
+++ csw-config/src/main/scala/csw/services/config/server/ConfigServiceApp.scala
@@ -1,14 +1,18 @@
 package csw.services.config.server
 
+import java.io.File
 import java.nio.file.Paths
 
+import akka.Done
 import akka.actor.ActorSystem
 import akka.http.scaladsl.model.ContentTypes
 import akka.http.scaladsl.model.HttpEntity.Chunked
-import akka.http.scaladsl.server.{HttpApp, Route}
+import akka.http.scaladsl.server.{Directive1, HttpApp, Route, StandardRoute}
 import csw.services.config.internal.JsonSupport
-import csw.services.config.models.{ConfigId, ConfigSource}
+import csw.services.config.models.{ConfigData, ConfigId, ConfigSource}
 import csw.services.config.scaladsl.ConfigManager
+
+import scala.concurrent.Future
 
 class ConfigServiceApp(configManager: ConfigManager) extends HttpApp with JsonSupport {
 
@@ -16,26 +20,33 @@ class ConfigServiceApp(configManager: ConfigManager) extends HttpApp with JsonSu
 
   import actorSystem.dispatcher
 
+  val pathParam: Directive1[File] = parameter('path).map(filePath ⇒ Paths.get(filePath).toFile)
+  val idParam: Directive1[Option[ConfigId]] = parameter('id.?).map(_.map(new ConfigId(_)))
+  val maxResultsParam: Directive1[Int] = parameter('maxResults.as[Int] ? 100)
+  val commentParam: Directive1[String] = parameter('comment ? "")
+  val oversizeParam: Directive1[Boolean] = parameter('oversize.as[Boolean] ? false)
+  val fileDataParam: Directive1[ConfigSource] = fileUpload("conf").map { case (fileInfo, source) ⇒ ConfigSource(source) }
+
+  def streamData(configDataF: Future[Option[ConfigData]]): StandardRoute = rejectEmptyResponse & complete {
+    configDataF.map { configDataO ⇒
+      configDataO.map { configData =>
+        Chunked.fromData(ContentTypes.`application/octet-stream`, configData.source)
+      }
+    }
+  }
+
   override protected def route: Route = {
     get {
       path("get") {
-        parameters('path, 'id.?) { (filePath, id) ⇒
-          rejectEmptyResponse {
-            complete {
-              configManager.get(Paths.get(filePath).toFile, id.map(ConfigId.apply)).map { maybeConfigData ⇒
-                maybeConfigData.map(configData ⇒ Chunked.fromData(ContentTypes.`application/octet-stream`, configData.source))
-              }
-            }
-          }
+        (pathParam & idParam) { (filePath, maybeConfigId) ⇒
+          streamData(configManager.get(filePath, maybeConfigId))
         }
       } ~
         path("exists") {
-          parameter('path) { filePath ⇒
-            rejectEmptyResponse {
-              complete {
-                configManager.exists(Paths.get(filePath).toFile).map { found ⇒
-                  if (found) Some("ok") else None
-                }
+          pathParam { filePath ⇒
+            rejectEmptyResponse & complete {
+              configManager.exists(filePath).map { found ⇒
+                if (found) Some(Done) else None
               }
             }
           }
@@ -44,45 +55,37 @@ class ConfigServiceApp(configManager: ConfigManager) extends HttpApp with JsonSu
           complete(configManager.list())
         } ~
         path("history") {
-          parameters('path, 'maxResults.as[Int] ? 100) { (filePath, maxCount) ⇒
-            complete(configManager.history(Paths.get(filePath).toFile, maxCount))
+          (pathParam & maxResultsParam) { (filePath, maxCount) ⇒
+            complete(configManager.history(filePath, maxCount))
           }
         } ~
         path("getDefault") {
-          parameters('path) { filePath ⇒
-            rejectEmptyResponse {
-              complete {
-                configManager.getDefault(Paths.get(filePath).toFile).map { maybeConfigData ⇒
-                  maybeConfigData.map(configData ⇒ Chunked.fromData(ContentTypes.`application/octet-stream`, configData.source))
-                }
-              }
-            }
+          pathParam { filePath ⇒
+            streamData(configManager.getDefault(filePath))
           }
         }
     } ~
       post {
         path("setDefault") {
-          parameters('path, 'id.?) { (filePath, id) ⇒
-            complete(configManager.setDefault(Paths.get(filePath).toFile, id.map(ConfigId.apply)).map(_ ⇒ "ok"))
+          (pathParam & idParam) { (filePath, maybeConfigId) ⇒
+            complete(configManager.setDefault(filePath, maybeConfigId).map(_ ⇒ Done))
           }
         } ~
           path("resetDefault") {
-            parameters('path) { filePath ⇒
-              complete(configManager.resetDefault(Paths.get(filePath).toFile).map(_ ⇒ "ok"))
+            pathParam { filePath ⇒
+              complete(configManager.resetDefault(filePath).map(_ ⇒ Done))
             }
           } ~
-          parameters('path, 'comment ? "") { (filePath, comment) ⇒
-            fileUpload("conf") { case (fileInfo, source) ⇒
-              path("create") {
-                parameter('oversize.as[Boolean] ? false) { oversize ⇒
-                  val eventualId = configManager.create(Paths.get(filePath).toFile, ConfigSource(source), oversize, comment)
-                  complete(eventualId.map(_.id))
-                }
-              } ~
-                path("update") {
-                  val eventualId = configManager.update(Paths.get(filePath).toFile, ConfigSource(source), comment)
-                  complete(eventualId.map(_.id))
-                }
+          path("create") {
+            (pathParam & fileDataParam & oversizeParam & commentParam) { (filePath, configSource, oversize, comment) ⇒
+              val eventualId = configManager.create(filePath, configSource, oversize, comment)
+              complete(eventualId.map(_.id))
+            }
+          } ~
+          path("update") {
+            (pathParam & fileDataParam & commentParam) { (filePath, configSource, comment) ⇒
+              val eventualId = configManager.update(filePath, configSource, comment)
+              complete(eventualId.map(_.id))
             }
           }
       }
