a4d658d1cfd097f8aa9c2a2fc9353fcc82288471
Pritam Kadam <phkadam2008@gmail.com>
2017-04-11 22:36:16 +0530

    #100: Pritam :  Added unit tests for ConfigData model.

---
+++ csw-config/src/test/scala/csw/services/config/api/models/ConfigDataTest.scala
@@ -0,0 +1,60 @@
+package csw.services.config.api.models
+
+import java.io.InputStream
+
+import akka.NotUsed
+import akka.actor.ActorSystem
+import akka.stream.scaladsl.Source
+import akka.stream.testkit.TestSubscriber.Probe
+import akka.stream.testkit.scaladsl.TestSink
+import akka.util.ByteString
+import csw.services.config.api.commons.ActorRuntime
+import csw.services.config.api.commons.TestFutureExtension.RichFuture
+import org.scalatest.{FunSuite, Matchers}
+
+class ConfigDataTest extends FunSuite with Matchers {
+
+  implicit val actorSystem = ActorSystem("config-service")
+  private val actorRuntime = new ActorRuntime(actorSystem)
+  import actorRuntime._
+
+  val expectedStringConfigData =
+    """
+      |This is a string for testing.
+      |Which should be converted to ByteString first.
+      |Then create a source from ByteString
+      |""".stripMargin
+
+  private val sourceOfByteString: Source[ByteString, NotUsed] = Source.single(ByteString(expectedStringConfigData.getBytes))
+
+  test("should able to retrieve string from Config Data source") {
+    val actualStringConfigData = new ConfigData(sourceOfByteString).toStringF.await
+    actualStringConfigData shouldEqual expectedStringConfigData
+  }
+
+  test("should able to generate InputStream from Config Data source") {
+    val configDataInputStream: InputStream = new ConfigData(sourceOfByteString).toInputStream
+    scala.io.Source.fromInputStream(configDataInputStream).mkString shouldEqual expectedStringConfigData
+  }
+
+  test("should create source of ByteString from string") {
+    val fromString: ConfigData = ConfigData.fromString(expectedStringConfigData)
+    val probe: Probe[ByteString] = fromString.source.runWith(TestSink.probe)
+
+    probe.requestNext().utf8String shouldEqual expectedStringConfigData
+  }
+
+  test("should create source of ByteString from bytes data") {
+    val fromBytes: ConfigData = ConfigData.fromBytes(expectedStringConfigData.getBytes)
+    val probe: Probe[ByteString] = fromBytes.source.runWith(TestSink.probe)
+
+    probe.requestNext().utf8String.getBytes shouldEqual expectedStringConfigData.getBytes
+  }
+
+  test("should create Config data of source of ByteString from ByteString source") {
+    val fromSource: ConfigData = ConfigData.fromSource(sourceOfByteString)
+
+    fromSource.source shouldEqual sourceOfByteString
+  }
+
+}
