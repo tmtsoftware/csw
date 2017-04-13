33370d15bd043e02d90ab9878795636c1db630b9
Pritam Kadam <phkadam2008@gmail.com>
2017-04-13 11:58:07 +0530

    #014 : Pritam | Mahendra : Added more test for different types of Connection as per the feedback from TMT(jason)

--- csw-location/src/test/scala/csw/services/location/models/ConnectionTest.scala
+++ csw-location/src/test/scala/csw/services/location/models/ConnectionTest.scala
@@ -1,13 +1,37 @@
 package csw.services.location.models
 
-import csw.services.location.models.Connection.AkkaConnection
+import csw.services.location.models.Connection.{AkkaConnection, HttpConnection, TcpConnection}
 import org.scalatest.{FunSuite, Matchers}
 
 class ConnectionTest extends FunSuite with Matchers {
 
-  test("should able to form a connection name with componentId name, componentId type and connection type separated by '-'") {
+  test("should able to form a string representation for akka connection for trombone HCD") {
     val expectedAkkaConnectionName = "tromboneHcd-hcd-akka"
     val akkaConnection = AkkaConnection(ComponentId("tromboneHcd", ComponentType.HCD))
+    akkaConnection.name shouldBe expectedAkkaConnectionName
+  }
+
+  test("should able to form a string representation for tcp connection for redis") {
+    val expectedTcpConnectionName = "redis-service-tcp"
+    val tcpConnection = TcpConnection(ComponentId("redis", ComponentType.Service))
+    tcpConnection.name shouldBe expectedTcpConnectionName
+  }
+
+  test("should able to form a string representation for http connection for config service") {
+    val expectedHttpConnectionName = "config-service-http"
+    val httpConnection = HttpConnection(ComponentId("config", ComponentType.Service))
+    httpConnection.name shouldBe expectedHttpConnectionName
+  }
+
+  test("should able to form a string representation for akka connection for trombone container") {
+    val expectedAkkaConnectionName = "tromboneContainer-container-akka"
+    val akkaConnection = AkkaConnection(ComponentId("tromboneContainer", ComponentType.Container))
+    akkaConnection.name shouldBe expectedAkkaConnectionName
+  }
+
+  test("should able to form a string representation for akka connection for trombone assembly") {
+    val expectedAkkaConnectionName = "tromboneAssembly-assembly-akka"
+    val akkaConnection = AkkaConnection(ComponentId("tromboneAssembly", ComponentType.Assembly))
     akkaConnection.name shouldBe expectedAkkaConnectionName
   }
 
