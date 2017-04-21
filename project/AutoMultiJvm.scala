c3d572a047ec63d46ee7077c3e9765ece0022de1
Pritam Kadam <phkadam2008@gmail.com>
2017-04-21 22:58:45 +0530

    #000: Pritam: Fix failing build. Add sbt assembly merge setting to concat application.conf file.

--- project/AutoMultiJvm.scala
+++ project/AutoMultiJvm.scala
@@ -4,13 +4,21 @@ import sbt._
 object AutoMultiJvm extends AutoPlugin {
   import com.typesafe.sbt.SbtMultiJvm
   import SbtMultiJvm.MultiJvmKeys._
+  import sbtassembly.Plugin.AssemblyKeys._
+  import sbtassembly.Plugin.MergeStrategy
 
   override def projectSettings: Seq[Setting[_]] = SbtMultiJvm.multiJvmSettings ++ Seq(
     test := {
       (test in Test).value
       (test in MultiJvm).value
     },
-    multiNodeHosts in MultiJvm := multiNodeHostNames
+    multiNodeHosts in MultiJvm := multiNodeHostNames,
+    mergeStrategy in assembly in MultiJvm := {
+      case "application.conf" => MergeStrategy.concat
+      case x =>
+        val oldStrategy = (mergeStrategy in assembly in MultiJvm).value
+        oldStrategy(x)
+    }
   )
 
   def multiNodeHostNames: Seq[String] = sys.env.get("multiNodeHosts") match {
