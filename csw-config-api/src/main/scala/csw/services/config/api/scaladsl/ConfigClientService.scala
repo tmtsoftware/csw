5e766c00fd28b0e5f8571f39f91d71bc2513cd06
Mahendra Kulkarni <mahendra.pk@gmail.com>
2017-05-04 13:29:13 +0530

    #138: Nishant | Mahendra | Mushtaq - Extract client interface into a separate trait.

---
+++ csw-config-api/src/main/scala/csw/services/config/api/scaladsl/ConfigClientService.scala
@@ -0,0 +1,33 @@
+package csw.services.config.api.scaladsl
+
+import java.nio.file.Path
+import java.time.Instant
+
+import csw.services.config.api.commons.FileType
+import csw.services.config.api.models._
+
+import scala.concurrent.Future
+
+/**
+ * Defines an interface for storing and retrieving configuration information
+ */
+trait ConfigClientService {
+
+  /**
+   * Returns true if the given path exists and is being managed
+   *
+   * @param path the file path relative to the repository root
+   * @param id revision of the file
+   * @return true the file exists
+   */
+  def exists(path: Path, id: Option[ConfigId] = None): Future[Boolean]
+
+  /**
+   * Gets and returns the active version of the file stored under the given path.
+   * If no active was set, this returns the version with which the file was created i.e. 1.
+   *
+   * @param path the file path relative to the repository root
+   * @return     a future object that can be used to access the file's data, if found
+   */
+  def getActive(path: Path): Future[Option[ConfigData]]
+}
