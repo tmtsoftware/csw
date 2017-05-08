322668bba77d760108bdd436ea7a160ae464f039
weiss <weiss.personal@gmail.com>
2017-05-08 13:11:00 -0700

    cleaned up and finshed, including expected output and dummy registrations.

--- examples/src/main/scala/csw/services/examples/LocationServiceExampleClientApp.scala
+++ examples/src/main/scala/csw/services/examples/LocationServiceExampleClientApp.scala
@@ -3,9 +3,9 @@ package csw.services.examples
 import akka.actor.{Actor, ActorLogging, Props}
 import akka.stream.{ActorMaterializer, Materializer}
 import akka.stream.scaladsl.Sink
-import csw.services.location.models.ComponentType
-import csw.services.location.models.ConnectionType
-import csw.services.location.models.{AkkaLocation, LocationRemoved, LocationUpdated}
+import scala.concurrent.duration._
+import csw.services.location.models.Connection.{AkkaConnection, HttpConnection}
+import csw.services.location.models._
 import csw.services.location.scaladsl.{ActorSystemFactory, LocationService, LocationServiceFactory}
 
 import scala.concurrent.Await
@@ -21,8 +21,24 @@ object LocationServiceExampleClientApp extends App {
   implicit val system = ActorSystemFactory.remote()
   implicit val mat = ActorMaterializer()
 
+  // create an actor ref to use for a dummy HCD registration
+  private val dummyActorRef = system.actorOf(Props(new Actor {
+    override def receive: Receive = {
+      case "print" => println("hello world")
+    }
+  }), name="my-actor-1")
   system.actorOf(LocationServiceExampleClient.props(locationService))
 
+  private val timeout = 5.seconds
+
+  // add some dummy registrations for illustrative purposes
+  private val httpConnection   = HttpConnection(ComponentId("configuration", ComponentType.Service))
+  private val httpRegistration = HttpRegistration(httpConnection, 8080, "path123")
+  private val httpRegResult = Await.result(locationService.register(httpRegistration), timeout)
+
+  private val hcdConnection   = AkkaConnection(ComponentId("hcd1", ComponentType.HCD))
+  private val hcdRegistration = AkkaRegistration(hcdConnection, dummyActorRef)
+  private val hcdRegResult = Await.result(locationService.register(hcdRegistration), timeout)
 }
 
 object LocationServiceExampleClient {
@@ -40,62 +56,141 @@ object LocationServiceExampleClient {
 class LocationServiceExampleClient(locationService: LocationService)(implicit mat: Materializer) extends Actor with ActorLogging {
 
   import LocationServiceExampleClient._
-  import scala.concurrent.duration._
 
   private val timeout = 5.seconds
   private val waitForResolveLimit = 30.seconds
+
+
+
+
+  // EXAMPLE DEMO START
+
+  // This demo shows some basics of using the location service.  Before running this example,
+  // optionally use the location service csw-location-agent to start a redis server:
+  //
+  //   $ csw-location-agent --name redis --command "redis-server --port %port"
+  //
+  // Not only does this serve as an example for starting applications that are not written in CSW,
+  // but it also will help demonstrate location filtering later in this demo.
 
   // find connection to LocationServiceExampleComponent in location service
   // [do this before starting LocationServiceExampleComponent.  this should return Future[None]]
   private val exampleConnection = LocationServiceExampleComponent.connection
 
+  println(s"Attempting to find connection $exampleConnection ...")
   private val findResult = Await.result(locationService.find(exampleConnection), timeout)
-  println(s"Find result: $findResult")     // Should be "Find Result: None"
+  println(s"Find result: $findResult")
+
+  // Output should be:
+  //    Attempting to find connection AkkaConnection(ComponentId(LocationServiceExampleComponent,Assembly)) ...
+  //    Find result: None
 
   // resolve connection to LocationServiceExampleComponent
   // [start LocationServiceExampleComponent after this command but before timeout]
-  private val resolveResult = Await.result(locationService.resolve(exampleConnection, waitForResolveLimit), waitForResolveLimit+timeout)
-  println(s"Resolve result: $resolveResult")
+  println(s"Attempting to resolve $exampleConnection with a wait of $waitForResolveLimit ...")
+  private val resolveResult = Await.result(locationService.resolve(exampleConnection, waitForResolveLimit), waitForResolveLimit + timeout)
+  if (resolveResult.isEmpty) {
+    println(s"Timeout waiting for location $exampleConnection to resolve.")
+  } else {
+    println(s"Resolve result: ${locationInfoToString(resolveResult.get)}")
+  }
+
+  // Output should be:
+  //    Attempting to resolve AkkaConnection(ComponentId(LocationServiceExampleComponent,Assembly)) with a wait of 30 seconds ...
+
+  // If you then start the LocationServiceExampleComponentApp,
+  // Output should be:
+  //    Resolve result: LocationServiceExampleComponent-assembly-akka, component type=Assembly, connection type=AkkaType
+
+  // If not,
+  // Output should be:
+  //    Timeout waiting for location AkkaConnection(ComponentId(LocationServiceExampleComponent,Assembly)) to resolve.
 
   // list connections in location service
   private val connectionList = Await.result(locationService.list, timeout)
+  println("All Registered Connections:")
+  connectionList.foreach(c => println(s"--- ${locationInfoToString(c)}"))
 
-  connectionList.foreach(c => println(s"Location $c"))
+  // Output should be:
+  //    All Registered Connections:
+  //    --- hcd1-hcd-akka, component type=HCD, connection type=AkkaType
+  //    --- redis-service-tcp, component type=Service, connection type=TcpType
+  //    --- configuration-service-http, component type=Service, connection type=HttpType
+  //    --- LocationServiceExampleComponent-assembly-akka, component type=Assembly, connection type=AkkaType
+
 
   // filter connections based on connection type
-
   private val componentList = Await.result(locationService.list(ComponentType.Assembly), timeout)
+  println("Registered Assemblies:")
+  componentList.foreach(c => println(s"--- ${locationInfoToString(c)}"))
 
-  componentList.foreach(c => println(s"Assembly Location $c"))
-
-  // output should be:
+  // Output should be:
+  //    Registered Assemblies:
+  //    --- LocationServiceExampleComponent-assembly-akka, component type=Assembly, connection type=AkkaType
 
   // filter connections based on component type
-
   private val akkaList = Await.result(locationService.list(ConnectionType.AkkaType), timeout)
+  println("Registered Akka connections:")
+  akkaList.foreach(c => println(s"--- ${locationInfoToString(c)}"))
 
-  akkaList.foreach(c => println(s"Akka Location $c"))
+  // Output should be:
+  //    Registered Akka connections:
+  //    --- hcd1-hcd-akka, component type=HCD, connection type=AkkaType
+  //    --- LocationServiceExampleComponent-assembly-akka, component type=Assembly, connection type=AkkaType
+
+  if (resolveResult.isDefined) {
+    // the following two methods are examples of two ways to track a connection.
+    // both are implemented but only one is really needed.
+
+
+    // track connection to LocationServiceExampleComponent
+    // Calls track method for example connection and forwards location messages to this actor
+    println(s"Starting to track $exampleConnection")
+    locationService.track(exampleConnection).to(Sink.actorRef(self, AllDone)).run()
+
+    // subscribe to LocationServiceExampleComponent events
+    println(s"Starting a subscription to $exampleConnection")
+    locationService.subscribe(exampleConnection, trackingEvent => {
+      // the following println is to distinguish subscription events from tracking events
+      println("subscription event")
+      self ! trackingEvent
+    })
+
+    // [tracking shows component unregister and re-register]
 
 
 
-  // track connection to LocationServiceExampleComponent
-  // Calls track method for example connection and forwards location messages to this actor
-  locationService.track(exampleConnection).to(Sink.actorRef(self, AllDone)).run()
+    // Output should be:
+    //    Starting to track AkkaConnection(ComponentId(LocationServiceExampleComponent,Assembly))
+    //    Starting a subscription to AkkaConnection(ComponentId(LocationServiceExampleComponent,Assembly))
+    //    subscription event
+    //    Location updated LocationServiceExampleComponent-assembly-akka, component type=Assembly, connection type=AkkaType
+    //    Location updated LocationServiceExampleComponent-assembly-akka, component type=Assembly, connection type=AkkaType
+
+    // If you now stop the LocationServiceExampleComponentApp,
+    // Output should be:
+    //    subscription event
+    //    Location removed AkkaConnection(ComponentId(LocationServiceExampleComponent,Assembly))
+    //    Location removed AkkaConnection(ComponentId(LocationServiceExampleComponent,Assembly))
+
+    // If you start the LocationServiceExampleComponentApp again,
+    // Output should be:
+    //    Location updated LocationServiceExampleComponent-assembly-akka, component type=Assembly, connection type=AkkaType
+    //    subscription event
+    //    Location updated LocationServiceExampleComponent-assembly-akka, component type=Assembly, connection type=AkkaType
 
 
-  // subscribe to LocationServiceExampleComponent events
-  locationService.subscribe(exampleConnection, trackingEvent => {
-    log.info("subscription event")
-    self ! trackingEvent
-  })
-
-  // [tracking shows component unregister and re-register]
+  }
+  def locationInfoToString(loc: Location): String = {
+    val connection = loc.connection
+    s"${connection.name}, component type=${connection.componentId.componentType}, connection type=${connection.connectionType}"
+  }
 
   override def receive: Receive = {
 
     // Receive a location from the location service and if it is an akka location, send it a message
     case LocationUpdated(loc) =>
-      log.info(s"Location updated $loc")
+      println(s"Location updated ${locationInfoToString(loc)}")
       loc match {
         case AkkaLocation(_, _, actorRef) =>
           actorRef ! LocationServiceExampleComponent.ClientMessage
@@ -104,7 +199,7 @@ class LocationServiceExampleClient(locationService: LocationService)(implicit ma
 
     // A location was removed
     case LocationRemoved(conn) =>
-      log.info(s"Location removed $conn")
+      println(s"Location removed $conn")
 
     case x =>
       log.error(s"Received unexpected message $x")
