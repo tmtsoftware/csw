d13be373e923db38958c85f2ff0be46b3033b432
Kim Gillies <kgjunk@mac.com>
2017-05-16 15:16:44 -0400

    Added additional asserts to test to check that active is set on create.

--- csw-config-server/src/test/scala/csw/services/config/server/svn/SvnConfigServiceTest.scala
+++ csw-config-server/src/test/scala/csw/services/config/server/svn/SvnConfigServiceTest.scala
@@ -11,6 +11,8 @@ class SvnConfigServiceTest extends ConfigServiceTest {
   val serverWiring                          = new ServerWiring()
   override val configService: ConfigService = serverWiring.configService
 
+  import serverWiring.actorRuntime._
+
   override protected def afterAll(): Unit = {
     serverWiring.actorSystem.terminate().await
     super.afterAll()
@@ -22,10 +24,15 @@ class SvnConfigServiceTest extends ConfigServiceTest {
     val filePath       = Paths.get("/tmt/tcp/redis/text/redis.conf")
     val activeFilePath = Paths.get(filePath.toString + serverWiring.settings.`active-config-suffix`)
 
-    configService.create(filePath, ConfigData.fromString(configValue1), annex = false, "initial commit").await
+    val configId1 = configService.create(filePath, ConfigData.fromString(configValue1), annex = false, "initial commit").await
 
     configService.exists(filePath).await shouldBe true
     configService.exists(activeFilePath).await shouldBe true
+
+    // Should have active version set by create
+    configService.getActiveVersion(filePath).await.get shouldBe configId1
+    // Active content should be initial version
+    configService.getActive(filePath).await.get.toStringF.await shouldBe configValue1
 
     configService.resetActiveVersion(filePath, "resetting active version to latest").await
 
