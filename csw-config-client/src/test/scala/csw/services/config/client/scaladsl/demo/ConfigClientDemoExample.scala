6eb78588d46b46fd5626997cd860dce316d32741
Mahendra Kulkarni <mahendra.pk@gmail.com>
2017-05-17 17:02:06 +0530

    #089 - Mahendra - csw-config-client documentation for create-update-delete, getLatest, getActive.

--- csw-config-client/src/test/scala/csw/services/config/client/scaladsl/demo/ConfigClientDemoExample.scala
+++ csw-config-client/src/test/scala/csw/services/config/client/scaladsl/demo/ConfigClientDemoExample.scala
@@ -1,18 +1,21 @@
 package csw.services.config.client.scaladsl.demo
 
+import java.io.InputStream
 import java.nio.file.Paths
 
-import csw.services.config.api.commons.TestFutureExtension.RichFuture
-import csw.services.config.api.models.ConfigData
+import csw.services.config.api.models.{ConfigData, ConfigId}
 import csw.services.config.api.scaladsl.{ConfigClientService, ConfigService}
 import csw.services.config.client.scaladsl.ConfigClientFactory
 import csw.services.config.server.ServerWiring
 import csw.services.config.server.commons.TestFileUtils
+import csw.services.config.server.commons.TestFutureExtension.RichFuture
 import csw.services.location.commons.ClusterAwareSettings
 import csw.services.location.scaladsl.LocationServiceFactory
-import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach, FunSuite, Matchers}
+import org.scalatest._
 
-import scala.concurrent.Future
+import scala.async.Async._
+import scala.concurrent.duration.DurationLong
+import scala.concurrent.{Await, Future}
 
 class ConfigClientDemoExample extends FunSuite with Matchers with BeforeAndAfterEach with BeforeAndAfterAll {
 
@@ -27,9 +30,9 @@ class ConfigClientDemoExample extends FunSuite with Matchers with BeforeAndAfter
 
   //#create-api
   //config client API
-  val configClientService: ConfigClientService = ConfigClientFactory.clientApi(actorSystem, locationService)
+  val clientApi: ConfigClientService = ConfigClientFactory.clientApi(actorSystem, locationService)
   //config admin API
-  val configAdminService: ConfigService = ConfigClientFactory.adminApi(actorSystem, locationService)
+  val adminApi: ConfigService = ConfigClientFactory.adminApi(actorSystem, locationService)
   //#create-api
 
   override protected def beforeEach(): Unit =
@@ -46,41 +49,155 @@ class ConfigClientDemoExample extends FunSuite with Matchers with BeforeAndAfter
     locationService.shutdown().await
   }
 
-  val configValue1: String =
+  //#declare_string_config
+  val defaultStrConf: String =
     """
       |axisName1 = tromboneAxis1
       |axisName2 = tromboneAxis2
       |axisName3 = tromboneAxis3
       |""".stripMargin
+  //#declare_string_config
 
   test("exists") {
     //#exists-snip1
     //construct the path
-    val file = Paths.get("/tmt/trmobone/assembly/hcd.conf")
+    val filePath = Paths.get("/tmt/trmobone/assembly/hcd.conf")
     //#exists-snip1
 
     // create file using admin API first
-    configAdminService.create(file, ConfigData.fromString(configValue1), annex = false, "First commit").await
+    adminApi.create(filePath, ConfigData.fromString(defaultStrConf), annex = false, "First commit").await
 
+    val doneF =
+      //#exists-snip2
+      async {
+        //check if file exists with config service
+        val exists: Boolean = await(clientApi.exists(filePath))
+        exists shouldBe true
+      }
     //#exists-snip2
-    //check if file exists with config service
-    val exists: Future[Boolean] = configClientService.exists(file)
-    exists.await shouldBe true
-    //#exists-snip2
+    Await.result(doneF, 5.seconds)
   }
 
   test("getActive") {
-    //#getActive-snip1
-    // construct the path
-    val file = Paths.get("/tmt/trmobone/assembly/hcd.conf")
-    //#getActive-snip1
+    val doneF =
+      //#getActive-snip1
+      async {
+        // construct the path
+        val filePath = Paths.get("/tmt/trmobone/assembly/hcd.conf")
+        //#getActive-snip1
 
-    configAdminService.create(file, ConfigData.fromString(configValue1), annex = false, "First commit").await
+        await(adminApi.create(filePath, ConfigData.fromString(defaultStrConf), annex = false, "First commit"))
 
+        //#getActive-snip2
+        val activeFile: Option[ConfigData] = await(clientApi.getActive(filePath))
+        await(activeFile.get.toStringF) shouldBe defaultStrConf
+      }
     //#getActive-snip2
-    // check that getActive call before any setActive call should return the file with id with which it was created
-    val activeFile: Future[Option[ConfigData]] = configClientService.getActive(file)
-    activeFile.await.get.toStringF.await shouldBe configValue1
-    //#getActive-snip2
+    Await.result(doneF, 5.seconds)
+  }
+
+  test("create-update-delete") {
+    val futC =
+      //#create
+      async {
+        //construct ConfigData from String containing ASCII text
+        val configString: String =
+          """
+        // Name: ComponentType ConnectionType
+        {
+          name: lgsTromboneHCD
+          type: Hcd
+          connectionType: [akka]
+        }
+        """.stripMargin
+        val config1: ConfigData = ConfigData.fromString(configString)
+
+        //construct ConfigData from a local file containing binary data
+        val srcFilePath         = getClass.getClassLoader.getResource("smallBinary.bin").toURI
+        val config2: ConfigData = ConfigData.fromPath(Paths.get(srcFilePath))
+
+        //construct ConfigData from Array[Byte] by reading a local file
+        val stream: InputStream = getClass.getClassLoader.getResourceAsStream("smallBinary.bin")
+
+        def byteArray: Array[Byte] = Stream.continually(stream.read).takeWhile(_ != -1).map(_.toByte).toArray
+
+        val config3 = ConfigData.fromBytes(byteArray)
+
+        //store the config, at a specified path as normal text file
+        val id1: ConfigId =
+          await(adminApi.create(Paths.get("/hcd/trombone/overnight.conf"), config1, false, "review done"))
+
+        //store the config, at a specified path as a binary file in annex store
+        val id2: ConfigId =
+          await(adminApi.create(Paths.get("/hcd/trombone/firmware.bin"), config2, true, "smoke test done"))
+
+        //store the config, at a specified path as a binary file in annex store
+        val id3: ConfigId =
+          await(adminApi.create(Paths.get("/hcd/trombone/debug.bin"), config3, true, "new file from vendor"))
+
+        //CAUTION: for demo example setup these IDs are returned. Don't assume them in production setup.
+        id1 shouldEqual ConfigId(1)
+        id2 shouldEqual ConfigId(3)
+        id3 shouldEqual ConfigId(5)
+      }
+    Await.result(futC, 5.seconds)
+    //#create
+
+    //#update
+    val futU = async {
+      val destPath = Paths.get("/hcd/trombone/debug.bin")
+      val newId = adminApi
+        .update(destPath, ConfigData.fromString(defaultStrConf), comment = "debug statements")
+        .await
+
+      //validate the returned id
+      newId shouldEqual ConfigId(7)
+    }
+    Await.result(futU, 5.seconds)
+    //#update
+
+    //#delete
+    val futD = async {
+      val unwantedFilePath = Paths.get("/hcd/trombone/debug.bin")
+      adminApi.delete(unwantedFilePath, "no longer needed").await
+    }
+    Await.result(futD, 5.seconds)
+    //#delete
+  }
+
+  test("getById") {
+    //#getById
+    val doneF = async {
+      // create a file using API first
+      val filePath = Paths.get("/tmt/trmobone/assembly/hcd.conf")
+      val id: ConfigId =
+        await(adminApi.create(filePath, ConfigData.fromString(defaultStrConf), annex = false, "First commit"))
+
+      //validate
+      val actualData = await(adminApi.getById(filePath, id)).get
+      await(actualData.toStringF) shouldBe defaultStrConf
+    }
+    Await.result(doneF, 5.seconds)
+    //#getById
+  }
+
+  test("getLatest") {
+    //#getLatest
+    val assertionF: Future[Assertion] = async {
+      //create a file
+      val filePath = Paths.get("/test.conf")
+      await(adminApi.create(filePath, ConfigData.fromString(defaultStrConf), annex = false,
+          "commit initial configuration"))
+
+      //override the contents
+      val newContent = "I changed the contents!!!"
+      await(adminApi.update(filePath, ConfigData.fromString(newContent), "changed!!"))
+
+      //get the latest file
+      val newConfigData = await(adminApi.getLatest(filePath)).get
+      await(newConfigData.toStringF) shouldBe newContent
+    }
+    Await.result(assertionF, 5.seconds)
+    //#getLatest
   }
 }
