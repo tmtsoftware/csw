ca572a0654e46057a2cc359f441668dff37efcbd
Saloni Vithalani <skvithalani@yahoo.com>
2017-05-20 12:40:39 +0530

    #114: Saloni: Removed File logging from tests

--- csw-location/src/test/scala/csw/services/location/CswTestSuite.scala
+++ csw-location/src/test/scala/csw/services/location/CswTestSuite.scala
@@ -1,11 +1,11 @@
 package csw.services.location
 
 import csw.services.location.commons.TestFutureExtension.RichFuture
-import csw.services.logging.LoggingSystem
+import csw.services.logging.{LoggingSystem, StdOutAppender}
 import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach, FunSuite, Matchers}
 
 abstract class CswTestSuite extends FunSuite with Matchers with BeforeAndAfterAll with BeforeAndAfterEach {
-  private val loggingSystem = LoggingSystem()
+  private val loggingSystem = LoggingSystem(appenderBuilders = Seq(StdOutAppender))
 
   protected def afterAllTests(): Unit
 
--- csw-logging/src/main/scala/csw/services/logging/LoggingSystemFactory.scala
+++ csw-logging/src/main/scala/csw/services/logging/LoggingSystemFactory.scala
@@ -4,6 +4,5 @@ import java.net.InetAddress
 
 object LoggingSystemFactory {
   def make(): LoggingSystem =
-    new LoggingSystem("serviceName1", "serviceVersion1", InetAddress.getLocalHost.getHostName,
-      Seq(StdOutAppender, FileAppender))
+    LoggingSystem("serviceName1", "serviceVersion1", InetAddress.getLocalHost.getHostName, Seq(StdOutAppender))
 }
