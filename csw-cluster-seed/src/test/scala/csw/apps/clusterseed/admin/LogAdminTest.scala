43f8e487aafc481b1fe4f729ca8205b7e5a67a76
Pritam Kadam <phkadam2008@gmail.com>
2017-06-01 21:55:15 +0530

    #127: Pritam : Add test for fetching log meta data from log admin http server.

--- csw-cluster-seed/src/test/scala/csw/apps/clusterseed/admin/LogAdminTest.scala
+++ csw-cluster-seed/src/test/scala/csw/apps/clusterseed/admin/LogAdminTest.scala
@@ -3,14 +3,18 @@ package csw.apps.clusterseed.admin
 import akka.actor.Props
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.{HttpMethods, HttpRequest, StatusCodes, Uri}
+import akka.http.scaladsl.unmarshalling.Unmarshal
+import com.typesafe.config.ConfigFactory
 import csw.apps.clusterseed.admin.TromboneHcd._
+import csw.apps.clusterseed.admin.http.HttpSupport
+import csw.apps.clusterseed.utils.AdminLogTestSuite
 import csw.services.location.commons.ClusterAwareSettings
 import csw.services.location.models.Connection.AkkaConnection
 import csw.services.location.models.{AkkaRegistration, ComponentId, ComponentType}
-import csw.services.logging.internal.LoggingLevels.Level
-import csw.services.logging.internal.{LoggingLevels, SetComponentLogLevel}
+import csw.services.logging.internal.LoggingLevels.{ERROR, Level, WARN}
+import csw.services.logging.internal.{GetComponentLogMetadata, LoggingLevels, SetComponentLogLevel}
+import csw.services.logging.models.{FilterSet, LogMetadata}
 import csw.services.logging.scaladsl.{ComponentLogger, LoggingSystem}
-import csw.apps.clusterseed.utils.AdminLogTestSuite
 
 import scala.concurrent.Await
 import scala.concurrent.duration.DurationDouble
@@ -38,16 +42,53 @@ class TromboneHcd(componentName: String, loggingSystem: LoggingSystem) extends T
     case LogError                    => log.error("Level is error")
     case LogFatal                    => log.fatal("Level is fatal")
     case SetComponentLogLevel(level) ⇒ loggingSystem.addFilter(componentName, level)
+    case GetComponentLogMetadata     ⇒ sender ! loggingSystem.getLogMetadata()
     case x: Any                      => log.error(Map("@msg" -> "Unexpected actor message", "message" -> x.toString))
   }
 }
 
-class LogAdminTest extends AdminLogTestSuite {
+class LogAdminTest extends AdminLogTestSuite with HttpSupport {
   import adminWiring.actorRuntime._
   val componentName    = "tromboneHcd"
   val tromboneActorRef = actorSystem.actorOf(TromboneHcd.props(componentName, loggingSystem), name = "TromboneActor")
   val connection       = AkkaConnection(ComponentId(componentName, ComponentType.HCD))
   Await.result(adminWiring.locationService.register(AkkaRegistration(connection, tromboneActorRef)), 5.seconds)
+
+  // DEOPSCSW-127: Runtime update for logging characteristics
+  test("should able to get the current component log meta data") {
+
+    // send http get metadata request and verify the response has correct log levels
+    val getLogMetadataUri = Uri.from(scheme = "http", host = ClusterAwareSettings.hostname, port = 7878,
+      path = s"/admin/logging/${connection.name}/level")
+
+    val getLogMetadataRequest   = HttpRequest(HttpMethods.GET, uri = getLogMetadataUri)
+    val getLogMetadataResponse1 = Await.result(Http().singleRequest(getLogMetadataRequest), 5.seconds)
+    val logMetadata1            = Await.result(Unmarshal(getLogMetadataResponse1).to[LogMetadata], 5.seconds)
+
+    getLogMetadataResponse1.status shouldBe StatusCodes.OK
+
+    val config     = ConfigFactory.load().getConfig("com.persist.logging")
+    val logLevel   = Level(config.getString("logLevel"))
+    val akkaLevel  = Level(config.getString("akkaLogLevel"))
+    val slf4jLevel = Level(config.getString("slf4jLogLevel"))
+    val filterSet  = FilterSet.from(config)
+
+    logMetadata1 shouldBe LogMetadata(logLevel, akkaLevel, slf4jLevel, filterSet)
+
+    // updating default and akka log level
+    loggingSystem.setLevel(LoggingLevels.ERROR)
+    loggingSystem.setAkkaLevel(LoggingLevels.WARN)
+
+    // verify getLogMetadata http request gives updated log levels in response
+    val getLogMetadataResponse2 = Await.result(Http().singleRequest(getLogMetadataRequest), 5.seconds)
+    val logMetadata2            = Await.result(Unmarshal(getLogMetadataResponse2).to[LogMetadata], 5.seconds)
+
+    logMetadata2 shouldBe LogMetadata(ERROR, WARN, slf4jLevel, filterSet)
+
+    // reset log levels to default
+    loggingSystem.setLevel(logLevel)
+    loggingSystem.setAkkaLevel(akkaLevel)
+  }
 
   // DEOPSCSW-127: Runtime update for logging characteristics
   test("should able to set filter of the component dynamically through http end point") {
@@ -99,7 +140,5 @@ class LogAdminTest extends AdminLogTestSuite {
       val currentLogLevel = log("@severity").toString.toLowerCase
       Level(currentLogLevel) >= LoggingLevels.ERROR shouldBe true
     }
-
   }
-
 }
