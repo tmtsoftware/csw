1288f931b156048830753542d78383ae3697b804
Pritam Kadam <phkadam2008@gmail.com>
2017-06-26 14:49:20 +0530

    #000: Pritam : Delete folder only if path starts with /tmp/ in test scope.

--- csw-logging/src/test/scala/csw/services/logging/utils/FileUtils.scala
+++ csw-logging/src/test/scala/csw/services/logging/utils/FileUtils.scala
@@ -10,6 +10,11 @@ import scala.collection.mutable
 object FileUtils {
 
   def deleteRecursively(file: File): Unit = {
+    // just to be safe, don't delete anything that is not in /tmp/
+    val p = file.getPath
+    if (!p.startsWith("/tmp/"))
+      throw new RuntimeException(s"Refusing to delete $file since not in /tmp/")
+
     if (file.isDirectory)
       file.listFiles.foreach(deleteRecursively)
     if (file.exists && !file.delete)
