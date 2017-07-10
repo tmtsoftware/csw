9405447052b4a175db309a2cc5f9773b4e2bb8fe
Mahendra Kulkarni <mahendra.pk@gmail.com>
2017-07-10 14:28:32 +0530

    #273 : Mahendra | Saloni - Moving Category enum in separate file.

--- csw-logging/src/main/scala/csw/services/logging/commons/Keys.scala
+++ csw-logging/src/main/scala/csw/services/logging/commons/Keys.scala
@@ -1,10 +1,5 @@
 package csw.services.logging.commons
 
-import enumeratum.{Enum, EnumEntry}
-import enumeratum.EnumEntry.Lowercase
-
-import scala.collection.immutable
-
 private[logging] object Keys {
 
   val CATEGORY       = "@category"
@@ -27,17 +22,4 @@ private[logging] object Keys {
   // File rotation hour of the day (This is considered as UTC Time)
   val FILE_ROTATION_HOUR: Long = 12L
 
-}
-
-private[logging] sealed abstract class Category extends EnumEntry with Lowercase with Serializable {
-
-  def name: String = entryName
-}
-
-private[logging] object Category extends Enum[Category] {
-  override def values: immutable.IndexedSeq[Category] = findValues
-
-  case object Common extends Category
-  case object Gc     extends Category
-  case object Time   extends Category
 }
