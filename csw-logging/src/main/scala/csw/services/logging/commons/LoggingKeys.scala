6db00d3a1194b6e9f9459edcefd7381b16965a66
Pritam Kadam <phkadam2008@gmail.com>
2017-07-12 11:53:47 +0530

    #000: Pritam : Formatted LoggingKeys as per scalafmt rules.

--- csw-logging/src/main/scala/csw/services/logging/commons/LoggingKeys.scala
+++ csw-logging/src/main/scala/csw/services/logging/commons/LoggingKeys.scala
@@ -2,45 +2,43 @@ package csw.services.logging.commons
 
 private[logging] object LoggingKeys {
 
-                                          // SOURCE
+  // SOURCE
 
-  val CATEGORY       = "@category"        // Set in LogActor. Set to common unless alternative message type passed,
-                                          //   in which case the value is provided as part of message.
-                                          //   Each category logs to a different file, with category name
-                                          //   name embedded in filename.
-                                          // This Key is not written to the log.
+  // Set in LogActor. Set to common unless alternative message type passed,
+  // in which case the value is provided as part of message.
+  // Each category logs to a different file, with category name
+  // name embedded in filename.
+  // This Key is not written to the log.
+  val CATEGORY = "@category"
 
   // Full header keys, same for all messages by a System
-  val HOST           = "@host"            // LoggingSystem - full header
-  val NAME           = "@name"            // Logging System - full header (name of LoggingSystem)
-  val VERSION        = "@version"         // LoggingSystem - full header (user supplied when instantiating LoggingSystem)
-  val SERVICE        = "@service"         // this appears to be intended for the CSW service name and version.
-                                          //   Not used except for custom appender in test.
-                                          //   should this be in full header?
-
+  val HOST    = "@host"    // LoggingSystem - full header
+  val NAME    = "@name"    // Logging System - full header (name of LoggingSystem)
+  val VERSION = "@version" // LoggingSystem - full header (user supplied when instantiating LoggingSystem)
+  val SERVICE = "@service" // this appears to be intended for the CSW service name and version. Not used except for custom appender in test. Should this be in full header?
 
   // The following keys are always present
-  val TIMESTAMP = "timestamp"             // Logger gets this when contructing Log Message
-  val SEVERITY       = "@severity"        // Set by logger, put into Log Message
+  val TIMESTAMP = "timestamp" // Logger gets this when constructing Log Message
+  val SEVERITY  = "@severity" // Set by logger, put into Log Message
 
-  val FILE      = "file"                  // source location macro
-  val CLASS     = "class"                 // source location marco (constructed as package.class from source location)
-  val LINE      = "line"                  // source location macro
+  val FILE  = "file"  // source location macro
+  val CLASS = "class" // source location marco (constructed as package.class from source location)
+  val LINE  = "line"  // source location macro
 
   //  These items are only present if applicable.
-  val COMPONENT_NAME = "@componentName"   // Passed into LoggerImpl on instantiation of a ComponentLogger.  Passed in Log Message
-  val ACTOR     = "actor"                 // Passed into LoggerImpl on instantiation of a ComponentLogger.Actor.  Passed in Log Message
+  val COMPONENT_NAME = "@componentName" // Passed into LoggerImpl on instantiation of a ComponentLogger.  Passed in Log Message
+  val ACTOR          = "actor"          // Passed into LoggerImpl on instantiation of a ComponentLogger.Actor.  Passed in Log Message
 
   // Following items are passed in by user, and are optional (except message)
-  val MESSAGE   = "message"               // User, via logger methods.  Passed to actor in Log Message
-  val MSG            = "@msg"             // User, via logger methods.  Passed to actor in Log message (for optional Map of user keys)
-  val TRACE_ID       = "@traceId"         // User, via logger methods.  Passed to actor in Log Message
-  val EX        = "ex"                    // User, via logger methods.  Passed to actor in Log Message
+  val MESSAGE  = "message"  // User, via logger methods.  Passed to actor in Log Message
+  val MSG      = "@msg"     // User, via logger methods.  Passed to actor in Log message (for optional Map of user keys)
+  val TRACE_ID = "@traceId" // User, via logger methods.  Passed to actor in Log Message
+  val EX       = "ex"       // User, via logger methods.  Passed to actor in Log Message
 
-  val KIND      = "kind"                  // in Log message, but never set?
+  val KIND = "kind" // in Log message, but never set?
 
-  val METHOD    = "method"                // this and items below are used and populated
-  val TRACE     = "trace"                 // iff an ex has been passed to actor.
-  val STACK     = "stack"                 // these values are determined from ex throwable
-  val CAUSE     = "CAUSE"                 // see above
+  val METHOD = "method" // this and items below are used and populated
+  val TRACE  = "trace"  // iff an ex has been passed to actor.
+  val STACK  = "stack"  // these values are determined from ex throwable
+  val CAUSE  = "CAUSE"  // see above
 }
