7a3dfa4e5d522d3e33b57c8ab1cf2aa4a9530417
Saloni Vithalani <skvithalani@gmail.com>
2017-07-18 10:57:13 +0530

    Revert "#000: Saloni :  Cleaned some parts of logging"
    This reverts commit df1729426dcced00d81ce6dfa0282a660db7cbd6.

--- csw-logging/src/main/java/csw/services/logging/javadsl/JLogAppenderBuilders.java
+++ csw-logging/src/main/java/csw/services/logging/javadsl/JLogAppenderBuilders.java
@@ -5,13 +5,6 @@ import csw.services.logging.appenders.LogAppenderBuilder;
 import csw.services.logging.appenders.StdOutAppender$;
 
 public class JLogAppenderBuilders {
-
-    /**
-     * Java representation of StdOutAppender object.
-     */
     public static final LogAppenderBuilder StdOutAppender = StdOutAppender$.MODULE$;
-    /**
-     * Java representation of FileAppender object.
-     */
     public static final LogAppenderBuilder FileAppender = FileAppender$.MODULE$;
 }
--- csw-logging/src/main/scala/csw/services/logging/javadsl/JRequestId.scala
+++ csw-logging/src/main/scala/csw/services/logging/javadsl/JRequestId.scala
@@ -2,9 +2,6 @@ package csw.services.logging.javadsl
 
 import csw.services.logging.scaladsl.RequestId
 
-/**
- * Java representation of RequestId with default parameters
- */
 object JRequestId {
   val id = RequestId()
 }
