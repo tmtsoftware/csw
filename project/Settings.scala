9d17a5cb0d51358a066daeffc32a09cdf6fd3197
Pritam Kadam <phkadam2008@gmail.com>
2017-07-26 12:18:03 +0530

    #000: Pritam :  Enable project filter for JavaUnidoc.

--- project/Settings.scala
+++ project/Settings.scala
@@ -6,6 +6,7 @@ import Keys._
 import com.typesafe.sbt.site.SitePlugin.autoImport._
 import sbtunidoc.BaseUnidocPlugin.autoImport.{unidoc, unidocProjectFilter}
 import sbtunidoc.ScalaUnidocPlugin.autoImport.ScalaUnidoc
+import sbtunidoc.JavaUnidocPlugin.autoImport.JavaUnidoc
 
 object Settings {
   def mergeSiteWith(p: Project): Setting[Task[Seq[(File, String)]]] =
@@ -15,6 +16,8 @@ object Settings {
 
   def docExclusions(projects: Seq[ProjectReference]): Seq[Setting[_]] =
     projects.map(p ⇒ sources in (Compile, doc) in p := Seq.empty) ++ Seq(
-      unidocProjectFilter in (ScalaUnidoc, unidoc) := inAnyProject -- inProjects(projects: _*)
+      unidocProjectFilter in (ScalaUnidoc, unidoc) := inAnyProject -- inProjects(projects: _*),
+      unidocProjectFilter in (JavaUnidoc, unidoc) := inAnyProject -- inProjects(projects: _*)
     )
+
 }
