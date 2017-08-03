e934706e4bc7a7bd4d3103c15c1f0cfed2006e6c
Pritam Kadam <phkadam2008@gmail.com>
2017-08-03 21:38:25 +0530

    #183: Pritam : Added java tests for ChoiceKey

---
+++ csw-params/src/test/java/csw/param/parameters/JChoiceKeyTypeTest.java
@@ -0,0 +1,75 @@
+package csw.param.parameters;
+
+import csw.param.UnitsOfMeasure;
+import csw.param.models.Choice;
+import csw.param.models.Choices;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.util.Arrays;
+
+public class JChoiceKeyTypeTest {
+    private String keyName = " choiceKey";
+    private Choices choices = Choices.from("A", "B", "C");
+    
+    private GChoiceKey choiceKey = JKeyTypes.ChoiceKey().make(keyName, choices);
+
+    @Test
+    public void choiceKeyShouldHaveNameTypeAndChoices() {
+        Assert.assertEquals(choices, choiceKey.choices());
+        Assert.assertEquals(keyName, choiceKey.keyName());
+        Assert.assertEquals(choices, choiceKey.choices());
+    }
+
+    @Test
+    public void shouldAbleToCreateChoiceParameterWithoutUnits() {
+        Choice choice1 = new Choice("A");
+        Choice choice2 = new Choice("B");
+        Choice[] choicesArr = {choice1, choice2};
+
+        // set with varargs
+        Parameter<Choice> choiceParameter = choiceKey.set(choice1, choice2);
+        Assert.assertEquals(UnitsOfMeasure.NoUnits$.MODULE$, choiceParameter.units());
+        Assert.assertEquals(choice1, choiceParameter.jGet(0).get());
+        Assert.assertEquals(choice2, choiceParameter.jGet(1).get());
+        Assert.assertEquals(choice1, choiceParameter.head());
+        Assert.assertEquals(choice1, choiceParameter.value(0));
+        Assert.assertEquals(choice2, choiceParameter.value(1));
+        Assert.assertEquals(2, choiceParameter.size());
+        Assert.assertArrayEquals(choicesArr, (Choice[])choiceParameter.values());
+        Assert.assertEquals(Arrays.asList(choicesArr), choiceParameter.jValues());
+    }
+
+    @Test
+    public void shouldAbleToCreateChoiceParameterWithUnits() {
+        Choice choice1 = new Choice("A");
+        Choice choice2 = new Choice("B");
+        Choice[] choicesArr = {choice1, choice2};
+
+        // set with Array and Units
+        UnitsOfMeasure.kilometers$ kilometers = UnitsOfMeasure.kilometers$.MODULE$;
+        Parameter<Choice> choiceParameter = choiceKey.set(choicesArr, kilometers);
+        Assert.assertEquals(kilometers, choiceParameter.units());
+        Assert.assertEquals(choice1, choiceParameter.jGet(0).get());
+        Assert.assertEquals(choice2, choiceParameter.jGet(1).get());
+        Assert.assertEquals(choice1, choiceParameter.head());
+        Assert.assertEquals(choice1, choiceParameter.value(0));
+        Assert.assertEquals(choice2, choiceParameter.value(1));
+        Assert.assertEquals(2, choiceParameter.size());
+        Assert.assertArrayEquals(choicesArr, (Choice[])choiceParameter.values());
+        Assert.assertEquals(Arrays.asList(choicesArr), choiceParameter.jValues());
+    }
+
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+
+    @Test
+    public void shouldThrowExceptionForInvalidChoice() {
+        Choice invalidChoice = new Choice("D");
+        exception.expect(AssertionError.class);
+        choiceKey.set(invalidChoice);
+    }
+
+}
