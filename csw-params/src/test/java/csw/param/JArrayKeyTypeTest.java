83b9985922c829c7c7cac72284b1d92aaeb51e36
Pritam Kadam <phkadam2008@gmail.com>
2017-08-03 21:05:02 +0530

    #183: Pritam | Mahendra : Added java tests for ArrayKeyType

--- csw-params/src/test/java/csw/param/JArrayKeyTypeTest.java
+++ csw-params/src/test/java/csw/param/JArrayKeyTypeTest.java
@@ -4,6 +4,7 @@ import csw.param.models.ArrayData;
 import csw.param.models.JArrayData;
 import csw.param.parameters.JKeyTypes;
 import csw.param.parameters.Key;
+import csw.param.parameters.KeyType;
 import csw.param.parameters.Parameter;
 import org.junit.Assert;
 import org.junit.Test;
@@ -12,6 +13,47 @@ import java.util.List;
 
 public class JArrayKeyTypeTest {
 
+    private UnitsOfMeasure.NoUnits$ NoUnit = UnitsOfMeasure.NoUnits$.MODULE$;
+
+    void commonAssertions(String keyName, KeyType keyType, ArrayData[] testData, Parameter parameter, UnitsOfMeasure.Units unit) {
+        Assert.assertEquals(keyName, keyName);
+        Assert.assertEquals(keyType,parameter.keyType());
+
+        List paramValuesAsList = parameter.jValues();
+        Assert.assertEquals(testData.length, paramValuesAsList.size());
+        Assert.assertEquals(testData[0], paramValuesAsList.get(0));
+        Assert.assertEquals(testData[1], paramValuesAsList.get(1));
+
+        Assert.assertEquals(testData[0], parameter.head());
+        Assert.assertEquals(unit, parameter.units());
+
+        Object[] paramValuesAsArray = (Object[]) parameter.values();
+        Assert.assertEquals(testData.length, paramValuesAsArray.length);
+        Assert.assertEquals(testData[0], paramValuesAsArray[0]);
+        Assert.assertEquals(testData[1], paramValuesAsArray[1]);
+    }
+
+    @Test
+    public void testByteArrayKeyParameter() {
+        String keyName = "ByteKey";
+        Key<ArrayData<Byte>> key = JKeyTypes.ByteArrayKey().make(keyName);
+        Byte[] byteArray1 = {1, 2, 3};
+        Byte[] byteArray2 = {4, 5, 6, 7};
+
+        ArrayData<Byte> data1 = JArrayData.fromArray(byteArray1);
+        ArrayData<Byte> data2 = JArrayData.fromArray(byteArray2);
+
+        // key.set without Units
+        ArrayData[] arrayData = new ArrayData[]{data1, data2};
+        Parameter<ArrayData<Byte>> parameterWithoutUnits = key.set(data1, data2);
+        commonAssertions(keyName, JKeyTypes.ByteArrayKey(), arrayData, parameterWithoutUnits, NoUnit);
+
+        UnitsOfMeasure.milliseconds$ millisecondsUnit = UnitsOfMeasure.milliseconds$.MODULE$;
+        // key.set with Units
+        Parameter<ArrayData<Byte>> parameterWithUnits = key.set(arrayData, millisecondsUnit);
+        commonAssertions(keyName, JKeyTypes.ByteArrayKey(), arrayData, parameterWithUnits, millisecondsUnit);
+    }
+    
     @Test
     public void testShortArrayKeyParameter() {
         String keyName = "shortKey";
@@ -19,43 +61,102 @@ public class JArrayKeyTypeTest {
         Short[] shortArray1 = {10, 20, 30};
         Short[] shortArray2 = {100, 200, 300, 400};
 
-        ArrayData<Short> paramArrayData1 = JArrayData.fromArray(shortArray1);
-        ArrayData<Short> paramArrayData2 = JArrayData.fromArray(shortArray2);
+        ArrayData<Short> data1 = JArrayData.fromArray(shortArray1);
+        ArrayData<Short> data2 = JArrayData.fromArray(shortArray2);
 
         // key.set without Units
-        ArrayData<Short>[] arrayData = new ArrayData[]{paramArrayData1, paramArrayData2};
-        Parameter<ArrayData<Short>> parameterWithoutUnits = key.set(arrayData);
-
-        List<ArrayData<Short>> paramValuesAsList = parameterWithoutUnits.jValues();
-        ArrayData<Short>[] paramValues = (ArrayData<Short>[]) parameterWithoutUnits.values();
-
-        Assert.assertEquals(keyName, key.keyName());
-        Assert.assertEquals(JKeyTypes.ShortArrayKey(),key.keyType());
-        Assert.assertEquals(arrayData.length, paramValuesAsList.size());
-        Assert.assertEquals(arrayData.length, paramValues.length);
-        Assert.assertEquals(paramArrayData1, paramValues[0]);
-        Assert.assertEquals(paramArrayData2, paramValues[1]);
-        Assert.assertEquals(paramArrayData1, paramValuesAsList.get(0));
-        Assert.assertEquals(paramArrayData2, paramValuesAsList.get(1));
-        Assert.assertEquals(paramArrayData1, parameterWithoutUnits.head());
-        Assert.assertEquals(UnitsOfMeasure.NoUnits$.MODULE$, parameterWithoutUnits.units());
+        ArrayData[] arrayData = new ArrayData[]{data1, data2};
+        Parameter<ArrayData<Short>> parameterWithoutUnits = key.set(data1, data2);
+        commonAssertions(keyName, JKeyTypes.ShortArrayKey(), arrayData, parameterWithoutUnits, NoUnit);
 
         UnitsOfMeasure.degrees$ degreesUnit = UnitsOfMeasure.degrees$.MODULE$;
         // key.set with Units
         Parameter<ArrayData<Short>> parameterWithUnits = key.set(arrayData, degreesUnit);
-        List<ArrayData<Short>> paramValuesAsList2 = parameterWithoutUnits.jValues();
-        ArrayData<Short>[] paramValues2 = (ArrayData<Short>[]) parameterWithoutUnits.values();
-
-        Assert.assertEquals(degreesUnit, parameterWithUnits.units());
-        Assert.assertEquals(keyName, key.keyName());
-        Assert.assertEquals(JKeyTypes.ShortArrayKey(),key.keyType());
-        Assert.assertEquals(arrayData.length, paramValuesAsList2.size());
-        Assert.assertEquals(arrayData.length, paramValues2.length);
-        Assert.assertEquals(paramArrayData1, paramValues2[0]);
-        Assert.assertEquals(paramArrayData2, paramValues2[1]);
-        Assert.assertEquals(paramArrayData1, paramValuesAsList2.get(0));
-        Assert.assertEquals(paramArrayData2, paramValuesAsList2.get(1));
-        Assert.assertEquals(paramArrayData1, parameterWithoutUnits.head());
+        commonAssertions(keyName, JKeyTypes.ShortArrayKey(), arrayData, parameterWithUnits, degreesUnit);
     }
 
+    @Test
+    public void testLongArrayKeyParameter() {
+        String keyName = "longKey";
+        Key<ArrayData<Long>> key = JKeyTypes.LongArrayKey().make(keyName);
+        Long[] longArray1 = {100L, 200L, 300L};
+        Long[] longArray2 = {400L, 500L, 600L, 700L};
+
+        ArrayData<Long> data1 = JArrayData.fromArray(longArray1);
+        ArrayData<Long> data2 = JArrayData.fromArray(longArray2);
+
+        // key.set without Units
+        ArrayData[] arrayData = new ArrayData[]{data1, data2};
+        Parameter<ArrayData<Long>> parameterWithoutUnits = key.set(data1, data2);
+        commonAssertions(keyName, JKeyTypes.LongArrayKey(), arrayData, parameterWithoutUnits, NoUnit);
+
+        UnitsOfMeasure.millimeters$ millimetersUnit = UnitsOfMeasure.millimeters$.MODULE$;
+        // key.set with Units
+        Parameter<ArrayData<Long>> parameterWithUnits = key.set(arrayData, millimetersUnit);
+        commonAssertions(keyName, JKeyTypes.LongArrayKey(), arrayData, parameterWithUnits, millimetersUnit);
+    }
+    
+    @Test
+    public void testIntArrayKeyParameter() {
+        String keyName = "integerKey";
+        Key<ArrayData<Integer>> key = JKeyTypes.IntArrayKey().make(keyName);
+        Integer[] integerArray1 = {100, 200, 300};
+        Integer[] integerArray2 = {400, 500, 600, 700};
+
+        ArrayData<Integer> data1 = JArrayData.fromArray(integerArray1);
+        ArrayData<Integer> data2 = JArrayData.fromArray(integerArray2);
+
+        // key.set without Units
+        ArrayData[] arrayData = new ArrayData[]{data1, data2};
+        Parameter<ArrayData<Integer>> parameterWithoutUnits = key.set(data1, data2);
+
+        commonAssertions(keyName, JKeyTypes.IntArrayKey(), arrayData, parameterWithoutUnits, NoUnit);
+
+        UnitsOfMeasure.millimeters$ millimetersUnit = UnitsOfMeasure.millimeters$.MODULE$;
+        // key.set with Units
+        Parameter<ArrayData<Integer>> parameterWithUnits = key.set(arrayData, millimetersUnit);
+        commonAssertions(keyName, JKeyTypes.IntArrayKey(), arrayData, parameterWithUnits, millimetersUnit);
+    }
+
+    @Test
+    public void testFloatArrayKeyParameter() {
+        String keyName = "floatKey";
+        Key<ArrayData<Float>> key = JKeyTypes.FloatArrayKey().make(keyName);
+        Float[] floatArray1 = {100f, 200f, 300f};
+        Float[] floatArray2 = {400f, 500f, 600f, 700f};
+
+        ArrayData<Float> data1 = JArrayData.fromArray(floatArray1);
+        ArrayData<Float> data2 = JArrayData.fromArray(floatArray2);
+
+        // key.set without Units
+        ArrayData[] arrayData = new ArrayData[]{data1, data2};
+        Parameter<ArrayData<Float>> parameterWithoutUnits = key.set(data1, data2);
+        commonAssertions(keyName, JKeyTypes.FloatArrayKey(), arrayData, parameterWithoutUnits, NoUnit);
+
+        UnitsOfMeasure.millimeters$ millimetersUnit = UnitsOfMeasure.millimeters$.MODULE$;
+        // key.set with Units
+        Parameter<ArrayData<Float>> parameterWithUnits = key.set(arrayData, millimetersUnit);
+        commonAssertions(keyName, JKeyTypes.FloatArrayKey(), arrayData, parameterWithUnits, millimetersUnit);
+    }
+
+    @Test
+    public void testDoubleArrayKeyParameter() {
+        String keyName = "doubleKey";
+        Key<ArrayData<Double>> key = JKeyTypes.DoubleArrayKey().make(keyName);
+        Double[] doubleArray1 = {100d, 200d, 300d};
+        Double[] doubleArray2 = {400d, 500d, 600d, 700d};
+
+        ArrayData<Double> data1 = JArrayData.fromArray(doubleArray1);
+        ArrayData<Double> data2 = JArrayData.fromArray(doubleArray2);
+
+        // key.set without Units
+        ArrayData[] arrayData = new ArrayData[]{data1, data2};
+        Parameter<ArrayData<Double>> parameterWithoutUnits = key.set(data1, data2);
+        commonAssertions(keyName, JKeyTypes.DoubleArrayKey(), arrayData, parameterWithoutUnits, NoUnit);
+
+        UnitsOfMeasure.millimeters$ millimetersUnit = UnitsOfMeasure.millimeters$.MODULE$;
+        // key.set with Units
+        Parameter<ArrayData<Double>> parameterWithUnits = key.set(arrayData, millimetersUnit);
+        commonAssertions(keyName, JKeyTypes.DoubleArrayKey(), arrayData, parameterWithUnits, millimetersUnit);
+    }
 }
