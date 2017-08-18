1da2c0ddef905bc7f9dba283f0100e3f670f3340
Mahendra Kulkarni <mahendra.pk@gmail.com>
2017-08-18 14:31:45 +0530

    #000 : Mahendra | Saloni - Removed unnecessary ComponentInfo companion object

--- csw-vslice/src/main/scala/csw/common/framework/models/Component.scala
+++ csw-vslice/src/main/scala/csw/common/framework/models/Component.scala
@@ -44,67 +44,6 @@ case class ComponentInfo(componentName: String,
   def getConnections: Optional[java.util.List[Connection]] = maybeConnections.map(_.toList.asJava).asJava
 }
 
-/**
- * Represents a Component, such as an assembly, HCD (Hardware Control Daemon) or SC (Sequence Component).
- */
-object ComponentInfo {
-
-//  private def createHCD(context: ActorContext, cinfo: ComponentInfo, supervisorIn: Option[ActorRef]): ActorRef = {
-//
-//    val supervisor = supervisorIn match {
-//      case None                => context.self // Will be parent which is supervisor
-//      case Some(supervisorRef) => supervisorRef
-//    }
-//
-//    // Form props for component
-//    val props = Props(Class.forName(cinfo.componentClassName), cinfo, supervisor)
-//
-//    context.actorOf(props, s"${cinfo.componentName}-${cinfo.componentType}")
-//  }
-//
-//  private def createAssembly(context: ActorContext, cinfo: AssemblyInfo, supervisorIn: Option[ActorRef]): ActorRef = {
-//
-//    val supervisor = supervisorIn match {
-//      case None                => context.self // Will be parent which is supervisor
-//      case Some(supervisorRef) => supervisorRef
-//    }
-//
-//    val props = Props(Class.forName(cinfo.componentClassName), cinfo, supervisor)
-//
-//    context.actorOf(props, s"${cinfo.componentName}-${cinfo.componentType}")
-//  }
-//
-//  /**
-//   * Creates a component from the given componentInfo
-//   * @param context the actor context
-//   * @param componentInfo describes the component
-//   * @param supervisorIn optional supervisor actor to use instead of the default
-//   * @return the component's supervisor
-//   */
-//  def create(context: ActorContext, componentInfo: ComponentInfo, supervisorIn: Option[ActorRef] = None): ActorRef =
-//    componentInfo match {
-//      case hcd: HcdInfo =>
-//        createHCD(context, hcd, supervisorIn)
-//      case ass: AssemblyInfo =>
-//        createAssembly(context, ass, supervisorIn)
-//      case cont: ContainerInfo =>
-//        ContainerComponent.create(cont)
-//    }
-//
-//  // This is for JComponent create
-//  import scala.compat.java8.OptionConverters._
-//
-//  /**
-//   * Java API to create a component from the given componentInfo
-//   * @param context the actor context
-//   * @param componentInfo describes the component
-//   * @param supervisorIn optional supervisor actor to use instead of the default
-//   * @return the component's supervisor
-//   */
-//  def create(context: ActorContext, componentInfo: ComponentInfo, supervisorIn: Optional[ActorRef]): ActorRef =
-//    create(context, componentInfo, supervisorIn.asScala)
-}
-
 trait Component {
   def info: ComponentInfo
 }
