6fa667f346d1c77ac8f3256120ad95752c02bcfd
Pritam Kadam <phkadam2008@gmail.com>
2017-08-29 21:42:33 +0530

    #000: Pritam : Extending franework multi-jvm test to exercise standalone mode.

--- csw-vslice/src/multi-jvm/scala/csw.common.framework/FrameworkApp.scala
+++ csw-vslice/src/multi-jvm/scala/csw.common.framework/FrameworkApp.scala
@@ -2,14 +2,14 @@ package csw.common.framework
 
 import akka.actor.ActorSystem
 import com.typesafe.config.{Config, ConfigFactory}
-import csw.common.framework.internal.wiring.{Container, FrameworkWiring}
+import csw.common.framework.internal.wiring.{FrameworkWiring, Standalone}
 import csw.services.location.commons.ClusterSettings
 
 object FrameworkApp extends App {
   private val clusterSettings: ClusterSettings = ClusterSettings().withManagementPort(5555)
   private val system: ActorSystem              = clusterSettings.system
   private val wiring                           = FrameworkWiring.make(system)
-  private val config: Config                   = ConfigFactory.load("laser_container.conf")
+  private val config: Config                   = ConfigFactory.load("eaton_hcd_standalone.conf")
 
-  Container.spawn(config, wiring)
+  Standalone.spawn(config, wiring)
 }
--- csw-vslice/src/multi-jvm/scala/csw.common.framework/FrameworkTest.scala
+++ csw-vslice/src/multi-jvm/scala/csw.common.framework/FrameworkTest.scala
@@ -5,22 +5,31 @@ import akka.typed.scaladsl.adapter.UntypedActorSystemOps
 import akka.typed.testkit.TestKitSettings
 import akka.typed.testkit.scaladsl.TestProbe
 import com.typesafe.config.ConfigFactory
+import csw.common.components.{ComponentStatistics, SampleComponentState}
 import csw.common.framework.internal.container.ContainerMode
-import csw.common.framework.internal.wiring.{Container, FrameworkWiring}
+import csw.common.framework.internal.supervisor.SupervisorMode
+import csw.common.framework.internal.wiring.{Container, FrameworkWiring, Standalone}
 import csw.common.framework.models.ContainerCommonMessage.GetContainerMode
 import csw.common.framework.models.ContainerExternalMessage.GetComponents
-import csw.common.framework.models.{Components, ContainerMessage}
-import csw.services.location.helpers.{LSNodeSpec, OneMemberAndSeed}
+import csw.common.framework.models.PubSub.Subscribe
+import csw.common.framework.models.RunningMessage.Lifecycle
+import csw.common.framework.models.SupervisorCommonMessage.{ComponentStateSubscription, GetSupervisorMode}
+import csw.common.framework.models.ToComponentLifecycleMessage.GoOffline
+import csw.common.framework.models.{Components, ContainerMessage, SupervisorExternalMessage}
+import csw.param.states.CurrentState
+import csw.services.location.helpers.{LSNodeSpec, TwoMembersAndSeed}
 import csw.services.location.models.Connection.AkkaConnection
 import csw.services.location.models.{ComponentId, ComponentType}
+import csw.services.logging.scaladsl.LoggingSystemFactory
 
 import scala.concurrent.Await
 import scala.concurrent.duration.DurationInt
 
 class FrameworkTestMultiJvmNode1 extends FrameworkTest(0)
 class FrameworkTestMultiJvmNode2 extends FrameworkTest(0)
+class FrameworkTestMultiJvmNode3 extends FrameworkTest(0)
 
-class FrameworkTest(ignore: Int) extends LSNodeSpec(config = new OneMemberAndSeed) {
+class FrameworkTest(ignore: Int) extends LSNodeSpec(config = new TwoMembersAndSeed) {
 
   import config._
 
@@ -29,7 +38,7 @@ class FrameworkTest(ignore: Int) extends LSNodeSpec(config = new OneMemberAndSee
 
 //  LoggingSystemFactory.start("framework", "1.0", "localhost", system)
 
-  test("should able to create multiple containers across jvm's") {
+  test("should able to create multiple containers across jvm's and start component in standalone mode") {
 
     runOn(seed) {
       val wiring       = FrameworkWiring.make(system, locationService)
@@ -41,11 +50,11 @@ class FrameworkTest(ignore: Int) extends LSNodeSpec(config = new OneMemberAndSee
       containerRef ! GetContainerMode(containerModeProbe.ref)
       containerModeProbe.expectMsg(ContainerMode.Idle)
 
-      Thread.sleep(500)
+      Thread.sleep(2000)
 
       containerRef ! GetContainerMode(containerModeProbe.ref)
       containerModeProbe.expectMsg(ContainerMode.Running)
-      enterBarrier("container-running")
+      enterBarrier("running")
 
       val wfsContainerLocationF =
         locationService.find(AkkaConnection(ComponentId("WFS_Container", ComponentType.Container)))
@@ -59,37 +68,58 @@ class FrameworkTest(ignore: Int) extends LSNodeSpec(config = new OneMemberAndSee
       efsContainerTypedRef ! GetComponents(componentsProbe.ref)
       val components = componentsProbe.expectMsgType[Components].components
       components.size shouldBe 3
+      enterBarrier("offline")
     }
 
-    runOn(member) {
+    runOn(member1) {
       val wiring       = FrameworkWiring.make(system, locationService)
       val containerRef = Container.spawn(ConfigFactory.load("wfs_container.conf"), wiring)
 
       val containerModeProbe = TestProbe[ContainerMode]
-      val componentsProbe    = TestProbe[Components]
 
       containerRef ! GetContainerMode(containerModeProbe.ref)
       containerModeProbe.expectMsg(ContainerMode.Idle)
 
-      Thread.sleep(500)
+      Thread.sleep(2000)
 
       containerRef ! GetContainerMode(containerModeProbe.ref)
       containerModeProbe.expectMsg(ContainerMode.Running)
-      enterBarrier("container-running")
+      enterBarrier("running")
 
-      val laserContainerLocationF =
-        locationService.find(AkkaConnection(ComponentId("LGSF_Container", ComponentType.Container)))
-      val laserContainerLocation = Await.result(laserContainerLocationF, 5.seconds).get
+      val etonSupervisorF =
+        locationService.find(AkkaConnection(ComponentId("Eton", ComponentType.HCD)))
+      val etonSupervisorLocation = Await.result(etonSupervisorF, 5.seconds).get
 
-      val laserContainerTypedRef = laserContainerLocation.typedRef[ContainerMessage]
+      val etonSupervisorTypedRef = etonSupervisorLocation.typedRef[SupervisorExternalMessage]
+      val compStateProbe         = TestProbe[CurrentState]
 
-      laserContainerTypedRef ! GetContainerMode(containerModeProbe.ref)
-      containerModeProbe.expectMsg(ContainerMode.Running)
+      etonSupervisorTypedRef ! ComponentStateSubscription(Subscribe(compStateProbe.ref))
+      etonSupervisorTypedRef ! ComponentStatistics(1)
 
-      laserContainerTypedRef ! GetComponents(componentsProbe.ref)
-      val components = componentsProbe.expectMsgType[Components].components
-      components.size shouldBe 3
+      import SampleComponentState._
+      compStateProbe.expectMsg(CurrentState(prefix, Set(choiceKey.set(domainChoice))))
+
+      etonSupervisorTypedRef ! Lifecycle(GoOffline)
+      enterBarrier("offline")
     }
+
+    runOn(member2) {
+      val wiring        = FrameworkWiring.make(system, locationService)
+      val supervisorRef = Standalone.spawn(ConfigFactory.load("eaton_hcd_standalone.conf"), wiring)
+
+      Thread.sleep(2000)
+      val supervisorStateProbe = TestProbe[SupervisorMode]
+
+      supervisorRef ! GetSupervisorMode(supervisorStateProbe.ref)
+      supervisorStateProbe.expectMsg(SupervisorMode.Running)
+      enterBarrier("running")
+
+      enterBarrier("offline")
+      Thread.sleep(50)
+      supervisorRef ! GetSupervisorMode(supervisorStateProbe.ref)
+      supervisorStateProbe.expectMsg(SupervisorMode.RunningOffline)
+    }
+
     enterBarrier("end")
   }
 }
