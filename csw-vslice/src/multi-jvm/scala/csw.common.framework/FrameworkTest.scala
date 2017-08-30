05b6eb6106c663bbc6c780d24148fefeaa5544bd
Pritam Kadam <phkadam2008@gmail.com>
2017-08-30 23:46:59 +0530

    #000: Pritam : Refactored framework multi-jvm test.

--- csw-vslice/src/multi-jvm/scala/csw.common.framework/FrameworkTest.scala
+++ csw-vslice/src/multi-jvm/scala/csw.common.framework/FrameworkTest.scala
@@ -13,9 +13,13 @@ import csw.common.framework.models.ContainerCommonMessage.GetContainerMode
 import csw.common.framework.models.ContainerExternalMessage.GetComponents
 import csw.common.framework.models.PubSub.Subscribe
 import csw.common.framework.models.RunningMessage.Lifecycle
-import csw.common.framework.models.SupervisorCommonMessage.{ComponentStateSubscription, GetSupervisorMode}
+import csw.common.framework.models.SupervisorCommonMessage.{
+  ComponentStateSubscription,
+  GetSupervisorMode,
+  LifecycleStateSubscription
+}
 import csw.common.framework.models.ToComponentLifecycleMessage.GoOffline
-import csw.common.framework.models.{Components, ContainerMessage, SupervisorExternalMessage}
+import csw.common.framework.models.{Components, ContainerMessage, LifecycleStateChanged, SupervisorExternalMessage}
 import csw.param.states.CurrentState
 import csw.services.location.helpers.{LSNodeSpec, TwoMembersAndSeed}
 import csw.services.location.models.Connection.AkkaConnection
@@ -40,23 +44,34 @@ class FrameworkTest(ignore: Int) extends LSNodeSpec(config = new TwoMembersAndSe
   test("should able to create multiple containers across jvm's and start component in standalone mode") {
 
     runOn(seed) {
+      val containerModeProbe   = TestProbe[ContainerMode]
+      val componentsProbe      = TestProbe[Components]
+      val supervisorStateProbe = TestProbe[LifecycleStateChanged]
+
       val wiring       = FrameworkWiring.make(system, locationService)
       val containerRef = Container.spawn(ConfigFactory.load("laser_container.conf"), wiring)
-
-      val containerModeProbe = TestProbe[ContainerMode]
-      val componentsProbe    = TestProbe[Components]
 
       containerRef ! GetContainerMode(containerModeProbe.ref)
       containerModeProbe.expectMsg(ContainerMode.Idle)
 
-      Thread.sleep(2000)
+      containerRef ! GetComponents(componentsProbe.ref)
+      val laserContainerComponents = componentsProbe.expectMsgType[Components].components
+
+      // check that all the components within supervisor moves to Running mode
+      laserContainerComponents.foreach(_.supervisor ! LifecycleStateSubscription(Subscribe(supervisorStateProbe.ref)))
+      supervisorStateProbe.expectMsgType[LifecycleStateChanged].state shouldBe SupervisorMode.Running
+      supervisorStateProbe.expectMsgType[LifecycleStateChanged].state shouldBe SupervisorMode.Running
+      supervisorStateProbe.expectMsgType[LifecycleStateChanged].state shouldBe SupervisorMode.Running
+
+      Thread.sleep(100)
 
       containerRef ! GetContainerMode(containerModeProbe.ref)
       containerModeProbe.expectMsg(ContainerMode.Running)
       enterBarrier("running")
 
+      // resolve and send message to container running in different jvm or on different physical machine
       val wfsContainerLocationF =
-        locationService.find(AkkaConnection(ComponentId("WFS_Container", ComponentType.Container)))
+        locationService.resolve(AkkaConnection(ComponentId("WFS_Container", ComponentType.Container)), 2.seconds)
       val wfsContainerLocation = Await.result(wfsContainerLocationF, 5.seconds).get
 
       val efsContainerTypedRef = wfsContainerLocation.typedRef[ContainerMessage]
@@ -71,22 +86,34 @@ class FrameworkTest(ignore: Int) extends LSNodeSpec(config = new TwoMembersAndSe
     }
 
     runOn(member1) {
+      val containerModeProbe   = TestProbe[ContainerMode]
+      val componentsProbe      = TestProbe[Components]
+      val supervisorStateProbe = TestProbe[LifecycleStateChanged]
+
       val wiring       = FrameworkWiring.make(system, locationService)
       val containerRef = Container.spawn(ConfigFactory.load("wfs_container.conf"), wiring)
-
-      val containerModeProbe = TestProbe[ContainerMode]
 
       containerRef ! GetContainerMode(containerModeProbe.ref)
       containerModeProbe.expectMsg(ContainerMode.Idle)
 
-      Thread.sleep(2000)
+      containerRef ! GetComponents(componentsProbe.ref)
+      val laserContainerComponents = componentsProbe.expectMsgType[Components].components
+
+      // check that all the components within supervisor moves to Running mode
+      laserContainerComponents.foreach(_.supervisor ! LifecycleStateSubscription(Subscribe(supervisorStateProbe.ref)))
+      supervisorStateProbe.expectMsgType[LifecycleStateChanged].state shouldBe SupervisorMode.Running
+      supervisorStateProbe.expectMsgType[LifecycleStateChanged].state shouldBe SupervisorMode.Running
+      supervisorStateProbe.expectMsgType[LifecycleStateChanged].state shouldBe SupervisorMode.Running
+
+      Thread.sleep(100)
 
       containerRef ! GetContainerMode(containerModeProbe.ref)
       containerModeProbe.expectMsg(ContainerMode.Running)
       enterBarrier("running")
 
+      // resolve and send message to component running in different jvm or on different physical machine
       val etonSupervisorF =
-        locationService.find(AkkaConnection(ComponentId("Eton", ComponentType.HCD)))
+        locationService.resolve(AkkaConnection(ComponentId("Eton", ComponentType.HCD)), 2.seconds)
       val etonSupervisorLocation = Await.result(etonSupervisorF, 5.seconds).get
 
       val etonSupervisorTypedRef = etonSupervisorLocation.typedRef[SupervisorExternalMessage]
@@ -103,20 +130,19 @@ class FrameworkTest(ignore: Int) extends LSNodeSpec(config = new TwoMembersAndSe
     }
 
     runOn(member2) {
-      val wiring        = FrameworkWiring.make(system, locationService)
-      val supervisorRef = Standalone.spawn(ConfigFactory.load("eaton_hcd_standalone.conf"), wiring)
+      val supervisorStateProbe = TestProbe[LifecycleStateChanged]
+      val wiring               = FrameworkWiring.make(system, locationService)
+      val supervisorRef        = Standalone.spawn(ConfigFactory.load("eaton_hcd_standalone.conf"), wiring)
 
-      Thread.sleep(2000)
-      val supervisorStateProbe = TestProbe[SupervisorMode]
-
-      supervisorRef ! GetSupervisorMode(supervisorStateProbe.ref)
-      supervisorStateProbe.expectMsg(SupervisorMode.Running)
+      supervisorRef ! LifecycleStateSubscription(Subscribe(supervisorStateProbe.ref))
+      supervisorStateProbe.expectMsgType[LifecycleStateChanged].state shouldBe SupervisorMode.Running
       enterBarrier("running")
 
       enterBarrier("offline")
       Thread.sleep(50)
-      supervisorRef ! GetSupervisorMode(supervisorStateProbe.ref)
-      supervisorStateProbe.expectMsg(SupervisorMode.RunningOffline)
+      val supervisorModeProbe = TestProbe[SupervisorMode]
+      supervisorRef ! GetSupervisorMode(supervisorModeProbe.ref)
+      supervisorModeProbe.expectMsg(SupervisorMode.RunningOffline)
     }
 
     enterBarrier("end")
