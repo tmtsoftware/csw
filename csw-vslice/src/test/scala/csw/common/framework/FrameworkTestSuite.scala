6be63cf4091bc711faf367106e7791733c4f72a4
Nishant Vishwakarma <nishantv12@gmail.com>
2017-09-01 13:31:16 +0530

    #177 : Nishant : Move TestMocks class in a different file

--- csw-vslice/src/test/scala/csw/common/framework/FrameworkTestSuite.scala
+++ csw-vslice/src/test/scala/csw/common/framework/FrameworkTestSuite.scala
@@ -1,64 +1,20 @@
 package csw.common.framework
 
-import akka.typed.scaladsl.adapter._
+import akka.actor
 import akka.typed.scaladsl.{Actor, ActorContext}
 import akka.typed.testkit.TestKitSettings
-import akka.typed.testkit.scaladsl.TestProbe
 import akka.typed.{ActorRef, ActorSystem}
 import akka.util.Timeout
-import akka.{actor, testkit, Done}
 import csw.common.components.ComponentDomainMessage
-import csw.common.framework.internal.pubsub.PubSubBehaviorFactory
-import csw.common.framework.internal.supervisor.SupervisorBehavior
 import csw.common.framework.models.PubSub.PublisherMessage
 import csw.common.framework.models._
 import csw.common.framework.scaladsl.{ComponentBehaviorFactory, ComponentHandlers}
 import csw.param.states.CurrentState
-import csw.services.location.models.Connection.AkkaConnection
-import csw.services.location.models.{AkkaRegistration, RegistrationResult}
-import csw.services.location.scaladsl.{ActorSystemFactory, LocationService, RegistrationFactory}
-import org.mockito.ArgumentMatchers
-import org.mockito.Mockito._
-import org.scalatest.mockito.MockitoSugar
+import csw.services.location.scaladsl.ActorSystemFactory
 import org.scalatest.{BeforeAndAfterAll, FunSuite, Matchers}
 
+import scala.concurrent.Await
 import scala.concurrent.duration.DurationLong
-import scala.concurrent.{Await, Future, Promise}
-
-class TestMocks(
-    implicit untypedSystem: actor.ActorSystem,
-    system: ActorSystem[Nothing],
-    settings: TestKitSettings
-) extends MockitoSugar {
-  val akkaRegistration                                              = AkkaRegistration(mock[AkkaConnection], testkit.TestProbe("test-probe").testActor)
-  val locationService: LocationService                              = mock[LocationService]
-  val registrationResult: RegistrationResult                        = mock[RegistrationResult]
-  val registrationFactory: RegistrationFactory                      = mock[RegistrationFactory]
-  val eventualDone: Future[Done]                                    = Promise[Done].success(Done).future
-  val pubSubBehaviorFactory: PubSubBehaviorFactory                  = mock[PubSubBehaviorFactory]
-  val lifecycleStateProbe: TestProbe[PubSub[LifecycleStateChanged]] = TestProbe[PubSub[LifecycleStateChanged]]
-  val compStateProbe: TestProbe[PubSub[CurrentState]]               = TestProbe[PubSub[CurrentState]]
-  val eventualRegistrationResult: Future[RegistrationResult] =
-    Promise[RegistrationResult].success(registrationResult).future
-
-  when(registrationFactory.akkaTyped(ArgumentMatchers.any[AkkaConnection], ArgumentMatchers.any[ActorRef[_]]))
-    .thenReturn(akkaRegistration)
-  when(locationService.register(akkaRegistration)).thenReturn(eventualRegistrationResult)
-  when(registrationResult.unregister()).thenReturn(eventualDone)
-  when(
-    pubSubBehaviorFactory.make[LifecycleStateChanged](
-      ArgumentMatchers.any[ActorContext[SupervisorMessage]],
-      ArgumentMatchers.eq(SupervisorBehavior.PubSubLifecycleActor)
-    )
-  ).thenReturn(lifecycleStateProbe.ref)
-  when(
-    pubSubBehaviorFactory.make[CurrentState](
-      ArgumentMatchers.any[ActorContext[SupervisorMessage]],
-      ArgumentMatchers.eq(SupervisorBehavior.PubSubComponentActor)
-    )
-  ).thenReturn(compStateProbe.ref)
-
-}
 
 abstract class FrameworkTestSuite extends FunSuite with Matchers with BeforeAndAfterAll {
   implicit val untypedSystem: actor.ActorSystem = ActorSystemFactory.remote()
---
+++ csw-vslice/src/test/scala/csw/common/framework/TestMocks.scala
@@ -0,0 +1,55 @@
+package csw.common.framework
+
+import akka.typed.scaladsl.ActorContext
+import akka.typed.scaladsl.adapter._
+import akka.typed.testkit.TestKitSettings
+import akka.typed.testkit.scaladsl.TestProbe
+import akka.typed.{ActorRef, ActorSystem}
+import akka.{actor, testkit, Done}
+import csw.common.framework.internal.pubsub.PubSubBehaviorFactory
+import csw.common.framework.internal.supervisor.SupervisorBehavior
+import csw.common.framework.models.{LifecycleStateChanged, PubSub, SupervisorMessage}
+import csw.param.states.CurrentState
+import csw.services.location.models.Connection.AkkaConnection
+import csw.services.location.models.{AkkaRegistration, RegistrationResult}
+import csw.services.location.scaladsl.{LocationService, RegistrationFactory}
+import org.mockito.ArgumentMatchers
+import org.mockito.Mockito.when
+import org.scalatest.mockito.MockitoSugar
+
+import scala.concurrent.{Future, Promise}
+
+class TestMocks(
+    implicit untypedSystem: actor.ActorSystem,
+    system: ActorSystem[Nothing],
+    settings: TestKitSettings
+) extends MockitoSugar {
+  val akkaRegistration                                              = AkkaRegistration(mock[AkkaConnection], testkit.TestProbe("test-probe").testActor)
+  val locationService: LocationService                              = mock[LocationService]
+  val registrationResult: RegistrationResult                        = mock[RegistrationResult]
+  val registrationFactory: RegistrationFactory                      = mock[RegistrationFactory]
+  val eventualDone: Future[Done]                                    = Promise[Done].success(Done).future
+  val pubSubBehaviorFactory: PubSubBehaviorFactory                  = mock[PubSubBehaviorFactory]
+  val lifecycleStateProbe: TestProbe[PubSub[LifecycleStateChanged]] = TestProbe[PubSub[LifecycleStateChanged]]
+  val compStateProbe: TestProbe[PubSub[CurrentState]]               = TestProbe[PubSub[CurrentState]]
+  val eventualRegistrationResult: Future[RegistrationResult] =
+    Promise[RegistrationResult].success(registrationResult).future
+
+  when(registrationFactory.akkaTyped(ArgumentMatchers.any[AkkaConnection], ArgumentMatchers.any[ActorRef[_]]))
+    .thenReturn(akkaRegistration)
+  when(locationService.register(akkaRegistration)).thenReturn(eventualRegistrationResult)
+  when(registrationResult.unregister()).thenReturn(eventualDone)
+  when(
+    pubSubBehaviorFactory.make[LifecycleStateChanged](
+      ArgumentMatchers.any[ActorContext[SupervisorMessage]],
+      ArgumentMatchers.eq(SupervisorBehavior.PubSubLifecycleActor)
+    )
+  ).thenReturn(lifecycleStateProbe.ref)
+  when(
+    pubSubBehaviorFactory.make[CurrentState](
+      ArgumentMatchers.any[ActorContext[SupervisorMessage]],
+      ArgumentMatchers.eq(SupervisorBehavior.PubSubComponentActor)
+    )
+  ).thenReturn(compStateProbe.ref)
+
+}
