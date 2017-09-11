3a18d373a876e44eba04adefa158065b81ac9c4e
Nishant Vishwakarma <nishantv12@gmail.com>
2017-09-11 17:53:13 +0530

    #171 : Nishant : Create a guardian actor which will create typed actors from untyped actor system

--- csw-framework/src/main/scala/csw/common/framework/internal/extensions/RichSystemExtension.scala
+++ csw-framework/src/main/scala/csw/common/framework/internal/extensions/RichSystemExtension.scala
@@ -1,8 +1,7 @@
 package csw.common.framework.internal.extensions
 
 import akka.actor.{ActorSystem, Scheduler}
-import akka.typed.internal.ActorSystemImpl
-import akka.typed.internal.ActorSystemImpl.CreateSystemActor
+import akka.typed.scaladsl.Actor
 import akka.typed.scaladsl.AskPattern.Askable
 import akka.typed.scaladsl.adapter.UntypedActorSystemOps
 import akka.typed.{ActorRef, Behavior, Props}
@@ -14,11 +13,21 @@ import scala.concurrent.duration.DurationLong
 object RichSystemExtension {
   implicit val timeout: Timeout = Timeout(2.seconds)
 
+  sealed trait GuardianBehaviorMsg
+  case class CreateActor[T](behavior: Behavior[T], name: String, props: Props)(val replyTo: ActorRef[ActorRef[T]])
+      extends GuardianBehaviorMsg
+
+  def behavior: Behavior[GuardianBehaviorMsg] = Actor.immutable {
+    case (ctx, create: CreateActor[t]) ⇒
+      create.replyTo ! ctx.spawn(create.behavior, create.name, create.props)
+      Actor.same
+  }
+
   class RichSystem(val system: ActorSystem) {
     implicit def sched: Scheduler = system.scheduler
-    private val rootActor         = system.spawn(ActorSystemImpl.systemGuardianBehavior, "system")
+    private val rootActor         = system.spawn(behavior, "system")
     def spawnTyped[T](behavior: Behavior[T], name: String, props: Props = Props.empty): Future[ActorRef[T]] = {
-      rootActor ? CreateSystemActor(behavior, name, props)
+      rootActor ? CreateActor(behavior, name, props)
     }
   }
 }
