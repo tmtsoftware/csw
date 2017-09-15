0b9e3c2b3a7a8d7942541bd4b5ad5330db6a8bb3
Mahendra Kulkarni <mahendra.pk@gmail.com>
2017-09-15 09:41:59 +0530

    missing types

--- csw-params/src/main/scala/csw/param/pb/ItemType.scala
+++ csw-params/src/main/scala/csw/param/pb/ItemType.scala
@@ -11,9 +11,3 @@ trait ItemType[T, S <: ItemType[T, S]] extends GeneratedMessage with Message[S] 
   def keyType(implicit tag: ClassTag[T]): KeyType[T] = KeyType.values.find(_.tag == tag).get.asInstanceOf[KeyType[T]]
   def as[R](implicit mapper: TypeMapper[S, R]): R    = mapper.toCustom(this.asInstanceOf[S])
 }
-
-//trait ItemTypeCompanion {
-//  def from[T](_values: Seq[T]): ItemType[T] = new ItemType[T] {
-//    override def values: Seq[T] = _values
-//  }
-//}
