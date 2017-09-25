35f6e7312a6fcf3a161fce4001e69d393eefe2ce
Nishant Vishwakarma <nishantv12@gmail.com>
2017-09-25 16:17:58 +0530

    #199 : Nishant | Mahendra : Replace println with assert in ObsIdTest

--- csw-params/src/test/scala/csw/param/models/ObsIdTest.scala
+++ csw-params/src/test/scala/csw/param/models/ObsIdTest.scala
@@ -1,38 +1,41 @@
 package csw.param.models
 
-import org.scalatest.FunSpec
+import org.scalatest.prop.TableDrivenPropertyChecks._
+import org.scalatest.prop.TableFor2
+import org.scalatest.prop.Tables.Table
+import org.scalatest.{FunSuite, Matchers}
 
-class ObsIdTest extends FunSpec {
+import scala.util.matching.Regex
+
+class ObsIdTest extends FunSuite with Matchers {
 
   val obsID1 = "2022A-Q-P012-O123"     // without file
   val obsID2 = "2022A-Q-P012-O123-234" // with file
 
-  describe("RegEx") {
+  val testData: TableFor2[String, ObsId2] = Table(
+    ("obsId_String", "obsId_model"),
+    ("2022A-Q-P012-O123", ObsId2("2022", "A", "Q", "012", "123", None)),
+    ("2022A-Q-P012-O123-234", ObsId2("2022", "A", "Q", "012", "123", Some("234")))
+  )
+
+  def parseToObsId(str: String, reg: Regex): Option[ObsId2] = str match {
+    case reg(year, sem, kind, prog, obs, null) => Some(ObsId2(year, sem, kind, prog, obs, None))
+    case reg(year, sem, kind, prog, obs, file) => Some(ObsId2(year, sem, kind, prog, obs, Some(file)))
+    case _                                     => None
+  }
+
+  test("RegEx") {
     //YYYY(A|B|E)-(Q|C)-PXXX-OXXX-XXXX
     val reg = """(\d{4})(A|B|E)-(C|Q)-P(\d{1,3})-O(\d{1,3})(?:-)?(\d+)?""".r
 
-    "2022A-Q-P012-O123" match {
-      case reg(year, sem, kind, prog, obs, null) => {
-        println(s"Its: $year $sem with $kind and prog $prog, obs $obs")
-        println("Kind: " + kind.getClass)
-      }
-      case reg(year, sem, kind, prog, obs, file) =>
-        println(s"Its: $year $sem with $kind and prog $prog, obs $obs, file $file")
-      case _ => println("Fail")
-    }
-    "2022A-Q-P012-O123-234" match {
-      case reg(year, sem, kind, prog, obs, null) => println(s"Its: $year $sem with $kind and prog $prog, obs $obs")
-      case reg(year, sem, kind, prog, obs, file) =>
-        println(s"Its: $year $sem with $kind and prog $prog, obs $obs, file $file")
-      case _ => println("Fail")
+    forAll(testData) { (obsId: String, obsId2: ObsId2) ⇒
+      parseToObsId(obsId, reg).get shouldBe obsId2
     }
   }
 
-  describe("Basdic ObsID Test") {
-
+  test("Basdic ObsID Test") {
     val ob1: ObsId2 = obsID1
-
-    println("OB1: " + ob1)
+    assert(ob1 == ObsId2("2022", "A", "Q", "012", "123", None))
   }
 
 }
