89c44b0ebba780efcd1379ecb964743186887e22
Nishant Vishwakarma <nishantv12@gmail.com>
2017-10-09 15:44:25 +0530

    Nishant | Mahendra : Match current and demand state using streams

--- csw-vslice/src/main/scala/csw/trombone/assembly/Matchers.scala
+++ csw-vslice/src/main/scala/csw/trombone/assembly/Matchers.scala
@@ -1,18 +1,23 @@
 package csw.trombone.assembly
 
 import akka.actor.Scheduler
+import akka.stream.scaladsl.{Sink, Source}
+import akka.stream.{ActorMaterializer, OverflowStrategy}
 import akka.typed.ActorRef
 import akka.typed.scaladsl.ActorContext
 import akka.typed.scaladsl.AskPattern._
+import akka.typed.scaladsl.adapter._
 import akka.util.Timeout
 import csw.ccs.MultiStateMatcherMsgs.StartMatch
 import csw.ccs._
+import csw.messages.PubSub.Subscribe
 import csw.messages.params.states.{CurrentState, DemandState}
-import csw.messages.{CommandExecutionResponse, PubSub}
+import csw.messages.{CommandExecutionResponse, Completed, Error, PubSub}
 import csw.trombone.hcd.TromboneHcdState
 
 import scala.concurrent.Future
 import scala.concurrent.duration.DurationLong
+import scala.util.control.NonFatal
 
 object Matchers {
 
@@ -50,19 +55,27 @@ object Matchers {
     }
   }
 
-  def matchState(
-      ctx: ActorContext[_],
-      stateMatcher: StateMatcher,
-      currentStateSource: ActorRef[PubSub[CurrentState]],
-      timeout: Timeout = Timeout(5.seconds)
-  ): Future[CommandExecutionResponse] = {
-    implicit val t: Timeout           = Timeout(timeout.duration + 1.seconds)
-    implicit val scheduler: Scheduler = ctx.system.scheduler
+  def matchState(ctx: ActorContext[_],
+                 stateMatcher: StateMatcher,
+                 currentStateSource: ActorRef[PubSub[CurrentState]],
+                 timeout: Timeout = Timeout(5.seconds)): Future[CommandExecutionResponse] = {
 
-    val matcher: ActorRef[MultiStateMatcherMsgs.WaitingMsg] =
-      ctx.spawnAnonymous(MultiStateMatcherActor.make(currentStateSource, timeout))
-    matcher ? { x: ActorRef[CommandExecutionResponse] ⇒
-      StartMatch(x, stateMatcher)
-    }
+    import ctx.executionContext
+    implicit val mat = ActorMaterializer()(ctx.system.toUntyped)
+
+    val source = Source
+      .actorRef[CurrentState](256, OverflowStrategy.fail)
+      .mapMaterializedValue { ref ⇒
+        currentStateSource ! Subscribe[CurrentState](ref)
+      }
+      .filter(cs ⇒ cs.prefixStr == stateMatcher.prefix && stateMatcher.check(cs))
+      .completionTimeout(timeout.duration)
+
+    source
+      .runWith(Sink.head)
+      .map(_ ⇒ Completed)
+      .recover {
+        case NonFatal(ex) ⇒ Error("")
+      }
   }
 }
