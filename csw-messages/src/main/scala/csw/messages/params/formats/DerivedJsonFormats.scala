abb3070f36888aa61a7d2195e1b38eb95893444e
Mahendra Kulkarni <mahendra.pk@gmail.com>
2017-11-02 15:43:47 +0530

    #307 - Mahendra - Simplified JsonFormats in csw-messages.

--- csw-messages/src/main/scala/csw/messages/params/formats/DerivedJsonFormats.scala
+++ csw-messages/src/main/scala/csw/messages/params/formats/DerivedJsonFormats.scala
@@ -7,10 +7,13 @@ import play.api.libs.json._
 
 trait DerivedJsonFormats { self ⇒
 
+  private def formatFactory[S: Writes: Reads, J]: Format[J] =
+    Format(implicitly[Reads[S]].asInstanceOf[Reads[J]], implicitly[Writes[S]].asInstanceOf[Writes[J]])
+
   //scala
   implicit val charFormat: Format[Char] = new Format[Char] {
     override def reads(json: JsValue): JsResult[Char] = json match {
-      case JsString(str) if str.length == 1 => JsSuccess(str.charAt(0))
+      case JsString(str) if str.length == 1 => JsSuccess(str.head)
       case _                                => JsError("error.expected.char")
     }
 
@@ -18,43 +21,21 @@ trait DerivedJsonFormats { self ⇒
   }
 
   //java
+  implicit val booleanFormat: Format[lang.Boolean] = formatFactory[Boolean, java.lang.Boolean]
   implicit val characterFormat: Format[lang.Character] = new Format[lang.Character] {
     override def reads(json: JsValue): JsResult[lang.Character] = json match {
-      case JsString(str) if str.length == 1 => JsSuccess(str.charAt(0))
+      case JsString(str) if str.length == 1 => JsSuccess(str.head)
       case _                                => JsError("error.expected.char")
     }
 
     override def writes(o: lang.Character): JsValue = JsString(o.toString)
   }
-
-  implicit val booleanWrites: Writes[java.lang.Boolean] =
-    implicitly[Writes[Boolean]].asInstanceOf[Writes[java.lang.Boolean]]
-  implicit val booleanReads: Reads[java.lang.Boolean] =
-    implicitly[Reads[Boolean]].asInstanceOf[Reads[java.lang.Boolean]]
-
-  implicit val characterWrites: Writes[java.lang.Character] = implicitly[Writes[java.lang.Character]]
-  implicit val characterReads: Reads[java.lang.Character]   = implicitly[Reads[java.lang.Character]]
-
-  implicit val byteWrites: Writes[java.lang.Byte] = implicitly[Writes[Byte]].asInstanceOf[Writes[java.lang.Byte]]
-  implicit val byteReads: Reads[java.lang.Byte]   = implicitly[Reads[Byte]].asInstanceOf[Reads[java.lang.Byte]]
-
-  implicit val shortWrites: Writes[java.lang.Short] = implicitly[Writes[Short]].asInstanceOf[Writes[java.lang.Short]]
-  implicit val shortReads: Reads[java.lang.Short]   = implicitly[Reads[Short]].asInstanceOf[Reads[java.lang.Short]]
-
-  implicit val longWrites: Writes[java.lang.Long] = implicitly[Writes[Long]].asInstanceOf[Writes[java.lang.Long]]
-  implicit val longReads: Reads[java.lang.Long]   = implicitly[Reads[Long]].asInstanceOf[Reads[java.lang.Long]]
-
-  implicit val integerWrites: Writes[java.lang.Integer] =
-    implicitly[Writes[Int]].asInstanceOf[Writes[java.lang.Integer]]
-  implicit val integerReads: Reads[java.lang.Integer] = implicitly[Reads[Int]].asInstanceOf[Reads[java.lang.Integer]]
-
-  implicit val floatWrites: Writes[java.lang.Float] = implicitly[Writes[Float]].asInstanceOf[Writes[java.lang.Float]]
-  implicit val floatReads: Reads[java.lang.Float]   = implicitly[Reads[Float]].asInstanceOf[Reads[java.lang.Float]]
-
-  implicit val doubleWrites: Writes[java.lang.Double] =
-    implicitly[Writes[Double]].asInstanceOf[Writes[java.lang.Double]]
-  implicit val doubleReads: Reads[java.lang.Double] = implicitly[Reads[Double]].asInstanceOf[Reads[java.lang.Double]]
-
+  implicit val byteFormat: Format[lang.Byte]       = formatFactory[Byte, java.lang.Byte]
+  implicit val shortFormat: Format[lang.Short]     = formatFactory[Short, java.lang.Short]
+  implicit val longFormat: Format[lang.Long]       = formatFactory[Long, java.lang.Long]
+  implicit val integerFormat: Format[lang.Integer] = formatFactory[Int, java.lang.Integer]
+  implicit val floatFormat: Format[lang.Float]     = formatFactory[Float, java.lang.Float]
+  implicit val doubleFormat: Format[lang.Double]   = formatFactory[Double, java.lang.Double]
   implicit val timestampFormat: Format[Instant] = new Format[Instant] {
     override def reads(json: JsValue): JsResult[Instant] = JsSuccess(Instant.parse(json.as[String]))
     override def writes(instant: Instant): JsValue       = JsString(instant.toString)
