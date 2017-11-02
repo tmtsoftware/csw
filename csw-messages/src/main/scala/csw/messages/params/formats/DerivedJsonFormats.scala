4f3ca9b6415af9f3bd7f084ac976ac7cffc82e92
Mahendra Kulkarni <mahendra.pk@gmail.com>
2017-11-02 14:21:49 +0530

    #307 - Mahendra - csw-messages code cleanup.

--- csw-messages/src/main/scala/csw/messages/params/formats/DerivedJsonFormats.scala
+++ csw-messages/src/main/scala/csw/messages/params/formats/DerivedJsonFormats.scala
@@ -5,22 +5,9 @@ import java.time.Instant
 
 import play.api.libs.json._
 
-trait JavaFormats { self ⇒
-  //JSON Formats for Java by converting scala types to java types.
-//  implicit val booleanFormat: JsonFormat[lang.Boolean] = BooleanJsonFormat.asInstanceOf[JsonFormat[java.lang.Boolean]]
-//  implicit val characterFormat: JsonFormat[lang.Character] =
-//    CharJsonFormat.asInstanceOf[JsonFormat[java.lang.Character]]
-//  implicit val byteFormat: JsonFormat[lang.Byte]     = ByteJsonFormat.asInstanceOf[JsonFormat[java.lang.Byte]]
-//  implicit val shortFormat: JsonFormat[lang.Short]   = ShortJsonFormat.asInstanceOf[JsonFormat[java.lang.Short]]
-//  implicit val longFormat: JsonFormat[lang.Long]     = LongJsonFormat.asInstanceOf[JsonFormat[java.lang.Long]]
-//  implicit val integerFormat: JsonFormat[Integer]    = IntJsonFormat.asInstanceOf[JsonFormat[java.lang.Integer]]
-//  implicit val floatFormat: JsonFormat[lang.Float]   = FloatJsonFormat.asInstanceOf[JsonFormat[java.lang.Float]]
-//  implicit val doubleFormat: JsonFormat[lang.Double] = DoubleJsonFormat.asInstanceOf[JsonFormat[java.lang.Double]]
-//  implicit val timestampFormat: JsonFormat[java.time.Instant] = new JsonFormat[Instant] {
-//    override def write(obj: Instant): JsValue = JsString(obj.toString)
-//    override def read(json: JsValue): Instant = Instant.parse(json.convertTo[String])
-//  }
+trait DerivedJsonFormats { self ⇒
 
+  //scala
   implicit val charFormat: Format[Char] = new Format[Char] {
     override def reads(json: JsValue): JsResult[Char] = json match {
       case JsString(str) if str.length == 1 => JsSuccess(str.charAt(0))
@@ -30,7 +17,8 @@ trait JavaFormats { self ⇒
     override def writes(o: Char): JsValue = JsString(o.toString)
   }
 
-  implicit val JCharFormat: Format[lang.Character] = new Format[lang.Character] {
+  //java
+  implicit val characterFormat: Format[lang.Character] = new Format[lang.Character] {
     override def reads(json: JsValue): JsResult[lang.Character] = json match {
       case JsString(str) if str.length == 1 => JsSuccess(str.charAt(0))
       case _                                => JsError("error.expected.char")
