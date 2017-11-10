a111bdde06173c5a2c8667b05ba6f8070aa4c2a5
Nishant Vishwakarma <nishantv12@gmail.com>
2017-11-10 10:21:27 +0530

    #207 : Nishant : Remove Automatic response to sender. Only subscribers will receive automatic response

--- csw-command/src/main/scala/csw/ccs/internal/CommandStatusService.scala
+++ csw-command/src/main/scala/csw/ccs/internal/CommandStatusService.scala
@@ -20,28 +20,20 @@ class CommandStatusService(
     msg match {
       case Query(runId, replyTo)          ⇒ replyTo ! commandStatus.get(runId)
       case Add(runId, replyTo)            ⇒ commandStatus.add(runId, replyTo)
-      case UpdateCommand(commandResponse) ⇒ commandStatus.updateCommandStatus(commandResponse)
+      case UpdateCommand(commandResponse) ⇒ updateCommandStatus(commandResponse)
       case Subscribe(runId, replyTo)      ⇒ commandStatus.subscribe(runId, replyTo)
       case UnSubscribe(runId, replyTo)    ⇒ commandStatus.unSubscribe(runId, replyTo)
     }
     this
   }
 
-  def updateCommandStatus(runId: RunId, commandResponse: CommandResponse): Unit = {
-    updateCommandStatus(runId, commandResponse)
-    publishFinalStateToSender(runId, commandResponse)
-    publishToSubscribers(runId)
+  def updateCommandStatus(commandResponse: CommandResponse): Unit = {
+    commandStatus.updateCommandStatus(commandResponse)
+    publishToSubscribers(commandResponse.runId)
   }
 
   private def publishToSubscribers(runId: RunId): Unit = {
     val commandState = commandStatus.cmdToCmdStatus(runId)
     commandState.subscribers.foreach(_ ! commandState.commandStatus.currentCmdStatus)
-  }
-
-  private def publishFinalStateToSender(runId: RunId, commandResponse: CommandResponse): Unit = {
-    if (commandResponse.resultType.isInstanceOf[CommandResultType.Final]) {
-      val commandState = commandStatus.cmdToCmdStatus(runId)
-      commandState.replyTo ! commandState.commandStatus.currentCmdStatus
-    }
   }
 }
