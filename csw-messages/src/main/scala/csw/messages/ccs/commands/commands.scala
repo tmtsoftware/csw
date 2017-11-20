47e4d8c4cb317fccf99b8bf8406885c095c9e16c
Saloni Vithalani <skvithalani@gmail.com>
2017-11-20 18:09:03 +0530

    #222 : Saloni : Refactor Command to remove paramSetType

--- csw-messages/src/main/scala/csw/messages/ccs/commands/commands.scala
+++ csw-messages/src/main/scala/csw/messages/ccs/commands/commands.scala
@@ -8,8 +8,6 @@ import csw.messages.params.models.{ObsId, Prefix, RunId}
  */
 sealed trait Command { self: ParameterSetType[_] ⇒
 
-  def paramSetType(): ParameterSetType[_] = self
-
   /**
    * A name identifying the type of parameter set, such as "setup", "observe".
    * This is used in the JSON and toString output.
@@ -47,7 +45,7 @@ sealed trait SequenceCommand extends Command { self: ParameterSetType[_] ⇒
  * Marker trait for control parameter sets
  */
 sealed trait ControlCommand extends Command { self: ParameterSetType[_] ⇒
-  def getLockToken: Option[String] = paramSetType().get(LockToken.key) match {
+  def getLockToken: Option[String] = self.get(LockToken.key) match {
     case Some(tokenParam) ⇒ tokenParam.get(0)
     case None             ⇒ None
   }
@@ -104,48 +102,6 @@ object Setup {
  * @param prefix identifies the target subsystem
  * @param paramSet an optional initial set of parameters (keys with values)
  */
-case class Wait private (
-    runId: RunId,
-    obsId: ObsId,
-    prefix: Prefix,
-    paramSet: Set[Parameter[_]] = Set.empty
-) extends ParameterSetType[Wait]
-    with ParameterSetKeyData
-    with SequenceCommand {
-
-  override protected def create(data: Set[Parameter[_]]) = new Wait(runId, obsId, prefix, data)
-
-  // This is here for Java to construct with String
-  def this(obsId: ObsId, prefix: String) = this(RunId(), obsId, Prefix(prefix))
-}
-
-object Wait {
-
-  // The default apply method is used only internally while reading the incoming json and de-serializing it to observe model
-  private[messages] def apply(
-      runId: RunId,
-      obsId: ObsId,
-      prefix: Prefix,
-      paramSet: Set[Parameter[_]]
-  ): Wait =
-    new Wait(runId, obsId, prefix, paramSet) //madd is not required as this version of apply is only used for reading json
-
-  // The apply method is used to create Observe command by end-user. RunId is not accepted and will be created internally to guarantee unique value.
-  def apply(
-      obsId: ObsId,
-      prefix: Prefix,
-      paramSet: Set[Parameter[_]] = Set.empty
-  ): Wait = new Wait(RunId(), obsId, prefix).madd(paramSet) //madd ensures check for duplicate key
-}
-
-/**
- * A parameter set for setting telescope and instrument parameters. Constructor is private to ensure RunId is created internally to guarantee unique value.
- *
- * @param runId unique ID for this parameter set
- * @param obsId the observation id
- * @param prefix identifies the target subsystem
- * @param paramSet an optional initial set of parameters (keys with values)
- */
 case class Observe private (
     runId: RunId,
     obsId: ObsId,
@@ -180,4 +136,46 @@ object Observe {
       paramSet: Set[Parameter[_]] = Set.empty
   ): Observe =
     new Observe(RunId(), obsId, prefix).madd(paramSet) //madd ensures check for duplicate key
+}
+
+/**
+ * A parameter set for setting telescope and instrument parameters. Constructor is private to ensure RunId is created internally to guarantee unique value.
+ *
+ * @param runId unique ID for this parameter set
+ * @param obsId the observation id
+ * @param prefix identifies the target subsystem
+ * @param paramSet an optional initial set of parameters (keys with values)
+ */
+case class Wait private (
+    runId: RunId,
+    obsId: ObsId,
+    prefix: Prefix,
+    paramSet: Set[Parameter[_]] = Set.empty
+) extends ParameterSetType[Wait]
+    with ParameterSetKeyData
+    with SequenceCommand {
+
+  override protected def create(data: Set[Parameter[_]]) = new Wait(runId, obsId, prefix, data)
+
+  // This is here for Java to construct with String
+  def this(obsId: ObsId, prefix: String) = this(RunId(), obsId, Prefix(prefix))
+}
+
+object Wait {
+
+  // The default apply method is used only internally while reading the incoming json and de-serializing it to observe model
+  private[messages] def apply(
+      runId: RunId,
+      obsId: ObsId,
+      prefix: Prefix,
+      paramSet: Set[Parameter[_]]
+  ): Wait =
+    new Wait(runId, obsId, prefix, paramSet) //madd is not required as this version of apply is only used for reading json
+
+  // The apply method is used to create Observe command by end-user. RunId is not accepted and will be created internally to guarantee unique value.
+  def apply(
+      obsId: ObsId,
+      prefix: Prefix,
+      paramSet: Set[Parameter[_]] = Set.empty
+  ): Wait = new Wait(RunId(), obsId, prefix).madd(paramSet) //madd ensures check for duplicate key
 }
