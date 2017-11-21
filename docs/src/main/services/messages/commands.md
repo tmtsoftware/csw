8e98dc0a3cb1bd06232b26282ae83d1c0a5675ff
Mahendra Kulkarni <mahendra.pk@gmail.com>
2017-11-21 15:11:47 +0530

    #309 - Mahendra - Examples for Json serialization and Unique Key constraint.

--- docs/src/main/services/messages/commands.md
+++ docs/src/main/services/messages/commands.md
@@ -7,26 +7,56 @@ It is an observation Id and can be constructed by creating instance of `ObsId`.
 Scala
 :   @@snip [KeysAndParametersTest.scala](../../../../../examples/src/test/scala/csw/services/messages/CommandsTest.scala) { #obsid }
 
-### Setup
+### Prefix
+
+It is a combination of [Subsystem](subsystem.html) and Subsystem's prefix. Component developer should supply a valid prefix string and the subsystem will be automatically parsed from it. 
+
+See below examples:
+
+Scala
+:   @@snip [KeysAndParametersTest.scala](../../../../../examples/src/test/scala/csw/services/messages/CommandsTest.scala) { #prefix }
+
+### Setup Command
 
 This command is used to describe a goal that a system should match. Component developer will require to supply following arguments to create `Setup` command.
 
  * **[ObsId:](commands.html#ObsId)**  An observation Id.
- * **Prefix:** It is a combination of [Subsystem](subsystem.html) and Subsystem's prefix.
- * **paramSet:** Set of Parameters.
+ * **Prefix:**
+ * **paramSet:** Optional Set of Parameters. Default is empty.
  
 Scala
 :   @@snip [KeysAndParametersTest.scala](../../../../../examples/src/test/scala/csw/services/messages/CommandsTest.scala) { #setup }
  
  
-### Observe
+### Observe Command
 This command describes a science observation. Sent only to Science Detector Assemblies and Sequencers.
 
 Scala
 :   @@snip [KeysAndParametersTest.scala](../../../../../examples/src/test/scala/csw/services/messages/CommandsTest.scala) { #observe }
 
-### Wait
+### Wait Command
 This command causes a Sequencer to wait until notified.
 
 Scala
 :   @@snip [KeysAndParametersTest.scala](../../../../../examples/src/test/scala/csw/services/messages/CommandsTest.scala) { #wait }
+
+### JSON serialization
+Commands can be serialized in JSON. The library has provided JsonSupport helper class and methods to serialize Setup, Observe and Wait commands.
+
+Scala
+:   @@snip [KeysAndParametersTest.scala](../../../../../examples/src/test/scala/csw/services/messages/CommandsTest.scala) { #json-serialization }
+
+### Unique Key constraint
+
+By choice, a ParameterSet in either **Setup, Observe,** or **Wait** command will be optimized to store only unique keys. In other words, trying to store multiple keys with same name, will be automatically optimized by removing duplicates.
+
+@@@ note
+
+Parameters are stored in a Set, which is an unordered collection of items. Hence, it's not predictable whether first or last duplicate copy will be retained. Hence, cautiously avoid adding duplicate keys.
+
+@@@    
+
+Here are some examples that illustrate this point:
+
+Scala
+:   @@snip [KeysAndParametersTest.scala](../../../../../examples/src/test/scala/csw/services/messages/CommandsTest.scala) { #unique-key }
--- examples/src/test/scala/csw/services/messages/CommandsTest.scala
+++ examples/src/test/scala/csw/services/messages/CommandsTest.scala
@@ -1,172 +1,314 @@
 package csw.services.messages
 
-import java.io
 import java.time.Instant
 
 import csw.messages.ccs.commands.{Observe, Setup, Wait}
-import csw.messages.params.generics.KeyType.ByteKey
+import csw.messages.params.formats.JsonSupport
+import csw.messages.params.generics.KeyType.{ByteKey, DoubleMatrixKey}
 import csw.messages.params.generics.{Key, KeyType, Parameter}
-import csw.messages.params.models.{ObsId, Prefix, Units}
-import org.scalatest.FunSpec
+import csw.messages.params.models._
+import org.scalatest.{FunSpec, Matchers}
 
 import scala.util.Try
 
-class CommandsTest extends FunSpec {
+class CommandsTest extends FunSpec with Matchers {
 
   //#obsid
   val obsId: ObsId = ObsId("Obs001")
   //#obsid
 
-  it("Should show usage of Setup command") {
-    //#setup
-    //keys
-    val k1: Key[Int]    = KeyType.IntKey.make("encoder")
-    val k2: Key[String] = KeyType.StringKey.make("stringThing")
-    val k2bad: Key[Int] = KeyType.IntKey.make("stringThing")
-    val k3: Key[Int]    = KeyType.IntKey.make("filter")
-    val k4: Key[Float]  = KeyType.FloatKey.make("correction")
+  describe("Examples of Prefix") {
+    it("should show usage of utility functions") {
 
-    //prefix
-    val prefix: Prefix = Prefix("wfos.red.detector")
+      //#prefix
+      //using constructor, supplying subsystem and prefix both
+      val prefix1: Prefix = Prefix(Subsystem.NFIRAOS, "nfiraos.ncc.trombone")
 
-    //parameters
-    val i1: Parameter[Int]    = k1.set(22)
-    val i2: Parameter[String] = k2.set("A")
+      //just by supplying prefix
+      val prefix2: Prefix = Prefix("tcs.mobie.blue.filter")
 
-    //create setup, add sequentially using add
-    val sc1: Setup = Setup(obsId, prefix).add(i1).add(i2)
+      //invalid prefix that cant be mapped to a valid subsystem will automatically get Subsystem.BAD
+      val badPrefix: Prefix = Prefix("abcdefgh")
 
-    //access keys
-    val k1Exists: Boolean = sc1.exists(k1) //true
+      //use implicit conversion to convert from String to Prefix
+      val prefix4: Prefix = "wfos.prog.cloudcover"
 
-    //access parameters
-    val tryParam1: Try[Parameter[Int]] = Try(sc1(k1))    //success
-    val tryk2Bad: Try[Parameter[Int]]  = Try(sc1(k2bad)) //failure
+      //use subsystem companion(static for Java) method to get subsystem from prefix string
+      Prefix
 
-    //add more than one parameters, using madd
-    val sc2: Setup     = sc1.madd(k3.set(1, 2, 3, 4).withUnits(Units.day), k4.set(1.0f, 2.0f))
-    val paramSize: Int = sc2.size
+      //#prefix
 
-    //add binary payload
-    val byteKey1: Key[Byte] = ByteKey.make("byteKey1")
-    val byteKey2: Key[Byte] = ByteKey.make("byteKey2")
-    val bytes1: Array[Byte] = Array[Byte](10, 20)
-    val bytes2: Array[Byte] = Array[Byte](30, 40)
-
-    val b1: Parameter[Byte] = byteKey1.set(bytes1)
-    val b2: Parameter[Byte] = byteKey2.set(bytes2)
-
-    val sc3: Setup = Setup(obsId, prefix, Set(b1, b2))
-
-    //remove a key
-    val sc4: Setup = sc3.remove(b1)
-
-    //list all keys
-    val allKeys: Set[String] = sc4.paramSet.map(_.keyName)
-
-    //#setup
-
-    //validations
-    assert(k1Exists)
-    assert(tryParam1.isSuccess)
-    assert(tryk2Bad.isFailure)
-    assert(paramSize === 4)
-    assert(sc3.size === 2)
-    assert(sc4.size === 1)
-    assert(allKeys.size === 1)
+      //validations
+      assert(prefix1.subsystem === Subsystem.NFIRAOS)
+      assert(prefix2.subsystem === Subsystem.TCS)
+      assert(prefix4.subsystem === Subsystem.WFOS)
+      assert(badPrefix.subsystem === Subsystem.BAD)
+    }
   }
 
-  it("Should show usage of Observe command") {
-    //#observe
-    //keys
-    val k1: Key[Boolean] = KeyType.BooleanKey.make("repeat")
-    val k2: Key[Int]     = KeyType.IntKey.make("expTime")
-    val k2bad: Key[Int]  = KeyType.IntKey.make("stringThing")
-    val k3: Key[Int]     = KeyType.IntKey.make("filter")
-    val k4: Key[Instant] = KeyType.TimestampKey.make("creation-time")
+  describe("Examples of commands") {
 
-    //prefix
-    val prefix: Prefix = Prefix("wfos.red.detector")
+    it("Should show usage of Setup command") {
+      //#setup
+      //keys
+      val k1: Key[Int]    = KeyType.IntKey.make("encoder")
+      val k2: Key[String] = KeyType.StringKey.make("stringThing")
+      val k2bad: Key[Int] = KeyType.IntKey.make("stringThing")
+      val k3: Key[Int]    = KeyType.IntKey.make("filter")
+      val k4: Key[Float]  = KeyType.FloatKey.make("correction")
 
-    //parameters
-    val i1: Parameter[Boolean] = k1.set(true, false, true, false)
-    val i2: Parameter[Int]     = k2.set(1, 2, 3, 4)
+      //prefix
+      val prefix: Prefix = Prefix("wfos.red.detector")
 
-    //create observe, add sequentially using add
-    val oc1: Observe = Observe(obsId, prefix).add(i1).add(i2)
+      //parameters
+      val i1: Parameter[Int]    = k1.set(22)
+      val i2: Parameter[String] = k2.set("A")
 
-    //access params using apply method
-    val k1Param: Parameter[Boolean] = oc1(k1) //true
-    val values: Array[Boolean]      = k1Param.values
+      //create setup, add sequentially using add
+      val sc1: Setup = Setup(obsId, prefix).add(i1).add(i2)
 
-    //access parameters
-    val tryParam1: Try[Parameter[Boolean]] = Try(oc1(k1))    //success
-    val tryk2Bad: Try[Parameter[Int]]      = Try(oc1(k2bad)) //failure
+      //access keys
+      val k1Exists: Boolean = sc1.exists(k1) //true
 
-    //add more than one parameters, using madd
-    val oc2: Observe   = oc1.madd(k3.set(1, 2, 3, 4).withUnits(Units.day), k4.set(Instant.now()))
-    val paramSize: Int = oc2.size
+      //access parameters
+      val tryParam1: Try[Parameter[Int]] = Try(sc1(k1))    //success
+      val tryk2Bad: Try[Parameter[Int]]  = Try(sc1(k2bad)) //failure
 
-    //update existing key with set
-    val oc3: Observe = oc1.add(k2.set(5, 6, 7, 8))
+      //add more than one parameters, using madd
+      val sc2: Setup     = sc1.madd(k3.set(1, 2, 3, 4).withUnits(Units.day), k4.set(1.0f, 2.0f))
+      val paramSize: Int = sc2.size
 
-    //remove a key
-    val oc4: Observe = oc2.remove(k4)
+      //add binary payload
+      val byteKey1: Key[Byte] = ByteKey.make("byteKey1")
+      val byteKey2: Key[Byte] = ByteKey.make("byteKey2")
+      val bytes1: Array[Byte] = Array[Byte](10, 20)
+      val bytes2: Array[Byte] = Array[Byte](30, 40)
 
-    //#observe
+      val b1: Parameter[Byte] = byteKey1.set(bytes1)
+      val b2: Parameter[Byte] = byteKey2.set(bytes2)
 
-    //validations
-    assert(tryParam1.isSuccess)
-    assert(tryk2Bad.isFailure)
-    assert(paramSize === 4)
-    assert(oc3(k2).values === Array(5, 6, 7, 8))
-    assert(oc4.size === 3)
+      val sc3: Setup = Setup(obsId, prefix, Set(b1, b2))
+
+      //remove a key
+      val sc4: Setup = sc3.remove(b1)
+
+      //list all keys
+      val allKeys: Set[String] = sc4.paramSet.map(_.keyName)
+
+      //#setup
+
+      //validations
+      assert(k1Exists)
+      assert(tryParam1.isSuccess)
+      assert(tryk2Bad.isFailure)
+      assert(paramSize === 4)
+      assert(sc3.size === 2)
+      assert(sc4.size === 1)
+      assert(allKeys.size === 1)
+    }
+
+    it("Should show usage of Observe command") {
+      //#observe
+      //keys
+      val k1: Key[Boolean] = KeyType.BooleanKey.make("repeat")
+      val k2: Key[Int]     = KeyType.IntKey.make("expTime")
+      val k2bad: Key[Int]  = KeyType.IntKey.make("stringThing")
+      val k3: Key[Int]     = KeyType.IntKey.make("filter")
+      val k4: Key[Instant] = KeyType.TimestampKey.make("creation-time")
+
+      //prefix
+      val prefix: Prefix = Prefix("wfos.red.detector")
+
+      //parameters
+      val i1: Parameter[Boolean] = k1.set(true, false, true, false)
+      val i2: Parameter[Int]     = k2.set(1, 2, 3, 4)
+
+      //create observe, add sequentially using add
+      val oc1: Observe = Observe(obsId, prefix).add(i1).add(i2)
+
+      //access params using apply method
+      val k1Param: Parameter[Boolean] = oc1(k1) //true
+      val values: Array[Boolean]      = k1Param.values
+
+      //access parameters
+      val tryParam1: Try[Parameter[Boolean]] = Try(oc1(k1))    //success
+      val tryk2Bad: Try[Parameter[Int]]      = Try(oc1(k2bad)) //failure
+
+      //add more than one parameters, using madd
+      val oc2: Observe   = oc1.madd(k3.set(1, 2, 3, 4).withUnits(Units.day), k4.set(Instant.now()))
+      val paramSize: Int = oc2.size
+
+      //update existing key with set
+      val oc3: Observe = oc1.add(k2.set(5, 6, 7, 8))
+
+      //remove a key
+      val oc4: Observe = oc2.remove(k4)
+
+      //#observe
+
+      //validations
+      assert(tryParam1.isSuccess)
+      assert(tryk2Bad.isFailure)
+      assert(paramSize === 4)
+      assert(values === Array(true, false, true, false))
+      assert(oc3(k2).values === Array(5, 6, 7, 8))
+      assert(oc4.size === 3)
+    }
+
+    it("Should show usage of Wait command") {
+      //#wait
+      //keys
+      val k1: Key[Boolean] = KeyType.BooleanKey.make("repeat")
+      val k2: Key[Int]     = KeyType.IntKey.make("expTime")
+      val k2bad: Key[Int]  = KeyType.IntKey.make("stringThing")
+      val k3: Key[Int]     = KeyType.IntKey.make("filter")
+      val k4: Key[Instant] = KeyType.TimestampKey.make("creation-time")
+
+      //prefix
+      val prefix: Prefix = Prefix("wfos.red.detector")
+
+      //parameters
+      val i1: Parameter[Boolean] = k1.set(true, false, true, false)
+      val i2: Parameter[Int]     = k2.set(1, 2, 3, 4)
+
+      //create wait, add sequentially using add
+      val wc1: Wait = Wait(obsId, prefix).add(i1).add(i2)
+
+      //access params using get method
+      val k1Param: Option[Parameter[Boolean]] = wc1.get(k1)
+      val values: Array[Boolean]              = k1Param.map(_.values).getOrElse(Array.empty[Boolean])
+
+      //access parameters
+      val tryParam1: Try[Parameter[Boolean]] = Try(wc1(k1))    //success
+      val tryk2Bad: Try[Parameter[Int]]      = Try(wc1(k2bad)) //failure
+
+      //add more than one parameters, using madd
+      val wc2: Wait      = wc1.madd(k3.set(1, 2, 3, 4).withUnits(Units.day), k4.set(Instant.now()))
+      val paramSize: Int = wc2.size
+
+      //update existing key with set
+      val wc3: Wait = wc1.add(k2.set(5, 6, 7, 8))
+
+      //remove a key
+      val wc4: Wait = wc2.remove(k4)
+
+      //#wait
+
+      //validations
+      assert(tryParam1.isSuccess)
+      assert(values === Array(true, false, true, false))
+      assert(tryk2Bad.isFailure)
+      assert(paramSize === 4)
+      assert(wc3(k2).values === Array(5, 6, 7, 8))
+      assert(wc4.size === 3)
+    }
   }
 
-  it("Should show usage of Wait command") {
-    //#wait
-    //keys
-    val k1: Key[Boolean] = KeyType.BooleanKey.make("repeat")
-    val k2: Key[Int]     = KeyType.IntKey.make("expTime")
-    val k2bad: Key[Int]  = KeyType.IntKey.make("stringThing")
-    val k3: Key[Int]     = KeyType.IntKey.make("filter")
-    val k4: Key[Instant] = KeyType.TimestampKey.make("creation-time")
+  describe("Examples of serialization") {
+    it("should show reading and writing of commands") {
 
-    //prefix
-    val prefix: Prefix = Prefix("wfos.red.detector")
+      //#json-serialization
+      import play.api.libs.json.{JsValue, Json}
 
-    //parameters
-    val i1: Parameter[Boolean] = k1.set(true, false, true, false)
-    val i2: Parameter[Int]     = k2.set(1, 2, 3, 4)
+      //key
+      val k1: Key[MatrixData[Double]] = DoubleMatrixKey.make("myMatrix")
+      //values
+      val m1: MatrixData[Double] = MatrixData.fromArrays(
+        Array(1.0, 2.0, 3.0),
+        Array(4.1, 5.1, 6.1),
+        Array(7.2, 8.2, 9.2)
+      )
 
-    //create wait, add sequentially using add
-    val wc1: Wait = Wait(obsId, prefix).add(i1).add(i2)
+      //parameter
+      val i1: Parameter[MatrixData[Double]] = k1.set(m1)
 
-    //access params using get method
-    val k1Param: Option[Parameter[Boolean]] = wc1.get(k1)
-    val values: Array[Boolean]              = k1Param.map(_.values).getOrElse(Array.empty[Boolean])
+      //commands
+      val sc = Setup(obsId, Prefix("wfos.blue.filter")).add(i1)
+      val oc = Observe(obsId, Prefix("wfos.blue.filter")).add(i1)
+      val wc = Wait(obsId, Prefix("wfos.blue.filter")).add(i1)
 
-    //access parameters
-    val tryParam1: Try[Parameter[Boolean]] = Try(wc1(k1))    //success
-    val tryk2Bad: Try[Parameter[Int]]      = Try(wc1(k2bad)) //failure
+      //json support - write
+      val scJson: JsValue = JsonSupport.writeSequenceCommand(sc)
+      val ocJson: JsValue = JsonSupport.writeSequenceCommand(oc)
+      val wcJson: JsValue = JsonSupport.writeSequenceCommand(wc)
 
-    //add more than one parameters, using madd
-    val wc2: Wait      = wc1.madd(k3.set(1, 2, 3, 4).withUnits(Units.day), k4.set(Instant.now()))
-    val paramSize: Int = wc2.size
+      //optionally prettify
+      val str: String = Json.prettyPrint(scJson)
 
-    //update existing key with set
-    val wc3: Wait = wc1.add(k2.set(5, 6, 7, 8))
+      //construct command from string
+      val scFromPrettyStr = JsonSupport.readSequenceCommand[Setup](Json.parse(str))
 
-    //remove a key
-    val wc4: Wait = wc2.remove(k4)
+      //json support - read
+      val sc1: Setup   = JsonSupport.readSequenceCommand[Setup](scJson)
+      val oc1: Observe = JsonSupport.readSequenceCommand[Observe](ocJson)
+      val wc1: Wait    = JsonSupport.readSequenceCommand[Wait](wcJson)
+      //#json-serialization
 
-    //#wait
+      //validations
+      assert(sc === sc1)
+      assert(oc === oc1)
+      assert(wc === wc1)
+      assert(scFromPrettyStr === sc)
+    }
+  }
 
-    //validations
-    assert(tryParam1.isSuccess)
-    assert(tryk2Bad.isFailure)
-    assert(paramSize === 4)
-    assert(wc3(k2).values === Array(5, 6, 7, 8))
-    assert(wc4.size === 3)
+  describe("Examples of unique key constraint") {
+    it("should show duplicate keys are removed") {
+
+      //#unique-key
+
+      //keys
+      val encoderKey: Key[Int] = KeyType.IntKey.make("encoder")
+      val filterKey: Key[Int]  = KeyType.IntKey.make("filter")
+      val miscKey: Key[Int]    = KeyType.IntKey.make("misc.")
+
+      //ObsId
+      val obsId = ObsId("Obs001")
+
+      //prefix
+      val prefix = "wfos.blue.filter"
+
+      //params
+      val encParam1 = encoderKey.set(1)
+      val encParam2 = encoderKey.set(2)
+      val encParam3 = encoderKey.set(3)
+
+      val filterParam1 = filterKey.set(1)
+      val filterParam2 = filterKey.set(2)
+      val filterParam3 = filterKey.set(3)
+
+      val miscParam1 = miscKey.set(100)
+
+      //Setup command with duplicate key via constructor
+      val setup = Setup(
+        obsId,
+        prefix,
+        Set(encParam1, encParam2, encParam3, filterParam1, filterParam2, filterParam3)
+      )
+      //four duplicate keys are removed; now contains one Encoder and one Filter key
+      val uniqueKeys1 = setup.paramSet.toList.map(_.keyName)
+
+      //try adding duplicate keys via add + madd
+      val changedSetup = setup
+        .add(encParam3)
+        .madd(
+          filterParam1,
+          filterParam2,
+          filterParam3
+        )
+      //duplicate keys will not be added. Should contain one Encoder and one Filter key
+      val uniqueKeys2 = changedSetup.paramSet.toList.map(_.keyName)
+
+      //miscKey(unique) will be added; encoderKey(duplicate) will not be added
+      val finalSetUp = setup.madd(Set(miscParam1, encParam1))
+      //now contains encoderKey, filterKey, miscKey
+      val uniqueKeys3 = finalSetUp.paramSet.toList.map(_.keyName)
+      //#unique-key
+
+      //validations
+      uniqueKeys1 should contain theSameElementsAs List(encoderKey.keyName, filterKey.keyName)
+      uniqueKeys2 should contain theSameElementsAs List(encoderKey.keyName, filterKey.keyName)
+      uniqueKeys3 should contain theSameElementsAs List(encoderKey.keyName, filterKey.keyName, miscKey.keyName)
+    }
   }
 }
