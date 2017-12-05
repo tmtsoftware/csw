15e8cf41f102d08cd814bc737856d75e1fdb8f78
weiss <weiss.personal@gmail.com>
2017-12-05 13:02:36 -0800

    Use ComponentRef alias for ActorRef[ComponentMessage]

--- csw-command/src/main/scala/csw/services/ccs/javadsl/CommandExecutionService.scala
+++ csw-command/src/main/scala/csw/services/ccs/javadsl/CommandExecutionService.scala
@@ -3,33 +3,32 @@ package csw.services.ccs.javadsl
 import java.util.concurrent.CompletableFuture
 
 import akka.actor.Scheduler
-import akka.typed.ActorRef
-import csw.messages.ComponentMessage
-import csw.messages.ccs.commands.{CommandResponse, ControlCommand}
-import scala.compat.java8.FutureConverters._
-
 import akka.util.Timeout
+import csw.messages.ActorTypes.ComponentRef
+import csw.messages.ccs.commands.{CommandResponse, ControlCommand}
 import csw.messages.params.models.RunId
 import csw.services.ccs.common.ActorRefExts.RichComponentActor
 
+import scala.compat.java8.FutureConverters._
+
 object CommandExecutionService {
   def submit(
-      actorRef: ActorRef[ComponentMessage],
-      controlCommand: ControlCommand,
-      timeout: Timeout,
-      scheduler: Scheduler
+              actorRef: ComponentRef,
+              controlCommand: ControlCommand,
+              timeout: Timeout,
+              scheduler: Scheduler
   ): CompletableFuture[CommandResponse] =
     actorRef.submit(controlCommand)(timeout, scheduler).toJava.toCompletableFuture
 
   def oneway(
-      actorRef: ActorRef[ComponentMessage],
+      actorRef: ComponentRef,
       controlCommand: ControlCommand,
       timeout: Timeout,
       scheduler: Scheduler
   ): CompletableFuture[CommandResponse] = actorRef.submit(controlCommand)(timeout, scheduler).toJava.toCompletableFuture
 
   def getCommandResponse(
-      actorRef: ActorRef[ComponentMessage],
+      actorRef: ComponentRef,
       commandRunId: RunId,
       timeout: Timeout,
       scheduler: Scheduler
