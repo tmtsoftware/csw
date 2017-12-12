8303d168a5cb2c61cbd26f9bddb1b94e96dce35c
Nishant Vishwakarma <nishantv12@gmail.com>
2017-12-12 16:15:51 +0530

    #227 : Nishant : Create CommandDistributor for executing command on multiple targets and composing result

---
+++ csw-command/src/main/scala/csw/services/ccs/scaladsl/CommandDistributor.scala
@@ -0,0 +1,50 @@
+package csw.services.ccs.scaladsl
+
+import akka.stream.OverflowStrategy
+import akka.stream.scaladsl.{Sink, Source}
+import akka.typed.ActorRef
+import akka.typed.scaladsl.adapter._
+import csw.messages.CommandResponseManagerMessage.Subscribe
+import csw.messages.ComponentMessage
+import csw.messages.ccs.commands.CommandResponse.Accepted
+import csw.messages.ccs.commands.{CommandResponse, CommandResultType, ControlCommand}
+import csw.services.ccs.common.ActorRefExts.RichComponentActor
+
+import scala.concurrent.Future
+
+class CommandDistributor(controlCommand: ControlCommand) {
+
+  var commandToComponent: Map[ActorRef[ComponentMessage], ControlCommand] = _
+
+  def addSubCommand(componentRef: ActorRef[ComponentMessage], controlCommand: ControlCommand): CommandDistributor = {
+    commandToComponent = commandToComponent + (componentRef → controlCommand)
+    this
+  }
+
+  def addSubCommand(subCommands: Map[ActorRef[ComponentMessage], ControlCommand]): CommandDistributor = {
+    commandToComponent = commandToComponent ++ subCommands
+    this
+  }
+
+  def execute(): Future[CommandResponse] = {
+    var finalResponse: CommandResponse = CommandResponse.Completed(controlCommand.runId)
+
+    Source
+      .actorRef[CommandResponse](256, OverflowStrategy.fail)
+      .mapMaterializedValue { ref ⇒
+        commandToComponent.foreach { kv ⇒
+          kv._1.submit(kv._2).map {
+            case _: Accepted ⇒ kv._1 ! Subscribe(kv._2.runId, ref)
+            case response    ⇒ response
+          }
+        }
+      }
+      .to(Sink.foreach { x ⇒
+        finalResponse = x
+        if (x.resultType == CommandResultType.Negative)
+          throw new RuntimeException("")
+      })
+
+    Future.successful(finalResponse)
+  }
+}
