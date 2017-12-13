87c20b58a34e4095af6f58205709725d264be0cf
Nishant Vishwakarma <nishantv12@gmail.com>
2017-12-13 15:50:38 +0530

    #227 : Nishant : Update helper for getting response for multiple commands for a given component in actorRefExtensions and CommandExecutionService to use CommandDsitributor

--- csw-command/src/main/scala/csw/services/ccs/common/ActorRefExts.scala
+++ csw-command/src/main/scala/csw/services/ccs/common/ActorRefExts.scala
@@ -1,20 +1,18 @@
 package csw.services.ccs.common
 
-import akka.NotUsed
 import akka.actor.Scheduler
 import akka.stream.Materializer
-import akka.stream.scaladsl.{Sink, Source}
 import akka.typed.ActorRef
 import akka.typed.scaladsl.AskPattern._
 import akka.util.Timeout
 import csw.messages.CommandMessage.{Oneway, Submit}
 import csw.messages.ccs.commands.CommandResponse.Accepted
-import csw.messages.ccs.commands.{CommandResponse, CommandResultType, ControlCommand}
+import csw.messages.ccs.commands.{CommandResponse, ControlCommand}
 import csw.messages.params.models.RunId
 import csw.messages.{CommandResponseManagerMessage, ComponentMessage}
+import csw.services.ccs.scaladsl.CommandDistributor
 
 import scala.concurrent.{ExecutionContext, Future}
-import scala.util.{Failure, Success}
 
 object ActorRefExts {
   implicit class RichComponentActor(val componentActor: ActorRef[ComponentMessage]) extends AnyVal {
@@ -38,19 +36,7 @@ object ActorRefExts {
     def submitManyAndGetCommandResponse(
         controlCommands: Set[ControlCommand]
     )(implicit timeout: Timeout, scheduler: Scheduler, ec: ExecutionContext, mat: Materializer): Future[CommandResponse] = {
-
-      val source: Source[CommandResponse, NotUsed] = Source(controlCommands.toList)
-        .mapAsyncUnordered(10) { command ⇒
-          componentActor.submitAndGetCommandResponse(command)
-        }
-        .map {
-          case x if x.resultType == CommandResultType.Negative ⇒ throw new RuntimeException
-        }
-
-      source.runWith(Sink.ignore).transform {
-        case Success(_)  ⇒ Success(CommandResponse.Completed(RunId()))
-        case Failure(ex) ⇒ Success(CommandResponse.Error(RunId(), s"One of the command failed : ${ex.getMessage}"))
-      }
+      CommandDistributor(Map(componentActor → controlCommands.toList)).execute()
     }
   }
 }
