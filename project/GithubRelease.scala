8e9acd67a6da3f7182a5727991d0db987f76e476
Pritam Kadam <phkadam2008@gmail.com>
2018-02-02 16:11:02 +0530

    #354: Pritam : Include test reports in github release.

--- project/GithubRelease.scala
+++ project/GithubRelease.scala
@@ -3,14 +3,16 @@ import java.io.File
 import com.typesafe.sbt.packager.universal.UniversalPlugin.autoImport.Universal
 import ohnosequences.sbt.GithubRelease.keys.{ghreleaseAssets, ghreleaseRepoName, ghreleaseRepoOrg, githubRelease}
 import ohnosequences.sbt.SbtGithubReleasePlugin
-import sbt.Keys.{aggregate, crossTarget, packageBin}
+import sbt.Keys.{aggregate, crossTarget, packageBin, target}
 import sbt.io.{IO, Path}
-import sbt.taskKey
-import sbt.{AutoPlugin, Plugins, ProjectReference, Setting, Task}
+import sbt.{AutoPlugin, Def, Plugins, ProjectReference, Setting, Task, taskKey, _}
 
 object GithubRelease extends AutoPlugin {
 
   val coverageReportZipKey = taskKey[File]("Creates a distributable zip file containing the coverage report.")
+  val testReportZipKey     = taskKey[File]("Creates a distributable zip file containing the test reports.")
+
+  val aggregateFilter = ScopeFilter(inAggregates(ThisProject), inConfigurations(Compile))
 
   override def requires: Plugins = SbtGithubReleasePlugin
 
@@ -20,14 +22,26 @@ object GithubRelease extends AutoPlugin {
     aggregate in githubRelease := false,
     // this creates scoverage report zip file and required for GithubRelease task, it assumes that scoverage-report is already generated
     // and is available inside target folder (if it is not present, empty zip will be created)
-    coverageReportZipKey := {
-      lazy val coverageReportZip = new File(crossTarget.value, "scoverage-report.zip")
-      IO.zip(Path.allSubpaths(new File(crossTarget.value, "scoverage-report")), coverageReportZip)
-      coverageReportZip
-    }
+    coverageReportZipKey := coverageReportZipTask.value,
+    testReportZipKey := testReportsZipTask.value
   )
 
-  def githubReleases(projects: Seq[ProjectReference]): Setting[Task[Seq[sbt.File]]] =
-    ghreleaseAssets := projects.map(p ⇒ packageBin in Universal in p).join.value :+ coverageReportZipKey.value
+  private def coverageReportZipTask = Def.task {
+    lazy val coverageReportZip = new File(crossTarget.value, "scoverage-report.zip")
+    IO.zip(Path.allSubpaths(new File(crossTarget.value, "scoverage-report")), coverageReportZip)
+    coverageReportZip
+  }
 
+  private def testReportsZipTask = Def.task {
+    lazy val testReportsZip = new File(target.value, "test-reports.zip")
+    val testXmlReportFiles  = target.all(aggregateFilter).value flatMap (x ⇒ Path.allSubpaths(x / "test-reports"))
+    IO.zip(testXmlReportFiles, testReportsZip)
+    testReportsZip
+  }
+
+  def githubReleases(projects: Seq[ProjectReference]): Setting[Task[Seq[sbt.File]]] =
+    ghreleaseAssets := projects
+      .map(p ⇒ packageBin in Universal in p)
+      .join
+      .value :+ coverageReportZipKey.value :+ testReportZipKey.value
 }
