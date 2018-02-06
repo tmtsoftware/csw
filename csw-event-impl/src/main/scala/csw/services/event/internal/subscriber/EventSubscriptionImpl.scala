6616242dc7c7a8e1f026d358a10fe831de2935eb
Saloni Vithalani <skvithalani@gmail.com>
2018-02-06 17:04:09 +0530

    #337: Saloni: Implementing subscribe and unsubscribe in EventSubscriptionImpl

--- csw-event-impl/src/main/scala/csw/services/event/internal/subscriber/EventSubscriptionImpl.scala
+++ csw-event-impl/src/main/scala/csw/services/event/internal/subscriber/EventSubscriptionImpl.scala
@@ -5,15 +5,19 @@ import csw.messages.ccs.events.{Event, EventKey}
 import csw.services.event.internal.EventServiceCodec
 import csw.services.event.scaladsl.EventSubscription
 import csw_protobuf.events.PbEvent
-import io.lettuce.core.RedisClient
 import io.lettuce.core.pubsub.StatefulRedisPubSubConnection
 import io.lettuce.core.pubsub.api.reactive.RedisPubSubReactiveCommands
+import io.lettuce.core.{RedisClient, RedisURI}
 import reactor.core.publisher.FluxSink.OverflowStrategy
 
 class EventSubscriptionImpl(callback: Event ⇒ Unit) extends EventSubscription {
 
-  val redisConnection: StatefulRedisPubSubConnection[String, PbEvent] = RedisClient.create().connectPubSub(EventServiceCodec)
-  val reactive: RedisPubSubReactiveCommands[String, PbEvent]          = redisConnection.reactive()
+  private val redisURI: RedisURI = RedisURI.create("redis://localhost/")
+
+  val redisConnection: StatefulRedisPubSubConnection[String, PbEvent] =
+    RedisClient.create(redisURI).connectPubSub(EventServiceCodec)
+
+  val reactive: RedisPubSubReactiveCommands[String, PbEvent] = redisConnection.reactive()
 
   var eventKeys: Seq[EventKey] = Seq.empty
 
---
+++ csw-event-impl/src/test/scala/csw/services/event/internal/subscriber/EventSubscriptionImplTest.scala
@@ -0,0 +1,10 @@
+package csw.services.event.internal.subscriber
+
+import org.scalatest.FunSuite
+
+class EventSubscriptionImplTest extends FunSuite {
+  test("testSubscribe") {}
+
+  test("testUnsubscribe") {}
+
+}
