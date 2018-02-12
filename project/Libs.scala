03a9a63051258d2ebc10fdcbd073defdb53f207a
Mushtaq Ahmed <mushtaq.a@gmail.com>
2018-02-12 17:32:25 +0530

    upgrade versions

--- project/Libs.scala
+++ project/Libs.scala
@@ -5,14 +5,14 @@ import scalapb.compiler.Version.scalapbVersion
 object Libs {
   val ScalaVersion = "2.12.4"
 
-  val `scalatest`                    = "org.scalatest"          %% "scalatest"                    % "3.0.4" //Apache License 2.0
+  val `scalatest`                    = "org.scalatest"          %% "scalatest"                    % "3.0.5" //Apache License 2.0
   val `scala-java8-compat`           = "org.scala-lang.modules" %% "scala-java8-compat"           % "0.8.0" //BSD 3-clause "New" or "Revised" License
   val `scala-async`                  = "org.scala-lang.modules" %% "scala-async"                  % "0.9.7" //BSD 3-clause "New" or "Revised" License
   val `scopt`                        = "com.github.scopt"       %% "scopt"                        % "3.7.0" //MIT License
   val `acyclic`                      = "com.lihaoyi"            %% "acyclic"                      % "0.1.7" % Provided //MIT License
   val `junit`                        = "junit"                  % "junit"                         % "4.12" //Eclipse Public License 1.0
   val `junit-interface`              = "com.novocode"           % "junit-interface"               % "0.11" //BSD 2-clause "Simplified" License
-  val `mockito-core`                 = "org.mockito"            % "mockito-core"                  % "2.13.0" //MIT License
+  val `mockito-core`                 = "org.mockito"            % "mockito-core"                  % "2.15.0" //MIT License
   val `logback-classic`              = "ch.qos.logback"         % "logback-classic"               % "1.2.3" //Dual license: Either, Eclipse Public License v1.0 or GNU Lesser General Public License version 2.1
   val `akka-management-cluster-http` = "com.lightbend.akka"     %% "akka-management-cluster-http" % "0.6" //N/A at the moment
   val svnkit                         = "org.tmatesoft.svnkit"   % "svnkit"                        % "1.9.0" //TMate Open Source License
