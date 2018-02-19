27d831c813084bda3818e574ec3ce3cc98be19b7
dollyg <dollyg@thoughtworks.com>
2018-02-19 12:22:17 +0530

    #337: Mushtaq | Dolly: publish api for stream of events

--- csw-event-api/src/main/scala/csw/services/event/scaladsl/EventPublisher.scala
+++ csw-event-api/src/main/scala/csw/services/event/scaladsl/EventPublisher.scala
@@ -1,10 +1,12 @@
 package csw.services.event.scaladsl
 
-import akka.Done
+import akka.{Done, NotUsed}
+import akka.stream.scaladsl.Source
 import csw.messages.ccs.events.Event
 
 import scala.concurrent.Future
 
 trait EventPublisher {
+  def publish(source: Source[Event, NotUsed]): Future[Done]
   def publish(event: Event): Future[Done]
 }
--- csw-event-impl/src/main/scala/csw/services/event/internal/pubsub/EventPublisherImpl.scala
+++ csw-event-impl/src/main/scala/csw/services/event/internal/pubsub/EventPublisherImpl.scala
@@ -1,52 +1,30 @@
 package csw.services.event.internal.pubsub
 
-import akka.Done
 import akka.actor.ActorSystem
 import akka.stream._
-import akka.stream.scaladsl.{Sink, Source, SourceQueueWithComplete}
+import akka.stream.scaladsl.{Sink, Source}
+import akka.{Done, NotUsed}
 import csw.messages.ccs.events.Event
-import csw.services.event.internal.api.{EventPublishDriver, EventSubscriberDriver}
+import csw.services.event.internal.api.EventPublishDriver
 import csw.services.event.scaladsl.EventPublisher
 import csw_protobuf.events.PbEvent
+import async.Async._
 
-import scala.concurrent.{Future, Promise}
-import scala.util.Try
+import scala.concurrent.Future
 
 class EventPublisherImpl(eventPublishDriver: EventPublishDriver)(implicit system: ActorSystem) extends EventPublisher {
-
-  private val bufferSize    = 4096
-  private val maxSubStreams = 1024
 
   private val settings                        = ActorMaterializerSettings(system).withSupervisionStrategy(Supervision.getResumingDecider)
   private implicit lazy val mat: Materializer = ActorMaterializer(settings)
 
   import system.dispatcher
 
-  private val eventQueue: SourceQueueWithComplete[StreamElement] = Source
-    .queue[StreamElement](bufferSize, OverflowStrategy.dropHead)
-    .groupBy(maxSubStreams, _.key)
-    .mapAsync(1) { element ⇒
-      eventPublishDriver
-        .publish(element.key, element.value)
-        .flatMap(_ ⇒ eventPublishDriver.set(element.key, element.value))
-        .transformWith(element.complete)
-    }
-    .to(Sink.ignore)
-    .run()
+  override def publish(source: Source[Event, NotUsed]): Future[Done] = source.mapAsync(1)(publish).runWith(Sink.ignore)
 
-  override def publish(event: Event): Future[Done] = {
-    val element = new StreamElement(event)
-    eventQueue.offer(element)
-    element.future
+  override def publish(event: Event): Future[Done] = async {
+    val key: String    = event.eventKey.key
+    val value: PbEvent = Event.typeMapper.toBase(event)
+    await(eventPublishDriver.publish(key, value))
+    await(eventPublishDriver.set(key, value))
   }
-
-  private class StreamElement(event: Event) {
-    val p: Promise[Done]     = Promise[Done]
-    val key: String          = event.eventKey.key
-    val value: PbEvent       = Event.typeMapper.toBase(event)
-    val future: Future[Done] = p.future
-
-    def complete(t: Try[Done]): Future[Done] = p.complete(t).future
-  }
-
 }
