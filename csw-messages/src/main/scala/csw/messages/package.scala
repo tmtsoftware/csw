b758adc708e0051efcf336d4379b0bdd32f6f3ff
Pritam Kadam <phkadam2008@gmail.com>
2018-03-08 14:00:32 +0530

    #377: Pritam : Add package documentation for Messages.

--- csw-messages/src/main/scala/csw/messages/package.scala
+++ csw-messages/src/main/scala/csw/messages/package.scala
@@ -3,35 +3,51 @@ package csw
 /**
  * == Messages ==
  *
- * This project is intended to hold reusable utility classes used throughout the csw source code.
+ * This project is intended to hold reusable models and messages used throughout the csw source code.
  *
- * === Commands and Events ===
+ * This also provides out of the box support to cater to the diverse communication requirements.
+ * Consumer of this library will be able to create Commands, Events, States to store ParameterSets.
  *
- * This project contains classes and traits used for *commands* and *events*.
+ * == Imp Packages ==
+ *
+ * === commands and events ===
+ *
+ * This packages contains classes, traits and models used to create *commands* and *events*.
  * These are all based on type-safe keys and items (a set of values with optional units).
  * Each key has a specific type and the key's values must be of that type.
  *
- * There are a variety of similar classes, used for different purposes:
+ * Two types of [[csw.messages.commands.Command]] are supported:
+ *   - [[csw.messages.commands.SequenceCommand]]
+ *     - This commands are targeted to Sequencer. Subtypes of this are: Setup, Observe and Wait.
  *
- * Commands:
+ *   - [[csw.messages.commands.ControlCommand]]
+ *     - This commands are targeted to Assemblies and HCD's. Subtypes of this are: Setup and Observe.
  *
- * - Setup
- * - Observe
+ * Following are the concrete commands supported by csw:
+ *  - [[csw.messages.commands.Setup]]
+ *  - [[csw.messages.commands.Observe]]
+ *  - [[csw.messages.commands.Wait]]
  *
- * Events:
+ * Two types of [[csw.messages.events.Event]] are supported:
+ *  - [[csw.messages.events.SystemEvent]]
+ *  - [[csw.messages.events.ObserveEvent]]
  *
- * - StatusEvent
- * - ObserveEvent
- * - SystemEvent
+ * Another important feature provided by *commands* package is [[csw.messages.commands.matchers.Matcher]]
+ * One of the use case for using matcher is when Assembly sends [[csw.messages.CommandMessage.Oneway]] command to HCD
+ * and in response to this command HCD keeps publishing its current state.
+ * Then Assembly can use Matcher with the matching definition as provided by [[csw.messages.commands.matchers.StateMatcher]] to
+ * match against the current states published by HCD.
  *
- * State Variables:
+ * === location and framework ===
  *
- * - CurrentState
- * - DemandState
+ * This packages contains reusable classes, traits and models.
  *
- * The key/value store and event service make use of these classes, which need to be
- * serialized and deserialized for external storage (in Redis or Hornetq, for example).
- * The [[csw.messages.ParamSetSerializer]] class provides support for this.
+ * === params ===
+ *
+ * This package supports serialization and deserialization of commands, events and state variables in following formats:
+ *  - JSON : [[csw.messages.params.formats.JsonSupport]]
+ *  - Kryo : [[csw.messages.params.generics.ParamSetSerializer]]
+ *  - Protobuf : package pb contains utility and directory protobuf contains proto schema files.
  *
  * === Scala and Java APIs ===
  *
@@ -42,127 +58,116 @@ package csw
  * === Key Types ===
  *
  * A set of standard key types and matching items are defined. Each key accepts one or more values
- * of the given type. The values are stored internally in a Vector:
+ * of the given type.
  *
- * - Integer
+ * Following [[csw.messages.params.generics.KeyType]] are supported by csw:
  *
- * - ShortKey
- *
- * - LongKey
- *
- * - FloatKey
- *
- * - DoubleKey
- *
- * - StringKey
- *
- * - CharKey
- *
- * - Boolean
- *
- * The following keys support one or more values that are each one or two dimensional arrays (stored internally as Arrays):
- *
- * - IntArrayKey, IntMatrixKey
- *
- * - ShortArrayKey, ShortMatrixKey
- *
- * - LongArrayKey, LongMatrixKey
- *
- * - FloatArrayKey, FloatMatrixKey
- *
- * - DoubleArrayKey, DoubleMatrixKey
- *
- * In addition there is a GKey class that can be used for custom types. It is however recommended to
- * use only the standard key types, in oder to ensure that binary and JSON serialization and deserialization
- * works everywhere.
+ *         +--------------+-------------------------+---------------------------+
+ *         |  Primitive   |      Scala KeyType      |       Java KeyType        |
+ *         +--------------+-------------------------+---------------------------+
+ *         | Boolean      | KeyType.BooleanKey      | JKeyTypes.BooleanKey      |
+ *         | Character    | KeyType.CharKey         | JKeyTypes.JCharKey        |
+ *         | Byte         | KeyType.ByteKey         | JKeyTypes.ByteKey         |
+ *         | Short        | KeyType.ShortKey        | JKeyTypes.ShortKey        |
+ *         | Long         | KeyType.LongKey         | JKeyTypes.LongKey         |
+ *         | Int          | KeyType.IntKey          | JKeyTypes.IntKey          |
+ *         | Float        | KeyType.FloatKey        | JKeyTypes.FloatKey        |
+ *         | Double       | KeyType.DoubleKey       | JKeyTypes.DoubleKey       |
+ *         | String       | KeyType.StringKey       | JKeyTypes.StringKey       |
+ *         | Timestamp    | KeyType.TimestampKey    | JKeyTypes.TimestampKey    |
+ *         | ----------   | ----------              | ----------                |
+ *         | ByteArray    | KeyType.ByteArrayKey    | JKeyTypes.ByteArrayKey    |
+ *         | ShortArray   | KeyType.ShortArrayKey   | JKeyTypes.ShortArrayKey   |
+ *         | LongArray    | KeyType.LongArrayKey    | JKeyTypes.LongArrayKey    |
+ *         | IntArray     | KeyType.IntArrayKey     | JKeyTypes.IntArrayKey     |
+ *         | FloatArray   | KeyType.FloatArrayKey   | JKeyTypes.FloatArrayKey   |
+ *         | DoubleArray  | KeyType.DoubleArrayKey  | JKeyTypes.DoubleArrayKey  |
+ *         | ----------   | ----------              | ----------                |
+ *         | ByteMatrix   | KeyType.ByteMatrixKey   | JKeyTypes.ByteMatrixKey   |
+ *         | ShortMatrix  | KeyType.ShortMatrixKey  | JKeyTypes.ShortMatrixKey  |
+ *         | LongMatrix   | KeyType.LongMatrixKey   | JKeyTypes.LongMatrixKey   |
+ *         | IntMatrix    | KeyType.IntMatrixKey    | JKeyTypes.IntMatrixKey    |
+ *         | FloatMatrix  | KeyType.FloatMatrixKey  | JKeyTypes.FloatMatrixKey  |
+ *         | DoubleMatrix | KeyType.DoubleMatrixKey | JKeyTypes.DoubleMatrixKey |
+ *         | ----------   | ----------              | ----------                |
+ *         | Choice       | KeyType.ChoiceKey       | JKeyTypes.ChoiceKey       |
+ *         | RaDec        | KeyType.RaDecKey        | JKeyTypes.RaDecKey        |
+ *         | Struct       | KeyType.StructKey       | JKeyTypes.StructKey       |
+ *         +--------------+-------------------------+---------------------------+
  *
  * Example:
  *
+ * - Scala Example:
+ *
  * {{{
- *   val commandInfo = CommandInfo(ObsId("001"))
  *
- *   // Define a key for an event id
- *   val eventNum = Integer("eventNum")
+ *   // ============== Shows creation of Keys and Parameters ===================
  *
- *   val exposureTime = DoubleKey("exposureTime")
+ *   // making 2 keys
+ *   val k1: Key[Boolean] = KeyType.BooleanKey.make("booleanKey")
+ *   val k2: Key[Short]   = KeyType.ShortKey.make("shortKey")
  *
- *   // Define a key for image data
- *   val imageData = IntArrayKey("imageData")
+ *   // storing a single value
+ *   val booleanParam: Parameter[Boolean] = k1.set(true)
  *
- *   // Dummy image data
- *   val testImageData = IntArray(Array.ofDim[Int](10000))
+ *   // showing different ways for storing multiple values
+ *   val paramWithShorts1: Parameter[Short] = k2.set(1, 2, 3, 4)
+ *   val paramWithShorts2: Parameter[Short] = k2 -> (1, 2, 3, 4)
+ *   val paramWithShorts3: Parameter[Short] = k2 -> Array[Short](1, 2, 3, 4)
  *
- *   val prefix = "tcs.mobie.red.dat.exposureInfo"
+ *   // ==========================================================================
+ *   // ============== Shows creation of commands and events =====================
  *
- *   // ...
+ *   val prefix      = Prefix("wfos.prog.cloudcover")
+ *   val obsId       = ObsId("Obs001")
+ *   val commandName = CommandName("move")
  *
- *     val config = Setup(commandInfo, prefix)
- *       .add(eventNum.set(num))
- *       .add(exposureTime.set(1.0))
- *       .add(imageData.set(testImageData))
+ *   // create setup command with two parameters which are created in earlier section
+ *   val setup: Setup = Setup(prefix, commandName, Some(obsId), Set(booleanParam, paramWithShorts1))
  *
- *   // Or you can use the Scala DSL to do the same thing:
+ *   val eventName = EventName("filterMoved")
  *
- *     import csw.util.config.ConfigDSL._
- *
- *     sc(commandInfo, prefix,
- *        eventNum -> num,
- *        exposureTime -> 1.0,
- *        imageData -> testImageData)
+ *   // create observerEvent with two parameters which are created in earlier section
+ *   val observerEvent: ObserveEvent = ObserveEvent(prefix, eventName).madd(booleanParam, paramWithShorts1)
+ *   // ==========================================================================
  *
  * }}}
  *
- * Java Example:
+ * - Java Example:
  *
  * {{{
- *     import static javacsw.util.config.JItems.*;
- *     import static javacsw.util.config.JConfigDSL.*;
  *
- *     static final Parameters.CommandInfo info = new CommandInfo("Obs001");
- *     static final DoubleKey exposureTime = new DoubleKey("exposureTime");
+ *    // ============== Shows creation of Java Keys and Parameters ===================
  *
- *     // Define a key for an event id
- *     static final Integer eventNum = new Integer("eventNum");
+ *    // making 2 keys
+ *    Key<Boolean> k1 = JKeyTypes.BooleanKey().make("encoder");
+ *    Key<Short> k2   = JKeyTypes.ShortKey().make(keyName);
  *
- *     // Define a key for image data
- *     static final IntArrayKey imageData = new IntArrayKey("imageData");
+ *    // storing a single value
+ *    Parameter<Boolean> booleanParam = k1.set(true);
  *
- *     // Dummy image data
- *     static final JIntArray testImageData = JIntArray.fromArray(new int[10000]);
+ *    // showing different ways for storing multiple values
+ *    Short[] shortArray = {1, 2, 3, 4};
+ *    Parameter<Short> paramWithManyShorts1 = k2.set(shortArray);
+ *    Parameter<Short> paramWithManyShorts2 = k2.set((short) 1, (short) 2, (short) 3, (short) 4);
  *
- *     // Prefix to use for the event
- *     static final String prefix = "tcs.mobie.red.dat.exposureInfo";
+ *    // ===============================================================================
+ *    // ============== Shows creation of java commands and events =====================
  *
- *     // ...
+ *    Prefix prefix           = new Prefix("wfos.prog.cloudcover");
+ *    CommandName commandName = new CommandName("move");
+ *    ObsId obsId             = new ObsId("obsId");
  *
- *       Setup config = new Setup(commandInfo, prefix)
- *         .add(jset(eventNum, num))
- *         .add(jset(exposureTime, 1.0))
- *         .add(jset(imageData, testImageData));
+ *    // create setup command with two parameters which are created in earlier section
+ *    Setup setup = new Setup(prefix, commandName, Optional.of(obsId)).madd(booleanParam, paramWithManyShorts1);
  *
- *       // Alternative syntax
- *       Setup config = jadd(sc(commandInfo, prefix,
- *         jset(eventNum, num),
- *         jset(exposureTime, 1.0),
- *         jset(imageData, testImageData));
- * }}}
+ *    EventName eventName = new EventName("filterMoved")
  *
- * Scala Example using a matrix as the key value:
- * {{{
- *     val k1 = DoubleMatrixKey("myMatrix")
- *     val dm1 = DoubleMatrix(Array(Array[Double](1, 2, 3), Array[Double](2, 3, 6), Array[Double](4, 6, 12)))
- *     val sc1 = sc(commandInfo, "test", k1 -> dm1 withUnits UnitsOfMeasure.Deg)
- *     assert(sc1.get(k1).get.head(0, 0) == 1)
- * }}}
+ *    // create observerEvent with two parameters which are created in earlier section
+ *    ObserveEvent observeEvent = new ObserveEvent(prefix, eventName).madd(booleanParam, paramWithShorts1);
  *
- * Java Example:
+ *    // ===============================================================================
  *
- * {{{
- *    final DoubleMatrixKey k1 = DoubleMatrixKey("matrixTest");
- *    double[][] m1 = {{1., 2., 3.}, {4., 5., 6.}, {7., 8., 9.}};
- *    DoubleMatrix dm1 = DoubleMatrix(m1);
- *    Setup sc1 = jadd(sc(commandInfo, "test"), jset(k1, dm1));
- *    assertTrue(jvalue(jitem(sc1, k1)).apply(0, 0) == 1);
  * }}}
  *
  */
