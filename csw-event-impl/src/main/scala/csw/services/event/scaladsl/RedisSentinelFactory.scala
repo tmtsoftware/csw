8dddfc3482767ec2a3770b7b8fb257ebc24fcdf5
dollyg <dollyg@thoughtworks.com>
2018-05-10 19:57:25 +0530

    #000: Dolly: Add factory for Redis Sentinel

---
+++ csw-event-impl/src/main/scala/csw/services/event/scaladsl/RedisSentinelFactory.scala
@@ -0,0 +1,37 @@
+package csw.services.event.scaladsl
+
+import java.net.URI
+
+import akka.stream.Materializer
+import csw.services.event.internal.redis.{RedisPublisher, RedisSubscriber}
+import csw.services.event.internal.wiring.EventServiceResolver
+import io.lettuce.core.{RedisClient, RedisURI}
+
+import scala.async.Async._
+import scala.concurrent.{ExecutionContext, Future}
+
+class RedisSentinelFactory(
+    redisClient: RedisClient,
+    eventServiceResolver: EventServiceResolver
+)(implicit ec: ExecutionContext, mat: Materializer) {
+
+  def publisher(host: String, port: Int, masterId: String): EventPublisher = {
+    val redisURI = RedisURI.Builder.sentinel(host, port, masterId).build()
+    new RedisPublisher(redisURI, redisClient)
+  }
+
+  def publisher(masterId: String): Future[EventPublisher] = async {
+    val uri: URI = await(eventServiceResolver.uri)
+    publisher(uri.getHost, uri.getPort, masterId)
+  }
+
+  def subscriber(host: String, port: Int, masterId: String): EventSubscriber = {
+    val redisURI = RedisURI.Builder.sentinel(host, port, masterId).build()
+    new RedisSubscriber(redisURI, redisClient)
+  }
+
+  def subscriber(masterId: String): Future[EventSubscriber] = async {
+    val uri: URI = await(eventServiceResolver.uri)
+    subscriber(uri.getHost, uri.getPort, masterId)
+  }
+}
