550411b8ea2ba11f1d9bf5e5f009bb6188f63612
Saloni Vithalani <skvithalani@gmail.com>
2018-05-26 13:30:57 +0530

    #000 : Saloni : Make Standalone.spawn and FrameworkWiring public

--- csw-framework/src/main/scala/csw/framework/internal/wiring/Standalone.scala
+++ csw-framework/src/main/scala/csw/framework/internal/wiring/Standalone.scala
@@ -10,8 +10,15 @@ import scala.concurrent.Future
 /**
  * Start a supervisor actor without a container, in it's own actor system, using the component information provided in a configuration file
  */
-private[csw] object Standalone {
+object Standalone {
 
+  /**
+   * Spawns a component in standalone mode
+   *
+   * @param config represents the componentInfo data
+   * @param wiring represents the class for initializing necessary instances to run a component(s)
+   * @return a Future that completes with actor ref of spawned component
+   */
   def spawn(
       config: com.typesafe.config.Config,
       wiring: FrameworkWiring
