36e51810b1519ce47230d1ab0cb47257f87f6a10
Pritam Kadam <phkadam2008@gmail.com>
2018-06-06 12:13:25 +0530

    #000: Pritam : Reorganize KafkaPublisher API's.

--- csw-event-impl/src/main/scala/csw/services/event/internal/kafka/KafkaPublisher.scala
+++ csw-event-impl/src/main/scala/csw/services/event/internal/kafka/KafkaPublisher.scala
@@ -24,9 +24,6 @@ class KafkaPublisher(
 
   private val parallelism = 100
 
-  override def publish[Mat](stream: Source[Event, Mat], onError: Event ⇒ Unit): Mat =
-    eventPublisherUtil.publishFromSource(stream, parallelism, publish, Some(onError))
-
   override def publish(event: Event): Future[Done] = {
     val promisedDone: Promise[Done] = Promise()
     try {
@@ -36,6 +33,18 @@ class KafkaPublisher(
     }
     promisedDone.future
   }
+
+  override def publish[Mat](source: Source[Event, Mat]): Mat =
+    eventPublisherUtil.publishFromSource(source, parallelism, publish, None)
+
+  override def publish[Mat](stream: Source[Event, Mat], onError: Event ⇒ Unit): Mat =
+    eventPublisherUtil.publishFromSource(stream, parallelism, publish, Some(onError))
+
+  override def publish(eventGenerator: ⇒ Event, every: FiniteDuration): Cancellable =
+    publish(eventPublisherUtil.eventSource(eventGenerator, every))
+
+  override def publish(eventGenerator: ⇒ Event, every: FiniteDuration, onError: Event ⇒ Unit): Cancellable =
+    publish(eventPublisherUtil.eventSource(eventGenerator, every), onError)
 
   override def shutdown(): Future[Done] = Future {
     scala.concurrent.blocking(kafkaProducer.close())
@@ -49,13 +58,4 @@ class KafkaPublisher(
     case (_, null)          ⇒ promisedDone.success(Done)
     case (_, ex: Exception) ⇒ promisedDone.failure(PublishFailedException(event))
   }
-
-  override def publish[Mat](source: Source[Event, Mat]): Mat =
-    eventPublisherUtil.publishFromSource(source, parallelism, publish, None)
-
-  override def publish(eventGenerator: ⇒ Event, every: FiniteDuration): Cancellable =
-    publish(eventPublisherUtil.eventSource(eventGenerator, every))
-
-  override def publish(eventGenerator: ⇒ Event, every: FiniteDuration, onError: Event ⇒ Unit): Cancellable =
-    publish(eventPublisherUtil.eventSource(eventGenerator, every), onError)
 }
