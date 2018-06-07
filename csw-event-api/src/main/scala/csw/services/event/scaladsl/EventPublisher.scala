a115d1e6cafdfb5b25efb5a29531967712a54892
Saloni Vithalani <skvithalani@gmail.com>
2018-06-07 12:02:49 +0530

    #000 : Saloni : Adding descriptive comments for publisher and subscriber. When documentation stories will be played then these comments will be converted to scaladoc.

--- csw-event-api/src/main/scala/csw/services/event/scaladsl/EventPublisher.scala
+++ csw-event-api/src/main/scala/csw/services/event/scaladsl/EventPublisher.scala
@@ -11,15 +11,25 @@ import scala.concurrent.duration.FiniteDuration
 
 trait EventPublisher {
 
+  // publish a single event
   def publish(event: Event): Future[Done]
 
+  // publish from a stream of events
   def publish[Mat](source: Source[Event, Mat]): Mat
 
+  // publish from a stream of events, and execute `onError` callback for each publish failed event
   def publish[Mat](source: Source[Event, Mat], onError: PublishFailure ⇒ Unit): Mat
 
+  // publish from a `eventGenerator` function, which will be executed at `every` frequency. `Cancellable` can be used to cancel
+  // the execution of `eventGenerator` function.
   def publish(eventGenerator: => Event, every: FiniteDuration): Cancellable
 
+  // publish from a `eventGenerator` function, which will be executed at `every` frequency. Also, provide `onError` callback
+  // for each publish failed event. Note that any exception thrown from `eventGenerator` or `onError` callback is expected
+  // to be handled by component developers.
   def publish(eventGenerator: ⇒ Event, every: FiniteDuration, onError: PublishFailure ⇒ Unit): Cancellable
 
+  // shuts down the connection for this publisher, using any api of publisher after shutting should give exceptions.
+  // This method will be called while component is getting shutdown gracefully.
   def shutdown(): Future[Done]
 }
--- csw-event-api/src/main/scala/csw/services/event/scaladsl/EventSubscriber.scala
+++ csw-event-api/src/main/scala/csw/services/event/scaladsl/EventSubscriber.scala
@@ -10,12 +10,18 @@ import scala.concurrent.duration.FiniteDuration
 
 trait EventSubscriber {
 
+  // subscribe to multiple eventKeys and get a stream of events received
   def subscribe(eventKeys: Set[EventKey]): Source[Event, EventSubscription]
 
+  // subscribe to multiple eventKeys and receive events at `every` frequency based on one of the given `mode` (RateAdapter or
+  // RateLimiter)
   def subscribe(eventKeys: Set[EventKey], every: FiniteDuration, mode: SubscriptionMode): Source[Event, EventSubscription]
 
+  // subscribe to multiple eventKeys and execute a future based callback for every event received. The callback is of event => future
+  // type, so that blocking operation withing callback can be placed in the future (separate thread than main thread)
   def subscribeAsync(eventKeys: Set[EventKey], callback: Event => Future[_]): EventSubscription
 
+  //subscribeAsync overload for receiving event at a `every` frequency based on one of the give `mode`
   def subscribeAsync(
       eventKeys: Set[EventKey],
       callback: Event => Future[_],
@@ -23,8 +29,11 @@ trait EventSubscriber {
       mode: SubscriptionMode
   ): EventSubscription
 
+  // subscribe to multiple events and execute the given callback for each received event. Note that any exception thrown from
+  // `callback` is expected to be handled by component developers.
   def subscribeCallback(eventKeys: Set[EventKey], callback: Event => Unit): EventSubscription
 
+  // subscribeCallback overload for receiving event at a `every` frequency based on one of the give `mode`
   def subscribeCallback(
       eventKeys: Set[EventKey],
       callback: Event => Unit,
@@ -32,8 +41,10 @@ trait EventSubscriber {
       mode: SubscriptionMode
   ): EventSubscription
 
+  // subscribe to multiple events and receive the event as actor message to given `actorRef`
   def subscribeActorRef(eventKeys: Set[EventKey], actorRef: ActorRef[Event]): EventSubscription
 
+  // subscribeActorRef overload for receiving event at a `every` frequency based on one of the give `mode`
   def subscribeActorRef(
       eventKeys: Set[EventKey],
       actorRef: ActorRef[Event],
@@ -41,11 +52,16 @@ trait EventSubscriber {
       mode: SubscriptionMode
   ): EventSubscription
 
+  // pattern subscribe for a given subsystem and pattern, and get a stream of events received
   def pSubscribe(subsystem: Subsystem, pattern: String): Source[Event, EventSubscription]
 
+  // pattern subscribe for a given subsystem and pattern, and execute a callback for each event received
   def pSubscribe(subsystem: Subsystem, pattern: String, callback: Event ⇒ Unit): EventSubscription
 
+  // get events for multiple keys, if an event is not published for any eventKey, then invalid event is returned for that eventKey.
+  // For all subscribe apis above, if for any eventKey no event is published then an invalid event is returned forr that eventKey.
   def get(eventKeys: Set[EventKey]): Future[Set[Event]]
 
+  // get event for a single eventKey, if an event is not published for any eventKey, then invalid event is returned for that eventKey.
   def get(eventKey: EventKey): Future[Event]
 }
