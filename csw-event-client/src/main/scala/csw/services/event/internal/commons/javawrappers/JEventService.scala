492e3a5db33e1ceba421ba5b552b572372e0ef26
dollyg <dollyg@thoughtworks.com>
2018-06-12 19:20:50 +0530

    #DEOPSCSW-342: Dolly | Nishant : Fix acyclic dependency

--- csw-event-client/src/main/scala/csw/services/event/internal/commons/javawrappers/JEventService.scala
+++ csw-event-client/src/main/scala/csw/services/event/internal/commons/javawrappers/JEventService.scala
@@ -2,7 +2,6 @@ package csw.services.event.internal.commons.javawrappers
 
 import java.util.concurrent.CompletableFuture
 
-import csw.services.event.internal.commons.EventServiceAdapter
 import csw.services.event.javadsl.{IEventPublisher, IEventService, IEventSubscriber}
 import csw.services.event.scaladsl.EventService
 
@@ -12,13 +11,13 @@ import scala.concurrent.ExecutionContext
 class JEventService(eventService: EventService)(implicit val executionContext: ExecutionContext) extends IEventService {
 
   override lazy val defaultPublisher: CompletableFuture[IEventPublisher] =
-    eventService.defaultPublisher.map(EventServiceAdapter.asJava).toJava.toCompletableFuture
+    eventService.defaultPublisher.map[IEventPublisher](new JEventPublisher(_)).toJava.toCompletableFuture
 
   override lazy val defaultSubscriber: CompletableFuture[IEventSubscriber] =
-    eventService.defaultSubscriber.map(EventServiceAdapter.asJava).toJava.toCompletableFuture
+    eventService.defaultSubscriber.map[IEventSubscriber](new JEventSubscriber(_)).toJava.toCompletableFuture
 
   override def makeNewPublisher(): CompletableFuture[IEventPublisher] =
-    eventService.makeNewPublisher().map(EventServiceAdapter.asJava).toJava.toCompletableFuture
+    eventService.makeNewPublisher().map[IEventPublisher](new JEventPublisher(_)).toJava.toCompletableFuture
 
   override def asScala: EventService = eventService
 }
