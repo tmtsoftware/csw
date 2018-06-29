bdc1a1013c12e4990ab9758eb32046141746dfe0
Nishant Vishwakarma <nishantv12@gmail.com>
2018-06-29 11:51:24 +0530

    #DEOPSCSW-368 : Nishant : Updated API documentation for EventService and IEventService

--- csw-event-api/src/main/scala/csw/services/event/javadsl/IEventService.scala
+++ csw-event-api/src/main/scala/csw/services/event/javadsl/IEventService.scala
@@ -6,15 +6,43 @@ import csw.services.event.scaladsl.EventService
 
 import scala.concurrent.ExecutionContext
 
+/**
+ * An interface to provide access to [[csw.services.event.javadsl.IEventPublisher]] and [[csw.services.event.javadsl.IEventSubscriber]].
+ */
 trait IEventService {
 
   implicit val executionContext: ExecutionContext
 
-  lazy val defaultPublisher: CompletableFuture[IEventPublisher]   = makeNewPublisher()
+  /**
+   * A default instance of [[csw.services.event.javadsl.IEventPublisher]].
+   * This could be shared across under normal operating conditions to share the underlying connection to event server.
+   */
+  lazy val defaultPublisher: CompletableFuture[IEventPublisher] = makeNewPublisher()
+
+  /**
+   * A default instance of [[csw.services.event.javadsl.IEventSubscriber]].
+   * This could be shared across under normal operating conditions to share the underlying connection to event server.
+   */
   lazy val defaultSubscriber: CompletableFuture[IEventSubscriber] = makeNewSubscriber()
 
+  /**
+   * Create a new instance of [[csw.services.event.javadsl.IEventPublisher]] with a separate underlying connection than the default instance.
+   * The new instance will be required when the location of Event Service is updated or in case the performance requirements
+   * of a publish operation demands a separate connection to be used.
+   * @return
+   */
   def makeNewPublisher(): CompletableFuture[IEventPublisher]
+
+  /**
+   * Create a new instance of [[csw.services.event.javadsl.IEventPublisher]] with a separate underlying connection than the default instance.
+   * The new instance will be required when the location of Event Service is updated or in case the performance requirements
+   * of a subscribe operation demands a separate connection to be used.
+   * @return A new instance of [[csw.services.event.javadsl.IEventSubscriber]]
+   */
   def makeNewSubscriber(): CompletableFuture[IEventSubscriber]
 
+  /**
+   * Returns the Scala API for this instance of event service
+   */
   def asScala: EventService
 }
--- csw-event-api/src/main/scala/csw/services/event/scaladsl/EventService.scala
+++ csw-event-api/src/main/scala/csw/services/event/scaladsl/EventService.scala
@@ -2,12 +2,37 @@ package csw.services.event.scaladsl
 
 import scala.concurrent.{ExecutionContext, Future}
 
+/**
+ * An interface to provide access to [[csw.services.event.scaladsl.EventPublisher]] and [[csw.services.event.scaladsl.EventSubscriber]].
+ */
 trait EventService {
   implicit val executionContext: ExecutionContext
 
-  lazy val defaultPublisher: Future[EventPublisher]   = makeNewPublisher()
+  /**
+   * A default instance of [[csw.services.event.scaladsl.EventPublisher]].
+   * This could be shared across under normal operating conditions to share the underlying connection to event server.
+   */
+  lazy val defaultPublisher: Future[EventPublisher] = makeNewPublisher()
+
+  /**
+   * A default instance of [[csw.services.event.scaladsl.EventSubscriber]].
+   * This could be shared across under normal operating conditions to share the underlying connection to event server.
+   */
   lazy val defaultSubscriber: Future[EventSubscriber] = makeNewSubscriber()
 
+  /**
+   * Create a new instance of [[csw.services.event.scaladsl.EventPublisher]] with a separate underlying connection than the default instance.
+   * The new instance will be required when the location of Event Service is updated or in case the performance requirements
+   * of a publish operation demands a separate connection to be used.
+   * @return
+   */
   def makeNewPublisher(): Future[EventPublisher]
+
+  /**
+   * Create a new instance of [[csw.services.event.scaladsl.EventPublisher]] with a separate underlying connection than the default instance.
+   * The new instance will be required when the location of Event Service is updated or in case the performance requirements
+   * of a subscribe operation demands a separate connection to be used.
+   * @return A new instance of [[csw.services.event.scaladsl.EventSubscriber]]
+   */
   def makeNewSubscriber(): Future[EventSubscriber]
 }
