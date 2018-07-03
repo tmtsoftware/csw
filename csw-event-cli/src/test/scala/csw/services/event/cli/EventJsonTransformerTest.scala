85f366ad75f1055945a6aaf1299b98e7b0976a19
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2018-07-03 16:52:01 +0530

    #DEOPSCSW-429 : Dolly | Bilal : Remove event parser
    Co-authored-by: Dolly <10329479+Dollyg@users.noreply.github.com>

--- csw-event-cli/src/test/scala/csw/services/event/cli/EventJsonTransformerTest.scala
+++ csw-event-cli/src/test/scala/csw/services/event/cli/EventJsonTransformerTest.scala
@@ -8,19 +8,28 @@ import scala.io.Source
 
 class EventJsonTransformerTest extends FunSuite with Matchers {
 
-  test("should be able to get top level keys in json") {
+  test("should be able to get top level key non struct key from json") {
 
-    val eventJson         = read[Js.Obj](Source.fromResource("input/event1.json").mkString)
-    val expectedEventJson = read[Js.Obj](Source.fromResource("expected/get_path_top_level.json").mkString)
+    val eventJson         = read[Js.Obj](Source.fromResource("get/input/event1.json").mkString)
+    val expectedEventJson = read[Js.Obj](Source.fromResource("get/expected/top_level_non_struct_key.json").mkString)
     val paths             = List(List("epoch"))
+    EventJsonTransformer.transformInPlace(eventJson, paths)
+    eventJson shouldBe expectedEventJson
+  }
+
+  test("should be able to get top level partial struct key from json") {
+
+    val eventJson         = read[Js.Obj](Source.fromResource("get/input/event1.json").mkString)
+    val expectedEventJson = read[Js.Obj](Source.fromResource("get/expected/top_level_partial_struct_key.json").mkString)
+    val paths             = List(List("struct-1"))
     EventJsonTransformer.transformInPlace(eventJson, paths)
     eventJson shouldBe expectedEventJson
   }
 
   test("should be able to get specified paths two levels deep in event in json format") {
 
-    val eventJson         = read[Js.Obj](Source.fromResource("input/event1.json").mkString)
-    val expectedEventJson = read[Js.Obj](Source.fromResource("expected/get_path_2_levels_deep.json").mkString)
+    val eventJson         = read[Js.Obj](Source.fromResource("get/input/event1.json").mkString)
+    val expectedEventJson = read[Js.Obj](Source.fromResource("get/expected/get_path_2_levels_deep.json").mkString)
     val paths             = List(List("struct-1", "ra"))
     EventJsonTransformer.transformInPlace(eventJson, paths)
     expectedEventJson shouldBe eventJson
@@ -28,8 +37,8 @@ class EventJsonTransformerTest extends FunSuite with Matchers {
 
   test("should be able to get multiple specified paths in event in json format") {
 
-    val eventJson         = read[Js.Obj](Source.fromResource("input/event1.json").mkString)
-    val expectedEventJson = read[Js.Obj](Source.fromResource("expected/get_multiple_paths.json").mkString)
+    val eventJson         = read[Js.Obj](Source.fromResource("get/input/event1.json").mkString)
+    val expectedEventJson = read[Js.Obj](Source.fromResource("get/expected/get_multiple_paths.json").mkString)
     val paths             = List(List("struct-1", "ra"), List("epoch"))
     EventJsonTransformer.transformInPlace(eventJson, paths)
     expectedEventJson shouldBe eventJson
@@ -37,10 +46,10 @@ class EventJsonTransformerTest extends FunSuite with Matchers {
 
   test("should be able to get specified paths for multiple events in json format") {
 
-    val event1Json         = read[Js.Obj](Source.fromResource("input/event1.json").mkString)
-    val event2Json         = read[Js.Obj](Source.fromResource("input/event2.json").mkString)
-    val expectedEvent1Json = read[Js.Obj](Source.fromResource("expected/get_multiple_events1.json").mkString)
-    val expectedEvent2Json = read[Js.Obj](Source.fromResource("expected/get_multiple_events2.json").mkString)
+    val event1Json         = read[Js.Obj](Source.fromResource("get/input/event1.json").mkString)
+    val event2Json         = read[Js.Obj](Source.fromResource("get/input/event2.json").mkString)
+    val expectedEvent1Json = read[Js.Obj](Source.fromResource("get/expected/get_multiple_events1.json").mkString)
+    val expectedEvent2Json = read[Js.Obj](Source.fromResource("get/expected/get_multiple_events2.json").mkString)
 
     val paths = List(List("struct-1", "ra"), List("struct-2", "struct-1", "ra"))
     EventJsonTransformer.transformInPlace(event1Json, paths)
