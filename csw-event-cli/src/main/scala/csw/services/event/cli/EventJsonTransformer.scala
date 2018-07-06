e3cd3bdda7fcefd4b821c48dbe99368a78ef6c3a
Mushtaq Ahmed <mushtaq.a@gmail.com>
2018-07-06 19:31:32 +0530

    #000: Wireup the immutable transformer.

--- csw-event-cli/src/main/scala/csw/services/event/cli/EventJsonTransformer.scala
+++ csw-event-cli/src/main/scala/csw/services/event/cli/EventJsonTransformer.scala
@@ -1,40 +1,14 @@
 package csw.services.event.cli
 
-import akka.japi.Option.Some
+import play.api.libs.json.JsObject
 import ujson.Js
+import ujson.play.PlayJson
 
 object EventJsonTransformer {
-
   def transform(eventJson: Js.Obj, paths: List[String]): Js.Obj = {
-    val eventJsonCopy = Js.Obj(ujson.copy(eventJson).obj)
-    transformInPlace0(eventJsonCopy, None, paths)
-    eventJsonCopy
+    val pathAsLists     = paths.map(_.split("/").toList)
+    val jsObject        = eventJson.transform(PlayJson).as[JsObject]
+    val transformedJson = JsonSelector(pathAsLists).transform(jsObject)
+    PlayJson.transform(transformedJson, Js).obj
   }
-
-  private def transformInPlace0(json: Js.Obj, parentPath: Option[String], paths: List[String]): Unit =
-    paths match {
-      case Nil ⇒
-      case _ ⇒
-        def fullPath(json: Js.Value): String = {
-          val keyName = json("keyName").str
-          if (parentPath.isDefined) s"${parentPath.get}/$keyName"
-          else keyName
-        }
-
-        json("paramSet") = json("paramSet").arr.filter(param ⇒ {
-          val currentFullPath = fullPath(param)
-          paths.contains(currentFullPath) || paths.exists(path => path.startsWith(s"$currentFullPath/"))
-        })
-
-        json("paramSet").arr.foreach { param =>
-          val currentFullPath = fullPath(param)
-          if (!paths.contains(currentFullPath))
-            param("values") = param("values").arr.filter {
-              case value: Js.Obj =>
-                transformInPlace0(value, Some(currentFullPath), paths)
-                value("paramSet").arr.nonEmpty //Remove empty paramSets
-              case _ => true
-            }
-        }
-    }
 }
---
+++ csw-event-cli/src/main/scala/csw/services/event/cli/JsonSelector.scala
@@ -0,0 +1,34 @@
+package csw.services.event.cli
+
+import play.api.libs.json.{JsObject, JsValue, Json}
+
+case class JsonSelector(paths: List[List[String]]) {
+  def transform(json: JsObject): JsObject = ParamSetJson(json, Nil).select.json
+
+  private case class ParamSetJson(json: JsObject, parents: List[String]) {
+    def paramSet: Seq[ParamJson] = json("paramSet").as[Seq[JsObject]].map(x => ParamJson(x, parents))
+
+    def select: ParamSetJson = paths match {
+      case Nil ⇒ this
+      case _ ⇒
+        val selectedSet = paramSet.filter(_.hasValidPath).map(_.select)
+        copy(json ++ Json.obj("paramSet" -> selectedSet.map(_.json)))
+    }
+  }
+
+  private case class ParamJson(json: JsObject, parents: List[String]) {
+    def hasValidPath: Boolean = paths.exists(_.startsWith(path))
+
+    def select: ParamJson = {
+      val selectedSets     = if (!paths.contains(path)) paramSets.map(_.select) else paramSets
+      val selectedSetsJson = selectedSets.filter(_.paramSet.nonEmpty).map(_.json)
+      copy(json ++ Json.obj("values" -> (selectedSetsJson ++ simpleValues)))
+    }
+
+    private def values: Seq[JsValue]         = json("values").as[Seq[JsValue]]
+    private def simpleValues: Seq[JsValue]   = values.filterNot(_.isInstanceOf[JsObject])
+    private def paramSets: Seq[ParamSetJson] = values.collect({ case x: JsObject => ParamSetJson(x, path) })
+
+    private def path: List[String] = parents :+ json("keyName").as[String]
+  }
+}
