f83735b77331d373c2195c950888aa7a787cd77b
Mushtaq Ahmed <mushtaq.a@gmail.com>
2018-07-06 14:47:39 +0530

    #000: Immutable implementation of json transformation.

--- csw-event-cli/src/main/scala/csw/services/event/cli/ParamSetJson.scala
+++ csw-event-cli/src/main/scala/csw/services/event/cli/ParamSetJson.scala
@@ -1,38 +1,32 @@
 package csw.services.event.cli
 
-import ujson.Js
+import play.api.libs.json.{JsObject, JsValue, Json}
 
-case class ParamSetJson(json: Js.Obj, parents: List[String]) {
-  val params: Seq[ParamJson] = json("paramSet").arr.map(x => ParamJson(x.obj, parents))
+case class ParamSetJson(json: JsObject, parents: List[String]) {
+  def paramSet: Seq[ParamJson] = json("paramSet").as[Seq[JsObject]].map(x => ParamJson(x, parents))
 
-  def prune(paths: List[List[String]]): Unit = {
-    paths match {
-      case Nil ⇒
-      case _ ⇒
-        json("paramSet") = params.collect {
-          case param if paths.exists(_.startsWith(param.path)) => param.json
-        }
-
-        for {
-          param <- params
-          if !paths.contains(param.path)
-          paramSet <- param.paramSets
-        } {
-          paramSet.prune(paths)
-          param.prune()
-        }
-    }
+  def prune(paths: List[List[String]]): ParamSetJson = paths match {
+    case Nil ⇒ this
+    case _ ⇒
+      val prunedSet = paramSet
+        .filter(param => paths.exists(_.startsWith(param.path)))
+        .map(_.prune(paths))
+      copy(json ++ Json.obj("paramSet" -> prunedSet.map(_.json)))
   }
 }
-case class ParamJson(json: Js.Value, parents: List[String]) {
-  def prune(): Unit = {
-    json("values") = paramSets.collect {
-      case paramSet if paramSet.params.nonEmpty => paramSet.json
-    }
+
+case class ParamJson(json: JsObject, parents: List[String]) {
+  def prune(paths: List[List[String]]): ParamJson = {
+    val prunedSets   = if (!paths.contains(path)) paramSets.map(_.prune(paths)) else paramSets
+    val prunedValues = prunedSets.filter(_.paramSet.nonEmpty).map(_.json) ++ simpleValues
+    copy(json ++ Json.obj("values" -> prunedValues))
   }
-  def keyName: String = json("keyName").str
-  def paramSets: Seq[ParamSetJson] = json("values").arr.collect {
-    case x: Js.Obj => ParamSetJson(x, path)
+  ///////////////
+  def values: Seq[JsValue] = json("values").as[Seq[JsValue]]
+  def paramSets: Seq[ParamSetJson] = values.collect {
+    case x: JsObject => ParamSetJson(x, path)
   }
-  def path: List[String] = parents :+ keyName
+  def simpleValues: Seq[JsValue] = values.filterNot(_.isInstanceOf[JsObject])
+  /////////////
+  def path: List[String] = parents :+ json("keyName").as[String]
 }
