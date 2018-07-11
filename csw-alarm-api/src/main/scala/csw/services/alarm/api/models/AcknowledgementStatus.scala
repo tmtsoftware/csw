7f103475ad5d8ec905f77fd718f1251662c2571f
Saloni Vithalani <skvithalani@gmail.com>
2018-07-11 12:05:21 +0530

    #DEOPSCSW-441 : Saloni | Bilal: Add AcknowledgementStatus model
    Co-authored-by: Bilal <bilal.m.fazlani@gmail.com>

---
+++ csw-alarm-api/src/main/scala/csw/services/alarm/api/models/AcknowledgementStatus.scala
@@ -0,0 +1,32 @@
+package csw.services.alarm.api.models
+
+import csw.messages.TMTSerializable
+import enumeratum.EnumEntry.Lowercase
+import enumeratum.{Enum, EnumEntry, PlayJsonEnum}
+import upickle.default.{macroRW, ReadWriter ⇒ RW}
+
+import scala.collection.immutable.IndexedSeq
+
+sealed abstract class AcknowledgementStatus extends EnumEntry with Lowercase with TMTSerializable {
+
+  /**
+   * The name of AcknowledgementStatus e.g. for `Acknowledged` status, the name will be represented as `acknowledged`
+   */
+  def name: String = entryName
+}
+
+object AcknowledgementStatus extends Enum[AcknowledgementStatus] with PlayJsonEnum[AcknowledgementStatus] {
+
+  /**
+   * Returns a sequence of all alarm types
+   */
+  def values: IndexedSeq[AcknowledgementStatus] = findValues
+
+  case object UnAcknowledged extends AcknowledgementStatus
+  case object Acknowledged   extends AcknowledgementStatus
+
+  implicit def acknowledgementStatusRw: RW[AcknowledgementStatus] = RW.merge(
+    macroRW[AcknowledgementStatus.UnAcknowledged.type],
+    macroRW[AcknowledgementStatus.Acknowledged.type]
+  )
+}
