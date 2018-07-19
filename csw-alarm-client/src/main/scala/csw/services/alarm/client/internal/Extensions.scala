1795ceee8ad9162f6fe4e9c15245e665ff582ca7
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2018-07-19 14:09:10 +0530

    #DEOPSCSW-449 : Saloni | Bilal : Extracted datetime related activities extension methods
    Co-authored-by: Saloni <skvithalani@yahoo.com>

---
+++ csw-alarm-client/src/main/scala/csw/services/alarm/client/internal/Extensions.scala
@@ -0,0 +1,56 @@
+package csw.services.alarm.client.internal
+
+import java.time.temporal.ChronoUnit
+import java.time.{ZoneOffset, ZonedDateTime}
+
+import scala.compat.java8.DurationConverters.DurationOps
+import scala.concurrent.duration.FiniteDuration
+
+object Extensions {
+
+  implicit class RichZonedDateTime(value: ZonedDateTime) {
+    def -(targetDate: ZonedDateTime): FiniteDuration = {
+      val duration = java.time.Duration.between(targetDate, value)
+      duration.toScala
+    }
+  }
+
+  implicit class RichInt(value: Int) {
+
+    val TWENTY_HOUR_FORMAT_MAX_LIMIT = 23
+    val TWENTY_HOUR_FORMAT_MIN_LIMIT = 0
+    val TWELEVE_FORMAT_MAX_LIMIT     = 12
+    val TWELEVE_FORMAT_MIN_LIMIT     = 1
+
+    /**
+     * Converts an integer to ZonedDateTime with UTC offset
+     * @return next instant of time with hour set to given value
+     */
+    def toHourOfDay: ZonedDateTime = {
+      hourOfDay(TWENTY_HOUR_FORMAT_MIN_LIMIT, TWENTY_HOUR_FORMAT_MAX_LIMIT)
+    }
+
+    def am: ZonedDateTime = {
+      hourOfDay(TWELEVE_FORMAT_MIN_LIMIT, TWELEVE_FORMAT_MAX_LIMIT)
+    }
+
+    def pm: ZonedDateTime = {
+      hourOfDay(TWELEVE_FORMAT_MIN_LIMIT, TWELEVE_FORMAT_MAX_LIMIT).plusHours(12)
+    }
+
+    private def hourOfDay(minHour: Int, maxHour: Int) = {
+      if (value < minHour || value > maxHour)
+        throw new IllegalArgumentException(s"time should be between 1 & $maxHour")
+
+      val currentDate = ZonedDateTime.now(ZoneOffset.UTC)
+
+      val date = currentDate
+        .truncatedTo(ChronoUnit.DAYS)
+        .withHour(value)
+
+      if (date.compareTo(currentDate) > 0)
+        date
+      else date.plusDays(1)
+    }
+  }
+}
--- csw-alarm-client/src/main/scala/csw/services/alarm/client/internal/shelve/ShelveTimeoutBehaviour.scala
+++ csw-alarm-client/src/main/scala/csw/services/alarm/client/internal/shelve/ShelveTimeoutBehaviour.scala
@@ -1,21 +1,18 @@
 package csw.services.alarm.client.internal.shelve
 
-import java.time.temporal.ChronoField.HOUR_OF_DAY
-import java.time.temporal.ChronoUnit
-import java.time.{Duration, ZoneOffset, ZonedDateTime}
+import java.time.{ZoneOffset, ZonedDateTime}
 
 import akka.actor.typed.Behavior
 import akka.actor.typed.scaladsl.{ActorContext, MutableBehavior, TimerScheduler}
 import csw.services.alarm.api.models.AlarmKey
+import csw.services.alarm.client.internal.Extensions.{RichInt, RichZonedDateTime}
 import csw.services.alarm.client.internal.shelve.ShelveTimeoutMessage.{
   CancelShelveTimeout,
   ScheduleShelveTimeout,
   ShelveHasTimedOut
 }
 
-import scala.compat.java8.DurationConverters.DurationOps
 import scala.concurrent.Future
-import scala.concurrent.duration.FiniteDuration
 
 class ShelveTimeoutBehaviour(
     ctx: ActorContext[ShelveTimeoutMessage],
@@ -23,7 +20,7 @@ class ShelveTimeoutBehaviour(
     unShelve: AlarmKey ⇒ Future[Unit]
 ) extends MutableBehavior[ShelveTimeoutMessage] {
 
-  private val shelveTimeoutInHourOfDay: Int = ctx.system.settings.config.getInt("shelve-timeout-hour-of-day")
+  private val shelveTimeoutHour: Int = ctx.system.settings.config.getInt("alarm.shelve-timeout-hour-of-day")
 
   override def onMessage(msg: ShelveTimeoutMessage): Behavior[ShelveTimeoutMessage] = {
     msg match {
@@ -36,22 +33,7 @@ class ShelveTimeoutBehaviour(
 
   private def scheduleShelveTimeout(key: AlarmKey): Unit = {
     val currentTime   = ZonedDateTime.now(ZoneOffset.UTC)
-    val shelveTimeout = timeUntilNextHourOfDay(currentTime)
+    val shelveTimeout = shelveTimeoutHour.toHourOfDay - currentTime
     timerScheduler.startSingleTimer(key.name, ShelveHasTimedOut(key), shelveTimeout)
-  }
-
-  private def timeUntilNextHourOfDay(currentTime: ZonedDateTime): FiniteDuration = {
-    val unshelveTime =
-      if (currentTime.get(HOUR_OF_DAY) < shelveTimeoutInHourOfDay)
-        currentTime
-          .withHour(shelveTimeoutInHourOfDay)
-          .truncatedTo(ChronoUnit.DAYS)
-      else
-        currentTime
-          .plus(1, ChronoUnit.DAYS)
-          .withHour(shelveTimeoutInHourOfDay)
-          .truncatedTo(ChronoUnit.DAYS)
-
-    Duration.between(currentTime, unshelveTime).toScala
   }
 }
