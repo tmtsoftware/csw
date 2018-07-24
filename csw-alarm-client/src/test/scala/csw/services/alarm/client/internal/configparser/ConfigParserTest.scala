3a4f591fa52e10e93efc75a0acdbea026c9be942
Pritam Kadam <phkadam2008@gmail.com>
2018-07-24 23:26:13 +0530

    #DEOPSCSW-451: Pritam: Add tests for config parser.

---
+++ csw-alarm-client/src/test/scala/csw/services/alarm/client/internal/configparser/ConfigParserTest.scala
@@ -0,0 +1,32 @@
+package csw.services.alarm.client.internal.configparser
+import com.typesafe.config.ConfigFactory
+import csw.services.alarm.api.models.{AlarmKey, AlarmMetadata, AlarmSeverity, AlarmType}
+import org.scalatest.{FunSuite, Matchers}
+import AlarmSeverity._
+
+class ConfigParserTest extends FunSuite with Matchers {
+
+  test("should able to parse valid alarm metadata config file") {
+    val config = ConfigFactory.parseResources("test-alarm.conf")
+
+    ConfigParser.parseAlarmMetadata(config) shouldEqual AlarmMetadata(
+      AlarmKey("nfiraos", "nfiraos.cc.trombone", "tromboneAxisHighLimitAlarm"),
+      description = "Warns when trombone axis has reached the low limit",
+      location = "south side",
+      AlarmType.Absolute,
+      List(Indeterminate, Okay, Warning, Major, Critical),
+      probableCause = "the trombone software has failed or the stage was driven into the low limit",
+      operatorResponse = "go to the NFIRAOS engineering user interface and select the datum axis command",
+      isAutoAcknowledgeable = true,
+      isLatchable = true
+    )
+  }
+
+  test("should able to parse valid alarm metadata's config file") {
+    val config = ConfigFactory.parseResources("test-alarms.conf")
+
+    val alarmMetadatas = ConfigParser.parseAlarmsMetadata(config)
+    alarmMetadatas.length shouldBe 3
+  }
+
+}
