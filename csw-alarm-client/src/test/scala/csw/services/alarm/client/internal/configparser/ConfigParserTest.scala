d0e3189a58b2edbf51246b4c5d1b9f06cc950fbd
Pritam Kadam <phkadam2008@gmail.com>
2018-07-25 16:44:29 +0530

    #DEOPSCSW-451: Pritam | Shubham: Add Config validator and parser tests for invalid scenarios.
    Co-authored-by: Shubham Jaybhaye <34642890+shubhamJay@users.noreply.github.com>

--- csw-alarm-client/src/test/scala/csw/services/alarm/client/internal/configparser/ConfigParserTest.scala
+++ csw-alarm-client/src/test/scala/csw/services/alarm/client/internal/configparser/ConfigParserTest.scala
@@ -1,5 +1,6 @@
 package csw.services.alarm.client.internal.configparser
 import com.typesafe.config.ConfigFactory
+import csw.services.alarm.api.exceptions.ConfigParseException
 import csw.services.alarm.api.models.AlarmSeverity._
 import csw.services.alarm.api.models.{AlarmMetadata, AlarmType}
 import org.scalatest.{FunSuite, Matchers}
@@ -8,7 +9,7 @@ import org.scalatest.{FunSuite, Matchers}
 class ConfigParserTest extends FunSuite with Matchers {
 
   test("should able to parse valid alarm metadata config file") {
-    val config = ConfigFactory.parseResources("test-alarm.conf")
+    val config = ConfigFactory.parseResources("test-alarms/valid-alarm.conf")
 
     val expectedAlarmMetadata = AlarmMetadata(
       subsystem = "nfiraos",
@@ -31,10 +32,36 @@ class ConfigParserTest extends FunSuite with Matchers {
   }
 
   test("should able to parse valid alarm metadata's config file") {
-    val config = ConfigFactory.parseResources("test-alarms.conf")
+    val config = ConfigFactory.parseResources("test-alarms/valid-alarms.conf")
 
     val alarmsMetadata = ConfigParser.parseAlarmsMetadata(config)
     alarmsMetadata.alarms.length shouldBe 3
+  }
+
+  test("should throw Exception while parsing invalid alarm metadata config file") {
+    val config = ConfigFactory.parseResources("test-alarms/invalid-alarm.conf")
+
+    val parseException = intercept[ConfigParseException] {
+      ConfigParser.parseAlarmMetadata(config)
+    }
+    // invalid-alarm.conf contains 3 errors:
+    // 1. isLatchable missing
+    // 2. invalid AlarmType
+    // 3. invalid supportedSeverities
+    // 3. invalid type for isAutoAcknowledgeable, expected boolean
+    parseException.reasons.length shouldBe 4
+  }
+
+  test("should throw Exception while parsing invalid alarms metadata config file") {
+    val config = ConfigFactory.parseResources("test-alarms/invalid-alarms.conf")
+
+    val parseException = intercept[ConfigParseException] {
+      ConfigParser.parseAlarmsMetadata(config)
+    }
+    // invalid-alarms.conf contains 2 errors:
+    // 1. subsystem missing from second alarm
+    // 2. invalid AlarmType in third alarm
+    parseException.reasons.length shouldBe 2
   }
 
 }
--- csw-alarm-client/src/test/scala/csw/services/alarm/client/internal/configparser/ConfigValidatorTest.scala
+++ csw-alarm-client/src/test/scala/csw/services/alarm/client/internal/configparser/ConfigValidatorTest.scala
@@ -3,16 +3,29 @@ import com.typesafe.config.ConfigFactory
 import csw.services.alarm.client.internal.configparser.SchemaRegistry._
 import org.scalatest.{FunSuite, Matchers}
 
+//DEOPSCSW-451: Create set of alarms based on Configuration file
 class ConfigValidatorTest extends FunSuite with Matchers {
 
   test("validation result should be successful for valid alarm metadata") {
-    val config = ConfigFactory.parseResources("test-alarm.conf")
+    val config = ConfigFactory.parseResources("test-alarms/valid-alarm.conf")
     ConfigValidator.validate(config, ALARM_SCHEMA) shouldEqual ValidationResult.Success
   }
 
   test("validation result should be successful for list of valid alarm's metadata") {
-    val config = ConfigFactory.parseResources("test-alarms.conf")
+    val config = ConfigFactory.parseResources("test-alarms/valid-alarms.conf")
     ConfigValidator.validate(config, ALARMS_SCHEMA) shouldEqual ValidationResult.Success
+  }
+
+  test("validation result should be failed for invalid alarm metadata") {
+    val config = ConfigFactory.parseResources("test-alarms/invalid-alarm.conf")
+    val result = ConfigValidator.validate(config, ALARM_SCHEMA)
+    result shouldBe a[ValidationResult.Failure]
+  }
+
+  test("validation result should be failed for invalid alarms metadata") {
+    val config = ConfigFactory.parseResources("test-alarms/invalid-alarms.conf")
+    val result = ConfigValidator.validate(config, ALARMS_SCHEMA)
+    result shouldBe a[ValidationResult.Failure]
   }
 
 }
