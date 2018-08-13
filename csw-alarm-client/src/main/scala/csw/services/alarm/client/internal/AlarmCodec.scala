b42d305c22fcacdbc30fa71e0c9d034b1e26f599
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2018-08-13 11:16:04 +0530

    #000 : Shubham | Bilal : Refactor AlarmCodec to reduce code duplication
    Co-authored-by: Shubham Jaybhaye <34642890+shubhamJay@users.noreply.github.com>

--- csw-alarm-client/src/main/scala/csw/services/alarm/client/internal/AlarmCodec.scala
+++ csw-alarm-client/src/main/scala/csw/services/alarm/client/internal/AlarmCodec.scala
@@ -12,14 +12,14 @@ import upickle.default._
 class AlarmCodec[K: ReadWriter, V: ReadWriter] extends RedisKeySpaceCodec[K, V] {
   private lazy val utf8StringCodec = new Utf8StringCodec()
 
-  override def encodeKey(key: K): ByteBuffer     = utf8StringCodec.encodeKey(writeJs(key).str)
-  override def decodeKey(byteBuf: ByteBuffer): K = read[K](Js.Str(utf8StringCodec.decodeKey(byteBuf)))
+  override def encodeKey(key: K): ByteBuffer     = utf8StringCodec.encodeKey(toKeyString(key))
+  override def decodeKey(byteBuf: ByteBuffer): K = fromKeyString(utf8StringCodec.decodeKey(byteBuf))
 
   override def encodeValue(value: V): ByteBuffer   = utf8StringCodec.encodeValue(write(value))
   override def decodeValue(byteBuf: ByteBuffer): V = read[V](utf8StringCodec.decodeValue(byteBuf))
 
-  override def toKeyString(key: K): String         = utf8StringCodec.decodeKey(encodeKey(key))
-  override def fromKeyString(keyString: String): K = decodeKey(utf8StringCodec.encodeKey(keyString))
+  override def toKeyString(key: K): String         = writeJs(key).str
+  override def fromKeyString(keyString: String): K = read[K](Js.Str(keyString))
 }
 
 object AlarmCodec extends AlarmRW {
