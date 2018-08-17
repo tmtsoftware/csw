3b7e39b8a8ba8910494e75ec35428666a101518e
Mushtaq Ahmed <mushtaq.a@gmail.com>
2018-08-17 12:24:45 +0530

    #000: Simple string codec

--- romaine/src/main/scala/romaine/codec/RomaineStringCodec.scala
+++ romaine/src/main/scala/romaine/codec/RomaineStringCodec.scala
@@ -7,4 +7,9 @@ trait RomaineStringCodec[T] {
 
 object RomaineStringCodec {
   def apply[T](implicit x: RomaineStringCodec[T]): RomaineStringCodec[T] = x
+
+  implicit val stringRomaineCodec: RomaineStringCodec[String] = new RomaineStringCodec[String] {
+    override def toString(x: String): String        = x
+    override def fromString(string: String): String = string
+  }
 }
