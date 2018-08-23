0944bcdb35f5fa44ded06aeb799c79548a79b6cc
Mushtaq Ahmed <mushtaq.a@gmail.com>
2018-08-23 17:35:28 +0530

    #000: move redis connection initialization method to romaine factory

--- romaine/src/main/scala/romaine/RomaineFactory.scala
+++ romaine/src/main/scala/romaine/RomaineFactory.scala
@@ -3,12 +3,14 @@ package romaine
 import io.lettuce.core.{RedisClient, RedisURI}
 import romaine.async.RedisAsyncApi
 import romaine.codec.{RomaineByteCodec, RomaineRedisCodec}
+import romaine.exceptions.RedisServerNotAvailable
 import romaine.reactive.RedisSubscriptionApi
 
 import scala.async.Async
 import scala.compat.java8.FutureConverters.CompletionStageOps
 import scala.concurrent.{ExecutionContext, Future}
 import scala.async.Async._
+import scala.util.control.NonFatal
 
 class RomaineFactory(redisClient: RedisClient)(implicit val ec: ExecutionContext) {
   def redisAsyncApi[K: RomaineByteCodec, V: RomaineByteCodec](redisURI: RedisURI): Future[RedisAsyncApi[K, V]] = {
@@ -22,8 +24,16 @@ class RomaineFactory(redisClient: RedisClient)(implicit val ec: ExecutionContext
     new RedisSubscriptionApi(
       () =>
         Async.async {
-          await(redisClient.connectPubSubAsync(new RomaineRedisCodec[K, V], await(redisURI)).toScala).reactive()
+          val connectionF = redisClient.connectPubSubAsync(new RomaineRedisCodec[K, V], await(redisURI)).toScala
+          await(init(() => connectionF)).reactive()
       }
     )
   }
+
+  private def init[T](conn: () ⇒ Future[T]): Future[T] = {
+    Future.unit.flatMap(_ => conn()).recover {
+      case NonFatal(ex) ⇒ throw RedisServerNotAvailable(ex.getCause)
+    }
+  }
+
 }
--- romaine/src/main/scala/romaine/reactive/RedisSubscriptionApi.scala
+++ romaine/src/main/scala/romaine/reactive/RedisSubscriptionApi.scala
@@ -4,11 +4,9 @@ import akka.NotUsed
 import akka.stream.scaladsl.{Keep, Source}
 import io.lettuce.core.pubsub.api.reactive.RedisPubSubReactiveCommands
 import reactor.core.publisher.FluxSink.OverflowStrategy
-import romaine.exceptions.RedisServerNotAvailable
 import romaine.extensions.SourceExtensions.RichSource
 
 import scala.concurrent.{ExecutionContext, Future}
-import scala.util.control.NonFatal
 
 class RedisSubscriptionApi[K, V](reactiveApiFactory: () => Future[RedisPubSubReactiveCommands[K, V]])(
     implicit ec: ExecutionContext
@@ -24,7 +22,7 @@ class RedisSubscriptionApi[K, V](reactiveApiFactory: () => Future[RedisPubSubRea
       overflowStrategy: OverflowStrategy,
       reactiveApiFactory: () => Future[RedisReactiveApi[K, V]]
   ): Source[RedisResult[K, V], RedisSubscription] = {
-    val reactiveApiF                                                    = init(reactiveApiFactory)
+    val reactiveApiF                                                    = reactiveApiFactory()
     val futureSource                                                    = reactiveApiF.map(_.observe(overflowStrategy))
     val redisKeyValueSource: Source[RedisResult[K, V], Future[NotUsed]] = Source.fromFutureSource(futureSource)
     val connectedF                                                      = futureSource.flatMap(_ => reactiveApiF.flatMap(_.subscribe(keys)))
@@ -35,11 +33,5 @@ class RedisSubscriptionApi[K, V](reactiveApiFactory: () => Future[RedisPubSubRea
         case (killSwitch, terminationSignal) ⇒
           new RedisSubscriptionImpl(keys, connectedF, killSwitch, terminationSignal, reactiveApiF)
       }
-  }
-
-  private def init[T](conn: () ⇒ Future[T]): Future[T] = {
-    Future.unit.flatMap(_ => conn()).recover {
-      case NonFatal(ex) ⇒ throw RedisServerNotAvailable(ex.getCause)
-    }
   }
 }
