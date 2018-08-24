1afde543050b497f4e7d2a46ee537fc5e3e53dc9
Mushtaq Ahmed <mushtaq.a@gmail.com>
2018-08-24 19:55:01 +0530

    #000: Use java.time API to calculate shelving duration

--- csw-alarm-client/src/main/scala/csw/services/alarm/client/internal/shelve/ShelveTimeoutActor.scala
+++ csw-alarm-client/src/main/scala/csw/services/alarm/client/internal/shelve/ShelveTimeoutActor.scala
@@ -1,18 +1,15 @@
 package csw.services.alarm.client.internal.shelve
 
-import java.time.{ZoneOffset, ZonedDateTime}
+import java.time.{Duration, LocalDate, ZoneOffset, ZonedDateTime}
 
 import akka.actor.typed.Behavior
 import akka.actor.typed.scaladsl.{Behaviors, TimerScheduler}
-import csw.services.alarm.api.models.Key.AlarmKey
 import csw.services.alarm.client.internal.AlarmServiceLogger
-import csw.services.alarm.client.internal.extensions.ZonedDateTimeExtensions.{RichInt, RichZonedDateTime}
-import csw.services.alarm.client.internal.shelve.ShelveTimeoutMessage.{
-  CancelShelveTimeout,
-  ScheduleShelveTimeout,
-  ShelveHasTimedOut
-}
+import csw.services.alarm.client.internal.shelve.ShelveTimeoutMessage._
 import csw.services.logging.scaladsl.Logger
+
+import scala.compat.java8.DurationConverters.DurationOps
+import scala.concurrent.duration.FiniteDuration
 
 object ShelveTimeoutActor {
   def behavior(
@@ -27,21 +24,22 @@ object ShelveTimeoutActor {
         log.debug(s"ShelveTimeoutActor received message :[$msg]")
 
         msg match {
-          case ScheduleShelveTimeout(key) ⇒ scheduleShelveTimeout(key, shelveTimeoutHour, timerScheduler)
-          case CancelShelveTimeout(key)   ⇒ timerScheduler.cancel(key.value)
-          case ShelveHasTimedOut(key)     ⇒ alarm.unshelve(key)
+          case ScheduleShelveTimeout(key) ⇒
+            timerScheduler.startSingleTimer(key.value, ShelveHasTimedOut(key), durationUntil(shelveTimeoutHour))
+          case CancelShelveTimeout(key) ⇒
+            timerScheduler.cancel(key.value)
+          case ShelveHasTimedOut(key) ⇒
+            alarm.unshelve(key)
         }
         Behaviors.same
       }
     }
 
-  private def scheduleShelveTimeout(
-      key: AlarmKey,
-      shelveTimeoutHour: Int,
-      timerScheduler: TimerScheduler[ShelveTimeoutMessage]
-  ): Unit = {
-    val currentTime   = ZonedDateTime.now(ZoneOffset.UTC)
-    val shelveTimeout = shelveTimeoutHour.toHourOfDay - currentTime
-    timerScheduler.startSingleTimer(key.value, ShelveHasTimedOut(key), shelveTimeout)
+  def durationUntil(shelveTimeoutHour: Int): FiniteDuration = {
+    val currentTime        = ZonedDateTime.now(ZoneOffset.UTC)
+    val targetTimeForToday = LocalDate.now().atStartOfDay(ZoneOffset.UTC).withHour(shelveTimeoutHour)
+    val durationForToday   = Duration.between(currentTime, targetTimeForToday)
+    val duration           = if (durationForToday.isNegative) durationForToday.plusDays(1) else durationForToday
+    duration.toScala
   }
 }
