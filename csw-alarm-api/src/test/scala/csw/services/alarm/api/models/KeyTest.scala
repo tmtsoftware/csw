8572092978bcb8d639d780ee34a2e718ee414565
Dollyg <dollyg@thoughtworks.com>
2018-08-30 17:37:37 +0530

    # DEOPSCSW-503: Dolly | Pritam: Fix API tests to use new key separator
    Co-authored-by: PK <phkadam2008@gmail.com>

--- csw-alarm-api/src/test/scala/csw/services/alarm/api/models/KeyTest.scala
+++ csw-alarm-api/src/test/scala/csw/services/alarm/api/models/KeyTest.scala
@@ -2,6 +2,7 @@ package csw.services.alarm.api.models
 import csw.messages.params.models.Subsystem
 import csw.messages.params.models.Subsystem.NFIRAOS
 import csw.services.alarm.api.models.Key.{AlarmKey, ComponentKey, GlobalKey, SubsystemKey}
+import csw.services.alarm.api.internal.Separators.KeySeparator
 import org.scalatest.prop.TableDrivenPropertyChecks
 import org.scalatest.{FunSuite, Matchers}
 
@@ -9,21 +10,21 @@ import org.scalatest.{FunSuite, Matchers}
 class KeyTest extends FunSuite with Matchers with TableDrivenPropertyChecks {
   test("AlarmKey should be representing a unique alarm") {
     val tromboneAxisHighLimitAlarm = AlarmKey(NFIRAOS, "trombone", "tromboneAxisHighLimitAlarm")
-    tromboneAxisHighLimitAlarm.value shouldEqual "nfiraos.trombone.tromboneaxishighlimitalarm"
+    tromboneAxisHighLimitAlarm.value shouldEqual s"nfiraos${KeySeparator}trombone${KeySeparator}tromboneaxishighlimitalarm"
   }
 
   test("SubsystemKey should be representing keys for all alarms of a subsystem") {
     val subsystemKey = SubsystemKey(NFIRAOS)
-    subsystemKey.value shouldEqual "nfiraos.*.*"
+    subsystemKey.value shouldEqual s"nfiraos$KeySeparator*$KeySeparator*"
   }
 
   test("ComponentKey should be representing keys for all alarms of a component") {
     val subsystemKey = ComponentKey(NFIRAOS, "trombone")
-    subsystemKey.value shouldEqual "nfiraos.trombone.*"
+    subsystemKey.value shouldEqual s"nfiraos${KeySeparator}trombone$KeySeparator*"
   }
 
   test("GlobalKey should be representing keys for all alarms") {
-    GlobalKey.value shouldEqual "*.*.*"
+    GlobalKey.value shouldEqual s"*$KeySeparator*$KeySeparator*"
   }
 
   val invalidCharacers = List("*", "[", "]", "-", "^")
