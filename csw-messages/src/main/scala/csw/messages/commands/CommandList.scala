b71aaa140d3e77bf4a75f2b49bcd523c5185fed2
Dollyg <dollyg@thoughtworks.com>
2018-09-05 10:45:26 +0530

    #000: Dolly | Mushtaq | Poorva : Remove redundant models from csw-messages
    Co-authored-by: Mushtaq Ahmed <5066+mushtaq@users.noreply.github.com>
    Co-authored-by: Poorva Gokhale <13188905+Poorva17@users.noreply.github.com>

--- csw-messages/src/main/scala/csw/messages/commands/CommandList.scala
+++
@@ -1,8 +0,0 @@
-package csw.messages.commands
-
-/**
- * Contains a list of commands that can be sent to a sequencer
- *
- * @param commands sequence of SequenceCommand
- */
-final case class CommandList(commands: Seq[SequenceCommand])
