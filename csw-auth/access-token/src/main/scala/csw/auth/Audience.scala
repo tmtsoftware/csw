1324161fcd832ad74563e5ed6dd933da2c4c52bf
Pritam Kadam <kpritam@thoughtworks.com>
2018-11-17 18:41:07 +0530

    #000: Small refactoring in Audience case class.

--- csw-auth/access-token/src/main/scala/csw/auth/Audience.scala
+++ csw-auth/access-token/src/main/scala/csw/auth/Audience.scala
@@ -1,24 +1,25 @@
 package csw.auth
+
 import play.api.libs.json._
 
 case class Audience(value: Seq[String])
 
 object Audience {
 
-  def apply(aud: String): Audience = new Audience(Seq(aud))
+  def apply(aud: String): Audience = Audience(Seq(aud))
 
-  def apply(): Audience = new Audience(Seq.empty)
+  def apply(): Audience = Audience(Seq.empty)
 
   implicit val format: Format[Audience] = new Format[Audience] {
     override def writes(obj: Audience): JsValue = obj.value match {
-      case List(head)      => JsString(head)
-      case l: List[String] => JsArray(l.map(JsString))
+      case head :: Nil ⇒ JsString(head)
+      case as          ⇒ JsArray(as.map(JsString))
     }
 
     override def reads(json: JsValue): JsResult[Audience] = json match {
-      case JsString(s)  => JsSuccess(Audience(s))
-      case JsArray(arr) => JsSuccess(Audience(arr.map(_.as[String])))
-      case JsNull       => JsSuccess(Audience())
+      case JsNull       ⇒ JsSuccess(Audience())
+      case JsString(s)  ⇒ JsSuccess(Audience(s))
+      case JsArray(arr) ⇒ JsSuccess(Audience(arr.map(_.as[String])))
       case _            => JsError(JsPath \ "aud", "parsing failed due to invalid value")
     }
   }
