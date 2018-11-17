164e4991588a1cf6468a3f18ae17a90b72b5aae6
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2018-11-17 17:41:35 +0530

    #DEOPSCSW-579: : Refactor access token
    to support more cases of serialisation & deserialisation

--- csw-auth/access-token/src/main/scala/csw/auth/Audience.scala
+++ csw-auth/access-token/src/main/scala/csw/auth/Audience.scala
@@ -1,19 +1,25 @@
 package csw.auth
-
 import play.api.libs.json._
 
-case class Audience(aud: List[String])
+case class Audience(value: Seq[String])
 
 object Audience {
 
+  def apply(aud: String): Audience = new Audience(Seq(aud))
+
+  def apply(): Audience = new Audience(Seq.empty)
+
   implicit val format: Format[Audience] = new Format[Audience] {
-    override def writes(obj: Audience): JsValue = JsArray(obj.aud.map(JsString))
+    override def writes(obj: Audience): JsValue = obj.value match {
+      case List(head)      => JsString(head)
+      case l: List[String] => JsArray(l.map(JsString))
+    }
 
     override def reads(json: JsValue): JsResult[Audience] = json match {
-      case JsString(s)  ⇒ JsSuccess(Audience(List(s)))
-      case JsArray(arr) ⇒ JsSuccess(Audience(arr.toList.map(_.as[String])))
-      case _            ⇒ JsError("Unable to parse")
+      case JsString(s)  => JsSuccess(Audience(s))
+      case JsArray(arr) => JsSuccess(Audience(arr.map(_.as[String])))
+      case JsNull       => JsSuccess(Audience())
+      case _            => JsError(JsPath \ "aud", "parsing failed due to invalid value")
     }
   }
-
 }
