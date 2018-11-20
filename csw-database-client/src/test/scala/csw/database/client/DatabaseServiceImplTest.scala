97d3a4a4015d401959060952b5c18657de21665b
Bharat Singh <10626464+Bharat1103@users.noreply.github.com>
2018-11-20 17:38:36 +0530

    #DEOPSCSW-601: Saloni | Bharat :  1. Add test case for creating a database, 2. Add test case for join and group by query
    Co-authored-by: Saloni Vithalani <skvithalani@yahoo.com>

--- csw-database-client/src/test/scala/csw/database/client/DatabaseServiceImplTest.scala
+++ csw-database-client/src/test/scala/csw/database/client/DatabaseServiceImplTest.scala
@@ -3,7 +3,9 @@ package csw.database.client
 import akka.actor.ActorSystem
 import csw.database.api.scaladsl.DatabaseService
 import csw.database.client.scaladsl.DatabaseServiceFactory
+import org.scalatest.concurrent.PatienceConfiguration.Timeout
 import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.time.{Seconds, Span}
 import org.scalatest.{BeforeAndAfterAll, FunSuite, Matchers}
 import ru.yandex.qatools.embed.postgresql.EmbeddedPostgres
 import ru.yandex.qatools.embed.postgresql.distribution.Version
@@ -25,40 +27,94 @@ class DatabaseServiceImplTest extends FunSuite with Matchers with ScalaFutures w
   private val databaseService: DatabaseService = factory.make(host, port, dbName)
 
   override def afterAll(): Unit = {
+    databaseService.execute("DROP DATABASE box_office;").futureValue(Timeout(Span(5, Seconds)))
+    databaseService.execute("DROP TABLE budget;").futureValue
     databaseService.execute("DROP TABLE films;").futureValue
     postgres.stop()
     system.terminate().futureValue
   }
 
-  test("should be able to create table and insert data in it") {
-    databaseService.execute("CREATE TABLE films (code char(10));").futureValue
-    databaseService.execute("INSERT INTO films VALUES ('movie_1');").futureValue
-    databaseService.execute("INSERT INTO films VALUES ('movie_4');").futureValue
-    databaseService.execute("INSERT INTO films VALUES ('movie_2');").futureValue
+  test("should be able to create a new Database") {
+    databaseService.execute("CREATE DATABASE box_office;").futureValue(Timeout(Span(5, Seconds)))
+    val resultSet = databaseService
+      .executeQuery(
+        """SELECT datname FROM pg_database WHERE datistemplate = false;"""
+      )
+      .futureValue
+    val databaseList =
+      Iterator.from(0).takeWhile(_ => resultSet.next()).map(_ => resultSet.getString(1)).toList
+    assert(databaseList contains "box_office")
+  }
+
+  test("should be able to create table with unique default IDs and insert data in it") {
+    databaseService
+      .execute(
+        "CREATE TABLE films (id SERIAL PRIMARY KEY, name VARCHAR (10) UNIQUE NOT NULL);"
+      )
+      .futureValue
+    databaseService.execute("INSERT INTO films VALUES (DEFAULT, 'movie_1');").futureValue
+    databaseService.execute("INSERT INTO films VALUES (DEFAULT, 'movie_4');").futureValue
+    databaseService.execute("INSERT INTO films VALUES (DEFAULT, 'movie_2');").futureValue
     val resultSet = databaseService.executeQuery("SELECT count(*) AS rowCount from films;").futureValue
     resultSet.next()
     resultSet.getInt("rowCount") shouldBe 3
+  }
 
+  test("should be able to create table with foreign key and insert data in it") {
+    databaseService
+      .execute(
+        """CREATE TABLE budget (
+          |id SERIAL PRIMARY KEY,
+          |movie_id INTEGER,
+          |movie_name VARCHAR(10),
+          |amount NUMERIC,
+          |FOREIGN KEY (movie_id) REFERENCES films(id) ON DELETE CASCADE
+          |);""".stripMargin
+      )
+      .futureValue
+    databaseService.execute("INSERT INTO budget VALUES (DEFAULT, 1, 'movie_1', 5000);").futureValue
+    databaseService.execute("INSERT INTO budget VALUES (DEFAULT, 2, 'movie_4', 6000);").futureValue
+    databaseService.execute("INSERT INTO budget VALUES (DEFAULT, 3, 'movie_2', 7000);").futureValue
+    databaseService.execute("INSERT INTO budget VALUES (DEFAULT, 3, 'movie_2', 3000);").futureValue
+
+    val resultSet =
+      databaseService.executeQuery("""
+          |SELECT films.name, SUM(budget.amount)
+          |    FROM films
+          |    INNER JOIN budget
+          |    ON films.id = budget.movie_id
+          |    GROUP BY  films.name;
+        """.stripMargin).futureValue
+
+    val result = Iterator
+      .from(0)
+      .takeWhile(_ => resultSet.next())
+      .map { _ =>
+        (resultSet.getString(1), resultSet.getInt(2))
+      }
+      .toList
+
+    assert(result === List(("movie_1", 5000), ("movie_4", 6000), ("movie_2", 10000)))
   }
 
   test("should be able to update record") {
-    databaseService.execute("UPDATE films SET code = 'movie_3' WHERE code = 'movie_2'").futureValue
+    databaseService.execute("UPDATE films SET name = 'movie_3' WHERE name = 'movie_2'").futureValue
     val resultSet =
-      databaseService.executeQuery("SELECT count(*) AS rowCount from films where code = 'movie_2';").futureValue
+      databaseService.executeQuery("SELECT count(*) AS rowCount from films where name = 'movie_2';").futureValue
     resultSet.next()
     resultSet.getInt("rowCount") shouldBe 0
   }
 
   test("should be able to delete records") {
-    databaseService.execute("DELETE from films WHERE code = 'movie_4'").futureValue
+    databaseService.execute("DELETE from films WHERE name = 'movie_4'").futureValue
     val resultSet = databaseService.executeQuery("SELECT count(*) AS rowCount from films;").futureValue
     resultSet.next()
     resultSet.getInt("rowCount") shouldBe 2
   }
 
   test("should be able to query records from the table") {
-    val resultSet = databaseService.executeQuery("SELECT * FROM films where code = 'movie_1';").futureValue
+    val resultSet = databaseService.executeQuery("SELECT * FROM films where name = 'movie_1';").futureValue
     resultSet.next()
-    resultSet.getString("code").trim shouldEqual "movie_1"
+    resultSet.getString("name").trim shouldEqual "movie_1"
   }
 }
