82f27fd6a6f586b06386efe1fb365ac71a1a3859
Dollyg <dollyg@thoughtworks.com>
2018-11-20 23:45:10 +0530

    #DEOPSCSW-530: Dolly : Move registration of native library to static block as it is a standard way

--- csw-time-client/src/main/scala/csw/time/client/internal/TimeLibrary.java
+++ csw-time-client/src/main/scala/csw/time/client/internal/TimeLibrary.java
@@ -4,13 +4,14 @@ import com.sun.jna.Native;
 import csw.time.client.internal.native_models.NTPTimeVal;
 import csw.time.client.internal.native_models.TimeSpec;
 
+// TimeLibrary is responsible for making native calls for time
 public class TimeLibrary {
 
-    public TimeLibrary() {
+    static {
         Native.register("c");
     }
 
-    public native int clock_gettime(int clockId, TimeSpec timeSpec);
+    public static native int clock_gettime(int clockId, TimeSpec timeSpec);
 
-    public native int ntp_gettimex(NTPTimeVal ntpTimeVal);
+    public static native int ntp_gettimex(NTPTimeVal ntpTimeVal);
 }
--- csw-time-client/src/main/scala/csw/time/client/internal/TimeServiceImpl.scala
+++ csw-time-client/src/main/scala/csw/time/client/internal/TimeServiceImpl.scala
@@ -14,12 +14,9 @@ class TimeServiceImpl() extends TimeService {
 
   override def TAITime(): CswInstant = time(TimeScales.TAIScale)
 
-  // TimeLibrary is responsible to make native calls
-  private val timeLib: TimeLibrary = new TimeLibrary()
-
   override def TAIOffset(): Int = {
     val timeVal = new NTPTimeVal()
-    timeLib.ntp_gettimex(timeVal)
+    TimeLibrary.ntp_gettimex(timeVal)
     timeVal.tai
   }
 
@@ -30,7 +27,7 @@ class TimeServiceImpl() extends TimeService {
     }
 
     val timeSpec = new TimeSpec()
-    timeLib.clock_gettime(clockId, timeSpec)
+    TimeLibrary.clock_gettime(clockId, timeSpec)
 
     val instant = Instant.ofEpochSecond(timeSpec.seconds, timeSpec.nanoseconds)
     CswInstant(instant, scale)
