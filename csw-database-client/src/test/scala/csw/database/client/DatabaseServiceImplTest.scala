5fe8389dcf1df8f84dbab02de0e2f0d8116fc919
Bharat Singh <10626464+Bharat1103@users.noreply.github.com>
2018-11-20 11:56:24 +0530

    #DEOPSCSW-601: Saloni | Bharat :  Use scala futures in test cases to get the results without using Await.result
    Co-authored-by: Saloni Vithalani <skvithalani@yahoo.com>

--- csw-database-client/src/test/scala/csw/database/client/DatabaseServiceImplTest.scala
+++ csw-database-client/src/test/scala/csw/database/client/DatabaseServiceImplTest.scala
@@ -1,19 +1,16 @@
 package csw.database.client
 
-import java.sql.{Connection, DriverManager}
-
 import akka.actor.ActorSystem
 import csw.database.api.scaladsl.DatabaseService
-import csw.database.client.internal.DatabaseServiceImpl
 import csw.database.client.scaladsl.DatabaseServiceFactory
+import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.{BeforeAndAfterAll, FunSuite, Matchers}
 import ru.yandex.qatools.embed.postgresql.EmbeddedPostgres
 import ru.yandex.qatools.embed.postgresql.distribution.Version
 
-import scala.concurrent.duration.DurationDouble
-import scala.concurrent.{Await, ExecutionContext}
+import scala.concurrent.ExecutionContext
 
-class DatabaseServiceImplTest extends FunSuite with Matchers with BeforeAndAfterAll {
+class DatabaseServiceImplTest extends FunSuite with Matchers with ScalaFutures with BeforeAndAfterAll {
   private val system                = ActorSystem("test")
   implicit val ec: ExecutionContext = system.dispatcher
 
@@ -28,41 +25,39 @@ class DatabaseServiceImplTest extends FunSuite with Matchers with BeforeAndAfter
   private val databaseService: DatabaseService = factory.make(host, port, dbName)
 
   override def afterAll(): Unit = {
+    databaseService.execute("DROP TABLE films;").futureValue
     postgres.stop()
-    Await.result(system.terminate(), 5.seconds)
+    system.terminate().futureValue
   }
 
   test("should be able to create table and insert data in it") {
-    Await.result(databaseService.execute("CREATE TABLE films (code char(10));"), 5.seconds)
-    Await.result(databaseService.execute("INSERT INTO films VALUES ('movie_1');"), 5.seconds)
-    Await.result(databaseService.execute("INSERT INTO films VALUES ('movie_4');"), 5.seconds)
-    Await.result(databaseService.execute("INSERT INTO films VALUES ('movie_2');"), 5.seconds)
-
-    val resultSet = Await.result(databaseService.executeQuery("SELECT count(*) AS rowCount from films;"), 5.seconds)
+    databaseService.execute("CREATE TABLE films (code char(10));").futureValue
+    databaseService.execute("INSERT INTO films VALUES ('movie_1');").futureValue
+    databaseService.execute("INSERT INTO films VALUES ('movie_4');").futureValue
+    databaseService.execute("INSERT INTO films VALUES ('movie_2');").futureValue
+    val resultSet = databaseService.executeQuery("SELECT count(*) AS rowCount from films;").futureValue
     resultSet.next()
     resultSet.getInt("rowCount") shouldBe 3
 
   }
 
   test("should be able to update record") {
-    Await.result(databaseService.execute("UPDATE films SET code = 'movie_3' WHERE code = 'movie_2'"), 5.seconds)
-
+    databaseService.execute("UPDATE films SET code = 'movie_3' WHERE code = 'movie_2'").futureValue
     val resultSet =
-      Await.result(databaseService.executeQuery("SELECT count(*) AS rowCount from films where code = 'movie_2';"), 5.seconds)
+      databaseService.executeQuery("SELECT count(*) AS rowCount from films where code = 'movie_2';").futureValue
     resultSet.next()
     resultSet.getInt("rowCount") shouldBe 0
   }
 
   test("should be able to delete records") {
-    Await.result(databaseService.execute("DELETE from films WHERE code = 'movie_4'"), 5.seconds)
-    val resultSet = Await.result(databaseService.executeQuery("SELECT count(*) AS rowCount from films;"), 5.seconds)
+    databaseService.execute("DELETE from films WHERE code = 'movie_4'").futureValue
+    val resultSet = databaseService.executeQuery("SELECT count(*) AS rowCount from films;").futureValue
     resultSet.next()
     resultSet.getInt("rowCount") shouldBe 2
   }
 
   test("should be able to query records from the table") {
-    val resultSet = Await.result(databaseService.executeQuery("SELECT * FROM films where code = 'movie_1';"), 5.seconds)
-
+    val resultSet = databaseService.executeQuery("SELECT * FROM films where code = 'movie_1';").futureValue
     resultSet.next()
     resultSet.getString("code").trim shouldEqual "movie_1"
   }
--- csw-database-client/src/test/scala/csw/database/client/JDatabaseServiceImplTest.java
+++ csw-database-client/src/test/scala/csw/database/client/JDatabaseServiceImplTest.java
@@ -6,6 +6,7 @@ import csw.database.client.scaladsl.DatabaseServiceFactory;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Test;
+import org.scalatest.junit.JUnitSuite;
 import ru.yandex.qatools.embed.postgresql.EmbeddedPostgres;
 import ru.yandex.qatools.embed.postgresql.distribution.Version;
 import scala.concurrent.Await;
@@ -20,11 +21,11 @@ import java.util.concurrent.TimeoutException;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
 
-public class JDatabaseServiceImplTest {
+public class JDatabaseServiceImplTest extends JUnitSuite {
 
     private static ActorSystem system;
     private static EmbeddedPostgres postgres;
-    private IDatabaseService databaseService;
+    private static IDatabaseService databaseService;
 
     public JDatabaseServiceImplTest() throws IOException {
         int port = 5434;
@@ -40,19 +41,43 @@ public class JDatabaseServiceImplTest {
 
     @AfterClass
     public static void afterAll() throws Exception {
+        databaseService.execute("DROP TABLE Jfilms;").get(5, SECONDS);
         postgres.stop();
         Await.result(system.terminate(), Duration.apply(5, SECONDS));
     }
 
     @Test
     public void shouldBeAbleToCreateTableAndInsertDataInIt() throws InterruptedException, ExecutionException, TimeoutException, SQLException {
-        databaseService.execute("CREATE TABLE films (code char(10));").get(5, SECONDS);
-        databaseService.execute("INSERT INTO films VALUES ('movie_1');").get(5, SECONDS);
-        databaseService.execute("INSERT INTO films VALUES ('movie_4');").get(5, SECONDS);
-        databaseService.execute("INSERT INTO films VALUES ('movie_2');").get(5, SECONDS);
+        databaseService.execute("CREATE TABLE Jfilms (code char(10));").get(5, SECONDS);
+        databaseService.execute("INSERT INTO Jfilms VALUES ('movie_1');").get(5, SECONDS);
+        databaseService.execute("INSERT INTO Jfilms VALUES ('movie_4');").get(5, SECONDS);
+        databaseService.execute("INSERT INTO Jfilms VALUES ('movie_2');").get(5, SECONDS);
 
-        ResultSet resultSet = databaseService.executeQuery("SELECT count(*) AS rowCount from films;").get(5, SECONDS);
+        ResultSet resultSet = databaseService.executeQuery("SELECT count(*) AS rowCount from Jfilms;").get(5, SECONDS);
         resultSet.next();
         Assert.assertEquals(3, resultSet.getInt("rowCount"));
+    }
+
+    @Test
+    public void shouldBeAbleToUpdateRecordInTable() throws InterruptedException, ExecutionException, TimeoutException, SQLException {
+        databaseService.execute("UPDATE Jfilms SET code = 'movie_3' WHERE code = 'movie_2'").get(5, SECONDS);
+        ResultSet resultSet = databaseService.executeQuery("SELECT count(*) AS rowCount from Jfilms where code = 'movie_2';").get(5, SECONDS);
+        resultSet.next();
+        Assert.assertEquals(0, resultSet.getInt("rowCount"));
+    }
+
+    @Test
+    public void shouldBeAbleToDeleteRecordsFromTable() throws InterruptedException, ExecutionException, TimeoutException, SQLException {
+        databaseService.execute("DELETE from Jfilms WHERE code = 'movie_4'").get(5, SECONDS);
+        ResultSet resultSet = databaseService.executeQuery("SELECT count(*) AS rowCount from Jfilms;").get(5, SECONDS);
+        resultSet.next();
+        Assert.assertEquals(2, resultSet.getInt("rowCount"));
+    }
+
+    @Test
+    public void shouldBeAbleToQueryRecordsFromTheTable() throws InterruptedException, ExecutionException, TimeoutException, SQLException {
+        ResultSet resultSet = databaseService.executeQuery("SELECT * FROM Jfilms where code = 'movie_1';").get(5, SECONDS);
+        resultSet.next();
+        Assert.assertEquals("movie_1", resultSet.getString("code").trim());
     }
 }
