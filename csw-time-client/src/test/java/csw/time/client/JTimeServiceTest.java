a24c83deee1c75adecc21af45e47966605693452
Dollyg <dollyg@thoughtworks.com>
2018-11-27 16:05:11 +0530

    #DEOPSCSW-533 : Dolly | Puneet : Access parts of date, time in java
    Co-authored-by: Puneet Patwari <puneet.patwari@thoughtworks.com>

--- csw-time-client/src/test/java/csw/time/client/JTimeServiceTest.java
+++ csw-time-client/src/test/java/csw/time/client/JTimeServiceTest.java
@@ -8,11 +8,15 @@ import csw.time.client.javadsl.tags.LinuxTag;
 import org.junit.Test;
 
 import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 import static org.junit.Assert.assertEquals;
 
 @LinuxTag
 public class JTimeServiceTest {
+
+    //------------------------------UTC-------------------------------
 
     //DEOPSCSW-533: Access parts of UTC date.time in Java and Scala
     @Test
@@ -26,6 +30,25 @@ public class JTimeServiceTest {
 
         assertEquals(expectedMillis, utcInstant.value().toEpochMilli());
     }
+
+    //DEOPSCSW-533: Access parts of UTC date.time in Java and Scala
+    @Test
+    public void shouldAccessPartsOfUtcInstant(){
+        UtcInstant utcInstant = new UtcInstant(Instant.parse("2007-12-03T10:15:30.00Z"));
+
+        ZoneId hstZone = ZoneId.of("-10:00");
+
+        ZonedDateTime hstZDT = utcInstant.value().atZone(hstZone);
+
+        assertEquals(2007, hstZDT.getYear());
+        assertEquals(12, hstZDT.getMonth().getValue());
+        assertEquals(3, hstZDT.getDayOfMonth());
+        assertEquals(0, hstZDT.getHour()); // since HST is -10:00 from UTC
+        assertEquals(15, hstZDT.getMinute());
+        assertEquals(30, hstZDT.getSecond());
+    }
+
+    //------------------------------TAI-------------------------------
 
     //DEOPSCSW-536: Access parts of TAI date/time in Java and Scala
     //DEOPSCSW-530: SPIKE: Get TAI offset and convert to UTC and Vice Versa
@@ -52,5 +75,22 @@ public class JTimeServiceTest {
         int offset = timeService.taiOffset();
 
         assertEquals(expectedOffset, offset);
+    }
+
+    //DEOPSCSW-536: Access parts of TAI date.time in Java and Scala
+    @Test
+    public void shouldAccessPartsOfTaiInstant(){
+        TaiInstant taiInstant = new TaiInstant(Instant.parse("2007-12-03T10:15:30.00Z"));
+
+        ZoneId hstZone = ZoneId.of("-10:00");
+
+        ZonedDateTime hstZDT = taiInstant.value().atZone(hstZone);
+
+        assertEquals(2007, hstZDT.getYear());
+        assertEquals(12, hstZDT.getMonth().getValue());
+        assertEquals(3, hstZDT.getDayOfMonth());
+        assertEquals(0, hstZDT.getHour()); // since HST is -10:00 from UTC
+        assertEquals(15, hstZDT.getMinute());
+        assertEquals(30, hstZDT.getSecond());
     }
 }
