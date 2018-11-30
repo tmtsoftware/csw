7a0f2bdbbdac091fad0a361dd4933b524b4a61c0
Dollyg <dollyg@thoughtworks.com>
2018-11-30 14:41:34 +0530

    #000 : Dolly | Puneet : Fix memory corruption errors coming from JNA glibc library. They were caused due to incorrect definition of NTPTimeVal Structure. Co-authored-by: Puneet Patwari <puneet.patwari@thoughtworks.com>

--- csw-time-client/src/main/scala/csw/time/client/internal/native_models/NTPTimeVal.java
+++ csw-time-client/src/main/scala/csw/time/client/internal/native_models/NTPTimeVal.java
@@ -7,13 +7,18 @@ import java.util.Arrays;
 import java.util.List;
 
 public class NTPTimeVal extends Structure {
-    public TimeVal time;        /* Current time */
-    public NativeLong maxerror;       /* Maximum error */
+    public TimeVal time;          /* Current time */
+    public NativeLong maxerror;   /* Maximum error */
     public NativeLong esterror;
     public int tai;
 
+    public NativeLong unused1;
+    public NativeLong unused2;
+    public NativeLong unused3;
+    public NativeLong unused4;
+
     @Override
     protected List<String> getFieldOrder() {
-        return Arrays.asList("time", "maxerror", "esterror", "tai");
+        return Arrays.asList("time", "maxerror", "esterror", "tai" , "unused1", "unused2", "unused3", "unused4");
     }
 }
