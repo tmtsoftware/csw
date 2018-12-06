f4a3c032e29f894e4eea68eeb08d1814176df2bc
Pritam Kadam <kpritam@thoughtworks.com>
2018-12-06 20:41:08 +0530

    #DEOPSCSW-539:: Pritam | Dolly :  Provide helpers to get local, hawaii etc. ZonedDateTime from UtcInstant and TaiInstant
    Co-authored-by: Dolly <10329479+Dollyg@users.noreply.github.com>

--- csw-time-api/src/main/scala/csw/time/api/models/CswInstant.scala
+++ csw-time-api/src/main/scala/csw/time/api/models/CswInstant.scala
@@ -1,9 +1,13 @@
 package csw.time.api.models
 
-import java.time.Instant
+import java.time.{Instant, ZoneId, ZonedDateTime}
 
 sealed trait CswInstant {
   val value: Instant
+
+  def atZone(zoneId: ZoneId): ZonedDateTime = value.atZone(zoneId)
+  def atLocal: ZonedDateTime                = atZone(ZoneId.systemDefault())
+  def atHawaii: ZonedDateTime               = atZone(ZoneId.of("US/Hawaii"))
 }
 
 object CswInstant {
--- csw-time-api/src/test/scala/csw/time/api/models/CswInstantTest.scala
+++ csw-time-api/src/test/scala/csw/time/api/models/CswInstantTest.scala
@@ -39,4 +39,35 @@ class CswInstantTest extends FunSuite with Matchers {
     hstZDT.getSecond shouldBe 30
   }
 
+  // DEOPSCSW-539: Ability to read local time that is synchronized with PTP time, at remote observing sites
+  test("should get hawaii/local date time as well as date time at provided zone id from UtcInstant") {
+    val dateTimeStr        = "2018-12-06T14:29:41.204Z"
+    val hawaiiDateTimeStr  = "2018-12-06T04:29:41.204-10:00[US/Hawaii]"
+    val localDateTimeStr   = "2018-12-06T19:59:41.204+05:30[Asia/Kolkata]"
+    val newyorkDateTimeStr = "2018-12-06T09:29:41.204-05:00[America/New_York]"
+
+    // Using TimeService you can get this utcInstant, which is synchronized with PTP
+    // and then you can use atHawaii, atLocal and atZone helpers defined on CswInstant
+    val utcInstant = UtcInstant(Instant.parse(dateTimeStr))
+
+    utcInstant.atHawaii.toString shouldBe hawaiiDateTimeStr
+    utcInstant.atLocal.toString shouldBe localDateTimeStr
+    utcInstant.atZone(ZoneId.of("America/New_York")).toString shouldBe newyorkDateTimeStr
+  }
+
+  // DEOPSCSW-539: Ability to read local time that is synchronized with PTP time, at remote observing sites
+  test("should get hawaii/local date time as well as date time at provided zone id from TaiInstant") {
+    val taiDateTimeStr     = "2018-12-06T14:29:41.204Z"
+    val hawaiiDateTimeStr  = "2018-12-06T04:29:41.204-10:00[US/Hawaii]"
+    val localDateTimeStr   = "2018-12-06T19:59:41.204+05:30[Asia/Kolkata]"
+    val newyorkDateTimeStr = "2018-12-06T09:29:41.204-05:00[America/New_York]"
+
+    // Using TimeService you can get this taiInstant, which is synchronized with PTP
+    // and then you can use atHawaii, atLocal and atZone helpers defined on CswInstant
+    val taiInstant = TaiInstant(Instant.parse(taiDateTimeStr))
+
+    taiInstant.atHawaii.toString shouldBe hawaiiDateTimeStr
+    taiInstant.atLocal.toString shouldBe localDateTimeStr
+    taiInstant.atZone(ZoneId.of("America/New_York")).toString shouldBe newyorkDateTimeStr
+  }
 }
