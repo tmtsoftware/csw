1473f8317e99718c7268653f1bfe11a2cdf69ef7
Pritam Kadam <kpritam@thoughtworks.com>
2018-12-17 12:01:12 +0530

    #000: Pritam: Provide static methods on TMTClock to get current utc and tai time.

--- csw-time-api/src/main/scala/csw/time/api/models/TMTTime.scala
+++ csw-time-api/src/main/scala/csw/time/api/models/TMTTime.scala
@@ -8,12 +8,9 @@ sealed trait TMTTime {
 
 object TMTTime {
   case class UTCTime(value: Instant) extends TMTTime {
-
     def at(zoneId: ZoneId): ZonedDateTime = value.atZone(zoneId)
-
-    def atLocal: ZonedDateTime = at(ZoneId.systemDefault())
-
-    def atHawaii: ZonedDateTime = at(ZoneId.of("US/Hawaii"))
+    def atLocal: ZonedDateTime            = at(ZoneId.systemDefault())
+    def atHawaii: ZonedDateTime           = at(ZoneId.of("US/Hawaii"))
   }
 
   case class TAITime(value: Instant) extends TMTTime
--- csw-time-client/src/main/scala/csw/time/client/TMTClock.scala
+++ csw-time-client/src/main/scala/csw/time/client/TMTClock.scala
@@ -2,12 +2,16 @@ package csw.time.client
 
 import java.time.Instant
 
+import csw.time.api.models.TMTTime.{TAITime, UTCTime}
 import csw.time.client.internal.{LinuxClock, NonLinuxClock, OSType, TMTClock}
 
 object TMTClock {
   private lazy val defaultInstance = instance()
 
-  def now(): Instant = defaultInstance.now()
+  // todo: can be removed if there is no usage for now
+  def now(): Instant    = defaultInstance.now() // this instant is extracted from utc time and does not include tai offset
+  def utcNow(): UTCTime = defaultInstance.utcTime()
+  def taiNow(): TAITime = defaultInstance.taiTime()
 
   private[time] def instance(offset: Int = 0): TMTClock = OSType.value match {
     case OSType.Linux => new LinuxClock()
