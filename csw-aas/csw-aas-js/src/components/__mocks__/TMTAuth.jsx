91aa7557a5ba1e66b51ae65da0e69d0f3b467b89
Poorva Gokhale <poorvasgokhale@gmail.com>
2018-12-28 15:45:15 +0530

    #DEOPSCSW-630: Poorva : Add tests for login component

---
+++ csw-aas/csw-aas-js/src/components/__mocks__/TMTAuth.jsx
@@ -0,0 +1,18 @@
+export const mockTMTAuth = {
+  token: 'some token'
+}
+
+export const mockKeycloak = mockTMTAuth
+const authenticated = Promise.resolve(true)
+
+export const TMTAuth = {
+  resolveAAS: jest.fn().mockImplementation(() => {
+    return 'http://mockhost:mockport'
+  }),
+  from: jest.fn().mockImplementation(() => {
+    return mockTMTAuth
+  }),
+  authenticate: jest.fn().mockImplementation(() => {
+    return {mockKeycloak, authenticated}
+  })
+}
---
+++ csw-aas/csw-aas-js/src/tests/components/Login.test.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Enzyme, {mount} from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import Login from '../../components/Login'
+import {TMTAuth} from '../../components/TMTAuth.jsx'
+
+jest.mock('../../components/TMTAuth.jsx')
+
+describe('<Login />', () => {
+  Enzyme.configure({ adapter: new Adapter() })
+
+  it('should call logout', async () => {
+    const props = {
+      config: {
+        'realm': 'example',
+        'clientId': 'example-app'
+      },
+      onAuthentication: jest.fn()
+    }
+
+    const resolveAASMock = jest.spyOn(TMTAuth, 'resolveAAS')
+    const authenticateMock = jest.spyOn(TMTAuth, 'authenticate')
+
+    await mount(<Login {...props} />)
+
+    expect(resolveAASMock).toHaveBeenCalled()
+    expect(authenticateMock).toHaveBeenCalledWith(props.config, {url: 'http://mockhost:mockport'})
+  })
+})
