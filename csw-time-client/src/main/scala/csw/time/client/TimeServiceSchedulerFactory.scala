66ca413c350ea76ca85e11a670040aa335979987
Dollyg <dollyg@thoughtworks.com>
2018-12-28 17:18:34 +0530

    #DEOPSCSW-553 : Dolly : Add API doc for TimeServiceSchedulerFactory

--- csw-time-client/src/main/scala/csw/time/client/TimeServiceSchedulerFactory.scala
+++ csw-time-client/src/main/scala/csw/time/client/TimeServiceSchedulerFactory.scala
@@ -4,8 +4,17 @@ import akka.actor.ActorSystem
 import csw.time.api.TimeServiceScheduler
 import csw.time.client.internal.TimeServiceSchedulerImpl
 
+/**
+ * Factory to create [[TimeServiceScheduler]]
+ */
 object TimeServiceSchedulerFactory {
 
+  /**
+   * API to create [[TimeServiceScheduler]]
+   *
+   * @param actorSystem an actorSystem required for scheduling tasks
+   * @return an instance of [[TimeServiceScheduler]] which can be used to schedule one-time/periodic tasks
+   */
   def make()(implicit actorSystem: ActorSystem): TimeServiceScheduler = new TimeServiceSchedulerImpl()
 
 }
