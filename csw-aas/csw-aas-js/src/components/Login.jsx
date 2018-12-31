8f9f438ed06d071b22bc9e31619b4c0ee1255b52
Poorva Gokhale <poorvasgokhale@gmail.com>
2018-12-31 11:56:38 +0530

    #DEOPSCSW-630: Poorva: Refactoring

--- csw-aas/csw-aas-js/src/components/Login.jsx
+++ csw-aas/csw-aas-js/src/components/Login.jsx
@@ -3,19 +3,13 @@ import PropTypes from 'prop-types'
 import {TMTAuth} from './TMTAuth.jsx'
 
 class Login extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {tmtAuth: null, isAuthenticated: false}
-  }
-
   instantiateAAS = async (url) => {
     const {keycloak, authenticated} = await TMTAuth.authenticate(this.props.config, url)
     authenticated.success(() => {
       const tmtAuth = TMTAuth.from(keycloak)
-      this.setState({tmtAuth, isAuthenticated: tmtAuth.isAuthenticated})
-      this.props.onAuthentication({tmtAuth: this.state.tmtAuth, isAuthenticated: this.state.isAuthenticated})
+      this.props.onAuthentication({tmtAuth: tmtAuth, isAuthenticated: tmtAuth.isAuthenticated})
     }).error(() => {
-      this.props.onAuthentication({tmtAuth: this.state.tmtAuth, isAuthenticated: this.state.isAuthenticated})
+      this.props.onAuthentication({tmtAuth: null, isAuthenticated: false})
     })
   };
 
