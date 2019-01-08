c5be913c3cc448ba94230931122d1ee558616e47
Puneet Patwari <puneet.patwari@thoughtworks.com>
2019-01-08 14:55:29 +0530

    #DEOPSCSW-594: Puneet : Resolve formatting errors

--- csw-benchmark/src/main/java/csw/benchmark/time/TimeBenchmark.scala
+++ csw-benchmark/src/main/java/csw/benchmark/time/TimeBenchmark.scala
@@ -1,10 +1,9 @@
 package csw.benchmark.time
-import java.util.concurrent.TimeUnit
 
+import java.util.concurrent.TimeUnit
 import org.openjdk.jmh.annotations.Mode
 import org.openjdk.jmh.infra.Blackhole
 import org.openjdk.jmh.annotations._
-
 
 @Warmup(iterations = 4)
 @Measurement(iterations = 10)
@@ -14,10 +13,10 @@ class TimeBenchmark {
   @BenchmarkMode(Array(Mode.AverageTime))
   @OutputTimeUnit(TimeUnit.MICROSECONDS)
   @Fork(jvmArgs = Array("-Djava.library.path=src/main/java/csw/benchmark/time"))
-  def testMethodJNI(b: Blackhole): Unit = { b.consume(new TimeJNI().gettime) }
+  def testMethodJNI(b: Blackhole): Unit = b.consume(new TimeJNI().gettime)
 
   @Benchmark
   @BenchmarkMode(Array(Mode.AverageTime))
   @OutputTimeUnit(TimeUnit.MICROSECONDS)
-  def testMethodJNA(b: Blackhole): Unit = { b.consume(TimeLibrary.clock_gettime(0, new TimeSpec)) }
+  def testMethodJNA(b: Blackhole): Unit = b.consume(TimeLibrary.clock_gettime(0, new TimeSpec))
 }
