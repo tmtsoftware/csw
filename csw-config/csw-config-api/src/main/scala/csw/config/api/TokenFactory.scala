8540ba5d2310609134fe3cb68dd16fb691ec6b83
Pritam Kadam <kpritam@thoughtworks.com>
2019-01-25 16:54:19 +0530

    Fix formatting.

--- csw-config/csw-config-api/src/main/scala/csw/config/api/TokenFactory.scala
+++ csw-config/csw-config-api/src/main/scala/csw/config/api/TokenFactory.scala
@@ -1,23 +1,23 @@
 package csw.config.api
 
 /**
-  * [[csw.config.api.scaladsl.ConfigService]] deals with some admin functionality.
-  * In order to perform those, one need to implement this interface.
-  *
-  * Refer `CliTokenFactory` class from `csw-config-cli` project which implements this interface.
-  */
+ * [[csw.config.api.scaladsl.ConfigService]] deals with some admin functionality.
+ * In order to perform those, one need to implement this interface.
+ *
+ * Refer `CliTokenFactory` class from `csw-config-cli` project which implements this interface.
+ */
 trait TokenFactory {
 
   /**
-    * This token is used by an [[csw.config.api.scaladsl.ConfigService]] to access an admin API.
-    * This inform the API that the bearer of the token has been authorized to access the API and
-    * perform specific actions specified by the scope that has been granted.
-    *
-    * Refer `csw-aas` project and their corresponding documentation to know more about
-    * how one can authenticate and authorize with auth server and get this token.
-    *
-    * @return raw token string which can then be used to access admin api
-    */
+   * This token is used by an [[csw.config.api.scaladsl.ConfigService]] to access an admin API.
+   * This inform the API that the bearer of the token has been authorized to access the API and
+   * perform specific actions specified by the scope that has been granted.
+   *
+   * Refer `csw-aas` project and their corresponding documentation to know more about
+   * how one can authenticate and authorize with auth server and get this token.
+   *
+   * @return raw token string which can then be used to access admin api
+   */
   def getToken: String
 
 }
--- csw-config/csw-config-cli/src/main/scala/csw/config/cli/CliTokenFactory.scala
+++ csw-config/csw-config-cli/src/main/scala/csw/config/cli/CliTokenFactory.scala
@@ -4,10 +4,10 @@ import csw.aas.native.api.NativeAppAuthAdapter
 import csw.config.api.TokenFactory
 
 /**
-  * Provides implementation for [[TokenFactory]] and uses [[NativeAppAuthAdapter]] underneath provided by `csw-aas-native`
-  *
-  * @param nativeAuthAdapter adapter which support login, storing and fetching tokens from [[csw.aas.native.api.AuthStore]]
-  */
+ * Provides implementation for [[TokenFactory]] and uses [[NativeAppAuthAdapter]] underneath provided by `csw-aas-native`
+ *
+ * @param nativeAuthAdapter adapter which support login, storing and fetching tokens from [[csw.aas.native.api.AuthStore]]
+ */
 class CliTokenFactory(nativeAuthAdapter: NativeAppAuthAdapter) extends TokenFactory {
 
   override def getToken: String =
