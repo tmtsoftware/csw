5b418cde34364a2f31cb06ff20d2cb711dcbcf8d
Pritam Kadam <kpritam@thoughtworks.com>
2019-01-25 11:02:18 +0530

    #DEOPSCSW-507: Pritam: Make SetSeverity message package private.

--- csw-alarm/csw-alarm-client/src/main/scala/csw/alarm/client/internal/auto_refresh/AutoRefreshSeverityMessage.scala
+++ csw-alarm/csw-alarm-client/src/main/scala/csw/alarm/client/internal/auto_refresh/AutoRefreshSeverityMessage.scala
@@ -8,7 +8,7 @@ sealed trait AutoRefreshSeverityMessage {
 }
 
 object AutoRefreshSeverityMessage {
-  case class AutoRefreshSeverity(key: AlarmKey, severity: AlarmSeverity) extends AutoRefreshSeverityMessage
-  case class SetSeverity(key: AlarmKey, severity: AlarmSeverity)         extends AutoRefreshSeverityMessage
-  case class CancelAutoRefresh(key: AlarmKey)                            extends AutoRefreshSeverityMessage
+  case class AutoRefreshSeverity(key: AlarmKey, severity: AlarmSeverity)               extends AutoRefreshSeverityMessage
+  private[auto_refresh] case class SetSeverity(key: AlarmKey, severity: AlarmSeverity) extends AutoRefreshSeverityMessage
+  case class CancelAutoRefresh(key: AlarmKey)                                          extends AutoRefreshSeverityMessage
 }
