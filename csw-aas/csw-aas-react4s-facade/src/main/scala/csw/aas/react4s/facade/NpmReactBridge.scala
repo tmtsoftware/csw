75cf240c3114cb9a1f9055db51d10814e1455d51
Pritam Kadam <kpritam@thoughtworks.com>
2019-02-08 14:13:09 +0530

    DEOPSCSW-640: Pritam: Simplify react bridge/

--- csw-aas/csw-aas-react4s-facade/src/main/scala/csw/aas/react4s/facade/NpmReactBridge.scala
+++ csw-aas/csw-aas-react4s-facade/src/main/scala/csw/aas/react4s/facade/NpmReactBridge.scala
@@ -1,5 +1,15 @@
 package csw.aas.react4s.facade
 
 import com.github.ahnfelt.react4s.ReactBridge
+import scala.scalajs.js
+import scala.scalajs.js.annotation.JSImport
 
 object NpmReactBridge extends ReactBridge(React, ReactDOM)
+
+@js.native
+@JSImport("react", JSImport.Namespace)
+object React extends ReactBridge.React
+
+@js.native
+@JSImport("react-dom", JSImport.Namespace)
+object ReactDOM extends js.Object
