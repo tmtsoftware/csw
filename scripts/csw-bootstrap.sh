f91f6fafa87d5aca912c33feaaf68b63dca047f8
Bharat Singh <10626464+Bharat1103@users.noreply.github.com>
2019-02-13 11:34:42 +0530

    #DEOPSCSW-489: Bharat : Add missing artifacts in csw-bootstrap.sh

--- scripts/csw-bootstrap.sh
+++ scripts/csw-bootstrap.sh
@@ -9,9 +9,13 @@ TARGET_PATH="$BASE_PATH"/target/coursier/stage/bin
 mkdir -p "$TARGET_PATH"
 cp "$BASE_PATH"/scripts/coursier "$TARGET_PATH"
 
-"$SCRIPTS_PATH"/coursier bootstrap -r jitpack com.github.tmtsoftware.csw::csw-location-server:"$1" -M csw.location.server.Main -o "$TARGET_PATH"/csw-location-server
+"$SCRIPTS_PATH"/coursier bootstrap -r jitpack -r https://jcenter.bintray.com/ com.github.tmtsoftware.csw::csw-location-server:"$1" -M csw.location.server.Main -o "$TARGET_PATH"/csw-location-server
 "$SCRIPTS_PATH"/coursier bootstrap -r jitpack com.github.tmtsoftware.csw::csw-location-agent:"$1" -M csw.location.agent.Main -o "$TARGET_PATH"/csw-location-agent
 "$SCRIPTS_PATH"/coursier bootstrap -r jitpack com.github.tmtsoftware.csw::csw-config-server:"$1" -M csw.config.server.Main -o "$TARGET_PATH"/csw-config-server
+"$SCRIPTS_PATH"/coursier bootstrap -r jitpack com.github.tmtsoftware.csw::csw-admin-server:"$1" -M csw.admin.server.Main -o "$TARGET_PATH"/csw-admin-server
+"$SCRIPTS_PATH"/coursier bootstrap -r jitpack com.github.tmtsoftware.csw::csw-config-cli:"$1" -M csw.config.cli.Main -o "$TARGET_PATH"/csw-config-cli
+"$SCRIPTS_PATH"/coursier bootstrap -r jitpack com.github.tmtsoftware.csw::csw-event-cli:"$1" -M csw.event.cli.Main -o "$TARGET_PATH"/csw-event-cli
+"$SCRIPTS_PATH"/coursier bootstrap -r jitpack com.github.tmtsoftware.csw::csw-alarm-cli:"$1" -M csw.alarm.cli.Main -o "$TARGET_PATH"/csw-alarm-cli
 
 cp -r "$SCRIPTS_PATH"/conf "$BASE_PATH"/target/coursier/stage
 cp "$SCRIPTS_PATH"/csw-auth/prod/configure.sh "$TARGET_PATH"
