0a844002fa9fe843b55738e324e47c7c5aea62a9
Pritam Kadam <kpritam@thoughtworks.com>
2019-02-22 18:27:14 +0530

    #000: Add sample perf test for time being.

---
+++ csw-event/csw-event-client/src/test/scala/csw/event/client/temp/PerfTest.scala
@@ -0,0 +1,51 @@
+package csw.event.client.temp
+
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import csw.event.api.scaladsl.{EventPublisher, EventSubscriber}
+import csw.event.client.EventServiceFactory
+import csw.event.client.helpers.TestFutureExt.RichFuture
+import csw.event.client.helpers.Utils
+import csw.location.api.scaladsl.LocationService
+import csw.location.client.scaladsl.HttpLocationServiceFactory
+import csw.params.events.Event
+import org.scalatest.FunSuite
+
+import scala.concurrent.ExecutionContextExecutor
+
+class PerfTest extends FunSuite {
+
+  private implicit val system: ActorSystem          = ActorSystem()
+  private implicit val ec: ExecutionContextExecutor = system.dispatcher
+  private implicit val mat: ActorMaterializer       = ActorMaterializer()
+  private val ls: LocationService                   = HttpLocationServiceFactory.makeLocalClient
+  private val factory                               = new EventServiceFactory().make(ls)
+  private var id                                    = 0
+  private val event                                 = Utils.makeEvent(id)
+
+  val subscriber: EventSubscriber = factory.defaultSubscriber
+  val publisher: EventPublisher   = factory.defaultPublisher
+
+  test("asd") {
+    publisher.publish(Utils.makeEvent(10)).await
+
+    val subscription = subscriber.subscribeCallback(Set(event.eventKey), report)
+
+    while (true) {
+      publisher.publish(Utils.makeEvent(id))
+      Thread.sleep(10)
+    }
+  }
+
+  private def report(event: Event): Unit = {
+    val currentTime = System.currentTimeMillis()
+    val origTime    = event.eventTime.value.toEpochMilli
+
+    println(currentTime - origTime)
+  }
+
+  private def eventGenerator() = {
+    id += 1
+    Utils.makeEvent(id)
+  }
+}
