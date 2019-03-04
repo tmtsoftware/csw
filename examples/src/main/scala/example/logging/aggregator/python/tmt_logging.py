2f7703cdb02bb6db4fc0e7b625496c01d80efff7
Saloni Vithalani <skvithalani@yahoo.com>
2019-03-04 12:45:02 +0530

    #DEOPSCSW-662: Saloni | Remove unnecessary code

--- examples/src/main/scala/example/logging/aggregator/python/tmt_logging.py
+++ examples/src/main/scala/example/logging/aggregator/python/tmt_logging.py
@@ -2,13 +2,10 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 from __future__ import absolute_import
-from datetime import datetime
 
 import codecs
 import json
-import logging
 import logging.config
-import sys
 import os
 import pathlib
 import tmt_formatter
@@ -26,6 +23,5 @@ dirPath = os.environ.get("TMT_LOG_HOME", "/tmp") + "/tmt/logs"
 pathlib.Path(dirPath).mkdir(parents=True, exist_ok=True)
 logPath = dirPath + "/app.log"
 config["logging"]["handlers"]["file_handler"]["filename"]=logPath
-# config["logging"]["handlers"]["file_handler"]["formatter"]["converter"]="time.gmtime"
 logging.config.dictConfig(config["logging"])
 
