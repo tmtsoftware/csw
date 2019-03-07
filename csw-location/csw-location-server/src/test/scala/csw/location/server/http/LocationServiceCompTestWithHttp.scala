7e7453958df4de8a5717ab9f343269f3e32d9ed6
Pritam Kadam <kpritam@thoughtworks.com>
2019-03-07 19:44:59 +0530

    #000: Enable logging in failing test.

--- csw-location/csw-location-server/src/test/scala/csw/location/server/http/LocationServiceCompTestWithHttp.scala
+++ csw-location/csw-location-server/src/test/scala/csw/location/server/http/LocationServiceCompTestWithHttp.scala
@@ -4,11 +4,17 @@ import akka.actor.CoordinatedShutdown.UnknownReason
 import csw.location.server.commons.TestFutureExtension.RichFuture
 import csw.location.server.internal.ServerWiring
 import csw.location.server.scaladsl.LocationServiceCompTest
+import csw.logging.client.appenders.StdOutAppender
+import csw.logging.client.internal.LoggingSystem
+import csw.logging.client.scaladsl.LoggingSystemFactory
 
 // DEOPSCSW-429: [SPIKE] Provide HTTP server and client for location service
 class LocationServiceCompTestWithHttp extends LocationServiceCompTest("http") {
 
   private val wiring = new ServerWiring
+
+  private val system: LoggingSystem = LoggingSystemFactory.start("", "", "", wiring.actorSystem)
+  system.setAppenders(List(StdOutAppender))
 
   override protected def beforeAll(): Unit = {
     wiring.locationHttpService.start().await
