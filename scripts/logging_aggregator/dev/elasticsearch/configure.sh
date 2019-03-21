a1acbd968fdb47e7cde568c3634a1f6a79da1411
Saloni Vithalani <skvithalani@yahoo.com>
2019-03-21 17:26:54 +0530

    #DEOPSCSW-646: Saloni : Add yml file for elasticsearch to log at TMT_LOG_HOME

--- scripts/logging_aggregator/dev/elasticsearch/configure.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# wait for Elasticsearch to start, then run the setup script to
-# create and configure the index.
-filebeat -c filebeat-init.yml -v -e setup --pipelines --modules system,postgresql,redis
