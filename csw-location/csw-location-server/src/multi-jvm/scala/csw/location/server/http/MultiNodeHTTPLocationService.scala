9cbd558bb8c6adc5677318345c2d0e2a4d5534e1
Saloni Vithalani <skvithalani@yahoo.com>
2019-04-12 11:05:08 +0530

    #000 : Saloni : Attempt to connection was shutdown flaky failure

--- csw-location/csw-location-server/src/multi-jvm/scala/csw/location/server/http/MultiNodeHTTPLocationService.scala
+++ csw-location/csw-location-server/src/multi-jvm/scala/csw/location/server/http/MultiNodeHTTPLocationService.scala
@@ -1,20 +1,28 @@
 package csw.location.server.http
 
+import akka.http.scaladsl.Http
 import csw.location.helpers.LSNodeSpec
 import csw.location.server.commons.TestFutureExtension.RichFuture
 import csw.location.server.internal.ServerWiring
 import org.scalatest.BeforeAndAfterAll
 
+import scala.concurrent.Future
 import scala.util.Try
 
 trait MultiNodeHTTPLocationService {
   self: LSNodeSpec[_] with BeforeAndAfterAll =>
+  private var eventualBinding: Future[Http.ServerBinding] = _
 
-  Try(ServerWiring.make(self.system).locationHttpService.start().await) match {
+  Try {
+    eventualBinding = ServerWiring.make(self.system).locationHttpService.start()
+    eventualBinding.await
+  } match {
     case _ => // ignore binding errors
   }
 
   override def afterAll(): Unit = {
+    import self.system.dispatcher
+    eventualBinding.flatMap(_.unbind()).await
     multiNodeSpecAfterAll()
   }
 
