b1000626f5fc2106522cb03a0c2cc7388158923b
Puneet Patwari <puneet.patwari@thoughtworks.com>
2019-06-24 15:49:32 +0530

    #000: Puneet | Saloni : Add cbor support for Location Service
    Co-authored-by: Saloni Vithalani <skvithalani@yahoo.com>

--- csw-location/csw-location-api/src/main/scala/csw/location/api/formats/cbor/LocationAkkaSerializer.scala
+++ csw-location/csw-location-api/src/main/scala/csw/location/api/formats/cbor/LocationAkkaSerializer.scala
@@ -4,49 +4,44 @@ import akka.actor.ExtendedActorSystem
 import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.adapter.UntypedActorSystemOps
 import akka.serialization.Serializer
+import csw.location.api.commons.LocationServiceLogger
 import csw.location.api.models.{Connection, Location, Registration, TrackingEvent}
 import io.bullet.borer.Cbor
 
 class LocationAkkaSerializer(_actorSystem: ExtendedActorSystem) extends Serializer with LocationCborSupport {
 
-  override implicit lazy val actorSystem: ActorSystem[_] = _actorSystem.toTyped
+  override val identifier: Int = 19924
 
-  override def identifier: Int = 19924
-
-  override def toBinary(o: AnyRef): Array[Byte] = {
-    try {
-      o match {
-        case x: Connection    => Cbor.encode(x).toByteArray
-        case x: Location      => Cbor.encode(x).toByteArray
-        case x: Registration  => Cbor.encode(x).toByteArray
-        case x: TrackingEvent => Cbor.encode(x).toByteArray
-        case _                => throw new RuntimeException(s"does not support encoding of $o")
-      }
-    } catch {
-      case ex => ex.printStackTrace(); throw ex
-    }
-  }
+  override implicit def actorSystem: ActorSystem[_] = _actorSystem.toTyped
+  val logger = LocationServiceLogger.getLogger
 
   override def includeManifest: Boolean = true
 
-  override def fromBinary(bytes: Array[Byte], manifest: Option[Class[_]]): AnyRef = {
-    try {
-      if (classOf[Connection].isAssignableFrom(manifest.get)) {
-        Cbor.decode(bytes).to[Connection].value
-      } else if (classOf[Location].isAssignableFrom(manifest.get)) {
-        Cbor.decode(bytes).to[Location].value
-      } else if (classOf[Registration].isAssignableFrom(manifest.get)) {
-        Cbor.decode(bytes).to[Registration].value
-      } else if (classOf[TrackingEvent].isAssignableFrom(manifest.get)) {
-        Cbor.decode(bytes).to[TrackingEvent].value
-      } else {
-        throw new RuntimeException("end")
-      }
-
-    } catch {
-      case ex => ex.printStackTrace(); throw ex
-
-    }
-
+  override def toBinary(o: AnyRef): Array[Byte] = o match {
+    case x: Connection    => Cbor.encode(x).toByteArray
+    case x: Location      => Cbor.encode(x).toByteArray
+    case x: Registration  => Cbor.encode(x).toByteArray
+    case x: TrackingEvent => Cbor.encode(x).toByteArray
+    case _ =>
+      val ex = new RuntimeException(s"does not support encoding of $o")
+      logger.error(ex.getMessage, ex = ex)
+      throw ex
   }
+
+  override def fromBinary(bytes: Array[Byte], manifest: Option[Class[_]]): AnyRef = {
+    if (classOf[Connection].isAssignableFrom(manifest.get)) {
+      Cbor.decode(bytes).to[Connection].value
+    } else if (classOf[Location].isAssignableFrom(manifest.get)) {
+      Cbor.decode(bytes).to[Location].value
+    } else if (classOf[Registration].isAssignableFrom(manifest.get)) {
+      Cbor.decode(bytes).to[Registration].value
+    } else if (classOf[TrackingEvent].isAssignableFrom(manifest.get)) {
+      Cbor.decode(bytes).to[TrackingEvent].value
+    } else {
+      val ex = new RuntimeException(s"does not support decoding of ${manifest.get}")
+      logger.error(ex.getMessage, ex = ex)
+      throw ex
+    }
+  }
+
 }
--- csw-location/csw-location-api/src/main/scala/csw/location/api/formats/cbor/LocationCborSupport.scala
+++ csw-location/csw-location-api/src/main/scala/csw/location/api/formats/cbor/LocationCborSupport.scala
@@ -1,13 +1,56 @@
 package csw.location.api.formats.cbor
 
+import java.net.URI
+
 import akka.Done
-import csw.location.api.models.{AkkaLocation, Registration}
-import csw.params.core.formats.CborHelpers
+import akka.actor.typed.scaladsl.adapter.{TypedActorRefOps, TypedActorSystemOps, _}
+import akka.actor.typed.{ActorRef, ActorSystem}
+import akka.serialization.{Serialization, SerializationExtension}
+import csw.location.api.models.Connection.{AkkaConnection, HttpConnection, TcpConnection}
+import csw.location.api.models._
+import csw.params.core.formats.{CborCommonSupport, CborHelpers}
 import io.bullet.borer.Codec
 import io.bullet.borer.derivation.MapBasedCodecs._
 
-trait LocationCborSupport extends ActorSystemDependentCborSupport {
-  implicit val akkaLocationCodec: Codec[AkkaLocation] = deriveCodec[AkkaLocation]
-  implicit val registrationCodec: Codec[Registration] = deriveCodec[Registration]
-  implicit val doneCodec: Codec[Done]                 = CborHelpers.bimap[String, Done](_ => Done, _ => "done")
+trait LocationCborSupport extends CborCommonSupport {
+  implicit def actorSystem: ActorSystem[_]
+
+  implicit lazy val connectionTypeCodec: Codec[ConnectionType] = CborHelpers.enumCodec[ConnectionType]
+  implicit lazy val componentTypeCodec: Codec[ComponentType]   = CborHelpers.enumCodec[ComponentType]
+  implicit lazy val componentIdCodec: Codec[ComponentId]       = deriveCodec[ComponentId]
+  implicit lazy val componentInfoCodec: Codec[ConnectionInfo]  = deriveCodec[ConnectionInfo]
+
+  implicit lazy val connectionCodec: Codec[Connection] =
+    CborHelpers.bimap[ConnectionInfo, Connection](Connection.from, _.connectionInfo)
+
+  implicit lazy val akkaConnectionCodec: Codec[AkkaConnection] = deriveCodec[AkkaConnection]
+  implicit lazy val httpConnectionCodec: Codec[HttpConnection] = deriveCodec[HttpConnection]
+  implicit lazy val tcpConnectionCodec: Codec[TcpConnection]   = deriveCodec[TcpConnection]
+//  implicit lazy val typedConnectionCodec: Codec[TypedConnection]   = deriveCodec[TypedConnection]
+
+  implicit lazy val uriCodec: Codec[URI] = CborHelpers.bimap[String, URI](new URI(_), _.toString)
+
+  implicit def actorRefCodec[T]: Codec[ActorRef[T]] =
+    CborHelpers.bimap[String, ActorRef[T]](
+      path => {
+        val provider = SerializationExtension(actorSystem.toUntyped).system.provider
+        provider.resolveActorRef(path)
+      },
+      actorRef => Serialization.serializedActorPath(actorRef.toUntyped)
+    )
+
+  implicit lazy val locationCodec: Codec[Location]         = deriveCodec[Location]
+  implicit lazy val akkaLocationCodec: Codec[AkkaLocation] = deriveCodec[AkkaLocation]
+  implicit lazy val httpLocationCodec: Codec[HttpLocation] = deriveCodec[HttpLocation]
+
+  implicit lazy val registrationCodec: Codec[Registration]         = deriveCodec[Registration]
+  implicit lazy val akkaRegistrationCodec: Codec[AkkaRegistration] = deriveCodec[AkkaRegistration]
+  implicit lazy val tcpRegistrationCodec: Codec[TcpRegistration]   = deriveCodec[TcpRegistration]
+  implicit lazy val httpRegistrationCodec: Codec[HttpRegistration] = deriveCodec[HttpRegistration]
+
+  implicit lazy val trackingEventCodec: Codec[TrackingEvent]     = deriveCodec[TrackingEvent]
+  implicit lazy val locationUpdatedCodec: Codec[LocationUpdated] = deriveCodec[LocationUpdated]
+  implicit lazy val locationRemovedCodec: Codec[LocationRemoved] = deriveCodec[LocationRemoved]
+
+  implicit lazy val doneCodec: Codec[Done] = CborHelpers.bimap[String, Done](_ => Done, _ => "done")
 }
