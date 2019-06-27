62682ff6aecffc556629bb3436ebbeb28130b3c1
Dollyg <dollyg@thoughtworks.com>
2019-06-27 20:24:09 +0530

    #000 : Dolly : Fix printing of warning message

--- csw-time/csw-time-clock/jvm/src/main/scala/csw/time/clock/natives/models/TMTClock.scala
+++ csw-time/csw-time-clock/jvm/src/main/scala/csw/time/clock/natives/models/TMTClock.scala
@@ -32,13 +32,12 @@ private[time] class LinuxClock extends TMTClock {
   override def offset: Int = {
     val timeVal = new NTPTimeVal()
     TimeLibrary.ntp_gettimex(timeVal)
-    if (timeVal.tai == 0)
-      println("=================================================================================================")
-    println("WARNING: Value of TAI OFFSET is 0. To set the TAI OFFSET on your machine,")
-    println(
-      "please follow instructions in TimeService Documentation [https://tmtsoftware.github.io/csw/services/time.html#dependencies]"
-    )
-    println("=================================================================================================")
+    if (timeVal.tai == 0) {
+        println("============================================================================================================================")
+        println("WARNING: Value of TAI OFFSET is 0. To set the TAI OFFSET on your machine,")
+        println("Please follow instructions in TimeService Documentation [https://tmtsoftware.github.io/csw/services/time.html#dependencies]")
+        println("============================================================================================================================")
+      }
     timeVal.tai
   }
   //#native-calls
