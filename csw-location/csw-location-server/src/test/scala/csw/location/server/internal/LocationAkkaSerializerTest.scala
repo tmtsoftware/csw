a4160aa8808bd8f84d6550443cfbd7be01f082b7
Puneet Patwari <puneet.patwari@thoughtworks.com>
2019-07-05 12:49:55 +0530

    #000: Saloni | Puneet : Add test for (de)serialization for Location models  Co-authored-by: Saloni Vithalani <skvithalani@yahoo.com>

--- csw-location/csw-location-server/src/test/scala/csw/location/server/internal/LocationAkkaSerializerTest.scala
+++ csw-location/csw-location-server/src/test/scala/csw/location/server/internal/LocationAkkaSerializerTest.scala
@@ -5,9 +5,9 @@ import java.net.URI
 import akka.actor.typed.scaladsl.adapter.TypedActorSystemOps
 import akka.actor.typed.{ActorSystem, Behavior}
 import akka.serialization.SerializationExtension
-import csw.location.api.models.ComponentType.{Assembly, Service}
-import csw.location.api.models.Connection.{AkkaConnection, HttpConnection}
-import csw.location.api.models.{AkkaLocation, ComponentId, HttpRegistration, LocationRemoved}
+import csw.location.api.models.ComponentType.Assembly
+import csw.location.api.models.Connection.{AkkaConnection, HttpConnection, TcpConnection}
+import csw.location.api.models._
 import csw.params.core.models.Prefix
 import org.scalatest.prop.TableDrivenPropertyChecks.forAll
 import org.scalatest.prop.Tables.Table
@@ -28,22 +28,79 @@ private[location] class LocationAkkaSerializerTest extends FunSuite with Matcher
     Await.result(system.whenTerminated, 2.seconds)
   }
 
-  test("should use location serializer for location model (de)serialization") {
-    val connection = AkkaConnection(ComponentId("TromboneAssembly", Assembly))
+  test("should use location serializer for Connection (de)serialization") {
     val testData = Table(
-      "Location models",
-      connection,
-      AkkaLocation(connection, prefix, new URI(""), system),
-      HttpRegistration(HttpConnection(ComponentId("ConfigService", Service)), 1234, ""),
-      LocationRemoved(connection)
+      "Connection models",
+      AkkaConnection(ComponentId("TromboneAssembly", Assembly)),
+      HttpConnection(ComponentId("TromboneAssembly", Assembly)),
+      TcpConnection(ComponentId("TromboneAssembly", Assembly))
     )
 
-    forAll(testData) { locationModel ⇒
-      val serializer = serialization.findSerializerFor(locationModel)
+    forAll(testData) { connection ⇒
+      val serializer = serialization.findSerializerFor(connection)
       serializer.getClass shouldBe classOf[LocationAkkaSerializer]
 
-      val bytes = serializer.toBinary(locationModel)
-      serializer.fromBinary(bytes, Some(locationModel.getClass)) shouldEqual locationModel
+      val bytes = serializer.toBinary(connection)
+      serializer.fromBinary(bytes, Some(connection.getClass)) shouldEqual connection
+    }
+  }
+
+  test("should use location serializer for Location (de)serialization") {
+    val akkaConnection = AkkaConnection(ComponentId("TromboneAssembly", Assembly))
+    val httpConnection = HttpConnection(ComponentId("TromboneAssembly", Assembly))
+    val tcpConnection  = TcpConnection(ComponentId("TromboneAssembly", Assembly))
+    val testData = Table(
+      "Location models",
+      AkkaLocation(akkaConnection, prefix, new URI(""), system),
+      HttpLocation(httpConnection, new URI("")),
+      TcpLocation(tcpConnection, new URI(""))
+    )
+
+    forAll(testData) { location ⇒
+      val serializer = serialization.findSerializerFor(location)
+      serializer.getClass shouldBe classOf[LocationAkkaSerializer]
+
+      val bytes = serializer.toBinary(location)
+      serializer.fromBinary(bytes, Some(location.getClass)) shouldEqual location
+    }
+  }
+
+  test("should use location serializer for Registration (de)serialization") {
+    val akkaConnection = AkkaConnection(ComponentId("TromboneAssembly", Assembly))
+    val httpConnection = HttpConnection(ComponentId("TromboneAssembly", Assembly))
+    val tcpConnection  = TcpConnection(ComponentId("TromboneAssembly", Assembly))
+    val testData = Table(
+      "Registration models",
+      AkkaRegistration(akkaConnection, prefix, system),
+      HttpRegistration(httpConnection, 1234, ""),
+      TcpRegistration(tcpConnection, 1234)
+    )
+
+    forAll(testData) { registration ⇒
+      val serializer = serialization.findSerializerFor(registration)
+      serializer.getClass shouldBe classOf[LocationAkkaSerializer]
+
+      val bytes = serializer.toBinary(registration)
+      serializer.fromBinary(bytes, Some(registration.getClass)) shouldEqual registration
+    }
+  }
+
+  test("should use location serializer for TrackingEvent (de)serialization") {
+    val akkaConnection = AkkaConnection(ComponentId("TromboneAssembly", Assembly))
+    val akkaLocation   = AkkaLocation(akkaConnection, prefix, new URI(""), system)
+
+    val testData = Table(
+      "TrackingEvent models",
+      LocationUpdated(akkaLocation),
+      LocationRemoved(akkaConnection)
+    )
+
+    forAll(testData) { trackingEvent ⇒
+      val serializer = serialization.findSerializerFor(trackingEvent)
+      serializer.getClass shouldBe classOf[LocationAkkaSerializer]
+
+      val bytes = serializer.toBinary(trackingEvent)
+      serializer.fromBinary(bytes, Some(trackingEvent.getClass)) shouldEqual trackingEvent
     }
   }
 }
