54fe2870bd9f1aff96dcbd74403e06f58ecb39a5
Shubham Chauhan <shubham25may1998@gmail.com>
2019-07-23 11:11:13 +0530

    #ESW-190: Chauhan | Dolly : Move SequenceTest from WordSpec to FunSuite
    Co-authored-by: Dolly <10329479+Dollyg@users.noreply.github.com>

--- csw-params/shared/src/test/scala/csw/params/commands/SequenceTest.scala
+++ csw-params/shared/src/test/scala/csw/params/commands/SequenceTest.scala
@@ -1,45 +1,40 @@
 package csw.params.commands
 
 import csw.params.core.models.Prefix
-import org.scalatest.{Matchers, WordSpec}
+import org.scalatest.{FunSuite, Matchers}
 
-class SequenceTest extends WordSpec with Matchers {
+class SequenceTest extends FunSuite with Matchers {
 
-  "apply" must {
-    "create sequence from provided list of commands" in {
-      val setup    = Setup(Prefix("test1"), CommandName("setup-test"), None)
-      val observe  = Observe(Prefix("test1"), CommandName("observe-test"), None)
-      val sequence = Sequence(setup, observe)
+  test("apply - create sequence from provided list of commands") {
+    val setup    = Setup(Prefix("test1"), CommandName("setup-test"), None)
+    val observe  = Observe(Prefix("test1"), CommandName("observe-test"), None)
+    val sequence = Sequence(setup, observe)
 
-      sequence.commands should ===(List(setup, observe))
-    }
+    sequence.commands should ===(List(setup, observe))
   }
 
-  "add" must {
-    "allow adding list of commands to existing sequence" in {
-      val setup    = Setup(Prefix("test1"), CommandName("setup-test"), None)
-      val observe  = Observe(Prefix("test2"), CommandName("observe-test"), None)
-      val sequence = Sequence(setup, observe)
+  test("add - allow adding list of commands to existing sequence") {
+    val setup    = Setup(Prefix("test1"), CommandName("setup-test"), None)
+    val observe  = Observe(Prefix("test2"), CommandName("observe-test"), None)
+    val sequence = Sequence(setup, observe)
 
-      val newSetup   = Setup(Prefix("test3"), CommandName("setup-test"), None)
-      val newObserve = Observe(Prefix("test4"), CommandName("setup-test"), None)
+    val newSetup   = Setup(Prefix("test3"), CommandName("setup-test"), None)
+    val newObserve = Observe(Prefix("test4"), CommandName("setup-test"), None)
 
-      val updateSequence = sequence.add(newSetup, newObserve)
-      updateSequence.commands should ===(List(setup, observe, newSetup, newObserve))
-    }
+    val updateSequence = sequence.add(newSetup, newObserve)
+    updateSequence.commands should ===(List(setup, observe, newSetup, newObserve))
+  }
 
-    "allow adding new sequence to existing sequence" in {
-      val setup    = Setup(Prefix("test1"), CommandName("setup-test"), None)
-      val observe  = Observe(Prefix("test2"), CommandName("observe-test"), None)
-      val sequence = Sequence(setup, observe)
+  test("add - allow adding new sequence to existing sequence") {
+    val setup    = Setup(Prefix("test1"), CommandName("setup-test"), None)
+    val observe  = Observe(Prefix("test2"), CommandName("observe-test"), None)
+    val sequence = Sequence(setup, observe)
 
-      val newSetup    = Setup(Prefix("test3"), CommandName("setup-test"), None)
-      val newObserve  = Observe(Prefix("test4"), CommandName("observe-test"), None)
-      val newSequence = Sequence(newSetup, newObserve)
+    val newSetup    = Setup(Prefix("test3"), CommandName("setup-test"), None)
+    val newObserve  = Observe(Prefix("test4"), CommandName("observe-test"), None)
+    val newSequence = Sequence(newSetup, newObserve)
 
-      val updateSequence = sequence.add(newSequence)
-      updateSequence.commands should ===(List(setup, observe, newSetup, newObserve))
-    }
-
+    val updateSequence = sequence.add(newSequence)
+    updateSequence.commands should ===(List(setup, observe, newSetup, newObserve))
   }
 }
