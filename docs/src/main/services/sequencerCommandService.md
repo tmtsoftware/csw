d0ff1512fff5dbd296043d254cf42d6d94a3ecbe
Shubham Jaybhaye <shubhamj7997@gmail.com>
2019-08-07 12:59:06 +0530

    #000: Chauhan | Shubham : Review sequencer command service docs.
    Co-authored-by: Shubham Chauhan <shubham25may1998@gmail.com>

--- docs/src/main/services/sequencerCommandService.md
+++ docs/src/main/services/sequencerCommandService.md
@@ -1,8 +1,12 @@
 # Sequencer Command Service
 
-Sequencer Command Service provides an API to submit a sequence to a sequencer.
+Sequencer command service provides ability to send `Sequence` of commands to a running `Sequencer`. It sends the provided sequence
+  to sequencer and returns `Future[Submit Response]`.
 
 ## Dependencies
+
+The Sequencer Command Service comes bundled with the Framework, no additional dependency is needs to be added to your `build.sbt`
+file if you are using it. To use the Sequencer Command Service without using the framework, add this to your `build.sbt` file:
 
 sbt
 :   @@@vars
@@ -12,13 +16,17 @@ sbt
     @@@
     
 ## Creating Sequencer Command Service
-For creating a Sequencer Command Service, you need an Akka location of a Sequencer.
+To create Sequencer Command Service, you need to resolve `AkkaLocation` of the `Sequencer` using `LocationService`. Pass the resolved
+location to `SequencerCommandServiceFactory` which will return handle of `SequencerCommandService`. 
 
 Scala
 :   @@snip [SequencerCommandServiceExample.scala](../../../../examples/src/main/scala/example/sequencerCommandService/SequencerCommandServiceExample.scala) { #create-sequence-command-service }
 
 ## Submitting sequence to a sequencer
-There is an API submit which submits the given Sequence to the Sequencer and returns a submit response.
+
+To submit sequence to Sequencer, `SequencerCommandService` provides `submit` api. Submit method takes a sequence and returns
+ `Future[SubmitResponse]`. It starts execution of the sent sequence and returns `Future[Submit Response]` which will complete
+  when sequence is finished or Sequencer is not available.
 
 Scala
 :   @@snip [SequencerCommandServiceExample.scala](../../../../examples/src/main/scala/example/sequencerCommandService/SequencerCommandServiceExample.scala) { #submit-sequence }
--- examples/src/main/scala/example/sequencerCommandService/SequencerCommandServiceExample.scala
+++ examples/src/main/scala/example/sequencerCommandService/SequencerCommandServiceExample.scala
@@ -4,10 +4,12 @@ import akka.actor.typed.{ActorSystem, SpawnProtocol}
 import akka.stream.ActorMaterializer
 import akka.stream.typed.scaladsl
 import csw.command.client.SequencerCommandServiceFactory
+import csw.command.client.internal.SequencerCommandServiceImpl
 import csw.location.client.ActorSystemFactory
 import csw.location.client.scaladsl.HttpLocationServiceFactory
 import csw.location.models.Connection.AkkaConnection
 import csw.location.models.{AkkaLocation, ComponentId, ComponentType}
+import csw.params.commands.CommandResponse.SubmitResponse
 import csw.params.commands.{CommandName, Sequence, Setup}
 import csw.params.core.models.Prefix
 
@@ -23,16 +25,16 @@ object SequencerCommandServiceExample extends App {
   private val locationService = HttpLocationServiceFactory.makeLocalClient(typedSystem, mat)
 
   // #create-sequence-command-service
-  private val sequencerConnection    = AkkaConnection(ComponentId("sequencer", ComponentType.Sequencer))
-  private val location: AkkaLocation = Await.result(locationService.resolve(sequencerConnection, 5.seconds), 5.seconds).get
+  private val connection             = AkkaConnection(ComponentId("sequencer", ComponentType.Sequencer))
+  private val location: AkkaLocation = Await.result(locationService.resolve(connection, 5.seconds), 5.seconds).get
 
-  val sequencerCommandService = SequencerCommandServiceFactory.make(location)
+  val sequencerCommandService: SequencerCommandServiceImpl = SequencerCommandServiceFactory.make(location)
   // #create-sequence-command-service
 
   // #submit-sequence
-  val sequence = Sequence(Setup(Prefix("test.move"), CommandName("command-1"), None))
+  val sequence: Sequence = Sequence(Setup(Prefix("test.move"), CommandName("command-1"), None))
 
-  sequencerCommandService.submit(sequence)
+  private val submitResponse: SubmitResponse = Await.result(sequencerCommandService.submit(sequence), 5.seconds)
   // #submit-sequence
 
 }
