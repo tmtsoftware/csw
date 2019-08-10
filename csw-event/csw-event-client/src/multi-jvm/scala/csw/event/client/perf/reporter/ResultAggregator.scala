1592ac6de7de127b4cfdb79e045034eae5a5f044
Pritam Kadam <kpritam@thoughtworks.com>
2019-08-10 10:28:23 +0530

    Fix possible thread safety issue in perf test

--- csw-event/csw-event-client/src/multi-jvm/scala/csw/event/client/perf/reporter/ResultAggregator.scala
+++ csw-event/csw-event-client/src/multi-jvm/scala/csw/event/client/perf/reporter/ResultAggregator.scala
@@ -3,13 +3,16 @@ package csw.event.client.perf.reporter
 import java.io.PrintStream
 import java.util.concurrent.TimeUnit.SECONDS
 
-import akka.actor.typed.{ActorRef, ActorSystem}
-import akka.util.ByteString
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.{ActorRef, ActorSystem, Behavior}
+import akka.util.{ByteString, Timeout}
 import csw.event.api.scaladsl.{EventSubscriber, EventSubscription}
+import csw.event.client.helpers.TestFutureExt.RichFuture
 import csw.event.client.perf.utils.EventUtils
 import csw.event.client.perf.utils.EventUtils._
 import csw.params.events.{Event, SystemEvent}
 import org.HdrHistogram.Histogram
+import org.scalatest.time.SpanSugar.convertDoubleToGrainOfTime
 
 class ResultAggregator(
     scenarioName: String,
@@ -19,70 +22,75 @@ class ResultAggregator(
     actorRef: ActorRef[AggregatedResult]
 )(implicit val system: ActorSystem[_]) {
 
-  private val histogram               = new Histogram(SECONDS.toNanos(10), 3)
-  private val initialLatencyHistogram = new Histogram(SECONDS.toNanos(10), 3)
-  private var throughput              = 0d
-  private var newEvent                = false
-  private var receivedPerfEventCount  = 0
-  private var totalDropped            = 0L
-  private var outOfOrderCount         = 0L
-  private var avgLatency              = 0L
+  private val eventHandler: Behavior[Event] = Behaviors.setup { _ =>
+    val histogram               = new Histogram(SECONDS.toNanos(10), 3)
+    val initialLatencyHistogram = new Histogram(SECONDS.toNanos(10), 3)
+    var throughput              = 0d
+    var newEvent                = false
+    var receivedPerfEventCount  = 0
+    var totalDropped            = 0L
+    var outOfOrderCount         = 0L
+    var avgLatency              = 0L
 
-  def startSubscription(): EventSubscription = subscriber.subscribeCallback(Set(EventUtils.perfEventKey), onEvent)
+    def aggregateResult(): (LatencyPlots, ThroughputPlots, InitialLatencyPlots) = {
 
-  private def onEvent(event: Event): Unit = event match {
-    case event: SystemEvent if newEvent =>
-      receivedPerfEventCount += 1
-      val histogramBuffer = ByteString(event.get(histogramKey).get.values).asByteBuffer
-      histogram.add(Histogram.decodeFromByteBuffer(histogramBuffer, SECONDS.toNanos(10)))
+      def percentile(histogram: Histogram, p: Double): Double = nanosToMicros(histogram.getValueAtPercentile(p))
 
-      val initialLatencyHistogramBuffer =
-        ByteString(event.get(initialLatencyHistogramKey).get.values).asByteBuffer
-      initialLatencyHistogram.add(Histogram.decodeFromByteBuffer(initialLatencyHistogramBuffer, SECONDS.toNanos(10)))
+      val throughputPlots = ThroughputPlots(
+        PlotResult().add(testName, throughput),
+        PlotResult().add(testName, totalDropped),
+        PlotResult().add(testName, outOfOrderCount)
+      )
 
-      throughput += event.get(throughputKey).get.head
-      outOfOrderCount += event.get(totalOutOfOrderKey).get.head
-      totalDropped += event.get(totalDroppedKey).get.head
-      val avgLatencyTmp = event.get(avgLatencyKey).get.head
-      avgLatency = if (avgLatency == 0) avgLatencyTmp else (avgLatency + avgLatencyTmp) / 2
+      val latencyPlots = LatencyPlots(
+        PlotResult().add(testName, percentile(histogram, 50.0)),
+        PlotResult().add(testName, percentile(histogram, 90.0)),
+        PlotResult().add(testName, percentile(histogram, 99.0)),
+        PlotResult().add(testName, nanosToMicros(avgLatency))
+      )
 
-      if (receivedPerfEventCount == expPerfEventCount) {
-        val (latencyPlots, throughputPlots, initialLatencyPlots) = aggregateResult()
-        actorRef ! AggregatedResult(latencyPlots, throughputPlots, initialLatencyPlots)
-      }
-    case _ => newEvent = true
+      val initialLatencyPlots = InitialLatencyPlots(
+        PlotResult().add(testName, percentile(initialLatencyHistogram, 50.0)),
+        PlotResult().add(testName, percentile(initialLatencyHistogram, 90.0)),
+        PlotResult().add(testName, percentile(initialLatencyHistogram, 99.0))
+      )
+
+      histogram.outputPercentileDistribution(
+        new PrintStream(BenchmarkFileReporter(s"$scenarioName/Aggregated-$testName", system, logSettings = false).fos),
+        1000.0
+      )
+      (latencyPlots, throughputPlots, initialLatencyPlots)
+    }
+
+    Behaviors.receiveMessage[Event] {
+      case event: SystemEvent if newEvent =>
+        receivedPerfEventCount += 1
+        val histogramBuffer = ByteString(event.get(histogramKey).get.values).asByteBuffer
+        histogram.add(Histogram.decodeFromByteBuffer(histogramBuffer, SECONDS.toNanos(10)))
+
+        val initialLatencyHistogramBuffer =
+          ByteString(event.get(initialLatencyHistogramKey).get.values).asByteBuffer
+        initialLatencyHistogram.add(Histogram.decodeFromByteBuffer(initialLatencyHistogramBuffer, SECONDS.toNanos(10)))
+
+        throughput += event.get(throughputKey).get.head
+        outOfOrderCount += event.get(totalOutOfOrderKey).get.head
+        totalDropped += event.get(totalDroppedKey).get.head
+        val avgLatencyTmp = event.get(avgLatencyKey).get.head
+        avgLatency = if (avgLatency == 0) avgLatencyTmp else (avgLatency + avgLatencyTmp) / 2
+
+        if (receivedPerfEventCount == expPerfEventCount) {
+          val (latencyPlots, throughputPlots, initialLatencyPlots) = aggregateResult()
+          actorRef ! AggregatedResult(latencyPlots, throughputPlots, initialLatencyPlots)
+        }
+        Behaviors.same
+      case _ => newEvent = true; Behaviors.same
+    }
   }
 
-  private def aggregateResult(): (LatencyPlots, ThroughputPlots, InitialLatencyPlots) = {
+  implicit private val timeout: Timeout          = Timeout(20.seconds)
+  private val eventHandlerActor: ActorRef[Event] = system.systemActorOf(eventHandler, "result-agg").await
 
-    def percentile(histogram: Histogram, p: Double): Double = nanosToMicros(histogram.getValueAtPercentile(p))
-
-    val throughputPlots = ThroughputPlots(
-      PlotResult().add(testName, throughput),
-      PlotResult().add(testName, totalDropped),
-      PlotResult().add(testName, outOfOrderCount)
-    )
-
-    val latencyPlots = LatencyPlots(
-      PlotResult().add(testName, percentile(histogram, 50.0)),
-      PlotResult().add(testName, percentile(histogram, 90.0)),
-      PlotResult().add(testName, percentile(histogram, 99.0)),
-      PlotResult().add(testName, nanosToMicros(avgLatency))
-    )
-
-    val initialLatencyPlots = InitialLatencyPlots(
-      PlotResult().add(testName, percentile(initialLatencyHistogram, 50.0)),
-      PlotResult().add(testName, percentile(initialLatencyHistogram, 90.0)),
-      PlotResult().add(testName, percentile(initialLatencyHistogram, 99.0))
-    )
-
-    histogram.outputPercentileDistribution(
-      new PrintStream(BenchmarkFileReporter(s"$scenarioName/Aggregated-$testName", system, logSettings = false).fos),
-      1000.0
-    )
-    (latencyPlots, throughputPlots, initialLatencyPlots)
-  }
-
+  def startSubscription(): EventSubscription = subscriber.subscribeActorRef(Set(EventUtils.perfEventKey), eventHandlerActor)
 }
 
 case class AggregatedResult(
