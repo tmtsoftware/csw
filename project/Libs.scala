79ec3a5ac3f1faa8086d02c531fc16dc3a6dca8d
Scala Steward <me@scala-steward.org>
2019-08-23 18:45:55 +0200

    Update reactive-streams to 1.0.3

--- project/Libs.scala
+++ project/Libs.scala
@@ -30,7 +30,7 @@ object Libs {
   val `jwt-play-json`       = "com.pauldijou"       %% "jwt-play-json"       % "3.1.0"
   val `lettuce`             = "io.lettuce"          % "lettuce-core"         % "5.1.8.RELEASE"
   val `reactor-core`        = "io.projectreactor"   % "reactor-core"         % "3.2.11.RELEASE"
-  val `reactive-streams`    = "org.reactivestreams" % "reactive-streams"     % "1.0.2"
+  val `reactive-streams`    = "org.reactivestreams" % "reactive-streams"     % "1.0.3"
   val `akka-stream-kafka`   = "com.typesafe.akka"   %% "akka-stream-kafka"   % "1.0.5"
 //  val `embedded-kafka`      = "io.github.embeddedkafka" %% "embedded-kafka"      % "2.3.0"
   val `embedded-redis` = "com.github.kstyrc" % "embedded-redis" % "0.6"
