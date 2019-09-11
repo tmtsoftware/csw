ed3990bc65dfafd2f60aebc480b846c617a3305f
Scala Steward <me@scala-steward.org>
2019-09-11 06:35:19 +0200

    Update postgresql to 42.2.7

--- project/Libs.scala
+++ project/Libs.scala
@@ -44,7 +44,7 @@ object Libs {
   val `play-json-derived-codecs` = dep("org.julienrf" %%% "play-json-derived-codecs" % "6.0.0")
 
   val `jna`             = "net.java.dev.jna"              % "jna"             % "5.4.0"
-  val `postgresql`      = "org.postgresql"                % "postgresql"      % "42.2.6"
+  val `postgresql`      = "org.postgresql"                % "postgresql"      % "42.2.7"
   val `hikaricp`        = "com.zaxxer"                    % "HikariCP"        % "3.3.1" //Apache License 2.0
   val `otj-pg-embedded` = "com.opentable.components"      % "otj-pg-embedded" % "0.13.1"
   val httpclient        = "org.apache.httpcomponents"     % "httpclient"      % "4.5.9"
