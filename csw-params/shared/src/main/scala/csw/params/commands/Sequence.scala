e824de833ff818527f442a4b25d6ce0fed9ff095
Pritam Kadam <kpritam@thoughtworks.com>
2019-09-13 12:12:33 +0530

    Provide java api for Sequence creation

--- csw-params/shared/src/main/scala/csw/params/commands/Sequence.scala
+++ csw-params/shared/src/main/scala/csw/params/commands/Sequence.scala
@@ -3,6 +3,8 @@ package csw.params.commands
 import csw.params.core.models.Id
 import csw.serializable.CommandSerializable
 
+import scala.jdk.CollectionConverters._
+
 final case class Sequence private[params] (runId: Id, commands: Seq[SequenceCommand]) extends CommandSerializable {
   def add(others: SequenceCommand*): Sequence = copy(commands = commands ++ others)
   def add(other: Sequence): Sequence          = copy(commands = commands ++ other.commands)
@@ -10,4 +12,6 @@ final case class Sequence private[params] (runId: Id, commands: Seq[SequenceComm
 
 object Sequence {
   def apply(command: SequenceCommand, commands: SequenceCommand*): Sequence = Sequence(Id(), command :: commands.toList)
+
+  def create(commands: java.util.List[SequenceCommand]) = Sequence(Id(), commands.asScala.toList)
 }
