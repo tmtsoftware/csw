b64f5c26041a3031065fd9f83c39340c248762e3
Mushtaq Ahmed <mushtaq.a@gmail.com>
2019-10-09 20:02:50 +0530

    use borer instead of play-json in alarm service

---
+++ csw-alarm/csw-alarm-api/src/main/scala/csw/alarm/api/internal/AlarmCodecs.scala
@@ -0,0 +1,15 @@
+package csw.alarm.api.internal
+
+import csw.alarm.models._
+import csw.params.core.formats.ParamCodecs
+import io.bullet.borer.Codec
+import io.bullet.borer.derivation.MapBasedCodecs.deriveCodec
+
+private[alarm] trait AlarmCodecs extends ParamCodecs {
+  implicit lazy val alarmMetadataFormat: Codec[AlarmMetadata] =
+    deriveCodec[AlarmMetadata]
+      .bimap[AlarmMetadata](identity, metadata => metadata.copy(supportedSeverities = metadata.allSupportedSeverities))
+
+  implicit val alarmMetadataSetFormat: Codec[AlarmMetadataSet] = deriveCodec[AlarmMetadataSet]
+  implicit lazy val alarmStatusFormat: Codec[AlarmStatus]      = deriveCodec[AlarmStatus]
+}
