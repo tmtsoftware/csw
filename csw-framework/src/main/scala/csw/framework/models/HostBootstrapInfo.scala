536f55136315b38ac699069b3058ac92b21fa9d9
Mushtaq Ahmed <mushtaq.a@gmail.com>
2019-10-10 12:46:17 +0530

    use borer instead of play-json in framework codec

--- csw-framework/src/main/scala/csw/framework/models/HostBootstrapInfo.scala
+++ csw-framework/src/main/scala/csw/framework/models/HostBootstrapInfo.scala
@@ -1,9 +1,3 @@
 package csw.framework.models
 
-import play.api.libs.json.{Json, OFormat}
-
 private[framework] case class HostBootstrapInfo(containers: Set[ContainerBootstrapInfo])
-
-case object HostBootstrapInfo {
-  private[csw] implicit val format: OFormat[HostBootstrapInfo] = Json.format[HostBootstrapInfo]
-}
