1ee416ec7e49c7b5886db400c91045c74639045c
Scala Steward <me@scala-steward.org>
2019-10-31 00:16:44 +0100

    Update lettuce-core to 5.2.1.RELEASE

--- project/Libs.scala
+++ project/Libs.scala
@@ -28,7 +28,7 @@ object Libs {
   val `scalatest`         = dep("org.scalatest"     %%% "scalatest"         % "3.0.8") //Apache License 2.0
 
   val `jwt-core`          = "com.pauldijou"       %% "jwt-core"          % "4.1.0"
-  val `lettuce`           = "io.lettuce"          % "lettuce-core"       % "5.2.0.RELEASE"
+  val `lettuce`           = "io.lettuce"          % "lettuce-core"       % "5.2.1.RELEASE"
   val `reactor-core`      = "io.projectreactor"   % "reactor-core"       % "3.3.0.RELEASE"
   val `reactive-streams`  = "org.reactivestreams" % "reactive-streams"   % "1.0.3"
   val `akka-stream-kafka` = "com.typesafe.akka"   %% "akka-stream-kafka" % "1.0.5"
