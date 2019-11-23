abdca989ea2552c2a3db7c8aa2ee4def87ba5095
Mushtaq Ahmed <mushtaq.a@gmail.com>
2019-11-23 08:55:39 +0530

    Allow both Akka and Http Location to be used for CommandService

--- csw-command/csw-command-client/src/main/scala/csw/command/client/CommandServiceFactory.scala
+++ csw-command/csw-command-client/src/main/scala/csw/command/client/CommandServiceFactory.scala
@@ -6,22 +6,15 @@ import akka.http.scaladsl.model.Uri.Path
 import csw.command.api.client.CommandServiceClient
 import csw.command.api.javadsl.ICommandService
 import csw.command.api.scaladsl.CommandService
+import csw.command.client.extensions.AkkaLocationExt.RichAkkaLocation
 import csw.command.client.internal.{CommandServiceImpl, JCommandServiceImpl}
 import csw.command.client.messages.ComponentMessage
-import csw.location.models.HttpLocation
+import csw.location.models.{AkkaLocation, HttpLocation, Location, TcpLocation}
 import msocket.impl.Encoding.JsonText
 import msocket.impl.post.HttpPostTransport
 import msocket.impl.ws.WebsocketTransport
 
-/**
- * The factory helps in creating CommandService api for scala and java both
- */
-trait ICommandServiceFactory {
-  def make(componentLocation: HttpLocation)(implicit actorSystem: ActorSystem[_]): CommandService
-  def jMake(componentLocation: HttpLocation, actorSystem: ActorSystem[_]): ICommandService
-}
-
-object CommandServiceFactory extends ICommandServiceFactory {
+object CommandServiceFactory {
 
   private[csw] def make(component: ActorRef[ComponentMessage])(implicit actorSystem: ActorSystem[_]): CommandService =
     new CommandServiceImpl(component)
@@ -33,15 +26,20 @@ object CommandServiceFactory extends ICommandServiceFactory {
    * @param actorSystem of the component used for executing commands to other components and wait for the responses
    * @return an instance of type CommandService
    */
-  def make(componentLocation: HttpLocation)(implicit actorSystem: ActorSystem[_]): CommandService = {
-    import csw.command.api.codecs.CommandServiceCodecs._
-    val baseUri      = componentLocation.uri.toString
-    val webSocketUri = Uri(baseUri).withScheme("ws").withPath(Path("/websocket-endpoint"))
-    val httpUri      = Uri(baseUri).withPath(Path("/post-endpoint"))
-    new CommandServiceClient(
-      new HttpPostTransport(httpUri.toString(), JsonText, () => None),
-      new WebsocketTransport(webSocketUri.toString(), JsonText)
-    )
+  def make(componentLocation: Location)(implicit actorSystem: ActorSystem[_]): CommandService = {
+    componentLocation match {
+      case c: TcpLocation  => throw new RuntimeException(s"can not create command service using TcpLocation $c")
+      case c: AkkaLocation => new CommandServiceImpl(c.componentRef)
+      case c: HttpLocation =>
+        import csw.command.api.codecs.CommandServiceCodecs._
+        val baseUri      = c.uri.toString
+        val webSocketUri = Uri(baseUri).withScheme("ws").withPath(Path("/websocket-endpoint"))
+        val httpUri      = Uri(baseUri).withPath(Path("/post-endpoint"))
+        new CommandServiceClient(
+          new HttpPostTransport(httpUri.toString(), JsonText, () => None),
+          new WebsocketTransport(webSocketUri.toString(), JsonText)
+        )
+    }
   }
 
   /**
@@ -51,6 +49,6 @@ object CommandServiceFactory extends ICommandServiceFactory {
    * @param actorSystem of the component used for executing commands to other components and wait for the responses
    * @return an instance of type ICommandService
    */
-  def jMake(componentLocation: HttpLocation, actorSystem: ActorSystem[_]): ICommandService =
+  def jMake(componentLocation: Location, actorSystem: ActorSystem[_]): ICommandService =
     new JCommandServiceImpl(make(componentLocation)(actorSystem))
 }
