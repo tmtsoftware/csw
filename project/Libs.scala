d4c09cee32ef549cbb731c39e9ef37946a220a9c
Mushtaq Ahmed <mushtaq.a@gmail.com>
2019-11-26 07:41:36 +0530

    upgrade msocket

--- project/Libs.scala
+++ project/Libs.scala
@@ -114,7 +114,7 @@ object Jooq {
 }
 
 object MSocket {
-  val Version            = "7b3f305"
+  val Version            = "0426b6a"
   val `msocket-api`      = dep("com.github.tmtsoftware.msocket" %%% "msocket-api" % Version)
   val `msocket-impl-jvm` = "com.github.tmtsoftware.msocket" %% "msocket-impl-jvm" % Version
 }
