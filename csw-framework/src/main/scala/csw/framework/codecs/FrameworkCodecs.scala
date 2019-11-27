1eae8a9aa4894430b5254079e876811a71ed57eb
Saloni Vithalani <skvithalani@yahoo.com>
2019-11-27 11:11:59 +0530

    #CSW-77 : Saloni: Reorganize ComponentInfoCodec, move from ComponentInfo companion to FrameworkCodecs and MessageCodecs

--- csw-framework/src/main/scala/csw/framework/codecs/FrameworkCodecs.scala
+++ csw-framework/src/main/scala/csw/framework/codecs/FrameworkCodecs.scala
@@ -1,11 +1,15 @@
 package csw.framework.codecs
 
+import csw.command.client.models.framework.ComponentInfo
 import csw.framework.models.{ContainerBootstrapInfo, ContainerInfo, HostBootstrapInfo}
+import csw.location.models.codecs.LocationCodecs
 import csw.params.core.formats.CommonCodecs
 import io.bullet.borer.Codec
 import io.bullet.borer.derivation.MapBasedCodecs.deriveCodec
+import msocket.api.codecs.BasicCodecs
 
-trait FrameworkCodecs extends CommonCodecs {
+trait FrameworkCodecs extends CommonCodecs with BasicCodecs with LocationCodecs {
+  implicit lazy val componentInfoCodec: Codec[ComponentInfo]                   = deriveCodec
   implicit lazy val containerInfoCodec: Codec[ContainerInfo]                   = deriveCodec
   implicit lazy val containerBootstrapInfoCodec: Codec[ContainerBootstrapInfo] = deriveCodec
   implicit lazy val hostBootstrapInfoCodec: Codec[HostBootstrapInfo]           = deriveCodec
