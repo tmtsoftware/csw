663792359039c179133ab00508762afe2b74b959
Saloni Vithalani <skvithalani@yahoo.com>
2019-11-27 11:53:38 +0530

    #CSW-77 : Saloni: Attempt to fix build failure

--- csw-framework/src/main/scala/csw/framework/codecs/FrameworkCodecs.scala
+++ csw-framework/src/main/scala/csw/framework/codecs/FrameworkCodecs.scala
@@ -3,12 +3,19 @@ package csw.framework.codecs
 import csw.command.client.models.framework.ComponentInfo
 import csw.framework.models.{ContainerBootstrapInfo, ContainerInfo, HostBootstrapInfo}
 import csw.location.models.codecs.LocationCodecs
-import csw.params.core.formats.CommonCodecs
 import io.bullet.borer.Codec
 import io.bullet.borer.derivation.MapBasedCodecs.deriveCodec
-import msocket.api.codecs.BasicCodecs
 
-trait FrameworkCodecs extends CommonCodecs with BasicCodecs with LocationCodecs {
+import scala.concurrent.duration.FiniteDuration
+
+trait FrameworkCodecs extends LocationCodecs {
+  implicit lazy val finiteDurationCodec: Codec[FiniteDuration] = Codec.bimap[String, FiniteDuration](
+    _.toString(),
+    _.split(" ") match {
+      case Array(length, unit) => FiniteDuration(length.toLong, unit)
+      case _                   => throw new RuntimeException("error.expected.duration.finite")
+    }
+  )
   implicit lazy val componentInfoCodec: Codec[ComponentInfo]                   = deriveCodec
   implicit lazy val containerInfoCodec: Codec[ContainerInfo]                   = deriveCodec
   implicit lazy val containerBootstrapInfoCodec: Codec[ContainerBootstrapInfo] = deriveCodec
