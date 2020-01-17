09c50953e0521386fe98b9ebe5c32fa178810965
ManishGowardipe <manish.gowardipe@thoughtworks.com>
2020-01-17 10:52:18 +0530

    #ESW-278: Manish | Mausam : Refactor code and add json generation for Unregister Location type

--- csw-contract-server/src/main/scala/csw/contract/services/Location.scala
+++ csw-contract-server/src/main/scala/csw/contract/services/Location.scala
@@ -2,18 +2,25 @@ package csw.contract.services
 
 import java.net.URI
 
+import akka.Done
 import csw.location.api.codec.LocationServiceCodecs
 import csw.location.api.exceptions.{LocationServiceError, RegistrationFailed}
 import csw.location.api.messages.LocationHttpMessage
-import csw.location.api.messages.LocationHttpMessage.Register
-import csw.location.models.Connection.AkkaConnection
+import csw.location.api.messages.LocationHttpMessage.{Register, Unregister}
+import csw.location.models.Connection.{AkkaConnection, HttpConnection}
 import csw.location.models._
 import csw.prefix.models.Prefix
 import io.bullet.borer.Json
+import msocket.api.codecs.BasicCodecs
 import play.api.libs.json
 import play.api.libs.json.{JsArray, JsObject}
 
-object Location extends LocationServiceCodecs {
+object Location {
+  val registerSamples: JsObject = RegisterSamples.samples;
+  val unRegisterSamples: JsObject = UnregisterSamples.samples;
+}
+
+object RegisterSamples extends LocationServiceCodecs {
   private val componentId                           = ComponentId(Prefix("tcs.filter.wheel"), ComponentType.HCD)
   private val akkaConnection                        = AkkaConnection(componentId)
   private val akkaRegistration: LocationHttpMessage = Register(AkkaRegistration(akkaConnection, new URI("some_path")))
@@ -24,7 +31,20 @@ object Location extends LocationServiceCodecs {
   private val akkaSuccessResponse                      = json.Json.parse(Json.encode(akkaLocation).toUtf8String)
   private val registrationFailedError                  = json.Json.parse(Json.encode(registrationFailed).toUtf8String)
 
-  val contractSamples: JsObject = JsObject(
+  val samples: JsObject = JsObject(
     Map("requests" -> JsArray(List(akkaRequest)), "responses" -> JsArray(List(akkaSuccessResponse, registrationFailedError)))
+  )
+}
+
+object UnregisterSamples extends LocationServiceCodecs with BasicCodecs {
+  private val componentId                     = ComponentId(Prefix("tcs.filter.wheel"), ComponentType.HCD)
+  private val connection                      = HttpConnection(componentId)
+  private val unregister: LocationHttpMessage = Unregister(connection)
+  private val done: Done                      = Done
+  private val request                         = json.Json.parse(Json.encode(unregister).toUtf8String)
+  private val successResponse                 = json.Json.parse(Json.encode(done).toUtf8String)
+
+  val samples: JsObject = JsObject(
+    Map("requests" -> JsArray(List(request)), "responses" -> JsArray(List(successResponse)))
   )
 }
--- csw-contract-server/src/main/scala/csw/contract/services/Main.scala
+++ csw-contract-server/src/main/scala/csw/contract/services/Main.scala
@@ -2,12 +2,26 @@ package csw.contract.services
 
 import java.io.{File, PrintWriter}
 
+import com.typesafe.config.ConfigFactory
 import play.api.libs.json
+import play.api.libs.json.JsObject
 
 object Main extends App {
 
-  val location     = new File("csw-contract-server/src/main/scala/csw/contract/services/location/register.json")
-  val print_Writer = new PrintWriter(location)
-  print_Writer.write(json.Json.prettyPrint(Location.contractSamples))
-  print_Writer.close()
+  val config = ConfigFactory.load()
+  val outputPath = config.getString("csw-contract.outputPath")
+  private val locationRegister: EndpointJson = EndpointJson(s"$outputPath/location/register.json", Location.registerSamples)
+  private val locationUnregister: EndpointJson = EndpointJson(s"$outputPath/location/unregister.json", Location.unRegisterSamples)
+
+  private val endpointJsons = List(locationRegister, locationUnregister)
+
+  endpointJsons.foreach(write)
+
+  def write(endpointJson: EndpointJson): Unit = {
+    val printWriter = new PrintWriter(endpointJson.filePath)
+    printWriter.write(json.Json.prettyPrint(endpointJson.samples))
+    printWriter.close()
+  }
 }
+
+case class EndpointJson(filePath: String, samples: JsObject)
