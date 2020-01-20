d5b5bb1648c71baa64e958a66eb4a99c4985ab50
Mushtaq Ahmed <mushtaq.a@gmail.com>
2020-01-20 15:08:14 +0530

    upgrade embedded-keycloak

--- project/Libs.scala
+++ project/Libs.scala
@@ -14,7 +14,7 @@ object Libs {
   val `silencer-lib`       = "com.github.ghik"        % "silencer-lib"        % silencerVersion cross CrossVersion.full
   val `mockito-scala`      = "org.mockito"            %% "mockito-scala"      % "1.7.1"
 
-  val `embedded-keycloak`            = "com.github.tmtsoftware.embedded-keycloak" %% "embedded-keycloak"            % "c822dfa" //Apache 2.0
+  val `embedded-keycloak`            = "com.github.tmtsoftware.embedded-keycloak" %% "embedded-keycloak"            % "8db3344" //Apache 2.0
   val `logback-classic`              = "ch.qos.logback"                           % "logback-classic"               % "1.2.3" //Dual license: Either, Eclipse Public License v1.0 or GNU Lesser General Public License version 2.1
   val `akka-management-cluster-http` = "com.lightbend.akka.management"            %% "akka-management-cluster-http" % "1.0.5"
   val `svnkit`                       = "org.tmatesoft.svnkit"                     % "svnkit"                        % "1.10.1" //TMate Open Source License
@@ -50,7 +50,6 @@ object Libs {
   val `config`          = "com.typesafe"                  % "config"          % "1.4.0"
   val `os-lib`          = "com.lihaoyi"                   %% "os-lib"         % "0.6.3"
   val `caffeine`        = "com.github.ben-manes.caffeine" % "caffeine"        % "2.8.1"
-
 }
 
 object Borer {
