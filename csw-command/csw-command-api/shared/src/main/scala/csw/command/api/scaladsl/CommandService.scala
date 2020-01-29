0cbab15e970b62682822995e7ea64ecb5529b3a7
Saloni Vithalani <skvithalani@yahoo.com>
2020-01-29 15:58:35 +0530

    #000 : Saloni : Attempt to fix build failure

--- csw-command/csw-command-api/shared/src/main/scala/csw/command/api/scaladsl/CommandService.scala
+++ csw-command/csw-command-api/shared/src/main/scala/csw/command/api/scaladsl/CommandService.scala
@@ -94,7 +94,7 @@ trait CommandService {
   def queryFinal(commandRunId: Id)(implicit timeout: Timeout): Future[SubmitResponse]
 
   /**
-   * Subscribe to the current state of a component corresponding to the [[AkkaLocation]] of the component
+   * Subscribe to the current state of a component corresponding to the [[csw.location.api.models.AkkaLocation]] of the component
    *
    * @param names subscribe to states which have any of the provided value for name.
    *              If no states are provided, all the current states will be received.
@@ -103,7 +103,7 @@ trait CommandService {
   def subscribeCurrentState(names: Set[StateName] = Set.empty): Source[CurrentState, Subscription]
 
   /**
-   * Subscribe to the current state of a component corresponding to the [[AkkaLocation]] of the component
+   * Subscribe to the current state of a component corresponding to the [[csw.location.api.models.AkkaLocation]] of the component
    *
    * @note Callbacks are not thread-safe on the JVM. If you are doing side effects/mutations inside the callback, you should ensure that it is done in a thread-safe way inside an actor.
    * @param callback the action to be applied on the CurrentState element received as a result of subscription
@@ -112,7 +112,7 @@ trait CommandService {
   def subscribeCurrentState(callback: CurrentState => Unit): Subscription
 
   /**
-   * Subscribe to the current state of a component corresponding to the [[AkkaLocation]] of the component
+   * Subscribe to the current state of a component corresponding to the [[csw.location.api.models.AkkaLocation]] of the component
    *
    * @note Callbacks are not thread-safe on the JVM. If you are doing side effects/mutations inside the callback, you should ensure that it is done in a thread-safe way inside an actor.
    * @param names subscribe to only those states which have any of the provided value for name
--- csw-framework/src/main/scala/csw/framework/javadsl/JComponentInfo.scala
+++ csw-framework/src/main/scala/csw/framework/javadsl/JComponentInfo.scala
@@ -19,7 +19,7 @@ object JComponentInfo {
    * The information needed to create a component. This class is created after de-serializing the config file for the component.
    *
    * @param prefix prefix for the component with `subsystem` and `name`
-   * @param componentType the type of the component as defined by [[ComponentType]]
+   * @param componentType the type of the component as defined by [[csw.location.api.models.ComponentType]]
    * @param className specifies the component to be created by name of the class of it's factory
    * @param locationServiceUsage specifies component's usage of location service
    * @param connections set of connections that will be used by this component for interaction
