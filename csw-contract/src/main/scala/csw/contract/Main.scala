21a8005bdbabcd5e940c3a8895113fda7e7f3c03
ManishGowardipe <manish.gowardipe@thoughtworks.com>
2020-02-03 17:26:26 +0530

    #ESW-278: Saloni | Manish : rename output to contracts as top folder
    Co-authored-by: Saloni Vithalani <skvithalani@yahoo.com>

--- csw-contract/src/main/scala/csw/contract/Main.scala
+++ csw-contract/src/main/scala/csw/contract/Main.scala
@@ -5,7 +5,7 @@ import csw.contract.generator.FilesGenerator
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val DefaultOutputPath   = "csw-contract/target/output"
+    val DefaultOutputPath   = "csw-contract/target/contracts"
     val DefaultResourcePath = "csw-contract/src/main/resources"
     val outputPath          = if (args.isEmpty) DefaultOutputPath else args(0)
     val resourcesPath       = if (args.isEmpty) DefaultResourcePath else args(1)
