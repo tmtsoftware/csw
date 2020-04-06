5bfbf7103808e67d06c82b51b6673bad22498dfa
Scala Steward <me@scala-steward.org>
2020-04-06 09:05:39 +0200

    Update mockito-scala to 1.13.6

--- project/Libs.scala
+++ project/Libs.scala
@@ -11,7 +11,7 @@ object Libs {
   val `scopt`              = "com.github.scopt"       %% "scopt"              % "3.7.1" //MIT License
   val `silencer-plugin`    = "com.github.ghik"        % "silencer-plugin"     % silencerVersion cross CrossVersion.full
   val `silencer-lib`       = "com.github.ghik"        % "silencer-lib"        % silencerVersion cross CrossVersion.full
-  val `mockito-scala`      = "org.mockito"            %% "mockito-scala"      % "1.13.1"
+  val `mockito-scala`      = "org.mockito"            %% "mockito-scala"      % "1.13.6"
 
   val `embedded-keycloak`            = "com.github.tmtsoftware.embedded-keycloak" %% "embedded-keycloak"            % "0.1.7" //Apache 2.0
   val `logback-classic`              = "ch.qos.logback"                           % "logback-classic"               % "1.2.3" //Dual license: Either, Eclipse Public License v1.0 or GNU Lesser General Public License version 2.1
