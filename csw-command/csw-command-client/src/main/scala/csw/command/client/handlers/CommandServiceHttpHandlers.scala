ec52fa0b0d9feb3628b95dc494b2b15c0d592645
Pritam Kadam <phkadam2008@gmail.com>
2020-04-17 19:29:05 +0530

    Refactor auth policy implementation in CommandServiceHttpHandlers

--- csw-command/csw-command-client/src/main/scala/csw/command/client/handlers/CommandServiceHttpHandlers.scala
+++ csw-command/csw-command-client/src/main/scala/csw/command/client/handlers/CommandServiceHttpHandlers.scala
@@ -3,13 +3,13 @@ package csw.command.client.handlers
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import csw.aas.http.AuthorizationPolicy.CustomPolicy
-import csw.aas.http.{AuthorizationPolicy, SecurityDirectives}
+import csw.aas.http.SecurityDirectives
 import csw.command.api.codecs.CommandServiceCodecs._
 import csw.command.api.messages.CommandServiceHttpMessage
 import csw.command.api.messages.CommandServiceHttpMessage._
 import csw.command.api.scaladsl.CommandService
-import csw.command.api.utils.CommandRoleMapping
-import csw.command.api.utils.CommandRoleMapping.{AllowedCommands, Role}
+import csw.command.client.models.CommandRoles
+import csw.params.commands.ControlCommand
 import csw.prefix.models.Prefix
 import msocket.impl.post.{HttpPostHandler, ServerHttpCodecs}
 
@@ -17,61 +17,24 @@ class CommandServiceHttpHandlers(
     commandService: CommandService,
     securityDirectives: SecurityDirectives,
     destinationPrefix: Option[Prefix] = None,
-    commandRoleMapping: CommandRoleMapping = CommandRoleMapping.empty
+    commandRoles: CommandRoles = CommandRoles.empty
 ) extends HttpPostHandler[CommandServiceHttpMessage]
     with ServerHttpCodecs {
+  private val destinationSubsystem = destinationPrefix.map(_.subsystem)
 
   override def handle(request: CommandServiceHttpMessage): Route = request match {
-    case Validate(controlCommand) =>
-      val subsystem   = destinationPrefix.get.subsystem.name
-      val commandName = subsystem + controlCommand.commandName.name
-      securityDirectives.sPost(commandPolicy(subsystem, commandName))(_ =>
-        complete(commandService.validate(controlCommand))
-      )
-    case Submit(controlCommand) =>
-      val subsystem   = destinationPrefix.get.subsystem.name
-      val commandName = subsystem + controlCommand.commandName.name
-      securityDirectives.sPost(commandPolicy(subsystem, commandName))(_ =>
-        complete(commandService.submit(controlCommand))
-      )
-    case Oneway(controlCommand) =>
-      val subsystem   = destinationPrefix.get.subsystem.name
-      val commandName = subsystem + controlCommand.commandName.name
-      securityDirectives.sPost(commandPolicy(subsystem, commandName))(_ =>
-        complete(commandService.oneway(controlCommand))
-      )
-    case Query(runId) => complete(commandService.query(runId))
+    case Validate(controlCommand) => sPost(controlCommand)(complete(commandService.validate(controlCommand)))
+    case Submit(controlCommand)   => sPost(controlCommand)(complete(commandService.submit(controlCommand)))
+    case Oneway(controlCommand)   => sPost(controlCommand)(complete(commandService.oneway(controlCommand)))
+    case Query(runId)             => complete(commandService.query(runId))
   }
-
-  private def commandPolicy(subsystem: String, commandName: Role): AuthorizationPolicy = {
-    commandPolicyUsingConfig(commandName) | subsystemPolicyWithoutRoleCheck(subsystem)
-  }
-
-  private def commandPolicyUsingConfig(commandName: String): CustomPolicy = {
-    CustomPolicy { token =>
-      commandRoleMapping.map.exists { entry: (Role, AllowedCommands) =>
-        token.realm_access.roles.contains(entry._1) && entry._2.contains(commandName)
-      }
+  private def sPost(controlCommand: ControlCommand)(route: Route) =
+    destinationSubsystem match {
+      case None => route // auth is disabled in this case
+      case Some(subsystem) =>
+        securityDirectives.sPost(
+          CustomPolicy(token => commandRoles.hasAccess(controlCommand.commandName.name, subsystem, token.realm_access.roles))
+        )(_ => route)
     }
-  }
-
-  private def subsystemPolicyWithoutRoleCheck(subsystem: String): CustomPolicy = {
-    CustomPolicy { token => token.realm_access.roles.exists { roleFromToken => roleFromToken.contains(subsystem) } }
-  }
-
-  //  private def commandPermissionForSubsystem(subsystem: String, commandName: String): CustomPolicy = {
-  //    CustomPolicy { token =>
-  //      commandRoleMapping.get.map
-  //        .filter { role => role._1.contains(subsystem) } // APS-user APS-eng APS-admin
-  //        .exists { entry: (Role, AllowedCommands) =>
-  //          token.realm_access.roles.contains(entry._1) && entry._2.contains(commandName)
-  //        }
-  //    }
-  //  }
-
-  //  private def rolePermissionWithoutSubsystemCheck(role: String): CustomPolicy = {
-  //    CustomPolicy { token => token.realm_access.roles.exists { roleFromToken => roleFromToken.contains(role) } }
-  //  }
-
 
 }
