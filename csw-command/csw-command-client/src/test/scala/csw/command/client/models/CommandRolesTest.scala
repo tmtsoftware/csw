018744d6dd79d46c75397a9454eb14dbed0082e8
Pritam Kadam <phkadam2008@gmail.com>
2020-04-18 11:46:38 +0530

    Use fine grained command key (prefix.commandName) in CommandRoles model as per latest discussion from slack

--- csw-command/csw-command-client/src/test/scala/csw/command/client/models/CommandRolesTest.scala
+++ csw-command/csw-command-client/src/test/scala/csw/command/client/models/CommandRolesTest.scala
@@ -1,24 +1,29 @@
 package csw.command.client.models
 
 import com.typesafe.config.ConfigFactory
-import csw.command.client.auth.CommandRoles
+import csw.command.client.auth.{CommandKey, CommandRoles, Roles}
 import csw.prefix.models.Subsystem.{IRIS, TCS}
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
 class CommandRolesTest extends AnyFunSuite with Matchers {
   private val testConfig = ConfigFactory.parseString("""
-                                           |setVoltage: [IRIS-user, APS-eng]
-                                           |setFirmware: [TCS-admin]
-                                           |setLimits: [WFOS-eng, TCS-eng]
+                                           |IRIS.instrument1.setVoltage: [IRIS-user, APS-eng]
+                                           |TCS.filter.wheel.setFirmware: [TCS-admin]
+                                           |WFOS.mirror.setLimits: [WFOS-eng, TCS-eng]
                                            |""".stripMargin)
 
-  test("CommandRoles.from should successfully parse config file and store it in lower case") {
+  private val setVoltageCmdKey  = CommandKey("iris.instrument1.setvoltage")
+  private val setFirmwareCmdKey = CommandKey("tcs.filter.wheel.setfirmware")
+  private val setLimitsCmdKey   = CommandKey("wfos.mirror.setlimits")
+
+  test("CommandRoles.from should successfully parse config file and store it in lower case in the appropriate models") {
+
     val expectedRoles = CommandRoles(
       Map(
-        "setvoltage"  -> Set("iris-user", "aps-eng"),
-        "setfirmware" -> Set("tcs-admin"),
-        "setlimits"   -> Set("wfos-eng", "tcs-eng")
+        setVoltageCmdKey  -> Roles(Set("iris-user", "aps-eng")),
+        setFirmwareCmdKey -> Roles(Set("tcs-admin")),
+        setLimitsCmdKey   -> Roles(Set("wfos-eng", "tcs-eng"))
       )
     )
     val actualRoles = CommandRoles.from(testConfig)
@@ -27,28 +32,28 @@ class CommandRolesTest extends AnyFunSuite with Matchers {
 
   test("CommandRoles.hasAccess should return true when user has correct role") {
     val roles = CommandRoles.from(testConfig)
-    roles.hasAccess("setVoltage", IRIS, Set("IRIS-user")) shouldBe true
-    roles.hasAccess("setVoltage", IRIS, Set("APS-eng")) shouldBe true
-    roles.hasAccess("setVoltage", IRIS, Set("IRIS-user", "APS-eng")) shouldBe true
+    roles.hasAccess(setVoltageCmdKey, IRIS, Roles(Set("IRIS-user"))) shouldBe true
+    roles.hasAccess(setVoltageCmdKey, IRIS, Roles(Set("APS-eng"))) shouldBe true
+    roles.hasAccess(setVoltageCmdKey, IRIS, Roles(Set("IRIS-user", "APS-eng"))) shouldBe true
   }
 
   test("CommandRoles.hasAccess should return false when user does not have required role") {
     val roles = CommandRoles.from(testConfig)
-    roles.hasAccess("setVoltage", IRIS, Set("WFOS-user")) shouldBe false
-    roles.hasAccess("setFirmware", TCS, Set("TCS-eng")) shouldBe false
+    roles.hasAccess(setVoltageCmdKey, IRIS, Roles(Set("WFOS-user"))) shouldBe false
+    roles.hasAccess(setFirmwareCmdKey, TCS, Roles(Set("TCS-eng"))) shouldBe false
   }
 
   test(
     "CommandRoles.hasAccess should return true when command name does not exist but has user level access to destination subsystem"
   ) {
     val roles = CommandRoles.from(testConfig)
-    roles.hasAccess("setExposure", IRIS, Set("IRIS-user")) shouldBe true
+    roles.hasAccess(CommandKey("IRIS.filter.setExposure"), IRIS, Roles(Set("IRIS-user"))) shouldBe true
   }
 
   test(
     "CommandRoles.hasAccess should return false when command name does not exist and does not have user level access to destination subsystem"
   ) {
     val roles = CommandRoles.from(testConfig)
-    roles.hasAccess("setExposure", IRIS, Set("WFOS-user")) shouldBe false
+    roles.hasAccess(CommandKey("IRIS.filter.setExposure"), IRIS, Roles(Set("WFOS-user"))) shouldBe false
   }
 }
