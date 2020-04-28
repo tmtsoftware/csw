f6ae4f21993871d5ad78be9bfe5cbeab94f61ce0
Himanshu Mertia <22441818+mertia-himanshu@users.noreply.github.com>
2020-04-28 22:17:03 +0530

    Use typed actor system instead of classic where ever possible

--- csw-framework/src/main/scala/csw/framework/internal/supervisor/HttpService.scala
+++ csw-framework/src/main/scala/csw/framework/internal/supervisor/HttpService.scala
@@ -2,7 +2,6 @@ package csw.framework.internal.supervisor
 
 import akka.actor.CoordinatedShutdown
 import akka.actor.typed.ActorSystem
-import akka.actor.typed.scaladsl.adapter._
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.Http.ServerBinding
 import akka.http.scaladsl.server.Route
@@ -34,7 +33,7 @@ class HttpService(
     val registrationResult = await(register(binding)) // create HttpRegistration and register it with location service
 
     // Add the task to unregister the HttpRegistration from location service.
-    CoordinatedShutdown(actorSystem.toClassic).addTask(
+    CoordinatedShutdown(actorSystem).addTask(
       CoordinatedShutdown.PhaseBeforeServiceUnbind,
       s"unregistering-${registrationResult.location}"
     )(() => registrationResult.unregister())
@@ -44,7 +43,7 @@ class HttpService(
   }
 
   private def bind() = {
-    Http()(actorSystem.toClassic).bindAndHandle(
+    Http().bindAndHandle(
       handler = route,
       interface = "0.0.0.0",
       port = 0
