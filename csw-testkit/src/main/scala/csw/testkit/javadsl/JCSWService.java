321cc9bd6b8ce91d27d7acc9da2f895b824ec347
Shubham Jaybhaye <shubhamj7997@gmail.com>
2020-05-06 10:33:53 +0530

    #000: Fix the broken link in doc

--- csw-testkit/src/main/scala/csw/testkit/javadsl/JCSWService.java
+++ csw-testkit/src/main/scala/csw/testkit/javadsl/JCSWService.java
@@ -3,7 +3,7 @@ package csw.testkit.javadsl;
 import csw.testkit.scaladsl.CSWService;
 
 /**
- * Helper class for Java to get handle of [[{@link CSWService}]]
+ * Helper class for Java to get handle of {@link csw.testkit.scaladsl.CSWService}
  *
  * Specify one or more services from following ADT's while creating FrameworkTestKit
  * and testkit will make sure that those services are started.
