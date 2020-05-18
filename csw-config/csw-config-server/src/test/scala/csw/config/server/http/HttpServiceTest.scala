163ff0f89bcbdf72b397150ea330ec3cffc4764b
Himanshu Mertia <22441818+mertia-himanshu@users.noreply.github.com>
2020-05-18 21:28:20 +0530

    #CSW-97: Himanshu : use future value instead of await in test

--- csw-config/csw-config-server/src/test/scala/csw/config/server/http/HttpServiceTest.scala
+++ csw-config/csw-config-server/src/test/scala/csw/config/server/http/HttpServiceTest.scala
@@ -4,33 +4,34 @@ import akka.actor.typed.{ActorSystem, SpawnProtocol}
 import csw.aas.core.commons.AASConnection
 import csw.config.server.ServerWiring
 import csw.config.server.commons.ConfigServiceConnection
-import csw.config.server.commons.TestFutureExtension.RichFuture
 import csw.location.api.exceptions.OtherLocationIsRegistered
 import csw.location.api.models
 import csw.location.api.models.NetworkType
 import csw.location.api.scaladsl.LocationService
 import csw.location.client.scaladsl.HttpLocationServiceFactory
 import csw.location.server.http.HTTPLocationService
-import csw.network.utils.Networks
+import csw.network.utils.{Networks, SocketUtils}
 
+import scala.concurrent.duration.DurationDouble
 import scala.util.control.NonFatal
 
 class HttpServiceTest extends HTTPLocationService {
 
   implicit val system: ActorSystem[SpawnProtocol.Command] = ActorSystem(SpawnProtocol(), "test")
   private val testLocationService: LocationService        = HttpLocationServiceFactory.makeLocalClient
+  implicit override val patienceConfig: PatienceConfig    = PatienceConfig(10.seconds, 100.millis)
 
   //register AAS with location service
   private val AASPort = 8080
 
   override def beforeAll(): Unit = {
     super.beforeAll()
-    testLocationService.register(models.HttpRegistration(AASConnection.value, AASPort, "auth")).await
+    testLocationService.register(models.HttpRegistration(AASConnection.value, AASPort, "auth")).futureValue
   }
 
   override def afterAll(): Unit = {
     system.terminate()
-    system.whenTerminated.await
+    system.whenTerminated.futureValue
     super.afterAll()
   }
 
@@ -50,43 +51,50 @@ class HttpServiceTest extends HTTPLocationService {
     val _servicePort = 4005
     val serverWiring = ServerWiring.make(Some(_servicePort))
     import serverWiring._
-    val (_, registrationResult) = httpService.registeredLazyBinding.await
-    locationService.find(ConfigServiceConnection.value).await.get.connection shouldBe ConfigServiceConnection.value
 
+    SocketUtils.isAddressInUse(hostname, _servicePort) shouldBe false
+
+    val (_, registrationResult) = httpService.registeredLazyBinding.futureValue
+
+    locationService.find(ConfigServiceConnection.value).futureValue.get.connection shouldBe ConfigServiceConnection.value
     val location = registrationResult.location
     location.uri.getHost shouldBe hostname
     location.uri.getPort shouldBe _servicePort
     location.connection shouldBe ConfigServiceConnection.value
-    actorRuntime.shutdown().await
+    SocketUtils.isAddressInUse(hostname, _servicePort) shouldBe true
+    actorRuntime.shutdown().futureValue
   }
 
   test("should not register with location service if server binding fails | CSW-97") {
-    val _servicePort = 3553 // Location Service runs on this port
-    val serverWiring = ServerWiring.make(Some(_servicePort))
-    val expectedMessage = s"Bind failed because of java.net.BindException: [/${hostname}:${_servicePort}] Address " +
-      s"already in use"
+    val _servicePort    = 3553 // Location Service runs on this port
+    val serverWiring    = ServerWiring.make(Some(_servicePort))
+    val address         = s"[/${hostname}:${_servicePort}]"
+    val expectedMessage = s"Bind failed because of java.net.BindException: $address Address already in use"
     import serverWiring._
 
-    val bindException = intercept[Exception] { httpService.registeredLazyBinding.await }
+    val bindException = intercept[Exception] { httpService.registeredLazyBinding.futureValue }
 
-    bindException.getMessage shouldBe expectedMessage
-    testLocationService.find(ConfigServiceConnection.value).await shouldBe None
-
+    bindException.getCause.getMessage shouldBe expectedMessage
+    testLocationService.find(ConfigServiceConnection.value).futureValue shouldBe None
   }
 
   test("should not start server if registration with location service fails") {
-    val _someOtherLocationPort = 21212
+    val _existingServicePort = 21212
     val _servicePort           = 4007
     val serverWiring           = ServerWiring.make(Some(_servicePort))
     import serverWiring._
-    locationService.register(models.HttpRegistration(ConfigServiceConnection.value, _someOtherLocationPort, "")).await
+    locationService
+      .register(models.HttpRegistration(ConfigServiceConnection.value, _existingServicePort, "", NetworkType.Public))
+      .futureValue
+    locationService.find(ConfigServiceConnection.value).futureValue.get.connection shouldBe ConfigServiceConnection.value
 
-    locationService.find(ConfigServiceConnection.value).await.get.connection shouldBe ConfigServiceConnection.value
+    SocketUtils.isAddressInUse(hostname, _servicePort) shouldBe false
 
-    a[OtherLocationIsRegistered] shouldBe thrownBy(httpService.registeredLazyBinding.await)
+    val otherLocationIsRegistered = intercept[Exception] { httpService.registeredLazyBinding.futureValue }
 
-    //TODO: Find a way to assert server is not bounded
-    try actorRuntime.shutdown().await
+    otherLocationIsRegistered.getCause shouldBe a[OtherLocationIsRegistered]
+    SocketUtils.isAddressInUse(hostname, _servicePort) shouldBe false
+    try actorRuntime.shutdown().futureValue
     catch {
       case NonFatal(_) =>
     }
