f7b1442adbd95e8d48613658d5fe10ff570153d8
Himanshu Mertia <22441818+mertia-himanshu@users.noreply.github.com>
2020-05-20 23:43:05 +0530

    #CSW-96: Himanshu : Try to fix port Conflict

--- csw-location/csw-location-server/src/test/scala/csw/location/server/MainTest.scala
+++ csw-location/csw-location-server/src/test/scala/csw/location/server/MainTest.scala
@@ -2,12 +2,20 @@ package csw.location.server
 
 import csw.location.api.models.NetworkType
 import csw.network.utils.{Networks, SocketUtils}
+import org.scalatest.BeforeAndAfterAll
 import org.scalatest.funsuite.AnyFunSuiteLike
 import org.scalatest.matchers.should.Matchers
 
-class MainTest extends AnyFunSuiteLike with Matchers {
+class MainTest extends AnyFunSuiteLike with Matchers with BeforeAndAfterAll {
   private val clusterSeedIp = Networks().hostname
-  private val httpPort      = 7654
+  //TODO bind to any port rather than 7654
+  private val httpPort = 7654
+
+  //TODO cleanup binding
+  override def afterAll(): Unit = {
+    System.clearProperty("CLUSTER_SEEDS")
+    super.afterAll()
+  }
 
   test("Bind location server to 127.0.0.1 without publicNetwork option | CSW-96, CSW-89") {
     val clusterPort = SocketUtils.getFreePort
