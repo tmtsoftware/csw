3074f5c5f3acb30a2429700339b8b93ecb336a5c
Himanshu Mertia <22441818+mertia-himanshu@users.noreply.github.com>
2020-05-22 17:34:36 +0530

    #CSW-96: Himanshu : Enable test , removed env variable usage in test which was causing build failure

--- csw-location/csw-location-server/src/main/scala/csw/location/server/Main.scala
+++ csw-location/csw-location-server/src/main/scala/csw/location/server/Main.scala
@@ -26,32 +26,37 @@ object Main {
 
   def start(args: Array[String], startLogging: Boolean = false): Option[(ServerBinding, ServerWiring)] =
     new ArgsParser(name).parse(args.toList).map {
-      case options @ Options(maybeClusterPort, publicNetwork) =>
-        require(
-          ClusterAwareSettings.seedNodes.nonEmpty,
-          "[ERROR] CLUSTER_SEEDS setting is not specified either as env variable or system property. Please check online documentation for this set-up."
-        )
-
-        val wiring = ServerWiring.make(maybeClusterPort, publicNetwork)
-
-        import wiring._
-        try {
-          if (startLogging) actorRuntime.startLogging(name, clusterSettings.hostname)
-
-          val locationBinding = Await.result(locationHttpService.start(options.httpBindHost), timeout)
-
-          actorRuntime.coordinatedShutdown.addTask(
-            CoordinatedShutdown.PhaseServiceUnbind,
-            "unbind-services"
-          )(() => locationBinding.terminate(30.seconds).map(_ => Done)(actorRuntime.ec))
-          (locationBinding, wiring)
-        }
-        catch {
-          case NonFatal(ex) =>
-            ex.printStackTrace()
-            Await.result(actorRuntime.shutdown(), timeout)
-            throw ex
-        }
+      case options @ Options(_, _) =>
+        requiredClusterSeedsSet()
+        start(startLogging, options)
     }
 
+  private[server] def start(startLogging: Boolean, options: Options): (ServerBinding, ServerWiring) = {
+    val wiring = ServerWiring.make(options.clusterPort, options.publicNetwork)
+
+    import wiring._
+    try {
+      if (startLogging) actorRuntime.startLogging(name, clusterSettings.hostname)
+      val locationBinding = Await.result(locationHttpService.start(options.httpBindHost), timeout)
+
+      actorRuntime.coordinatedShutdown.addTask(
+        CoordinatedShutdown.PhaseServiceUnbind,
+        "unbind-services"
+      )(() => locationBinding.terminate(30.seconds).map(_ => Done)(actorRuntime.ec))
+      (locationBinding, wiring)
+    }
+    catch {
+      case NonFatal(ex) =>
+        ex.printStackTrace()
+        Await.result(actorRuntime.shutdown(), timeout)
+        throw ex
+    }
+  }
+
+  private def requiredClusterSeedsSet(): Unit = {
+    require(
+      ClusterAwareSettings.seedNodes.nonEmpty,
+      "[ERROR] CLUSTER_SEEDS setting is not specified either as env variable or system property. Please check online documentation for this set-up."
+    )
+  }
 }
--- csw-location/csw-location-server/src/test/scala/csw/location/server/MainTest.scala
+++ csw-location/csw-location-server/src/test/scala/csw/location/server/MainTest.scala
@@ -4,63 +4,57 @@ import csw.aas.core.commons.AASConnection
 import csw.location.api.models.{HttpRegistration, NetworkType}
 import csw.location.api.scaladsl.LocationService
 import csw.location.client.scaladsl.HttpLocationServiceFactory
+import csw.location.server.cli.ArgsParser
 import csw.network.utils.{Networks, SocketUtils}
 import msocket.impl.HttpError
-import org.scalatest.BeforeAndAfterAll
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.funsuite.AnyFunSuiteLike
 import org.scalatest.matchers.should.Matchers
 
 import scala.concurrent.duration.DurationInt
-class MainTest extends AnyFunSuiteLike with Matchers with BeforeAndAfterAll with ScalaFutures {
-  private val clusterSeedIp             = Networks().hostname
-  private val httpPort                  = 7654
-  implicit val patience: PatienceConfig = PatienceConfig(5.seconds, 100.millis)
+class MainTest extends AnyFunSuiteLike with Matchers with ScalaFutures {
+  private val httpPort                          = 7654
+  private implicit val patience: PatienceConfig = PatienceConfig(5.seconds, 100.millis)
 
-  override def afterAll(): Unit = {
-    System.clearProperty("CLUSTER_SEEDS")
-  }
+  test("when publicNetwork option NOT given, should bind to 127.0.0.1 with auth DISABLED | CSW-96, CSW-89") {
+    val args              = Array("--clusterPort", s"${SocketUtils.getFreePort}")
+    val options           = new ArgsParser("csw-location-server").parse(args.toList).get
+    val `127.0.0.1`       = "127.0.0.1"
+    val (binding, wiring) = Main.start(startLogging = false, options = options)
 
-  ignore("when publicNetwork option NOT given, should bind to 127.0.0.1 with auth DISABLED | CSW-96, CSW-89") {
-    val clusterPort = SocketUtils.getFreePort
-    System.clearProperty("CLUSTER_SEEDS")
-    System.setProperty("CLUSTER_SEEDS", s"$clusterSeedIp:$clusterPort")
-    val `127.0.0.1` = "127.0.0.1"
-
-    val (binding, wiring) = Main.start(Array("--clusterPort", s"$clusterPort")).get
-
+    //assert location server running at 127.0.0.1
     binding.localAddress.getAddress.getHostAddress shouldBe `127.0.0.1`
     SocketUtils.isAddressInUse(`127.0.0.1`, httpPort) shouldBe true
 
-    // connect to location service running on 127.0.0.1, can access protected resource as auth is disabled
-    val locationServiceClient: LocationService = HttpLocationServiceFactory.makeLocalClient(wiring.actorSystem)
-    locationServiceClient.unregisterAll().futureValue
+    // Can access protected resource of location server running at 127.0.0.1, as auth is disabled
+    val httpClient: LocationService = HttpLocationServiceFactory.makeLocalClient(wiring.actorSystem)
+    httpClient.unregisterAll().futureValue
 
+    //Clean up
     binding.terminate(2.seconds).futureValue
     wiring.actorRuntime.shutdown().futureValue
   }
 
-  ignore("when publicNetwork option is given, should bind to Public Network IP with auth ENABLED | CSW-96, CSW-89") {
-    val clusterPort = SocketUtils.getFreePort
-    System.clearProperty("CLUSTER_SEEDS")
-    System.setProperty("CLUSTER_SEEDS", s"$clusterSeedIp:$clusterPort")
+  test("when publicNetwork option is given, should bind to Public Network IP with auth ENABLED | CSW-96, CSW-89") {
+    val args     = Array("--clusterPort", s"${SocketUtils.getFreePort}", "--publicNetwork")
+    val options  = new ArgsParser("csw-location-server").parse(args.toList).get
     val hostname = Networks(NetworkType.Public.envKey).hostname
 
-    val (binding, wiring) = Main.start(Array("--clusterPort", s"$clusterPort", "--publicNetwork")).get
-    //This is to make sure, it does not give AASResolution Failed error
-    //This register is done via httpService impl bypassing HTTP Layer, hence dont require auth, do not confuse it with
-    //calls from locationServiceClient like register, unregisterAll which will require auth, if auth is enabled
+    val (binding, wiring) = Main.start(startLogging = false, options = options)
+    //AAS location is registered here to make sure, it does not give AASResolution Failed error
     wiring.locationService.register(HttpRegistration(AASConnection.value, SocketUtils.getFreePort, "auth")).futureValue
 
+    //assert location server running at hostname
     binding.localAddress.getAddress.getHostAddress shouldBe hostname
     SocketUtils.isAddressInUse(hostname, httpPort) shouldBe true
 
-    // connect to location service running on hostname, cannot access protected resource as auth is enabled
-    val locationServiceClient: LocationService = HttpLocationServiceFactory.make(hostname)(wiring.actorSystem)
-    val exception                              = intercept[Exception](locationServiceClient.unregisterAll().futureValue)
+    // Can not access protected resource of location server running at hostname, as auth is enabled
+    val httpClient: LocationService = HttpLocationServiceFactory.make(hostname)(wiring.actorSystem)
+    val exception                   = intercept[Exception](httpClient.unregisterAll().futureValue)
     exception.getCause shouldBe a[HttpError]
     exception.getCause.asInstanceOf[HttpError].statusCode shouldBe 401
 
+    //Clean up
     binding.terminate(2.seconds).futureValue
     wiring.actorRuntime.shutdown().futureValue
   }
