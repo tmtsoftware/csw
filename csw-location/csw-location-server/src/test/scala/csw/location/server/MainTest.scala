1fd5d1a8911ccebec110e57a9a5ef048aecdf4c0
Himanshu Mertia <22441818+mertia-himanshu@users.noreply.github.com>
2020-05-22 14:14:22 +0530

    #CSW-96: Himanshu : Add auth enable/disable assertions in location server test

--- csw-location/csw-location-server/src/test/scala/csw/location/server/MainTest.scala
+++ csw-location/csw-location-server/src/test/scala/csw/location/server/MainTest.scala
@@ -1,7 +1,11 @@
 package csw.location.server
 
-import csw.location.api.models.NetworkType
+import csw.aas.core.commons.AASConnection
+import csw.location.api.models.{HttpRegistration, NetworkType}
+import csw.location.api.scaladsl.LocationService
+import csw.location.client.scaladsl.HttpLocationServiceFactory
 import csw.network.utils.{Networks, SocketUtils}
+import msocket.impl.HttpError
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.funsuite.AnyFunSuiteLike
@@ -17,7 +21,7 @@ class MainTest extends AnyFunSuiteLike with Matchers with BeforeAndAfterAll with
     System.clearProperty("CLUSTER_SEEDS")
   }
 
-  test("Bind location server to 127.0.0.1 without publicNetwork option | CSW-96, CSW-89") {
+  test("when publicNetwork option NOT given, should bind to 127.0.0.1 with auth DISABLED | CSW-96, CSW-89") {
     val clusterPort = SocketUtils.getFreePort
     System.clearProperty("CLUSTER_SEEDS")
     System.setProperty("CLUSTER_SEEDS", s"$clusterSeedIp:$clusterPort")
@@ -27,20 +31,36 @@ class MainTest extends AnyFunSuiteLike with Matchers with BeforeAndAfterAll with
 
     binding.localAddress.getAddress.getHostAddress shouldBe `127.0.0.1`
     SocketUtils.isAddressInUse(`127.0.0.1`, httpPort) shouldBe true
+
+    // connect to location service running on 127.0.0.1, can access protected resource as auth is disabled
+    val locationServiceClient: LocationService = HttpLocationServiceFactory.makeLocalClient(wiring.actorSystem)
+    locationServiceClient.unregisterAll().futureValue
+
     binding.terminate(2.seconds).futureValue
     wiring.actorRuntime.shutdown().futureValue
   }
 
-  test("Bind location server to Public Network IP with publicNetwork option | CSW-96, CSW-89") {
+  test("when publicNetwork option is given, should bind to Public Network IP with auth ENABLED | CSW-96, CSW-89") {
     val clusterPort = SocketUtils.getFreePort
     System.clearProperty("CLUSTER_SEEDS")
     System.setProperty("CLUSTER_SEEDS", s"$clusterSeedIp:$clusterPort")
     val hostname = Networks(NetworkType.Public.envKey).hostname
 
     val (binding, wiring) = Main.start(Array("--clusterPort", s"$clusterPort", "--publicNetwork")).get
+    //This is to make sure, it does not give AASResolution Failed error
+    //This register is done via httpService impl bypassing HTTP Layer, hence dont require auth, do not confuse it with
+    //calls from locationServiceClient like register, unregisterAll which will require auth, if auth is enabled
+    wiring.locationService.register(HttpRegistration(AASConnection.value, SocketUtils.getFreePort, "auth")).futureValue
 
     binding.localAddress.getAddress.getHostAddress shouldBe hostname
     SocketUtils.isAddressInUse(hostname, httpPort) shouldBe true
+
+    // connect to location service running on hostname, cannot access protected resource as auth is enabled
+    val locationServiceClient: LocationService = HttpLocationServiceFactory.make(hostname)(wiring.actorSystem)
+    val exception                              = intercept[Exception](locationServiceClient.unregisterAll().futureValue)
+    exception.getCause shouldBe a[HttpError]
+    exception.getCause.asInstanceOf[HttpError].statusCode shouldBe 401
+
     binding.terminate(2.seconds).futureValue
     wiring.actorRuntime.shutdown().futureValue
   }
