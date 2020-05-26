55a9e8fb90d1f1db22fce69119dd12543f5d8897
mausamyede <mausam.yede1@gmail.com>
2020-05-26 09:53:29 +0530

    #000: Mausam : Remove unused NetworksLogger

--- csw-network-utils/src/main/scala/csw/network/utils/commons/NetworksLogger.scala
+++
@@ -1,8 +0,0 @@
-package csw.network.utils.commons
-
-/**
- * All the logs generated from networks util will have a fixed prefix, which is "networks-util".
- * The prefix helps in production to filter out logs from a particular component and this case, it helps to filter out logs
- * generated from location service.
- */
-private[network] object NetworksLogger
