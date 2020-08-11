49b27e900ec2be34c3cd65ff6244e642773bee55
Poorva Gokhale <poorvasgokhale@gmail.com>
2020-08-11 20:39:40 +0530

    #CSW-108: Poorva: Fix test

--- csw-location/csw-location-server/src/test/scala/csw/location/server/internal/LocationServiceImplTest.scala
+++ csw-location/csw-location-server/src/test/scala/csw/location/server/internal/LocationServiceImplTest.scala
@@ -1,10 +1,12 @@
 package csw.location.server.internal
 
+import akka.actor.typed.SpawnProtocol
 import akka.actor.typed.scaladsl.Behaviors
-import akka.actor.typed.{ActorSystem, SpawnProtocol}
 import csw.location.api.AkkaRegistrationFactory
+import csw.location.api.extensions.ActorExtension.RichActor
 import csw.location.api.models.Connection.{AkkaConnection, HttpConnection}
 import csw.location.api.models._
+import csw.location.client.ActorSystemFactory
 import csw.location.server.commons.CswCluster
 import csw.prefix.models.{Prefix, Subsystem}
 import org.mockito.MockitoSugar
@@ -12,7 +14,7 @@ import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
 class LocationServiceImplTest extends AnyFunSuite with Matchers with MockitoSugar {
-  private val system         = ActorSystem(SpawnProtocol(), "test-system")
+  private val system         = ActorSystemFactory.remote(SpawnProtocol(), "test-system")
   private val mockCswCluster = mock[CswCluster]
   private val httpConnection = HttpConnection(ComponentId(Prefix(Subsystem.CSW, "ConfigServer"), ComponentType.Service))
   private val port           = 5003
@@ -38,14 +40,15 @@ class LocationServiceImplTest extends AnyFunSuite with Matchers with MockitoSuga
   test("should not use public or private cluster hostname for Akka Registration | CSW-97") {
     val componentId: ComponentId       = ComponentId(Prefix("tcs.filter.wheel"), ComponentType.HCD)
     val akkaConnection: AkkaConnection = AkkaConnection(componentId)
+    val actorRef                       = system.systemActorOf(Behaviors.empty, "test-actor")
     val akkaRegistration: AkkaRegistration =
-      AkkaRegistrationFactory.make(akkaConnection, system.systemActorOf(Behaviors.empty, "test-actor"))
+      AkkaRegistrationFactory.make(akkaConnection, actorRef)
 
     when(mockCswCluster.hostname) thenReturn ("some-private-ip")
     when(mockCswCluster.publicHostname) thenReturn ("some-public-ip")
 
     val locationService = new LocationServiceImpl(mockCswCluster)
-    locationService.getLocation(akkaRegistration).uri.toString shouldBe "some-actor-uri"
+    locationService.getLocation(akkaRegistration).uri shouldBe actorRef.toURI
     locationService.getLocation(akkaRegistration).uri.toString should not include "some-private-ip"
     locationService.getLocation(akkaRegistration).uri.toString should not include "some-public-ip"
   }
