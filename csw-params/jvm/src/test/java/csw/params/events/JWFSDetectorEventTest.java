8367d861126d180e559f4887459f67764f5d0238
Vinay Mahamuni <vinaymahamuni22@gmail.com>
2021-01-05 11:58:07 +0530

    #CSW-118:  Vinay | Manish :  Add java test for WFS detector observe events

---
+++ csw-params/jvm/src/test/java/csw/params/events/JWFSDetectorEventTest.java
@@ -0,0 +1,31 @@
+package csw.params.events;
+
+import csw.prefix.javadsl.JSubsystem;
+import csw.prefix.models.Prefix;
+import org.junit.Assert;
+import org.junit.Test;
+import org.scalatestplus.junit.JUnitSuite;
+
+public class JWFSDetectorEventTest extends JUnitSuite {
+    String sourcePrefix = "ESW.filter.wheel";
+    Prefix prefix = new Prefix(JSubsystem.ESW, "filter.wheel");
+
+    @Test
+    public void shouldCreatePublishSuccessEvent__CSW_118() {
+        ObserveEvent event = JWFSDetectorEvent.PublishSuccess().create(sourcePrefix);
+
+
+        Assert.assertEquals(prefix, event.source());
+        Assert.assertEquals("PublishSuccess", event.eventName().name());
+    }
+
+    @Test
+    public void shouldCreatePublishFailEvent__CSW_118() {
+        ObserveEvent event = JWFSDetectorEvent.PublishFail().create(sourcePrefix);
+
+
+        Assert.assertEquals(prefix, event.source());
+        Assert.assertEquals("PublishFail", event.eventName().name());
+    }
+}
+
