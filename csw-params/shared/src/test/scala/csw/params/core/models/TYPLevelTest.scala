3d84143be316d4c4750ed5ae6ff11573e66a1fa9
Poorva Gokhale <poorvasgokhale@gmail.com>
2021-01-12 17:00:43 +0530

    #CSW-121: Poorva: Add type level and exposure number models

---
+++ csw-params/shared/src/test/scala/csw/params/core/models/TYPLevelTest.scala
@@ -0,0 +1,26 @@
+package csw.params.core.models
+
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers
+
+class TYPLevelTest extends AnyFunSpec with Matchers {
+  describe("create TYPLevel") {
+    it("should create TYPLevel") {
+      val tYPLevel = TYPLevel("SCI0")
+      tYPLevel.toString should ===("SCI0")
+      tYPLevel should ===(TYPLevel(TYP.SCI, CalibrationLevel.Raw))
+    }
+
+    it("should throw exception if invalid TYP") {
+      val exception = intercept[NoSuchElementException](TYPLevel("INVALID0"))
+      exception.getMessage should ===(
+        "INVALID is not a member of Enum (SCI, CAL, ARC, IDP, DRK, MDK, FFD, NFF, BIA, TEL, FLX, SKY)"
+      )
+    }
+
+    it("should throw exception if invalid calibrationLevel") {
+      val exception = intercept[IllegalArgumentException](TYPLevel("SCI5"))
+      exception.getMessage should ===("Failed to parse calibration level 5: 5 is out of bounds (min 0, max 4)")
+    }
+  }
+}
