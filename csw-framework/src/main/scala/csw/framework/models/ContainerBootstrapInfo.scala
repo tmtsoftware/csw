c8b9aa0bfb76e93d8dbcd7d255674b7012baeffc
manish <gowardipemanish95@gmail.com>
2022-03-09 12:46:08 +0530

    #CSW-000: Manish : Remove unnecessary comment

--- csw-framework/src/main/scala/csw/framework/models/ContainerBootstrapInfo.scala
+++ csw-framework/src/main/scala/csw/framework/models/ContainerBootstrapInfo.scala
@@ -1,7 +1,6 @@
 package csw.framework.models
 
 /**
- * @param mode mode in which container needs to be started. Ex. Standalone or Container
  * @param configFilePath path of configuration file which is provided to container cmd app to start specified components from config
  * @param configFileLocation indicator to fetch config file either from local machine or from Configuration service
  */
