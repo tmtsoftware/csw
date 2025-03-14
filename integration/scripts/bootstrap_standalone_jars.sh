7237d6acae2cc1fc582e35e0e05470b4dde32084
Allan Brighton <Allan.Brighton@t-online.de>
2025-03-14 12:23:41 +0100

    Testing github actions

--- integration/scripts/bootstrap_standalone_jars.sh
+++ integration/scripts/bootstrap_standalone_jars.sh
@@ -4,5 +4,6 @@ echo "$0: PATH = $PATH"
 INTEGRATION_JAR="com.github.tmtsoftware.csw::integration:0.1.0-SNAPSHOT"
 
 sbt publishLocal
+cs update
 cs bootstrap -r jitpack $INTEGRATION_JAR -M csw.integtration.apps.AssemblyApp -o target/assembly-app --standalone -f
 cs bootstrap -r jitpack $INTEGRATION_JAR -M csw.integtration.apps.TestMultipleNicApp -o target/test-multiple-nic-app --standalone -f
