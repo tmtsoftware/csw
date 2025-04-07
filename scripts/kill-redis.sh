881ad17afee1a80c97f7951458b6c4173e79d695
Allan Brighton <Allan.Brighton@t-online.de>
2025-04-07 21:24:32 +0200

    Testing jenkins

--- scripts/kill-redis.sh
+++ scripts/kill-redis.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 set -x
-pids=`$(ps aux | grep 'redis' | grep -v grep | awk '{print $2}')`
+pids=`ps aux | grep 'redis' | grep -v grep | awk '{print $2}'`
 test "$pids" != "" && kill $pids
