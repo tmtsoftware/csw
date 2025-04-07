e7cfd163fbad7a70f386170cea9cddceab3c351f
Allan Brighton <Allan.Brighton@t-online.de>
2025-04-07 17:26:51 +0200

    Fixed script

--- scripts/kill-redis.sh
+++ scripts/kill-redis.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
+set -x
 pids=`$(ps aux | grep 'redis' | grep -v grep | awk '{print $2}')`
-if "$pids" != "";
-  echo "Killing existing redis instances"
-  kill $pids
-fi
+test "$pids" != "" && kill $pids
