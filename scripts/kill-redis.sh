9bc11b106cc8ba06fbaf6a1c57e8f7c75766544a
Allan Brighton <Allan.Brighton@t-online.de>
2025-04-07 22:11:46 +0200

    Testing jenkins

--- scripts/kill-redis.sh
+++ scripts/kill-redis.sh
@@ -3,3 +3,7 @@
 set -x
 pids=`ps aux | grep 'redis' | grep -v grep | awk '{print $2}'`
 test "$pids" != "" && kill $pids
+pids=`ps aux | grep 'redis' | grep -v grep | awk '{print $2}'`
+test "$pids" != "" && echo "These redis processes are still running: $pids"
+exit 0
+
